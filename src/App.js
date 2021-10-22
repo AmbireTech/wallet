6c73c3073f0df3bca228eefb4d183e8e6b451ffb
Ivo Georgiev <ivo@strem.io>
2021-10-22 15:55:12 +0300

    some crypto work done, but it seems we can rely on ethers for everything

--- src/App.js
+++ src/App.js
@@ -2,20 +2,54 @@ import './App.css'
 // @TODO LoginOrSignup in a separate file
 import { useState, useRef } from 'react'
 
+const { AbiCoder, keccak256 } = require('ethers').utils
+
 // @TODO another file
 // @TODO err-catching fetch helper
+const crypto = require('crypto')
+
+const CIPHER_LEGACY_ALG = 'aes-256-ctr'
+const CIPHER_ALG = 'aes-256-cbc'
+const IV_LENGTH = 16
+const HASH_ALG = 'sha256'
+
+const getKeyHash = key => {
+	const hash = crypto.createHash(HASH_ALG)
+	hash.update(key)
+	const hashed = hash.digest()
+	return hashed
+}
+const encrypt = (text, key) => {
+	if (!text) return ''
+	if (!key) {
+		throw new Error('ERR_ENCRYPTING_NO_PASS')
+	}
+	const iv = crypto.randomBytes(IV_LENGTH)
+	const cipher = crypto.createCipheriv(CIPHER_ALG, getKeyHash(key), iv)
+	const encr = cipher.update(text)
+	const encrypted = Buffer.concat([encr, cipher.final()])
+
+	return `${iv.toString('hex')}:${encrypted.toString('hex')}`
+}
+
 const fetch = require('node-fetch')
 const { generateAddress2 } = require('ethereumjs-util')
 const { getProxyDeployBytecode } = require('adex-protocol-eth/js/IdentityProxyDeploy')
 const { hexZeroPad } = require('ethers').utils
 
+
 function App() {
   const loginComponent = LoginOrSignup({ onAccRequest: async req => {
     console.log(req)
     // @TODO url
+    console.log(encrypt('privkey', req.passphrase))
     const { whitelistedFactories, whitelistedBaseIdentities } = await fetch('http://localhost:1934/relayer/cfg').then(r => r.json())
+    // @TODO ?
     const salt = hexZeroPad('0x01', 32)
-    const accHash = '' // @TODO
+    // @TODO: timeout
+		const quickAccount = [600, '0x00' /* TODO */, /*secondKeyAddr*/ '0x00']
+		const abiCoder = new AbiCoder()
+		const accHash = keccak256(abiCoder.encode(['tuple(uint, address, address)'], [quickAccount]))
     const quickAccManager = '0x'
     const privileges = [[quickAccManager, accHash]]
     const identityFactoryAddr = whitelistedFactories[whitelistedFactories.length - 1]
