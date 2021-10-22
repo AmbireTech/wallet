534d4c679564bfb57b7f37f9096fd35a599e8dc0
Ivo Georgiev <ivo@strem.io>
2021-10-22 16:48:52 +0300

    build QuickAccount, and encrypt/decrypt as an example

--- src/App.js
+++ src/App.js
@@ -2,64 +2,66 @@ import './App.css'
 // @TODO LoginOrSignup in a separate file
 import { useState, useRef } from 'react'
 
-const { AbiCoder, keccak256 } = require('ethers').utils
-
 // @TODO another file
 // @TODO err-catching fetch helper
-const crypto = require('crypto')
 
-const CIPHER_LEGACY_ALG = 'aes-256-ctr'
-const CIPHER_ALG = 'aes-256-cbc'
-const IV_LENGTH = 16
-const HASH_ALG = 'sha256'
-
-const getKeyHash = key => {
-	const hash = crypto.createHash(HASH_ALG)
-	hash.update(key)
-	const hashed = hash.digest()
-	return hashed
-}
-const encrypt = (text, key) => {
-	if (!text) return ''
-	if (!key) {
-		throw new Error('ERR_ENCRYPTING_NO_PASS')
-	}
-	const iv = crypto.randomBytes(IV_LENGTH)
-	const cipher = crypto.createCipheriv(CIPHER_ALG, getKeyHash(key), iv)
-	const encr = cipher.update(text)
-	const encrypted = Buffer.concat([encr, cipher.final()])
-
-	return `${iv.toString('hex')}:${encrypted.toString('hex')}`
-}
 
 const fetch = require('node-fetch')
 const { generateAddress2 } = require('ethereumjs-util')
 const { getProxyDeployBytecode } = require('adex-protocol-eth/js/IdentityProxyDeploy')
-const { hexZeroPad } = require('ethers').utils
+const { Wallet } = require('ethers')
+const { hexZeroPad, AbiCoder, keccak256, id } = require('ethers').utils
 
+const onAccRequest = async req => {
+  console.log(req)
+
+  // @TODO url
+  // @TODO we can do these fetches in parallel
+  const { whitelistedFactories, whitelistedBaseIdentities } = await fetch('http://localhost:1934/relayer/cfg').then(r => r.json())
+
+  // @TODO proper salt
+  const salt = hexZeroPad('0x01', 32)
+  const firstKeyWallet = Wallet.createRandom()
+
+  // @TODO fix this hack, use another source of randomness
+  // 6 words is 2048**6
+  const secondKeySecret = Wallet.createRandom({
+    extraEntropy: id(req.email+':'+Date.now())
+  }).mnemonic.phrase.split(' ').slice(0, 6).join(' ') + ' ' + req.email
+  const secondKeyAddress = await fetch('http://localhost:1934/second-key', {
+    method: 'POST',
+    headers: {'content-type': 'application/json'},
+    body: JSON.stringify({ secondKeySecret })
+  }).then(r => r.json()).then(r => r.address)
+
+  // @TODO: timelock value for the quickAccount
+  const quickAccount = [600, firstKeyWallet.address, secondKeyAddress]
+  const abiCoder = new AbiCoder()
+  const accHash = keccak256(abiCoder.encode(['tuple(uint, address, address)'], [quickAccount]))
+  // @TODO quickACcManager addr
+  const quickAccManager = '0x'
+  const privileges = [[quickAccManager, accHash]]
+  const identityFactoryAddr = whitelistedFactories[whitelistedFactories.length - 1]
+  const baseIdentityAddr = whitelistedBaseIdentities[whitelistedBaseIdentities.length - 1]
+  const bytecode = getProxyDeployBytecode(baseIdentityAddr, privileges, { privSlot: 0 })
+  const identityAddr = '0x' + generateAddress2(identityFactoryAddr, salt, bytecode).toString('hex')
+
+  // @TODO manage storage outside of this function, figure out what to return
+  let n = Date.now()
+  const encrypted = await firstKeyWallet.encrypt(req.passphrase)
+  console.log(encrypted, Date.now() - n)
+
+  n = Date.now()
+  console.log(await Wallet.fromEncryptedJson(encrypted, req.passphrase))
+  console.log(Date.now()-n)
+
+  console.log('identityAddr:', identityAddr, quickAccount)
+}
+//onAccRequest({ passphrase: 'testtest', email: 'ivo@strem.io' })
 
 function App() {
-  const loginComponent = LoginOrSignup({ onAccRequest: async req => {
-    console.log(req)
-    // @TODO url
-    console.log(encrypt('privkey', req.passphrase))
-    const { whitelistedFactories, whitelistedBaseIdentities } = await fetch('http://localhost:1934/relayer/cfg').then(r => r.json())
-    // @TODO ?
-    const salt = hexZeroPad('0x01', 32)
-    // @TODO: timeout
-		const quickAccount = [600, '0x00' /* TODO */, /*secondKeyAddr*/ '0x00']
-		const abiCoder = new AbiCoder()
-		const accHash = keccak256(abiCoder.encode(['tuple(uint, address, address)'], [quickAccount]))
-    const quickAccManager = '0x'
-    const privileges = [[quickAccManager, accHash]]
-    const identityFactoryAddr = whitelistedFactories[whitelistedFactories.length - 1]
-    const baseIdentityAddr = whitelistedBaseIdentities[whitelistedBaseIdentities.length - 1]
-    const bytecode = getProxyDeployBytecode(baseIdentityAddr, privileges, { privSlot: 0 })
-    const identityAddr = '0x' + generateAddress2(identityFactoryAddr, salt, bytecode).toString('hex')
+  const loginComponent = LoginOrSignup({ onAccRequest })
 
-    console.log('identityAddr:', identityAddr)
-
-  } })
   return (<div>
     <img src="https://www.ambire.com/ambire-logo-2.png"/>
     {loginComponent}
