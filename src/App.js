0eec68bb6bce9e9b9adb11e220af2d78f94a2752
Ivo Georgiev <ivo@strem.io>
2021-10-23 11:41:31 +0300

    add router, separate login/signup modes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import './App.css'
 // @TODO LoginOrSignup in a separate file
 import { useState, useRef } from 'react'
 
+
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 // @TODO another file
 // @TODO err-catching fetch helper
-
-
 const fetch = require('node-fetch')
 const { generateAddress2 } = require('ethereumjs-util')
 const { getProxyDeployBytecode } = require('adex-protocol-eth/js/IdentityProxyDeploy')
@@ -63,27 +69,51 @@ const onAccRequest = async req => {
 
   console.log('identityAddr:', identityAddr, quickAccount)
 }
-onAccRequest({ passphrase: 'testtest', email: 'ivo@strem.io' })
+//onAccRequest({ passphrase: 'testtest', email: 'ivo@strem.io' })
 
 function App() {
-  const loginComponent = LoginOrSignup({ onAccRequest })
-
-  return (<div>
-    <img src="https://www.ambire.com/ambire-logo-2.png"/>
-    {loginComponent}
-  </div>)
+  // @TODO default page
+  return (
+    <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </ul>
+        </nav>
+      <Switch>
+        <Route path="/login">
+          <section>
+            <img src="https://www.ambire.com/ambire-logo-2.png"/>
+            <LoginOrSignup onAccRequest={onAccRequest}></LoginOrSignup>
+          </section>
+        </Route>
+        <Route path="/signup">
+          <section>
+            <LoginOrSignup onAccRequest={onAccRequest} action="SIGNUP"></LoginOrSignup>
+          </section>
+        </Route>
+      </Switch>
+    </Router>
+    )
 }
 // @TODO remove this bit
 
 
-function LoginOrSignup({ onAccRequest }) {
+function LoginOrSignup({ action = 'LOGIN', onAccRequest }) {
   // @NOTE: preventDefault prevents validation
   const passConfirmInput = useRef(null)
-  const [state, setState] = useState({ email: '', passphrase: '', passphraseConfirm: '' })
+  const [state, setState] = useState({
+    email: '', passphrase: '', passphraseConfirm: '', action
+  })
   const onSubmit = e => {
     e.preventDefault()
     onAccRequest({
-      action: 'SIGNUP',
+      action: state.action,
       accType: 'QUICK',
       email: state.email,
       passphrase: state.passphrase
@@ -92,19 +122,26 @@ function LoginOrSignup({ onAccRequest }) {
   const onUpdate = updates => {
     const newState = { ...state, ...updates }
     setState(newState)
-    // @TODO translation string
-    passConfirmInput.current.setCustomValidity(
-      newState.passphrase !== newState.passphraseConfirm ? 'Passphrase must match' : ''
+    const shouldValidate = newState.action === 'SIGNUP'
+    const invalid = shouldValidate && (
+      newState.passphrase !== newState.passphraseConfirm
     )
+    // @TODO translation string
+    if (passConfirmInput.current) passConfirmInput.current.setCustomValidity(invalid ? 'Passphrase must match' : '')
   }
+  const isSignup = state.action === 'SIGNUP'
   return (
     <div className="loginOrSignup">
         <div id="loginEmailPass">
           <form onSubmit={onSubmit}>
             <input type="email" required placeholder="Email" value={state.email} onChange={e => onUpdate({ email: e.target.value })}></input>
             <input type="password" required minLength="8" placeholder="Passphrase" value={state.passphrase} onChange={e => onUpdate({ passphrase: e.target.value })}></input>
-            <input ref={passConfirmInput} required minLength="8" type="password" placeholder="Confirm passphrase" value={state.passphraseConfirm} onChange={e => onUpdate({ passphraseConfirm: e.target.value })}></input>
-            <input type="submit" value="Sign up"></input>
+            {
+              isSignup ? 
+                (<input ref={passConfirmInput} required minLength="8" type="password" placeholder="Confirm passphrase" value={state.passphraseConfirm} onChange={e => onUpdate({ passphraseConfirm: e.target.value })}></input>)
+                : (<></>)
+            }
+            <input type="submit" value={isSignup ? "Sign up" : "Login"}></input>
           </form>
         </div>
 
@@ -113,8 +150,8 @@ function LoginOrSignup({ onAccRequest }) {
         </div>
 
         <div id="loginOthers">
+          { /* TODO icon styling */ }
           <button><img src="https://cdn-images-1.medium.com/max/1200/1*Sek00YxqMdOJp5FsjveZiQ.png" width="25px"/>Trezor</button>
-
           <button><img src="https://www.ledger.com/wp-content/uploads/2020/02/puce_blue.png" width="25px"/>Ledger</button>
           <button><img src="https://cdn.iconscout.com/icon/free/png-256/metamask-2728406-2261817.png" width="25px"/>Metamask / Browser</button>
         </div>
