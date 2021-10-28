2c02c6871fa0126b4f9ac0d407b262ce292ee7b7
tymmesyde <tymmesyde@gmail.com>
2021-10-28 19:20:39 +0200

    move dashboard into a new component

--- src/App.js
+++ src/App.js
@@ -7,10 +7,9 @@ import {
   Route,
   Redirect
 } from 'react-router-dom'
-import { MdDashboard, MdLock, MdCompareArrows } from 'react-icons/md'
-import { BsPiggyBank } from 'react-icons/bs'
 import EmailLogin from './components/EmailLogin/EmailLogin'
 import AddAccount from './components/AddAccount/AddAcount'
+import Dashboard from './components/Dashboard/Dashboard'
 
 // @TODO consts/cfg
 const relayerURL = 'http://localhost:1934'
@@ -39,6 +38,13 @@ function App() {
     }
   }
 
+  const onAccountSelected = ev => {
+    accounts.forEach(acc => acc.selected = false)
+    accounts.find(x => x._id === ev.target.value).selected = true
+    localStorage.accounts = JSON.stringify(accounts)
+    setAccounts([ ...accounts ])
+  }
+
   return (
     <Router>
       {/*<nav>
@@ -55,35 +61,7 @@ function App() {
         </Route>
 
         <Route path="/dashboard">
-          <section id="dashboard">
-            <div id="sidebar">
-              <div className="logo"/>
-
-              <div className="balance">
-                <label>Balance</label>
-                <div className="balanceDollarAmount"><span className="dollarSign highlight">$</span>999<span className="highlight">.00</span></div>
-              </div>
-
-             {/* TODO proper navi, programmatic selected class */}
-              <div className="item selected"><MdDashboard size={30}/>Dashboard</div>
-              <div className="item"><MdLock size={30}/>Security</div>
-              <div className="item"><MdCompareArrows size={30}/>Transactions</div>
-              <div className="item"><BsPiggyBank size={30}/>Earn</div>
-
-            </div>
-
-            <div>
-              {/* TODO more elegant way to manage selected? */}
-              <select id="accountSelector" onChange={ ev => {
-                accounts.forEach(acc => acc.selected = false)
-                accounts.find(x => x._id === ev.target.value).selected = true
-                localStorage.accounts = JSON.stringify(accounts)
-                setAccounts([ ...accounts ])
-              } } defaultValue={(accounts.find(x => x.selected) || {})._id}>
-                {accounts.map(acc => (<option key={acc._id}>{acc._id}</option>))}
-              </select>
-            </div>
-          </section>
+          <Dashboard accounts={accounts} onAccountSelected={onAccountSelected}></Dashboard>
         </Route>
         <Route path="/security"></Route>
         <Route path="/transactions"></Route>
