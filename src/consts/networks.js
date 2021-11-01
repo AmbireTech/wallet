c68e06a2ddcf654714ee0dc75636466635d984b7
Ivo Georgiev <ivo@adex.network>
2021-11-01 20:26:01 +0200

    network hook updated such that we expose network.id

--- src/consts/networks.js
+++ src/consts/networks.js
@@ -1,27 +1,27 @@
-const networks = {
-	ethereum: {
+const networks = [{
+		id: 'ethereum',
 		chainId: 1,
 		rpc: 'https://mainnet.infura.io/v3/3d22938fd7dd41b7af4197752f83e8a1',
 		nativeAssetSymbol: 'ETH',
 		name: 'Ethereum'
-	},
-	polygon: {
+	},  {
+		id: 'polygon',
 		chainId: 137,
 		rpc: 'https://polygon-rpc.com/rpc',
 		nativeAssetSymbol: 'MATIC',
 		name: 'Polygon'
-	},
-	avalanche: {
+	}, {
+		id: 'avalanche',
 		chainId: 43114,
 		rpc: 'https://api.avax.network/ext/bc/C/rpc',
 		nativeAssetSymbol: 'AVAX',
 		name: 'Avalanche'
-	},
-	arbitrum: {
+	}, {
+		id: 'arbitrum',
 		chainId: 42161,
 		rpc: 'https://arb1.arbitrum.io/rpc',
 		nativeAssetSymbol: 'AETH',
 		name: 'Arbitrum'
-	},
-}
+}]
+
 export default networks
