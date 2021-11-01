e700dfe822913f1f64fbbd1ca5d168a0f5e6d493
Ivo Georgiev <ivo@strem.io>
2021-11-01 20:26:01 +0200

    network hook updated such that we expose network.id

--- src/hooks/network.js
+++ src/hooks/network.js
@@ -2,21 +2,19 @@ import { useState, useCallback } from 'react'
 import networks from '../consts/networks'
 
 export default function useNetwork ({ defaultNetwork = 'ethereum' } = {}) {
-    const [networkName, setNetworkName] = useState(() =>
-        networks[localStorage.network] ? localStorage.network : defaultNetwork
+    const [networkId, setNetworkId] = useState(() =>
+        networks.find(n => n.id === localStorage.network) ? localStorage.network : defaultNetwork
     )
     const setNetwork = useCallback(networkNameOrKey => {
-        let networkKey
-        if (networks[networkNameOrKey]) networkKey = networkNameOrKey
-        else networkKey = (Object.entries(networks).find(([key, val]) => val.name === networkNameOrKey) || [])[0]
-        if (!networkKey) throw new Error(`no network found: ${networkNameOrKey}`)
-        localStorage.network = networkKey
-        setNetworkName(networkKey)
-    }, [setNetworkName])
+        const network = networks.find(n => n.id === networkNameOrKey || n.name === networkNameOrKey)
+        if (!network) throw new Error(`no network found: ${networkNameOrKey}`)
+        localStorage.network = network.id
+        setNetworkId(network.id)
+    }, [setNetworkId])
 
     return {
         setNetwork,
-        network: networks[networkName],
+        network: networks.find(n => n.id === networkId),
         allNetworks: networks
     }
 }
