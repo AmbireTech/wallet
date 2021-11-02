ce111b6d12d15068ec1f07a524c2e3f58c1286ab
Tim <tymmesyde@gmail.com>
2021-11-02 12:02:17 +0100

    use NavLink instead of a useLocation hook

--- src/components/Wallet/Wallet.js
+++ src/components/Wallet/Wallet.js
@@ -1,21 +1,13 @@
 import './Wallet.css'
 
-import { useCallback } from 'react'
-import { Switch, Route, Redirect, useLocation } from 'react-router-dom'
+import { Switch, Route, Redirect, NavLink } from 'react-router-dom'
 import { MdDashboard, MdLock, MdCompareArrows, } from 'react-icons/md'
 import { GiReceiveMoney } from 'react-icons/gi'
 import { BsPiggyBank } from 'react-icons/bs'
 import { BiTransfer } from 'react-icons/bi'
 import Dashboard from './Dashboard/Dashboard'
 
-export default function Platform({ match, allNetworks, accounts, selectedAcc, onSelectAcc, network, setNetwork }) {
-    const location = useLocation();
-
-    const currentNestedRoute = useCallback(() => {
-        const [,, nested] = location.pathname.split('/');
-        return nested;
-    }, [location.pathname]);
-    
+export default function Platform({ match, allNetworks, accounts, selectedAcc, onSelectAcc, network, setNetwork }) {    
     return (
         <div id="platform">
             <div id="sidebar">
@@ -27,13 +19,36 @@ export default function Platform({ match, allNetworks, accounts, selectedAcc, on
                 </div>
 
                 {/* TODO proper navi, programmatic selected class */}
-                <div className={`item ${currentNestedRoute() === 'dashboard' ? 'selected' : ''}`}><MdDashboard size={30}/>Dashboard</div>
-                <div className={`item ${currentNestedRoute() === 'desposit' ? 'selected' : ''}`}><GiReceiveMoney size={30}/>Deposit</div>
-                <div className={`item ${currentNestedRoute() === 'transfer' ? 'selected' : ''}`}><BiTransfer size={30}/>Transfer</div>
-                <div className={`item ${currentNestedRoute() === 'security' ? 'selected' : ''}`}><MdLock size={30}/>Security</div>
-                <div className={`item ${currentNestedRoute() === 'swap' ? 'selected' : ''}`}><MdCompareArrows size={30}/>Transactions</div>
-                <div className={`item ${currentNestedRoute() === 'earn' ? 'selected' : ''}`}><BsPiggyBank size={30}/>Earn</div>
-
+                <NavLink to={match.url + "/dashboard"} activeClassName="selected">
+                    <div className="item">
+                        <MdDashboard size={30}/>Dashboard
+                    </div>
+                </NavLink>
+                <NavLink to={match.url + "/deposit"} activeClassName="selected">
+                    <div className="item">
+                        <GiReceiveMoney size={30}/>Deposit
+                    </div>
+                </NavLink>
+                <NavLink to={match.url + "/tansfer"} activeClassName="selected">
+                    <div className="item">
+                        <BiTransfer size={30}/>Transfer
+                    </div>
+                </NavLink>
+                <NavLink to={match.url + "/security"} activeClassName="selected">
+                    <div className="item">
+                        <MdLock size={30}/>Security
+                    </div>
+                </NavLink>
+                <NavLink to={match.url + "/swap"} activeClassName="selected">
+                    <div className="item">
+                        <MdCompareArrows size={30}/>Transactions
+                    </div>
+                </NavLink>
+                <NavLink to={match.url + "/earn"} activeClassName="selected">
+                    <div className="item">
+                        <BsPiggyBank size={30}/>Earn
+                    </div>
+                </NavLink>
             </div>
 
             {/* Top-right dropdowns */}
