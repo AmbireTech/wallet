fc615bb0bc2949e621ae48ec0ce52617f809ffbb
Ivo Georgiev <ivo@strem.io>
2021-11-02 10:42:39 +0200

    other post-merge fixes

--- src/components/Platform/Dashboard/Dashboard.js
+++ src/components/Platform/Dashboard/Dashboard.js
@@ -1,23 +1,11 @@
 import './Dashboard.css'
 
 import Deposit from './Deposit/Deposit'
-import useAccounts from '../../../hooks/accounts'
-import useWalletConnect from '../../../hooks/walletconnect'
 
-export default function Dashboard() {
-  const { selectedAcc } = useAccounts()
-  const { connections, disconnect, userAction } = useWalletConnect({ selectedAcc, chainId: 137 })
-
+export default function Dashboard({ selectedAcc }) {
     return (
         <section id="dashboard">
             <div id="dashboardArea">
-              {connections.map(({ session, uri }) =>
-                (<div key={session.peerId} style={{ marginBottom: 20 }}>
-                  <button onClick={() => disconnect(uri)}>Disconnect {session.peerMeta.name}</button>
-                </div>)
-              )}
-              {userAction ? (<><div>{userAction.bundle.txns[0][0]}</div><button onClick={userAction.fn}>Send txn</button></>) : (<></>)}
-
               <Deposit depositAddress={selectedAcc}></Deposit>
             </div>
         </section>
