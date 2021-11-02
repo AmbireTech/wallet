cbe915817d1c00a68f198f013411222b8880ed6a
Ivo Georgiev <ivo@strem.io>
2021-11-02 23:52:20 +0200

    WIP script to generate contract verification info

---
+++ scripts/generateContractInfo.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const node = require('node-fetch')
+
+const etherscanAPIKey = 'KJJ4NZ9EQHIFCQY5IJ775PT128YE15AV5S'
+
+const contracts = [
+	{ name: 'SUSHI', contract: '0x1b02da8cb0d097eb8d57a175b88c7d8b47997506' }
+]
+
+
