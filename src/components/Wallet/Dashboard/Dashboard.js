4a33d2265413c8baa7c8d3e8d95137f6b356b94c
Tim <tymmesyde@gmail.com>
2021-11-02 14:37:37 +0100

    pass selected address to providers and set default symbol to USDC

--- src/components/Wallet/Dashboard/Dashboard.js
+++ src/components/Wallet/Dashboard/Dashboard.js
@@ -6,7 +6,7 @@ export default function Dashboard({ selectedAcc, selectedNetwork }) {
     return (
         <section id="dashboard">
             <div id="dashboardArea">
-              <Deposit depositAddress={selectedAcc} selectedNetwork={selectedNetwork}></Deposit>
+              <Deposit selectedAcc={selectedAcc} selectedNetwork={selectedNetwork}></Deposit>
             </div>
         </section>
     )
