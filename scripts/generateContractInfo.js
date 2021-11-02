f641620858b08881c9e226973af3fc2a19631c23
Ivo Georgiev <ivo@adex.network>
2021-11-03 00:17:39 +0200

    some very basic txn parsing

--- scripts/generateContractInfo.js
+++ scripts/generateContractInfo.js
@@ -2,20 +2,23 @@
 const fetch = require('node-fetch')
 const fs = require('fs')
 
-const etherscanAPIKey = ''//'KJJ4NZ9EQHIFCQY5IJ775PT128YE15AV5S'
 const etherscans = {
-	ethereum: 'api.etherscan.io',
-	polygon: 'api.polygonscan.com'
+	ethereum: { host: 'api.etherscan.io', key: 'KJJ4NZ9EQHIFCQY5IJ775PT128YE15AV5S' },
+	polygon: { host: 'api.polygonscan.com', key: 'YE5YYHA7BH6IPBN5T71UKW5MPEFZ5HUGJJ' }
 }
 
+// @TODO dedupe ABIs and reference them by hash or something
 const contracts = [
-	{ name: 'SUSHI', network: 'polygon', contract: '0x1b02da8cb0d097eb8d57a175b88c7d8b47997506' }
+	{ name: 'SUSHI', network: 'polygon', contract: '0x1b02da8cb0d097eb8d57a175b88c7d8b47997506' },
+	{ name: 'SUSHI', network: 'ethereum', contract: '0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F' },
+	//{ name: 'SUSHI', network: 'polygon', contract: '0x1b02da8cb0d097eb8d57a175b88c7d8b47997506' },
 ]
 
 async function generate () {
 	let output = {}
 	await Promise.all(contracts.map(async ({ name, network, contract }) => {
-		const abiResp = await fetch(`https://${etherscans[network]}/api?module=contract&action=getabi&address=${contract}&apikey=${etherscanAPIKey}`)
+		const { host, key } = etherscans[network]
+		const abiResp = await fetch(`https://${host}/api?module=contract&action=getabi&address=${contract}&apikey=${key}`)
 			.then(r => r.json())
 		if (abiResp.status !== '1') throw abiResp
 		output[network+':'+contract] = { name, abi: JSON.parse(abiResp.result) }
