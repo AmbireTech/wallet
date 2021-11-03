2f1f9b8d8536d358742be3030bf4705d1fda3bd6
Tim <tymmesyde@gmail.com>
2021-11-03 19:40:05 +0100

    add a total usd balance state

--- src/hooks/balances.js
+++ src/hooks/balances.js
@@ -5,6 +5,8 @@ import { ZAPPER_API_KEY } from '../config';
 
 export default function useBalances({ currentNetwork, account }) {
     const [balances, setBalance] = useState([]);
+    const [totalTruncUSD, setTotalTruncUSD] = useState();
+    const [totalDecUSD, setTotalDecimalUSD] = useState();
 
     const updateBalances = (currentNetwork, address) => {
         setBalance([]);
@@ -26,7 +28,22 @@ export default function useBalances({ currentNetwork, account }) {
         updateBalances(currentNetwork, account);
     }, [currentNetwork, account]);
 
+    useEffect(() => {
+        const total = balances
+            .filter(({ meta }) => meta && meta.length)
+            .map(({ meta }) => meta.find(({ label }) => label === 'Total').value)
+            .reduce((acc, curr) => acc + curr, 0)
+            .toFixed(2);
+
+        const [truncated, decimal] = total.split('.');
+
+        setTotalTruncUSD(truncated);
+        setTotalDecimalUSD(decimal);
+    }, [balances]);
+
     return {
-        balances
+        balances,
+        totalTruncUSD,
+        totalDecUSD
     }
 }
