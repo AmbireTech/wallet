6fdde03c81a81b9c2156e7510209c7922ccc265c
Tim <tymmesyde@gmail.com>
2021-11-03 16:30:08 +0100

    display balances datas on Dashboard

--- src/components/Wallet/Dashboard/Dashboard.js
+++ src/components/Wallet/Dashboard/Dashboard.js
@@ -1,14 +1,51 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './Dashboard.css'
 
 export default function Dashboard({ balances }) {
+    const [positiveBalances, setPositivesBalances] = useState([]);
+
     useEffect(() => {
-        console.log(balances.filter(({ meta }) => meta.length && meta[0].value));
+        setPositivesBalances(balances.filter(({ products }) => products.length));
     }, [balances]);
 
     return (
         <section id="dashboard">
-
+            <div id="table">
+            {
+                positiveBalances.map(({ appId, products }) => 
+                    products.map(({ label, assets }) => (
+                        <div className="category" key={appId}>
+                            <div className="title">{ label }</div>
+                            <div className="list">
+                                {
+                                    assets.map(({ tokens }) => 
+                                        tokens.map(({ label, symbol, img, balance, balanceUSD }) => (
+                                            <div className="token">
+                                                <div className="icon">
+                                                    <img src={img}/>
+                                                </div>
+                                                <div className="name">
+                                                    { label }
+                                                </div>
+                                                <div className="separator"></div>
+                                                <div className="balance">
+                                                    <div className="currency">
+                                                        { balance } <span className="symbol">{ symbol }</span>
+                                                    </div>
+                                                    <div className="dollar">
+                                                        <span className="symbol">$</span> { balanceUSD }
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        ))
+                                    )
+                                }
+                            </div>
+                        </div>
+                    )
+                ))
+            }
+            </div>
         </section>
     )
 }
