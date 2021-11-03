7395fbe63f3edca9dce07468ab4cbabd4cd1b3e8
Tim <tymmesyde@gmail.com>
2021-11-03 15:17:50 +0100

    implement basic balance store with zapper api

--- src/components/Wallet/Dashboard/Dashboard.js
+++ src/components/Wallet/Dashboard/Dashboard.js
@@ -1,6 +1,11 @@
+import { useEffect } from 'react'
 import './Dashboard.css'
 
-export default function Dashboard() {
+export default function Dashboard({ balances }) {
+    useEffect(() => {
+        console.log(balances.filter(({ meta }) => meta.length && meta[0].value));
+    }, [balances]);
+
     return (
         <section id="dashboard">
 
