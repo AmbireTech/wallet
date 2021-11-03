2850ede14c51f6bd2d14954164adc7010dfe45cb
Tim <tymmesyde@gmail.com>
2021-11-03 13:35:58 +0100

    move Deposit to Wallet

--- src/components/Wallet/Dashboard/Dashboard.js
+++ src/components/Wallet/Dashboard/Dashboard.js
@@ -1,13 +1,9 @@
 import './Dashboard.css'
 
-import Deposit from './Deposit/Deposit'
-
-export default function Dashboard({ selectedAcc, selectedNetwork }) {
+export default function Dashboard() {
     return (
         <section id="dashboard">
-            <div id="dashboardArea">
-              <Deposit selectedAcc={selectedAcc} selectedNetwork={selectedNetwork}></Deposit>
-            </div>
+
         </section>
     )
 }
