ae590f7e27f4960cecd4829360f89cdb15473f46
Tim <tymmesyde@gmail.com>
2021-11-03 19:49:28 +0100

    style Dashboard

--- src/components/Wallet/Dashboard/Dashboard.js
+++ src/components/Wallet/Dashboard/Dashboard.js
@@ -11,51 +11,58 @@ export default function Dashboard({ balances, totalTruncUSD, totalDecUSD }) {
 
     return (
         <section id="dashboard">
-            <div id="total" className="panel">
-                <span className="green-highlight">$</span>
-                { totalTruncUSD }
-                <span className="green-highlight">.{ totalDecUSD }</span>
+            <div id="balance" className="panel">
+                <div className="title">Balance</div>
+                <div className="content">
+                    <div id="total">
+                        <span className="green-highlight">$</span> { totalTruncUSD }
+                        <span className="green-highlight">.{ totalDecUSD }</span>
+                    </div>
+                </div>
             </div>
             <div id="table" className="panel">
-            {
-                positiveBalances.map(({ products }) => 
-                    products.map(({ label, assets }, i) => (
-                        <div className="category" key={`category-${i}`}>
-                            <div className="title">{ label }</div>
-                            <div className="list">
-                                {
-                                    assets.map(({ tokens }) => 
-                                        tokens.map(({ label, collectionName, symbol, img, collectionImg, balance, balanceUSD }, i) => (
-                                            <div className="token" key={`token-${i}`}>
-                                                <div className="icon">
-                                                    {
-                                                        img || collectionImg ? 
-                                                            <img src={img || collectionImg} alt="Token Icon"/>
-                                                            :
-                                                            <GiToken size={20}/>
-                                                    }
-                                                </div>
-                                                <div className="name">
-                                                    { label || collectionName || symbol }
-                                                </div>
-                                                <div className="separator"></div>
-                                                <div className="balance">
-                                                    <div className="currency">
-                                                        { balance } <span className="symbol">{ symbol }</span>
+                <div className="title">Assets</div>
+                <div className="content">
+                {
+                    positiveBalances.map(({ products }) => 
+                        products.map(({ label, assets }, i) => (
+                            <div className="category" key={`category-${i}`}>
+                                <div className="title">{ label }</div>
+                                <div className="list">
+                                    {
+                                        assets.map(({ tokens }) => 
+                                            tokens.map(({ label, collectionName, symbol, img, collectionImg, balance, balanceUSD }, i) => (
+                                                <div className="token" key={`token-${i}`}>
+                                                    <div className="icon">
+                                                        {
+                                                            img || collectionImg ? 
+                                                                <img src={img || collectionImg} alt="Token Icon"/>
+                                                                :
+                                                                <GiToken size={20}/>
+                                                        }
                                                     </div>
-                                                    <div className="dollar">
-                                                        <span className="symbol">$</span> { balanceUSD }
+                                                    <div className="name">
+                                                        { label || collectionName || symbol }
+                                                    </div>
+                                                    <div className="separator"></div>
+                                                    <div className="balance">
+                                                        <div className="currency">
+                                                            { balance } <span className="symbol">{ symbol }</span>
+                                                        </div>
+                                                        <div className="dollar">
+                                                            <span className="symbol">$</span> { balanceUSD }
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        ))
-                                    )
-                                }
+                                            ))
+                                        )
+                                    }
+                                </div>
                             </div>
-                        </div>
-                    )
-                ))
-            }
+                        )
+                    ))
+                }
+                </div>
             </div>
         </section>
     )
