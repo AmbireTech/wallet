9cdf79bfc4b353cc25e2c822fb7daedff754eb45
Ivo Georgiev <ivo@strem.io>
2021-11-04 23:16:27 +0200

    add humanReadableTransactions (move getTransactionSummary)

--- src/components/SendTransaction/SendTransaction.js
+++ src/components/SendTransaction/SendTransaction.js
@@ -1,15 +1,9 @@
 //import { GrInspect } from 'react-icons/gr'
 // GiObservatory is also interesting
-import { Interface } from 'ethers/lib/utils'
 import { GiTakeMyMoney, GiSpectacles } from 'react-icons/gi'
 import { FaSignature } from 'react-icons/fa'
-import verifiedContracts from '../../consts/verifiedContracts'
-import networks from '../../consts/networks'
-import ERC20ABI from 'adex-protocol-eth/abi/ERC20'
+import { getTransactionSummary } from '../../lib/humanReadableTransactions'
 import './SendTransaction.css'
-
-const ERC20 = new Interface(ERC20ABI)
-const TRANSFER_SIGHASH = ERC20.getSighash(ERC20.getFunction('transfer').format())
 
 export default function SendTransaction ({ userAction }) {
     const actionable = userAction 
@@ -19,32 +13,6 @@ export default function SendTransaction ({ userAction }) {
             </>)
         : (<></>)
 
-    // @TODO custom parsing for univ2 contracts, exact output, etc.
-    const getSummary = txn => {
-        if (!userAction) return 'internal err: no user action'
-
-        const [to, value, data] = txn
-        let callSummary, sendSummary
-        // @TODO proper asset symbol
-        const network = networks.find(x => x.id === userAction.bundle.network)
-
-        const contractKey = userAction.bundle.network + ':' + to
-        const contractInfo = verifiedContracts[contractKey]
-
-        if (parseInt(value) > 0) sendSummary = `send ${(parseInt(value)/1e18).toFixed(4)} ${network.nativeAssetSymbol} to ${contractInfo ? contractInfo.name : to}`
-        if (data !== '0x') {
-            if (data.startsWith(TRANSFER_SIGHASH)) {
-                const [to, amount] = ERC20.decodeFunctionData('transfer', data)
-                // @TODO decimals
-                callSummary `send ${(amount/1e18).toFixed(4)} to ${to}`
-            } else if (contractInfo) {
-                const iface = new Interface(contractInfo.abi)
-                const parsed = iface.parseTransaction({ data, value })
-                callSummary = `Interaction with ${contractInfo.name}: ${parsed.name}`
-            } else callSummary = `unknown call to ${to}`
-        }
-        return [callSummary, sendSummary].filter(x => x).join(', ')
-    }
     return (<div id="sendTransaction">
         <h2>Pending transaction</h2>
         <div className="panelHolder">
@@ -58,7 +26,7 @@ export default function SendTransaction ({ userAction }) {
                             {userAction ? userAction.bundle.txns.map((txn, i) => (
                                 <li key={txn}>
                                     {i === userAction.bundle.txns.length - 1 ? 'Fee: ' : ''}
-                                    {getSummary(txn)}
+                                    {getTransactionSummary(txn, userAction.bundle)}
                                 </li>
                             )) : (<></>)}
                         </ul>
