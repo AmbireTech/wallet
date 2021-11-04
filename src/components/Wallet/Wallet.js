dfc7c3ae3b23921d364c070363aac4306a6944a1
Tim <tymmesyde@gmail.com>
2021-11-04 16:07:26 +0100

    refactor: pass portfolio as object

--- src/components/Wallet/Wallet.js
+++ src/components/Wallet/Wallet.js
@@ -8,7 +8,7 @@ import Deposit from "./Deposit/Deposit"
 import { usePortfolio } from '../../hooks'
 
 export default function Wallet(props) {
-  const { balances, totalUSD, isLoading } = usePortfolio({
+  const portfolio = usePortfolio({
     currentNetwork: props.network.id,
     account: props.selectedAcc
   })
@@ -16,11 +16,11 @@ export default function Wallet(props) {
   return (
     <div id="wallet">
       <TopBar {...props} />
-      <SideBar match={props.match} totalUSD={totalUSD} isLoading={isLoading}/>
+      <SideBar match={props.match} portfolio={portfolio}/>
       <div id="wallet-container">
         <Switch>
           <Route path={props.match.url + "/dashboard"}>
-            <Dashboard balances={balances} totalUSD={totalUSD} isLoading={isLoading} />
+            <Dashboard portfolio={portfolio} />
           </Route>
           <Route path={props.match.url + "/deposit"}>
             <Deposit selectedAcc={props.selectedAcc} selectedNetwork={props.network.id} />
