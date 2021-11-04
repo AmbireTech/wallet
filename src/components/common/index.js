36c22a212a59013269825f194030c86e56d5aca9
Tim <tymmesyde@gmail.com>
2021-11-04 14:57:48 +0100

    refactor: move Checkbox and Chart under common, add an index to common

---
+++ src/components/common/index.js
@@ -0,0 +1,13 @@
+import Chart from './Chart/Chart';
+import Checkbox from './Checkbox/Checkbox';
+import DropDown from './DropDown/DropDown';
+import Loading from './Loading/Loading';
+import TextInput from './TextInput/TextInput';
+
+export {
+    Chart,
+    Checkbox,
+    DropDown,
+    Loading,
+    TextInput
+};
