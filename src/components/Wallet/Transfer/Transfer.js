70f6531eacecbe481921d9330b2ad600cc920802
Tim <tymmesyde@gmail.com>
2021-11-05 15:06:29 +0100

    refactor(Transfer): improve components

--- src/components/Wallet/Transfer/Transfer.js
+++ src/components/Wallet/Transfer/Transfer.js
@@ -22,11 +22,13 @@ const Transfer = () => {
                <div className="title">
                    Send
                </div>
-               <form action="">
+               <div className="form">
                     <TextInput placeholder="Asset"/>
-                    <TextInput placeholder="Amount"/>
-                    <TextInput placeholder="Recipient"/>
-                </form>
+                    <TextInput placeholder="Amount" button="MAX"/>
+                    <TextInput placeholder="Recipient" info="Please double-check the recipient address, blockchain transactions are not reversible."/>
+                    <div className="separator"/>
+                    <Button>Send</Button>
+                </div>
            </div>
            <div className="panel">
                <div className="overlay">
@@ -35,7 +37,7 @@ const Transfer = () => {
                <div className="title">
                    Cross-chain
                </div>
-               <form action="">
+               <div className="form">
                     <Segments defaultValue={segments[0].value} segments={segments}/>
                     <TextInput placeholder="From"/>
                     <div className="separator">
@@ -43,7 +45,7 @@ const Transfer = () => {
                     </div>
                     <TextInput placeholder="To"/>
                     <Button>Transfer</Button>
-                </form>
+                </div>
            </div>
         </div>
     )
