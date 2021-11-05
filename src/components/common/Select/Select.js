566730db3851f4ec7d6031f7f9739945cdf11ff4
Tim <tymmesyde@gmail.com>
2021-11-05 16:21:05 +0100

    refactor: add icons to networks select and rework style

--- src/components/common/Select/Select.js
+++ src/components/common/Select/Select.js
@@ -4,19 +4,23 @@ import { useEffect, useRef, useState } from "react";
 import { BsChevronUp, BsChevronDown } from 'react-icons/bs'
 import useOnClickOutside from '../../../helpers/onClickOutside';
 
-const Select = ({ children, native, defaultValue, items, itemKey, itemLabel, onChange }) => {
+const Select = ({ children, native, defaultValue, items, onChange }) => {
     const ref = useRef();
     const [isOpen, setOpen] = useState();
-    const [selectedItem, setSelectedItem] = useState();
+    const [selectedItem, setSelectedItem] = useState({
+        label: null,
+        value: null,
+        icon: null
+    });
 
     const selectItem = item => {
         setSelectedItem(item);
-        onChange(item[itemKey]);
+        onChange(item.value);
     };
 
     useEffect(() => {
-        setSelectedItem(items.find(item => item[itemKey] === defaultValue));
-    }, [defaultValue, items, itemKey]);
+        setSelectedItem(items.find(item => item.value === defaultValue));
+    }, [defaultValue, items]);
 
     useOnClickOutside(ref, () => setOpen(false));
 
@@ -24,21 +28,39 @@ const Select = ({ children, native, defaultValue, items, itemKey, itemLabel, onC
         !native ? 
             <div className="select" onClick={() => setOpen(!isOpen)} ref={ref}>
                 <div className="value">
-                    { selectedItem ? selectedItem[itemLabel || itemKey] : null }
                     {
-                        isOpen ? 
-                            <BsChevronUp size={20}></BsChevronUp>
+                        selectedItem.icon ? 
+                            <div className="icon">
+                                <img src={selectedItem.icon} alt="Icon" />
+                            </div>
                             :
-                            <BsChevronDown size={20}></BsChevronDown>
+                            null
                     }
+                    { selectedItem.label || selectedItem.value }
+                    <div className="handle">
+                        {
+                            isOpen ? 
+                                <BsChevronUp size={20}></BsChevronUp>
+                                :
+                                <BsChevronDown size={20}></BsChevronDown>
+                        }
+                    </div>
                 </div>
                 {
                     isOpen ? 
                         <div className="list">
                             {
                                 items.map(item => (
-                                    <div className={`option ${item[itemKey] === selectedItem[itemKey] ? 'active' : ''}`} key={item[itemKey]} onClick={() => selectItem(item)}>
-                                        { item[itemLabel || itemKey] }
+                                    <div className={`option ${item.value === selectedItem.value ? 'active' : ''}`} key={item.value} onClick={() => selectItem(item)}>
+                                        {
+                                            item.icon ? 
+                                                <div className="icon">
+                                                    <img src={item.icon} alt="Icon" />
+                                                </div>
+                                                :
+                                                null
+                                        }
+                                        { item.label || item.value }
                                     </div>
                                 ))
                             }
@@ -51,8 +73,8 @@ const Select = ({ children, native, defaultValue, items, itemKey, itemLabel, onC
             <select className="select" onChange={ev => onChange(ev.target.value)} defaultValue={defaultValue}>
                 {
                     items.map(item => (
-                        <option key={item[itemKey]} value={item[itemKey]}>
-                            { item[itemLabel || itemKey] }
+                        <option key={item.value} value={item.value}>
+                            { item.label || item.value }
                         </option>
                     ))
                 }
--- src/consts/networks.js
+++ src/consts/networks.js
@@ -3,25 +3,29 @@ const networks = [{
 		chainId: 1,
 		rpc: 'https://mainnet.infura.io/v3/3d22938fd7dd41b7af4197752f83e8a1',
 		nativeAssetSymbol: 'ETH',
-		name: 'Ethereum'
+		name: 'Ethereum',
+		icon: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png'
 	},  {
 		id: 'polygon',
 		chainId: 137,
 		rpc: 'https://polygon-rpc.com/rpc',
 		nativeAssetSymbol: 'MATIC',
-		name: 'Polygon'
+		name: 'Polygon',
+		icon: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3890.png'
 	}, {
 		id: 'avalanche',
 		chainId: 43114,
 		rpc: 'https://api.avax.network/ext/bc/C/rpc',
 		nativeAssetSymbol: 'AVAX',
-		name: 'Avalanche'
+		name: 'Avalanche',
+		icon: 'https://s2.coinmarketcap.com/static/img/coins/64x64/5805.png'
 	}, {
 		id: 'arbitrum',
 		chainId: 42161,
 		rpc: 'https://arb1.arbitrum.io/rpc',
 		nativeAssetSymbol: 'AETH',
-		name: 'Arbitrum'
+		name: 'Arbitrum',
+		icon: 'https://bridge.arbitrum.io/images/Arbitrum_Symbol_-_Full_color_-_White_background.svg'
 }]
 
 export default networks
