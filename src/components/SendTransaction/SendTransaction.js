1beaa42fef621f9ccf52e20f5b65012e8be6d26d
Ivo Georgiev <ivo@adex.network>
2021-11-07 19:33:52 +0200

    getWallet implemented

--- src/components/SendTransaction/SendTransaction.js
+++ src/components/SendTransaction/SendTransaction.js
@@ -10,12 +10,10 @@ import { useEffect, useState } from 'react'
 // @TODO get rid of these, should be in the SignTransaction component
 import fetch from 'node-fetch'
 import { Bundle } from 'adex-protocol-eth/js'
-import { TrezorSubprovider } from '@0x/subproviders/lib/src/subproviders/trezor' // https://github.com/0xProject/0x-monorepo/issues/1400
-import TrezorConnect from 'trezor-connect'
-import { ethers, getDefaultProvider } from 'ethers'
+import { getDefaultProvider } from 'ethers'
 import { useHistory } from 'react-router'
 import { useToasts } from '../../hooks/toasts'
-import HDNode from 'hdkey'
+import { getWallet } from '../../lib/getWallet'
 
 const SPEEDS = ['slow', 'medium', 'fast', 'ape']
 
@@ -82,41 +80,25 @@ export default function SendTransaction ({ accounts, network, selectedAcc, reque
   const account = accounts.find(x => x.id === selectedAcc)
   if (!account) throw new Error('internal: account does not exist')
 
-  // @TODO
-  // add a transaction that's supposed to `simulate` the fee payment so that
-  // we factor in the gas for that; it's ok even if that txn ends up being
-  // more expensive (eg because user chose to pay in native token), cause we stay on the safe (higher) side
-  // or just add a fixed premium on gasLimit
-  // or just hardcode a certain gas limit
+  // @TODO Add a fixed premium on gasLimit depending on how we pay the fee, to account for the costs of paying the fee itself
   const bundle = frozenBundle || makeBundle(account, network.id, eligibleRequests)
+  // @TODO if we have a frozen bundle, ensure it is for the selected account
 
   const approveTxnImpl = async () => {
     if (!estimation) return
     // pay a fee to the relayer
     bundle.txns.push(['0x942f9CE5D9a33a82F88D233AEb3292E680230348', Math.round(estimation.feeInNative.fast*1e18).toString(10), '0x'])
+
     const provider = getDefaultProvider(network.rpc)
     await bundle.getNonce(provider)
 
     bundle.gasLimit = estimation.gasLimit
 
-    // @TODO we have to cache `providerTrezor` otherwise it will always ask us whether we wanna expose the pub key
-    const providerTrezor = new TrezorSubprovider({ trezorConnectClientApi: TrezorConnect })
-    // NOTE: for metamask, use `const provider = new ethers.providers.Web3Provider(window.ethereum)`
-    // as for Trezor/ledger, alternatively we can shim using https://www.npmjs.com/package/web3-provider-engine and then wrap in Web3Provider
-    const walletShim = {
-      signMessage: hash => providerTrezor.signPersonalMessageAsync(ethers.utils.hexlify(hash), bundle.signer.address)
-    }
-    providerTrezor._initialDerivedKeyInfo = {
-      "hdKey": HDNode.fromExtendedKey(localStorage.xpub),
-      "derivationPath":"m/44'/60'/0'/0",
-      "baseDerivationPath":"44'/60'/0'/0"
-    }
-    await bundle.sign(walletShim)
+    const wallet = getWallet({ signer: bundle.signer, signerExtra: account.signerExtra })
+    await bundle.sign(wallet)
     const bundleResult = await bundle.submit({ relayerURL, fetch })
     console.log(bundle, bundleResult)
-    //console.log(JSON.stringify(providerTrezor._initialDerivedKeyInfo), providerTrezor._initialDerivedKeyInfo)
     resolveMany(bundle.requestIds, { success: bundleResult.success, result: bundleResult.txId, message: bundleResult.message })
-    // we can now approveRequest in this and return the proper result
     // @TODO show a success toast with a URL to a block scanner
   }
   const approveTxn = () => {
@@ -203,7 +185,7 @@ function FeeSelector ({ estimation, feeMultiplier = 1, network, chosenSpeed = 'f
   </select>)
 
   const feeAmountSelectors = SPEEDS.map(speed => (
-    <div className={chosenSpeed === speed ? 'feeSquare selected' : 'feeSquare'}>
+    <div key={speed} className={chosenSpeed === speed ? 'feeSquare selected' : 'feeSquare'}>
       <div className='speed'>{speed}</div>
       {estimation.feeInUSD
         ? '$'+(estimation.feeInUSD[speed] * feeMultiplier)
