b686216725bd97d1ac512319ba3127d3bec2de67
Tim <tymmesyde@gmail.com>
2021-11-07 21:02:18 +0100

    refactor(Button): add disabled prop

--- src/components/common/Button/Button.js
+++ src/components/common/Button/Button.js
@@ -1,8 +1,8 @@
 import './Button.scss'
 
-const Button = ({ children, small, icon }) => {
+const Button = ({ children, small, icon, disabled }) => {
     return (
-        <button className={`buttonComponent ${small ? 'small' : ''}`}>
+        <button className={`buttonComponent ${small ? 'small' : ''}`} disabled={disabled}>
             <div className="icon">{ icon }</div>
             { children }
         </button>
