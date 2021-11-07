e2540fe425b76b0dedc092cadfb5738894ce177e
Ivo Georgiev <ivo@adex.network>
2021-11-08 00:16:18 +0200

    accountPresets updated with latest deployed contracts

--- src/consts/accountPresets.js
+++ src/consts/accountPresets.js
@@ -2,9 +2,9 @@
 // NOTE: those should only apply to new accounts, never for existing accounts, since some existing accounts may be on older versions of those
 const accountPresets = {
     salt: '0x0000000000000000000000000000000000000000000000000000000000000001',
-    baseIdentityAddr: '0xA2E9e41ee85AE792A8213736c7f9398a933F8184',
+    baseIdentityAddr: '0xaaff58975776d33fB1256717D9C85f499A1c3618',
     identityFactoryAddr: '0x447f228E6af15C2Df147235eCB9ABE53BD1f46Ad',
-    quickAccManager: '0x697d866d20a8E8886a0D0511e82846AC108Bc5B6',
+    quickAccManager: '0xA8AE248276ff3809E4Ec3a1fd4aaD82af940074a',
     quickAccTimelock: 259200 // 3 days
 }
 export default accountPresets
