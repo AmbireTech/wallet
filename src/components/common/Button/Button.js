5166a821ad7ede47d934ffde78c848ddf2da7367
Tim <tymmesyde@gmail.com>
2021-11-07 13:03:40 +0100

    refactor(Button): handle icon and small prop

--- src/components/common/Button/Button.js
+++ src/components/common/Button/Button.js
@@ -1,8 +1,9 @@
 import './Button.scss'
 
-const Button = ({ children }) => {
+const Button = ({ children, small, icon }) => {
     return (
-        <button className='buttonComponent'>
+        <button className={`buttonComponent ${small ? 'small' : ''}`}>
+            <div className="icon">{ icon }</div>
             { children }
         </button>
     )
