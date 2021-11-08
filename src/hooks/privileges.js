5e2b02c18fcc2f69f311ed14a0c0a8bb11b7f08d
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2021-11-08 16:37:23 +0200

    Lists all the related privileges to the selected account

--- src/hooks/privileges.js
+++ src/hooks/privileges.js
@@ -5,6 +5,13 @@ export default function usePrivileges({ identity, network, accounts }) {
   const [isLoading, setLoading] = useState(true);
   const [privileges, setPrivileges] = useState({});
 
+  // TODO: Remove mapVal
+  const mapVal = val => {
+		if (val === '0x0000000000000000000000000000000000000000000000000000000000000000') return false
+		if (val === '0x0000000000000000000000000000000000000000000000000000000000000001') return true
+		return val
+	}
+
   const updatePrivileges = async (identity, network) => {
     setLoading(true);
     let requestPrivResp = await getPrivileges(identity, network, {});
@@ -15,7 +22,7 @@ export default function usePrivileges({ identity, network, accounts }) {
       const filteredPrivBySelectedAccount = Object.keys(requestPrivResp.body.privileges).filter(
         (x) => x === signerAddress
       );
-      const filtered = filteredPrivBySelectedAccount.reduce((obj, key) => ({ ...obj, [key]: requestPrivResp.body.privileges[key] }), {});
+      const filtered = filteredPrivBySelectedAccount.reduce((obj, key) => ({ ...obj, [key]: mapVal(requestPrivResp.body.privileges[key]) }), {});
       
       console.log("tesrt", requestPrivResp.body);
       setPrivileges(filtered)
