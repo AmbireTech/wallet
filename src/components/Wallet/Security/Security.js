078fa8882344daa9f7b98eb4a0df28be10d77fc6
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2021-11-08 16:37:23 +0200

    Lists all the related privileges to the selected account

--- src/components/Wallet/Security/Security.js
+++ src/components/Wallet/Security/Security.js
@@ -2,27 +2,55 @@ import "./Security.scss";
 
 import usePrivileges from "../../../hooks/privileges";
 import { Loading } from "../../common";
+import { Select } from "../../common";
+
+const OPTIONS_PRIVILEGES = [
+  {
+    id: "1",
+    label: "YES",
+    value: true,
+  },
+  {
+    id: "2",
+    label: "NO",
+    value: false,
+  },
+];
 
 const Security = (props) => {
   const { privileges, updatedBlock, isLoading } = usePrivileges({
     identity: props.selectedAcc,
     network: props.selectedNetwork,
-    accounts: props.accounts
+    accounts: props.accounts,
   });
 
   console.log("priv", privileges);
 
+  const onSelectPriv = (val) => {
+    console.log("Priv value", val);
+  };
+
   return (
     <section id="security">
-      {isLoading && <Loading />}
-      {!isLoading && (
-        <div>
-          <h1>Security Page</h1>
-          {Object.keys(privileges).map(key => {
-            return <div>{key} {privileges[key]}</div>
+      <div className="panel">
+        <div className="title">Set Privileges</div>
+        {isLoading && <Loading />}
+        {!isLoading &&
+          Object.keys(privileges).map((key) => {
+            return (
+              <div className="content">
+                <div>{key}</div>
+                <Select
+                  defaultValue={privileges[key]}
+                  items={OPTIONS_PRIVILEGES}
+                  itemLabel="label"
+                  itemKey="value"
+                  onChange={(value) => onSelectPriv(value)}
+                />
+              </div>
+            );
           })}
-        </div>
-      )}
+      </div>
     </section>
   );
 };
