f2bea9e9c7a83c34aae17dbae573e83fca8c6fca
Tim <tymmesyde@gmail.com>
2021-11-08 15:03:32 +0100

    refactor(Segments): add onChange callback and small styling

--- src/components/common/Segments/Segments.js
+++ src/components/common/Segments/Segments.js
@@ -1,15 +1,25 @@
 import { useState } from 'react'
 import './Segments.scss'
 
-const Segments = ({ defaultValue, segments }) => {
+const Segments = ({ small, defaultValue, segments, onChange }) => {
     const [value, setValue] = useState(defaultValue);
 
+    const setSegment = value => {
+        setValue(value)
+        onChange(value)
+    }
+
     return (
-        <div className="segments">
+        <div className={`segments ${small ? 'small': ''}`}>
             {
                 segments.map(segment => (
-                    <div className={`segment ${segment.value === value ? 'active' : ''}`} key={segment.value} onClick={() => setValue(segment.value)}>
-                        <div className="icon">{ segment.icon }</div>
+                    <div className={`segment ${segment.value === value ? 'active' : ''}`} key={segment.value} onClick={() => setSegment(segment.value)}>
+                        {
+                            segment.icon ?
+                                <div className="icon">{ segment.icon }</div>
+                                :
+                                null
+                        }
                         { segment.value }
                     </div>
                 ))
