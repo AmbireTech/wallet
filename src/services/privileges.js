d8cb05947c8675eb7103513eb9a4f11dbdd8960c
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2021-11-08 14:12:28 +0200

    Added Security component, added privileges hook and service

---
+++ src/services/privileges.js
@@ -0,0 +1,17 @@
+import { fetch, fetchCaught } from "../lib/fetch";
+
+// @TODO consts/cfg
+const relayerURL = "http://localhost:1934";
+
+export const getPrivileges = async (identity, network, query) => {
+  const resp = await fetchCaught(
+    `${relayerURL}/identity/${identity}/${network}/privileges`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  return resp
+};
