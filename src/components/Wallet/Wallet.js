6ae75bcb69d4d24189b261d0e8136a2a6a347b34
IvoPaunov <ivo.paunov@gmail.com>
2021-11-10 15:37:14 +0200

    work on sushi frame with gnosis integration

--- src/components/Wallet/Wallet.js
+++ src/components/Wallet/Wallet.js
@@ -14,7 +14,7 @@ export default function Wallet(props) {
   return (
     <div id="wallet">
       <TopBar {...props} />
-      <SideBar match={props.match} portfolio={props.portfolio}/>
+      <SideBar match={props.match} portfolio={props.portfolio} />
       <div id="wallet-container">
         <Switch>
           <Route path={props.match.url + "/dashboard"}>
@@ -24,24 +24,26 @@ export default function Wallet(props) {
             <Deposit selectedAcc={props.selectedAcc} selectedNetwork={props.network.id} />
           </Route>
           <Route path={props.match.url + "/trading"}>
-            <Trading />
+            <Trading
+              gnosisConnect={props.gnosisConnect}
+              selectedAcc={props.selectedAcc}
+              network={props.network}s
+            />
           </Route>
           <Route path={props.match.url + "/transfer"}>
-            <Transfer portfolio={props.portfolio}/>
+            <Transfer portfolio={props.portfolio} />
           </Route>
           <Route path={props.match.url + "/security"}></Route>
           <Route path={props.match.url + "/transactions"}></Route>
           <Route path={props.match.url + "/swap"}></Route>
           <Route path={props.match.url + "/earn"}></Route>
-
-		  <Route path={props.match.url + "/plugins/gnosis"}>
-			  <PluginGnosisSafeApps
-				  gnosisConnect={props.gnosisConnect}
-				  selectedAcc={props.selectedAcc}
-				  network={props.network}
-			  />
-		  </Route>
-
+          <Route path={props.match.url + "/plugins/gnosis"}>
+            <PluginGnosisSafeApps
+              gnosisConnect={props.gnosisConnect}
+              selectedAcc={props.selectedAcc}
+              network={props.network}
+            />
+          </Route>
           <Route path={props.match.url + "/"}>
             <Redirect to={props.match.url + "/dashboard"} />
           </Route>
