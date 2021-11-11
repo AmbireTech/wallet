9e8d0fc71725db3a51451408adb2eb2c6d02edab
Tim <tymmesyde@gmail.com>
2021-11-11 13:56:14 +0100

    tweak collectables placeholder

--- src/components/Wallet/Dashboard/Collectables/CollectablesPlaceholder/CollectablesPlaceholder.js
+++ src/components/Wallet/Dashboard/Collectables/CollectablesPlaceholder/CollectablesPlaceholder.js
@@ -10,11 +10,11 @@ const CollectablesPlaceholder = () => {
             price: 32.16
         },
         {
-            collectionName: 'GLUEWAVE',
-            collectionImg: 'https://lh3.googleusercontent.com/LheZMF6AyTLPpfwABwAGZqoC0gTWrDlYloDdT2VX2u_GZRZnP-ZpDC90huElm3RXE-eU1d1YluQi8G1uVdeDdysUNLZWJte4jXk3zeY=s2500',
-            name: '000105040307070704',
-            image: 'https://lh3.googleusercontent.com/eP_F_6aa_BXuKfYq1w2aiS6UW3bp4-gByFoeKLMXYuWAkU6Xiyi7mkm31aoewCZ_eG9RAuhas8jxFwXTxAWuxq1E3ZxYg97cLUUE',
-            price: 7107.27
+            collectionName: 'Pudgy Penguins',
+            collectionImg: 'https://lh3.googleusercontent.com/L9PJp0pOXCscKsTpD1_6zoALxdjG5gIKHLykS_Wk5J_yk590drEqRhM1ElTEZCaWeTjFj2isfxFw2tcFKcbCPlu6-Eu5bUuxsL-x=s2500',
+            name: 'Pudgy Penguin #8675',
+            image: 'https://lh3.googleusercontent.com/J6gQQRnzsMLAOhYiN9qIhogxr2MKn17zfGf6nQFKr3WCZ34thm6H0w9izvnJysxJclsyr8c3yo0hshOaLU73aS-IgTm-CooJb1cw1S0',
+            price: 5675.00
         },
         {
             collectionName: 'Art Blocks Factory',
@@ -24,18 +24,18 @@ const CollectablesPlaceholder = () => {
             price: 263.46
         },
         {
-            collectionName: 'Pudgy Penguins',
-            collectionImg: 'https://lh3.googleusercontent.com/L9PJp0pOXCscKsTpD1_6zoALxdjG5gIKHLykS_Wk5J_yk590drEqRhM1ElTEZCaWeTjFj2isfxFw2tcFKcbCPlu6-Eu5bUuxsL-x=s2500',
-            name: 'Pudgy Penguin #8675',
-            image: 'https://lh3.googleusercontent.com/J6gQQRnzsMLAOhYiN9qIhogxr2MKn17zfGf6nQFKr3WCZ34thm6H0w9izvnJysxJclsyr8c3yo0hshOaLU73aS-IgTm-CooJb1cw1S0',
-            price: 5675.00
-        },
-        {
             collectionName: 'Sad Frogs District',
             collectionImg: 'https://lh3.googleusercontent.com/3U8N73XLrQOQPhJBAMNi7IPR1y3GGJ2zxmyUlit4IHCJURmKFPWClgatjbYfyz9drMk6-XSQTjSFqn7tKL0ZNoRbgvUu8OdLVREA=s2500',
             name: '1882',
             image: 'https://lh3.googleusercontent.com/3uwrfkdhHSfWm171BOjJHp3768tbLP9TeyTh2BnvgF6S1nIRI489SFq6EGjI9AISRUeyXXufEjB34Wj0bif_vl-y5h6GesEoJaaZcJg',
             price: 1563.59
+        },
+        {
+            collectionName: 'GLUEWAVE',
+            collectionImg: 'https://lh3.googleusercontent.com/LheZMF6AyTLPpfwABwAGZqoC0gTWrDlYloDdT2VX2u_GZRZnP-ZpDC90huElm3RXE-eU1d1YluQi8G1uVdeDdysUNLZWJte4jXk3zeY=s2500',
+            name: '000105040307070704',
+            image: 'https://lh3.googleusercontent.com/eP_F_6aa_BXuKfYq1w2aiS6UW3bp4-gByFoeKLMXYuWAkU6Xiyi7mkm31aoewCZ_eG9RAuhas8jxFwXTxAWuxq1E3ZxYg97cLUUE',
+            price: 7107.27
         }
     ]
 
