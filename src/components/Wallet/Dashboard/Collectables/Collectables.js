28ef6bbfae21cb77e2f92fdc213af270a8b363e5
Tim <tymmesyde@gmail.com>
2021-11-11 13:08:40 +0100

    refactor(Dashboard): add collectables placeholder

--- src/components/Wallet/Dashboard/Collectables/Collectables.js
+++ src/components/Wallet/Dashboard/Collectables/Collectables.js
@@ -1,30 +1,34 @@
 import './Collectables.scss'
 
 import { NavLink } from 'react-router-dom'
+import CollectablesPlaceholder from './CollectablesPlaceholder/CollectablesPlaceholder'
 
 const Collectables = ({ collectables }) => {
     return (
-        <div id="collectables">
-            {
-                collectables.map(({ tokens }) => tokens.map(({ network, address, collectionName, collectionImg, assets }) => assets.map(({ tokenId, assetName, assetImg, balanceUSD }) => (
-                    <div className="collectable" key={tokenId}>
-                        <NavLink to={`/wallet/nft/${network}/${address}/${tokenId}`}>
-                            <div className="artwork" style={{backgroundImage: `url(${assetImg})`}}/>
-                            <div className="info">
-                                <div className="collection">
-                                    <div className="collection-icon" style={{backgroundImage: `url(${collectionImg})`}}></div>
-                                    { collectionName }
+        !collectables.length ?
+            <CollectablesPlaceholder/>
+            :
+            <div id="collectables">
+                {
+                    collectables.map(({ tokens }) => tokens.map(({ network, address, collectionName, collectionImg, assets }) => assets.map(({ tokenId, assetName, assetImg, balanceUSD }) => (
+                        <div className="collectable" key={tokenId}>
+                            <NavLink to={`/wallet/nft/${network}/${address}/${tokenId}`}>
+                                <div className="artwork" style={{backgroundImage: `url(${assetImg})`}}/>
+                                <div className="info">
+                                    <div className="collection">
+                                        <div className="collection-icon" style={{backgroundImage: `url(${collectionImg})`}}></div>
+                                        { collectionName }
+                                    </div>
+                                    <div className="details">
+                                        <div className="name">{ assetName }</div>
+                                        <div className="value"><span className="purple-highlight">$</span> { balanceUSD.toFixed(2) }</div>
+                                    </div>
                                 </div>
-                                <div className="details">
-                                    <div className="name">{ assetName }</div>
-                                    <div className="value"><span className="purple-highlight">$</span> { balanceUSD.toFixed(2) }</div>
-                                </div>
-                            </div>
-                        </NavLink>
-                    </div>
-                ))))
-            }
-        </div>
+                            </NavLink>
+                        </div>
+                    ))))
+                }
+            </div>
     )
 }
 
