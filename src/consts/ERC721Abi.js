26042d550617adb5be8d8e9691c9151d6e40bbf3
Tim <tymmesyde@gmail.com>
2021-11-11 10:32:19 +0100

    refactor(Collectable): display owner

--- src/consts/ERC721Abi.js
+++ src/consts/ERC721Abi.js
@@ -1,6 +1,7 @@
 const ERC721Abi = [
     "function name() view returns (string memory)",
     "function tokenURI(uint256 _tokenId) view returns (string memory)",
+    "function ownerOf(uint256 tokenId) view returns (address)",
 ]
 
 export default ERC721Abi
