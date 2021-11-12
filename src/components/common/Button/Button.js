d4b70e968dc249d8d7a8225e3babdd2118a5544e
Tim <tymmesyde@gmail.com>
2021-11-12 12:10:03 +0100

    refactor(Button): add onClick event

--- src/components/common/Button/Button.js
+++ src/components/common/Button/Button.js
@@ -1,9 +1,9 @@
 import './Button.scss'
 
-const Button = ({ children, small, icon, disabled }) => {
+const Button = ({ children, small, icon, disabled, onClick }) => {
     return (
-        <button className={`buttonComponent ${small ? 'small' : ''}`} disabled={disabled}>
-            <div className="icon">{ icon }</div>
+        <button className={`buttonComponent ${small ? 'small' : ''}`} disabled={disabled} onClick={onClick}>
+            { icon ? <div className="icon-button">{ icon }</div> : null }
             { children }
         </button>
     )
