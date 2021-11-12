f15788463232152e341dc2d47464e201c53d855f
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2021-11-12 18:39:31 +0200

    Changed the UI of the Security Page, added functionality

--- src/components/Wallet/Security/Security.js
+++ src/components/Wallet/Security/Security.js
@@ -1,59 +1,97 @@
-import "./Security.scss";
+import './Security.scss'
 
-import { usePrivileges } from "../../../hooks";
-import { Loading, Select, TextInput } from "../../common";
-import { Interface } from "ethers/lib/utils";
-import privilegesOptions from "../../../consts/privilegesOptions";
+import { usePrivileges } from '../../../hooks'
+import { Loading, TextInput, Button } from '../../common'
+import { Interface } from 'ethers/lib/utils'
+import privilegesOptions from '../../../consts/privilegesOptions'
 
 const IDENTITY_INTERFACE = new Interface(
-  require("adex-protocol-eth/abi/Identity5.2")
-);
+  require('adex-protocol-eth/abi/Identity5.2')
+)
 
-const Security = (props) => {
+const Security = ({ selectedAcc, selectedNetwork, accounts, addRequest }) => {
   const { privileges, updatedBlock, isLoading } = usePrivileges({
-    identity: props.selectedAcc,
-    network: props.selectedNetwork,
-    accounts: props.accounts,
-  });
+    identity: selectedAcc,
+    network: selectedNetwork.id,
+    accounts: accounts,
+  })
 
-  const privList = Object.keys(privileges).map((key) => {
-    const onSelectPriv = (val) => {
-      if (val === privileges[key]) return
-      
-      const txn = {
-        to: props.selectedAcc,
-        data: IDENTITY_INTERFACE.encodeFunctionData("setAddrPrivilege", [
-          key,
-          val,
-        ]),
-      };
-      console.log('txn', txn)
-    };
+  const craftTransaction = (address, privLevel) => {
+    return {
+      to: selectedAcc,
+      data: IDENTITY_INTERFACE.encodeFunctionData('setAddrPrivilege', [
+        address,
+        privLevel,
+      ]),
+      value: '0x',
+    }
+  }
+
+  const addTransactionToAddRequest = txn => {
+    addRequest({
+      id: 'setPriv',
+      txn: txn,
+      chainId: selectedNetwork.chainId,
+      account: selectedAcc,
+    })
+  }
+
+  const onMakeDefaultBtnClicked = key => {
+    const txn = craftTransaction(key, privilegesOptions.true)
+    addTransactionToAddRequest(txn)
+
+    console.log('Make default', txn)
+  }
+
+  const onRemoveBtnClicked = key => {
+    const txn = craftTransaction(key, privilegesOptions.false)
+    addTransactionToAddRequest(txn)
+    console.log('Remove clicked', txn)
+  }
+
+  const selectedAccount = accounts.find(x => x.id === selectedAcc)
+
+  const privList = Object.keys(privileges).map(key => {
+    console.log('Selected Account', selectedAccount)
+    const isQuickAcc = selectedAccount.signer.hasOwnProperty('quickAccManager')
+    const privText = isQuickAcc
+      ? `Email/passphrase signer ${selectedAccount.email}`
+      : key
+    const signerAddress = isQuickAcc
+      ? selectedAccount.signer.quickAccManager
+      : selectedAccount.signer.address
+    const disabled = signerAddress === key
 
     return (
       <li key={key}>
-        <TextInput className="depositAddress" value={key} disabled/>
-        <Select
-          defaultValue={privileges[key]}
-          items={privilegesOptions}
-          itemLabel="label"
-          itemKey="value"
-          onChange={onSelectPriv}
-        />
+        <TextInput className="depositAddress" value={privText} disabled />
+        <div className="btns-wrapper">
+          <Button onClick={() => onMakeDefaultBtnClicked(key)} small>
+            Make default
+          </Button>
+          <Button
+            onClick={() => onRemoveBtnClicked(key)}
+            small
+            red
+            disabled={disabled}
+          >
+            Remove
+          </Button>
+        </div>
       </li>
-    );
-  });
+    )
+  })
 
   return (
     <section id="security">
       <div className="panel">
-        <div className="title">Set Privileges</div>
+        <div className="title">Security</div>
         {isLoading && <Loading />}
         {/* Set a msg if no privileges */}
         <ul className="content">{!isLoading && privList}</ul>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Security;
+export default Security
