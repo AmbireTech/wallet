2d00c3bdb70916488c067cc9a4f4a6a57590a98e
Ivo Georgiev <ivo@adex.network>
2021-11-14 03:47:17 +0200

    Transactions: render pending requests, TxnPreview args changed

--- src/components/common/TxnPreview/TxnPreview.js
+++ src/components/common/TxnPreview/TxnPreview.js
@@ -11,18 +11,19 @@ function getNetworkSymbol(networkId) {
   const network = networks.find(x => x.id === networkId)
   return network ? network.nativeAssetSymbol : 'UNKNW'
 }
-export default function TxnPreview ({ txn, onDismiss, bundle, isFirstFailing }) {
+export default function TxnPreview ({ txn, onDismiss, network, account, isFirstFailing }) {
   const [isExpanded, setExpanded] = useState(false)
-  const contractName = getContractName(txn, bundle.network)
+  console.log(network)
+  const contractName = getContractName(txn, network)
   return (
     <div className={isFirstFailing ? 'txnPreview firstFailing' : 'txnPreview'}>
-        <div>{getTransactionSummary(txn, bundle.network, bundle.identity)}</div>
+        <div>{getTransactionSummary(txn, network, account)}</div>
         {isFirstFailing ? (<div className='firstFailingLabel'>This is the first failing transaction.</div>) : (<></>)}
 
         {
           isExpanded ? (<div className='advanced'>
             <div><b>Interacting with (<i>to</i>):</b> {txn[0]}{contractName ? ` (${contractName})` : ''}</div>
-            <div><b>{getNetworkSymbol(bundle.network)} to be sent (<i>value</i>):</b> {formatUnits(txn[1], 18)}</div>
+            <div><b>{getNetworkSymbol(network)} to be sent (<i>value</i>):</b> {formatUnits(txn[1], 18)}</div>
             <div><b>Data:</b> {txn[2]}</div>
           </div>) : (<></>)
         }
