dee821e501f1b3f8dd83f86423abfa0df02e4d12
Ivo Georgiev <ivo@strem.io>
2021-11-14 03:34:55 +0200

    Transactions: render mined bundles

--- src/components/Wallet/Transactions/Transactions.js
+++ src/components/Wallet/Transactions/Transactions.js
@@ -1,6 +1,9 @@
+import './Transactions.scss'
+
 import { useRelayerData } from '../../../hooks'
 import TxnPreview from '../../common/TxnPreview/TxnPreview'
 import { Loading } from '../../common'
+import accountPresets from '../../../consts/accountPresets'
 
 function Transactions ({ relayerURL, selectedAcc, selectedNetwork }) {
   const { data, errMsg, isLoading } = useRelayerData(relayerURL ? `${relayerURL}/identity/${selectedAcc}/${selectedNetwork.id}/transactions` : null)
@@ -12,23 +15,34 @@ function Transactions ({ relayerURL, selectedAcc, selectedNetwork }) {
 
   return (
     <section id='transactions'>
-      <div className='panel'>
-        <div className='title'>Mined transactions</div>
-        {!relayerURL && (<h3 className='error'>Unsupported: not currently connected to a relayer.</h3>)}
-        {errMsg && (<h3 className='error'>Error getting mined transactions: {errMsg}</h3>)}
-        {isLoading && <Loading />}
-        {
-            // @TODO respect the limit and implement pagination
-            !isLoading && data && data.txns.map(bundle => (<div className='panel' key={bundle._id}>
-                {bundle.txns.map((txn, i) => (<TxnPreview
-                    key={i} // safe to do this, individual TxnPreviews won't change within a specific bundle
-                    txn={txn} bundle={bundle}/>
-                ))}
-            </div>))
-        }
-      </div>
+      <h3>Mined transactions</h3>
+      {!relayerURL && (<h3 className='error'>Unsupported: not currently connected to a relayer.</h3>)}
+      {errMsg && (<h3 className='error'>Error getting mined transactions: {errMsg}</h3>)}
+      {isLoading && <Loading />}
+      {
+          // @TODO respect the limit and implement pagination
+          !isLoading && data && data.txns.map(MinedBundle)
+      }
     </section>
   )
+}
+
+function MinedBundle(bundle) {
+  const lastTxn = bundle.txns[bundle.txns.length - 1]
+  // terribly hacky; @TODO fix
+  // all of the values are prob checksummed so we may not need toLowerCase
+  const hasFee = lastTxn[0].toLowerCase() === accountPresets.feeCollector.toLowerCase()
+    || lastTxn[2].toLowerCase().includes(accountPresets.feeCollector.toLowerCase().slice(2))
+  const txns = hasFee ? bundle.txns.slice(0, -1) : bundle.txns
+
+  return (<div className='minedBundle' key={bundle._id}>
+    {txns.map((txn, i) => (<TxnPreview
+      key={i} // safe to do this, individual TxnPreviews won't change within a specific bundle
+      txn={txn} bundle={bundle}/>
+    ))}
+    {hasFee && (<div className='fee'>Fee: </div>)}
+    <div>Submitted at: {bundle.submittedAt}</div>
+  </div>)
 }
 
 export default Transactions
