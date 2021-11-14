b0ae13cb8ee8c0b81154982f07fa5217d41af09d
Ivo Georgiev <ivo@strem.io>
2021-11-14 22:03:58 +0200

    new accountPresets

--- src/consts/accountPresets.js
+++ src/consts/accountPresets.js
@@ -2,9 +2,9 @@
 // NOTE: those should only apply to new accounts, never for existing accounts, since some existing accounts may be on older versions of those
 const accountPresets = {
     salt: '0x0000000000000000000000000000000000000000000000000000000000000001',
-    baseIdentityAddr: '0xD642b4c7ad09c79089B82CC28B19BB4437E59b61',
-    identityFactoryAddr: '0xf5D7BD04c8557247978d032314bA2863f13D617E',
-    quickAccManager: '0xA8AE248276ff3809E4Ec3a1fd4aaD82af940074a',
+    identityFactoryAddr: '0xBf07a0Df119Ca234634588fbDb5625594E2a5BCA',
+    baseIdentityAddr: '0x2A2b85EB1054d6f0c6c2E37dA05eD3E5feA684EF',
+    quickAccManager: '0xfF3f6D14DF43c112aB98834Ee1F82083E07c26BF',
     feeCollector: '0x942f9CE5D9a33a82F88D233AEb3292E680230348',
     quickAccTimelock: 259200 // 3 days
 }
