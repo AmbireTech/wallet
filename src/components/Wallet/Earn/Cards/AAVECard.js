5798e6913ae8f7505a674139606e0d9173cd408e
Tim <tymmesyde@gmail.com>
2021-11-15 19:31:29 +0100

    refactor(Earn): implement deposit for AAVE Card

--- src/components/Wallet/Earn/Cards/AAVECard.js
+++ src/components/Wallet/Earn/Cards/AAVECard.js
@@ -2,11 +2,13 @@ import { ethers, getDefaultProvider } from 'ethers'
 import { Interface } from 'ethers/lib/utils'
 import { useCallback, useEffect, useState } from 'react'
 import { useToasts } from '../../../../hooks/toasts'
+import ERC20Abi from 'adex-protocol-eth/abi/ERC20.json'
 import AAVELendingPoolAbi from '../../../../consts/AAVELendingPoolAbi'
 
 import AAVE_ICON from '../../../../resources/aave.svg'
 import Card from './Card/Card'
 
+const ERC20Interface = new Interface(ERC20Abi)
 const AAVELendingPool = new Interface(AAVELendingPoolAbi)
 const lendingPoolProvider = {
     ethereum: '0xb53c1a33016b2dc2ff3653530bff1848a515c8c5',
@@ -14,8 +16,9 @@ const lendingPoolProvider = {
     avalanche: '0xb6A86025F0FE1862B372cb0ca18CE3EDe02A318f'
 }
 const RAY = 10**27
+let lendingPoolAddress = null
 
-const AAVECard = ({ network, tokens }) => {
+const AAVECard = ({ network, tokens, account, addRequest }) => {
     const { addToast } = useToasts()
 
     const [tokenItems, setTokenItems] = useState([])
@@ -26,7 +29,7 @@ const AAVECard = ({ network, tokens }) => {
             const { rpc } = network
             const provider = getDefaultProvider(rpc)
             const lendingPoolProviderContract = new ethers.Contract(lendingPoolProvider[network.id], AAVELendingPool, provider)
-            const lendingPoolAddress = await lendingPoolProviderContract.getLendingPool()
+            lendingPoolAddress = await lendingPoolProviderContract.getLendingPool()
         
             const lendingPoolContract = new ethers.Contract(lendingPoolAddress, AAVELendingPool, provider)
             const reserves = await lendingPoolContract.getReservesList()
@@ -41,13 +44,14 @@ const AAVECard = ({ network, tokens }) => {
                 }
             }))
 
-            const tokenItems = tokensWithApr.map(({ img, symbol, address, balance, apr }) => ({
+            const tokenItems = tokensWithApr.map(({ img, symbol, address, balance, decimals, apr }) => ({
                 icon: img,
                 label: `${symbol} (${apr}% APR)`,
                 value: address,
                 address,
                 balance,
                 symbol,
+                decimals,
                 apr
             }))
 
@@ -69,10 +73,68 @@ const AAVECard = ({ network, tokens }) => {
         }
     }, [tokenItems])
 
+    const approveToken = async (tokenAddress, bigNumberHexAmount) => {
+        const ZERO = ethers.BigNumber.from(0)
+        const { rpc } = network
+        const provider = getDefaultProvider(rpc)
+        const tokenContract = new ethers.Contract(tokenAddress, ERC20Interface, provider)
+        const allowance = await tokenContract.allowance(account, tokenAddress)
+
+        if (allowance.lt(bigNumberHexAmount)) {
+            if (allowance.gt(ZERO)) addRequest({
+                id: `aave_pool_deposit_${Date.now()}`,
+                type: 'eth_sendTransaction',
+                chainId: network.chainId,
+                account,
+                txn: {
+                    to: lendingPoolAddress,
+                    value: bigNumberHexAmount,
+                    data: '0x'
+                }
+            })
+            addRequest({
+                id: `aave_pool_deposit_${Date.now()}`,
+                type: 'eth_sendTransaction',
+                chainId: network.chainId,
+                account,
+                txn: {
+                    to: tokenAddress,
+                    value: '0x0',
+                    data: ERC20Interface.encodeFunctionData('approve', [lendingPoolAddress, bigNumberHexAmount])
+                }
+            })
+	    }
+    }
+
+    const onValidate = async (type, tokenAddress, amount) => {
+        const token = tokenItems.find(({ address }) => address === tokenAddress)
+        const bigNumberHexAmount = ethers.utils.parseUnits(amount.toString(), token.decimals).toHexString()
+        await approveToken(tokenAddress, bigNumberHexAmount)
+
+        if (type === 'Deposit') {
+            try {
+                addRequest({
+                    id: `aave_pool_deposit_${Date.now()}`,
+                    type: 'eth_sendTransaction',
+                    chainId: network.chainId,
+                    account,
+                    txn: {
+                        to: lendingPoolAddress,
+                        value: '0x0',
+                        data: AAVELendingPool.encodeFunctionData('deposit', [tokenAddress, bigNumberHexAmount, account, 0])
+                    }
+                })
+            } catch(e) {
+                console.error(e)
+                addToast(`Error: ${e.message || e}`, { error: true })
+            }
+        }
+    }
+
     useEffect(() => initPool(), [tokens, initPool])
 
     return (
-        <Card icon={AAVE_ICON} details={details} tokens={tokenItems} onTokenSelect={onTokenSelect}/>
+        <Card icon={AAVE_ICON} details={details} tokens={tokenItems} onTokenSelect={onTokenSelect} onValidate={onValidate}/>
     )
 }
 
--- src/components/Wallet/Earn/Earn.js
+++ src/components/Wallet/Earn/Earn.js
@@ -2,14 +2,14 @@ import './Earn.scss'
 import AAVECard from './Cards/AAVECard'
 import { Loading } from '../../common'
 
-const Earn = ({ portfolio, selectedNetwork }) => {
+const Earn = ({ portfolio, selectedNetwork, selectedAcc, addRequest }) => {
     return (
         <div id="earn">
             {
                 portfolio.isBalanceLoading ?
                     <Loading/>
                     :
-                    <AAVECard network={{...selectedNetwork}} tokens={portfolio.tokens}/>
+                    <AAVECard network={{...selectedNetwork}} tokens={portfolio.tokens} account={selectedAcc} addRequest={addRequest}/>
             }
         </div>
     )
