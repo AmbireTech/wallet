daaf6c5349de166c9059cda72ecb55c2142c1a2d
Tim <tymmesyde@gmail.com>
2021-11-15 14:46:23 +0100

    refactor(Earn): add avalanche network for AAVE Lending Pool

--- src/components/Wallet/Earn/Cards/AAVECard.js
+++ src/components/Wallet/Earn/Cards/AAVECard.js
@@ -10,7 +10,8 @@ import Card from './Card/Card'
 const AAVELendingPool = new Interface(AAVELendingPoolAbi)
 const lendingPoolProvider = {
     ethereum: '0xb53c1a33016b2dc2ff3653530bff1848a515c8c5',
-    polygon: '0xd05e3E715d945B59290df0ae8eF85c1BdB684744'
+    polygon: '0xd05e3E715d945B59290df0ae8eF85c1BdB684744',
+    avalanche: '0xb6A86025F0FE1862B372cb0ca18CE3EDe02A318f'
 }
 const RAY = 10**27
 
