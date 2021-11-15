9c222c4ed529e256c0c9c512d31c0ddf307323af
Tim <tymmesyde@gmail.com>
2021-11-15 15:09:29 +0100

    refactor(AAVECard): update tokens label to show APR

--- src/components/Wallet/Earn/Earn.js
+++ src/components/Wallet/Earn/Earn.js
@@ -3,20 +3,13 @@ import AAVECard from './Cards/AAVECard'
 import { Loading } from '../../common'
 
 const Earn = ({ portfolio, selectedNetwork }) => {
-    const tokenItems = portfolio.tokens.map(({ img, symbol, address, balance }) => ({
-        icon: img,
-        label: symbol,
-        value: address,
-        balance: balance.toFixed(2)
-    }))
-
     return (
         <div id="earn">
             {
                 portfolio.isBalanceLoading ?
                     <Loading/>
                     :
-                    <AAVECard network={{...selectedNetwork}} tokens={tokenItems}/>
+                    <AAVECard network={{...selectedNetwork}} tokens={portfolio.tokens}/>
             }
         </div>
     )
