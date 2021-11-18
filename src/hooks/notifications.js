1c1f5a64fdb047ed54d93af8e79e21fd8d14c7cf
Tim <tymmesyde@gmail.com>
2021-11-18 18:34:16 +0100

    feat: show notification when balance increase

--- src/hooks/notifications.js
+++ src/hooks/notifications.js
@@ -1,12 +1,15 @@
 import { useEffect } from 'react'
 import { getTransactionSummary } from '../lib/humanReadableTransactions'
+import { ethers } from 'ethers'
 import networks from '../consts/networks'
 
 const REQUEST_TITLE_PREFIX = 'Ambire Wallet: '
 const SUPPORTED_TYPES =  ['eth_sendTransaction', 'personal_sign']
 let currentNotifs = []
+let isLastTotalBalanceInit = false
+let lastTokensBalanceRaw = []
 
-export default function useNotifications (requests) {
+export default function useNotifications (requests, portfolio) {
     useEffect(() => {
         if (window.Notification && Notification.permission !== 'denied') {
             Notification.requestPermission(() => {
@@ -14,6 +17,20 @@ export default function useNotifications (requests) {
             })
         }
     }, [])
+    
+    const showNotification = ({ id, title, body, requireInteraction }) => {
+        const notification = new Notification(title, {
+            requireInteraction: requireInteraction || false,
+            body,
+            icon: 'public/logo192.png',
+        })
+        //notification.onclose = 
+        notification.onclick = () => {
+            window.focus()
+            notification.close()
+        }
+        currentNotifs.push({ id, notification })
+    }
 
     requests.forEach(request => {
         if (!SUPPORTED_TYPES.includes(request.type)) return
@@ -27,18 +44,47 @@ export default function useNotifications (requests) {
                 ? 'you have a new message to sign'
                 : `new transaction request on ${network ? network.name : 'unknown network'}`
         )
-        const notification = new Notification(title, {
-            requireInteraction: true,
-            body: isSign ? 'Click to preview' : getTransactionSummary([request.txn.to, request.txn.value, request.txn.data], request.chainId, request.account),
-            icon: 'public/logo192.png',
+        const body = isSign ? 'Click to preview' : getTransactionSummary([request.txn.to, request.txn.value, request.txn.data], request.chainId, request.account)
+        showNotification({
+            id: request.id,
+            title,
+            body,
+            requireInteraction: true
         })
-        //notification.onclose = 
-        notification.onclick = () => {
-            window.focus()
-            notification.close()
-        }
-        currentNotifs.push({ id: request.id, notification })
     })
+
+    useEffect(() => {
+        if (!portfolio.isBalanceLoading && portfolio.balance) {
+            if (!isLastTotalBalanceInit) {
+                isLastTotalBalanceInit = true
+                lastTokensBalanceRaw = portfolio.tokens.map(({ address, balanceRaw }) => ({ address, balanceRaw }))
+            }
+
+            const changedAmounts = portfolio.tokens.filter(({ address, balanceRaw }) => {
+                const lastToken = lastTokensBalanceRaw.find(token => token.address === address)
+                return lastToken && balanceRaw > lastToken.balanceRaw ? true : false 
+            })
+
+            changedAmounts.forEach(({ address, symbol, decimals, balanceRaw }) => {
+                const lastToken = lastTokensBalanceRaw.find(token => token.address === address)
+                const amountRecieved = ethers.utils.formatUnits((balanceRaw - lastToken.balanceRaw).toString(), decimals)
+
+                showNotification({
+                    id: `received_amount_${Date.now()}`,
+                    title: `${amountRecieved} ${symbol} Received.`,
+                    body: `Your ${symbol} balance increased by ${amountRecieved} ${symbol}`
+                })
+
+                lastTokensBalanceRaw = [
+                    ...lastTokensBalanceRaw.filter(token => token.address !== address),
+                    {
+                        ...lastToken,
+                        balanceRaw
+                    }
+                ]
+            })
+        }
+    }, [portfolio])
 
     currentNotifs = currentNotifs.filter(({ id, notification }) => {
         if (!requests.find(r => r.id === id)) {
