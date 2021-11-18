a154c0a3e5866fd434d46645f51bcca080374256
Ivo Georgiev <ivo@adex.network>
2021-11-19 01:46:46 +0200

    notifications: do not pop up the sendTransactions dialog if we are not doing that

--- src/hooks/notifications.js
+++ src/hooks/notifications.js
@@ -27,7 +27,7 @@ export default function useNotifications (requests, onShow, portfolio, selectedA
         }
     }, [])
     
-    const showNotification = useCallback(({ id, title, body, requireInteraction }) => {
+    const showNotification = useCallback(({ id, title, body, requireInteraction, isSendTransaction }) => {
         const notification = new Notification(title, {
             requireInteraction: requireInteraction || false,
             body,
@@ -37,7 +37,7 @@ export default function useNotifications (requests, onShow, portfolio, selectedA
         notification.onclick = () => {
             window.focus()
             notification.close()
-            onShow()
+            isSendTransaction && onShow()
         }
         currentNotifs.push({ id, notification })
     }, [onShow])
@@ -58,6 +58,7 @@ export default function useNotifications (requests, onShow, portfolio, selectedA
             id: request.id,
             title,
             body,
+            isSendTransaction: request.type === 'eth_sendTransaction',
             requireInteraction: true
         })
     })
