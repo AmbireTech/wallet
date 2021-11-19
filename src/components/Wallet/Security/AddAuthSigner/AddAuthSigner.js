31c4b21cd719a38749ee7accd056833c314f1370
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2021-11-19 14:53:26 +0200

    added trezor/web3 functionalities WIP

--- src/components/Wallet/Security/AddAuthSigner/AddAuthSigner.js
+++ src/components/Wallet/Security/AddAuthSigner/AddAuthSigner.js
@@ -1,150 +1,280 @@
+import './AddAuthSigner.scss'
 import { useState, useEffect } from 'react'
 
 import { TextInput, Button, Modal, DropDown } from '../../../common'
 import { LedgerSubprovider } from '@0x/subproviders/lib/src/subproviders/ledger' // https://github.com/0xProject/0x-monorepo/issues/1400
 import { ledgerEthereumBrowserClientFactoryAsync } from '@0x/subproviders/lib/src' // https://github.com/0xProject/0x-monorepo/issues/1400
+import TrezorConnect from 'trezor-connect'
+import { TrezorSubprovider } from '@0x/subproviders/lib/src/subproviders/trezor' // https://github.com/0xProject/0x-monorepo/issues/1400
 
-const AddAuthSigner = (props) => {
-    const [signerAddress, setSignerAddress] = useState({ address: '', index: 0 })
-    const [signersToChoose, setChooseSigners] = useState(null)
-    const [addAccErr, setAddAccErr] = useState('')
-    const [modalToggle, setModalToggle] = useState(true)
-    const [textInputInfo, setTextInputInfo] = useState('')
-    const [disabled, setDisabled] = useState(true)
-  
-    const modalHandler = () => {
-      setModalToggle(prevState => !prevState)
-    }
-  
-    async function connectLedgerAndGetAccounts() {
-      const provider = new LedgerSubprovider({
-        networkId: 0, // @TODO: is this needed?
-        ledgerEthereumClientFactoryAsync: ledgerEthereumBrowserClientFactoryAsync,
-        //baseDerivationPath: this.baseDerivationPath
-      })
-      // NOTE: do not attempt to do both of these together (await Promise.all)
-      // there is a bug in the ledger subprovider (race condition), so it will think we're trying to make two connections simultaniously
-      // cause one call won't be aware of the other's attempt to connect
-      const addresses = await provider.getAccountsAsync(50)
-      const signerExtra = await provider
-        ._initialDerivedKeyInfoAsync()
-        .then(info => ({
-          type: 'ledger',
-          info: JSON.parse(JSON.stringify(info)),
-        }))
-      setChooseSigners({ addresses, signerExtra })
-      setModalToggle(true)
-    }
-  
-    const wrapErr = async fn => {
-      setAddAccErr('')
-      try {
-        await fn()
-      } catch (e) {
-        console.error(e)
-        setAddAccErr(`Unexpected error: ${e.message || e}`)
-      }
-    }
-  
-    const onSignerAddressClicked = value => {
-      setSignerAddress(value)
-      modalHandler()
-      setTextInputInfo(`Trezor/Ledger address # ${value.index + 1}`)
-    }
-  
-    const signersToChooseModal = (
-      <Modal show={modalToggle} modalClosed={modalHandler}>
-        <div
-          className="loginSignupWrapper chooseSigners"
-          style={{ background: 'transparent' }}
-        >
-          <h3>Choose a signer</h3>
-          <ul id="signersToChoose">
-            {signersToChoose
-              ? signersToChoose.addresses.map((addr, index) => (
-                  <li
-                    key={addr}
-                    onClick={() =>
-                      onSignerAddressClicked({ address: addr, index: index })
-                    }
-                  >
-                    {addr}
-                  </li>
-                ))
-              : null}
-          </ul>
-        </div>
-      </Modal>
-    )
-  
-    const addFromSignerButtons = (
-      <>
-        <button className="button" onClick={() => wrapErr()}>
-          {/* TODO: Implememnt func for trezor */}
-          <div
-            className="icon"
-            style={{ backgroundImage: 'url(./resources/trezor.png)' }}
-          />
-          Trezor
-        </button>
-        <button
-          className="button"
-          onClick={() => wrapErr(connectLedgerAndGetAccounts)}
-        >
-          <div
-            className="icon"
-            style={{ backgroundImage: 'url(./resources/ledger.png)' }}
-          />
-          Ledger
-        </button>
-        {/* TODO: this btn should be Web3 */}
-        {/* TODO: Implement func for Web3 */}
-        <button className="button" onClick={() => wrapErr()}>
-          <div
-            className="icon"
-            style={{ backgroundImage: 'url(./resources/metamask.png)' }}
-          />
-          Metamask / Browser
-        </button>
-      </>
-    )
-  
-    const onTextInput = value => {
-      if (textInputInfo.length) setTextInputInfo('')
-      setSignerAddress({ ...signerAddress, address: value })
-    }
+const AddAuthSigner = props => {
+  const [signerAddress, setSignerAddress] = useState({
+    address: '',
+    index: 0,
+    signerExtra: {},
+  })
+  const [signersToChoose, setChooseSigners] = useState(null)
+  const [addAccErr, setAddAccErr] = useState('')
+  const [modalToggle, setModalToggle] = useState(true)
+  const [textInputInfo, setTextInputInfo] = useState('')
+  const [disabled, setDisabled] = useState(true)
 
-    useEffect(() => {
-        const isAddressValid = /^0x[a-fA-F0-9]{40}$/.test(signerAddress.address)
-        setDisabled(!isAddressValid)
-    }, [signerAddress.address])
-  
-    return (
-      <div className="content">
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            marginBottom: '1em',
-          }}
-        >
-          <TextInput
-            placeholder='Enter address'
-            className="depositAddress"
-            value={signerAddress.address}
-            info={textInputInfo}
-            onInput={onTextInput}
-          />
-          <DropDown title="connect wallet">{addFromSignerButtons}</DropDown>
-          <div className="btns-wrapper">
-            <Button disabled={disabled} onClick={() => props.onAddBtnClicked(signerAddress.address)} small>Add</Button>
-          </div>
-        </div>
-        {signersToChoose && signersToChooseModal}
-        {addAccErr ? <h3 className="error">{addAccErr}</h3> : <></>}
-      </div>
-    )
+  async function connectLedgerAndGetAccounts() {
+    const provider = new LedgerSubprovider({
+      networkId: 0, // @TODO: is this needed?
+      ledgerEthereumClientFactoryAsync: ledgerEthereumBrowserClientFactoryAsync,
+      //baseDerivationPath: this.baseDerivationPath
+    })
+    // NOTE: do not attempt to do both of these together (await Promise.all)
+    // there is a bug in the ledger subprovider (race condition), so it will think we're trying to make two connections simultaniously
+    // cause one call won't be aware of the other's attempt to connect
+    const addresses = await provider.getAccountsAsync(100)
+    const signerExtra = await provider
+      ._initialDerivedKeyInfoAsync()
+      .then(info => ({
+        type: 'ledger',
+        info: JSON.parse(JSON.stringify(info)),
+      }))
+    setChooseSigners({ addresses, signerExtra })
+    setModalToggle(true)
   }
 
-  export default AddAuthSigner
+  async function connectTrezorAndGetAccounts() {
+    /*
+      const engine = new Web3ProviderEngine({ pollingInterval: this.pollingInterval })
+      engine.addProvider(new TrezorSubprovider({ trezorConnectClientApi: TrezorConnect, ...this.config }))
+      engine.addProvider(new CacheSubprovider())
+      engine.addProvider(new RPCSubprovider(this.url, this.requestTimeoutMs))
+      */
+    const provider = new TrezorSubprovider({
+      trezorConnectClientApi: TrezorConnect,
+    })
+    const addresses = await provider.getAccountsAsync(100)
+    setChooseSigners({
+      addresses,
+      signerExtra: {
+        type: 'trezor',
+        info: JSON.parse(JSON.stringify(provider._initialDerivedKeyInfo)),
+      },
+    })
+    setModalToggle(true)
+  }
+
+  async function connectWeb3AndGetAccounts() {
+    debugger
+    // @TODO: pending state; should bein the LoginORSignup (AddAccount) component
+    if (typeof window.ethereum === 'undefined') {
+      // @TODO catch this
+      throw new Error('MetaMask not available')
+    }
+    const ethereum = window.ethereum
+    const web3Accs = await ethereum.request({ method: 'eth_requestAccounts' })
+    if (!web3Accs.length) throw new Error('No accounts connected')
+    if (web3Accs.length === 1)
+      return onSignerAddressClicked({
+        address: web3Accs[0],
+        index: 0,
+        signerExtra: {},
+      })
+    setChooseSigners({ addresses: web3Accs })
+  }
+
+  const modalHandler = () => {
+    setModalToggle(prevState => !prevState)
+  }
+
+  const wrapErr = async fn => {
+    setAddAccErr('')
+    try {
+      await fn()
+    } catch (e) {
+      console.error(e)
+      setAddAccErr(`Unexpected error: ${e.message || e}`)
+    }
+  }
+
+  const onSignerAddressClicked = value => {
+    setSignerAddress(value)
+    modalHandler()
+    setTextInputInfo(`Trezor/Ledger address # ${value.index + 1}`)
+  }
+
+  const testData = [
+    '0x1111111111111111111111111111111111111111',
+    '0x63de53b82e37465bf6431231ad29f3133b884463',
+    '0xe1ae0619671dfb804b83d7a98ffaf937565743ce',
+    '0x1506828e1bdc9b0009c29bc6e9b224ada83eec4b',
+    '0xa357f01404492b3129ab3a30c6cd506773a1263e',
+    '0xdd16568285be734877668acae80c451605e791fa',
+    '0x63de53b82e37465bf6431231ad29f3133b884462',
+    '0xe1ae0619671dfb804b83d7a98ffaf937565743cw',
+    '0x1506828e1bdc9b0009c29bc6e9b224ada83eec42',
+    '0xa357f01404492b3129ab3a30c6cd506773a1263a',
+    '0xdd16568285be734877668acae80c451605e791f6',
+    '0x63de53b82e37465bf6431231ad29f3133b884461',
+    '0xe1ae0619671dfb804b83d7a98ffaf937565743c1',
+    '0x1506828e1bdc9b0009c29bc6e9b224ada83eec4a',
+    '0xa357f01404492b3129ab3a30c6cd506773a12636',
+    '0xdd16568285be734877668acae80c451605e791f5',
+    '0x63de53b82e37465bf6431231ad29f3133b884464',
+    '0xe1ae0619671dfb804b83d7a98ffaf937565743c3',
+    '0x1506828e1bdc9b0009c29bc6e9b224ada83eec47',
+    '0xa357f01404492b3129ab3a30c6cd506773a12631',
+    '0x0000000000000000000000000000000000000000',
+    '0x0000000000000000000000000000000000000001',
+    '0x0000000000000000000000000000000000000002',
+  ]
+
+  const paginate = (arr, size) => {
+    return arr.reduce((acc, val, i) => {
+      let idx = Math.floor(i / size)
+      let page = acc[idx] || (acc[idx] = [])
+      page.push(val)
+
+      return acc
+    }, [])
+  }
+
+  const [currentPage, setCurrentPage] = useState(0)
+  let pages = []
+  let pageSize = 5
+
+  if (signersToChoose) {
+    pages = paginate(signersToChoose.addresses, pageSize)
+
+    // let [prevBtnDisabled, setPrevBtnDisabled] = useState(false)
+    // let [nextBtnDisabled, setNextBtnDisabled] = useState(false)
+  }
+
+  const nextPage = () => {
+    if (currentPage === pages.length - 1) return
+    setCurrentPage(prevState => prevState + 1)
+  }
+
+  const prevPage = () => {
+    if (currentPage === 0) return
+    setCurrentPage(prevState => prevState - 1)
+  }
+
+  const signersToChooseModal = (
+    <Modal show={modalToggle} modalClosed={modalHandler}>
+      <div
+        className="loginSignupWrapper chooseSigners"
+        style={{ background: 'transparent' }}
+      >
+        <h3>Select a signer account</h3>
+        <ul id="signersToChoose" style={{ height: '275px', padding: '0' }}>
+          {signersToChoose
+            ? pages[currentPage].map((addr, index) => (
+                <li
+                  key={addr}
+                  onClick={() =>
+                    onSignerAddressClicked({
+                      address: addr,
+                      index: index,
+                      signerExtra: signersToChoose.signerExtra,
+                    })
+                  }
+                >
+                  {currentPage * pageSize + index + 1}&nbsp;
+                  {addr}
+                  {/* <a href={props.selectedNetwork.explorerUrl+'/address/'+addr} target='_blank' rel='noreferrer'>{props.selectedNetwork.explorerUrl.split('/')[2]}</a> */}
+                </li>
+              ))
+            : null}
+        </ul>
+        <div>
+          {currentPage + 1}/{pages.length}
+        </div>
+        <button type="button" onClick={prevPage}>
+          Prev
+        </button>
+        <button type="button" onClick={nextPage}>
+          Next
+        </button>
+      </div>
+    </Modal>
+  )
+
+  const addFromSignerButtons = (
+    <div className="wallet-btns-wrapper">
+      <button
+        className="button"
+        onClick={() => wrapErr(connectTrezorAndGetAccounts)}
+      >
+        <div
+          className="icon"
+          style={{ backgroundImage: 'url(./resources/trezor.png)' }}
+        />
+        Trezor
+      </button>
+      <button
+        className="button"
+        onClick={() => wrapErr(connectLedgerAndGetAccounts)}
+      >
+        <div
+          className="icon"
+          style={{ backgroundImage: 'url(./resources/ledger.png)' }}
+        />
+        Ledger
+      </button>
+      <button
+        className="button"
+        onClick={() => wrapErr(connectWeb3AndGetAccounts)}
+      >
+        <div
+          className="icon"
+          style={{ backgroundImage: 'url(./resources/metamask.png)' }}
+        />
+        Metamask / Browser
+      </button>
+    </div>
+  )
+
+  const onTextInput = value => {
+    if (textInputInfo.length) setTextInputInfo('')
+    setSignerAddress({ ...signerAddress, address: value })
+    console.log('signerAddress', signerAddress)
+  }
+
+  useEffect(() => {
+    const isAddressValid = /^0x[a-fA-F0-9]{40}$/.test(signerAddress.address)
+    setDisabled(!isAddressValid)
+  }, [signerAddress.address])
+
+  return (
+    <div className="content">
+      <div
+        style={{
+          display: 'flex',
+          // alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
+      >
+        <TextInput
+          placeholder="Enter address"
+          className="depositAddress"
+          value={signerAddress.address}
+          info={textInputInfo}
+          onInput={onTextInput}
+        />
+        <DropDown style={{ height: '60px' }} title="connect wallet">
+          {addFromSignerButtons}
+        </DropDown>
+        <div className="btns-wrapper">
+          <Button
+            disabled={disabled}
+            onClick={() => props.onAddBtnClicked(signerAddress)}
+            small
+          >
+            Add
+          </Button>
+        </div>
+      </div>
+      {signersToChoose && signersToChooseModal}
+      {addAccErr ? <h3 className="error">{addAccErr}</h3> : <></>}
+    </div>
+  )
+}
+
+export default AddAuthSigner
--- src/components/Wallet/Security/Security.js
+++ src/components/Wallet/Security/Security.js
@@ -7,10 +7,15 @@ import privilegesOptions from '../../../consts/privilegesOptions'
 import { useRelayerData } from '../../../hooks'
 import AddAuthSigner from './AddAuthSigner/AddAuthSigner'
 import { useToasts } from '../../../hooks/toasts'
+import { hexZeroPad, getAddress } from 'ethers/lib/utils'
+import { fetch, fetchPost } from '../../../lib/fetch'
 
 const IDENTITY_INTERFACE = new Interface(
   require('adex-protocol-eth/abi/Identity5.2')
 )
+
+const { generateAddress2 } = require('ethereumjs-util')
+const { getProxyDeployBytecode } = require('adex-protocol-eth/js/IdentityProxyDeploy')
 
 const Security = ({
   relayerURL,
@@ -20,15 +25,13 @@ const Security = ({
   addRequest,
   onAddAccount,
 }) => {
-  const { data, errMsg, isLoading } = useRelayerData(
-    relayerURL
-      ? `${relayerURL}/identity/${selectedAcc}/${selectedNetwork.id}/privileges`
-      : null
-  )
+  const url = relayerURL
+    ? `${relayerURL}/identity/${selectedAcc}/${selectedNetwork.id}/privileges`
+    : null
+
+  const { data, errMsg, isLoading } = useRelayerData(url)  
   const privileges = data ? data.privileges : {}
   const { addToast } = useToasts()
-
-  console.log('privileges', privileges)
 
   const craftTransaction = (address, privLevel) => {
     return {
@@ -50,28 +53,93 @@ const Security = ({
         chainId: selectedNetwork.chainId,
         account: selectedAcc,
       })
-    } catch(err) {
-        console.error(err)
-        addToast(`Error: ${err.message || err}`, { error: true })
+    } catch (err) {
+      console.error(err)
+      addToast(`Error: ${err.message || err}`, { error: true })
     }
   }
 
-  const onMakeDefaultBtnClicked = key => {
-    // @TODO
-  }
+  // const onMakeDefaultBtnClicked = key => {
+  //   // @TODO
+  // }
 
   const onRemoveBtnClicked = key => {
     const txn = craftTransaction(key, privilegesOptions.false)
     addTransactionToAddRequest(txn)
   }
 
-  const onAddBtnClickedHandler = (newSignerAddress) => {
-    const txn = craftTransaction(newSignerAddress, privilegesOptions.true)
-    addTransactionToAddRequest(txn)
+  const onAddBtnClickedHandler = newSignerAddress => {
+    console.log('newSigner', newSignerAddress)
+    // const txn = craftTransaction(newSignerAddress, privilegesOptions.true)
+    // addTransactionToAddRequest(txn)
   }
 
+  async function onMakeDefaultBtnClicked (addr, signerExtra) {
+    const addAccount = (acc, opts) => onAddAccount({ ...acc, signerExtra }, opts)
+    // when there is no relayer, we can only add the 'default' account created from that EOA
+    // @TODO in the future, it would be nice to do getLogs from the provider here to find out which other addrs we control
+    //   ... maybe we can isolate the code for that in lib/relayerless or something like that to not clutter this code
+    if (!relayerURL) return addAccount(await createFromEOA(addr), { select: true })
+    // otherwise check which accs we already own and add them
+    const owned = await getOwnedByEOAs([addr])
+    if (!owned.length) return addAccount(await createFromEOA(addr), { select: true })
+    else owned.forEach((acc, i) => addAccount(acc , { select: i === 0 }))
+  }
+
+  async function getOwnedByEOAs(eoas) {
+    let allUniqueOwned = {}
+
+    await Promise.all(eoas.map(
+        async signerAddr => {
+            const resp = await fetch(`${relayerURL}/identity/any/by-owner/${signerAddr}?includeFormerlyOwned=true`)
+            const privEntries = Object.entries(await resp.json())
+            // discard the privileges value, we do not need it as we wanna add all accounts EVER owned by this eoa
+            privEntries.forEach(([id, _]) => allUniqueOwned[id] = getAddress(signerAddr))
+        }
+    ))
+
+    return await Promise.all(
+        Object.entries(allUniqueOwned).map(([id, signer]) => getAccountByAddr(id, signer))
+    )
+}
+
+async function getAccountByAddr (idAddr, signerAddr) {
+  // In principle, we need these values to be able to operate in relayerless mode,
+  // so we just store them in all cases
+  // Plus, in the future this call may be used to retrieve other things
+  const { salt, identityFactoryAddr, baseIdentityAddr, bytecode } = await fetch(`${relayerURL}/identity/${idAddr}`)
+      .then(r => r.json())
+  if (!(salt && identityFactoryAddr && baseIdentityAddr && bytecode)) throw new Error(`Incomplete data from relayer for ${idAddr}`)
+  return {
+      id: idAddr,
+      salt, identityFactoryAddr, baseIdentityAddr, bytecode,
+      signer: { address: signerAddr }
+  }
+}
+
+async function createFromEOA (addr) {
+  const privileges = [[getAddress(addr), hexZeroPad('0x01', 32)]]
+  const { salt, baseIdentityAddr, identityFactoryAddr } = accountPresets
+  const bytecode = getProxyDeployBytecode(baseIdentityAddr, privileges, { privSlot: 0 })
+  const identityAddr = getAddress('0x' + generateAddress2(identityFactoryAddr, salt, bytecode).toString('hex'))
+
+  if (relayerURL) {
+      const createResp = await fetchPost(`${relayerURL}/identity/${identityAddr}`, {
+          salt, identityFactoryAddr, baseIdentityAddr,
+          privileges
+      })
+      if (!createResp.success && !(createResp.message && createResp.message.includes('already exists'))) throw createResp
+  }
+
+  return {
+      id: identityAddr,
+      salt, identityFactoryAddr, baseIdentityAddr, bytecode,
+      signer: { address: getAddress(addr) }
+  }
+}
+
   const selectedAccount = accounts.find(x => x.id === selectedAcc)
-  
+
   const privList = Object.entries(privileges)
     .map(([addr, privValue]) => {
       if (!privValue) return null
@@ -135,7 +203,7 @@ const Security = ({
       </div>
       <div className="panel">
         <div className="panel-title">Add new signer</div>
-        <AddAuthSigner onAddBtnClicked={onAddBtnClickedHandler} />
+        <AddAuthSigner onAddBtnClicked={onAddBtnClickedHandler} selectedNetwork={selectedNetwork} />
       </div>
     </section>
   )
