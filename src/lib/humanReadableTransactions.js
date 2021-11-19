9a2f66ee1a8f5ebf78ac1180dd68054f550c9369
Ivo Georgiev <ivo@strem.io>
2021-11-19 13:41:29 +0200

    humanReadableTransactions: use humanizeTx

--- src/lib/humanReadableTransactions.js
+++ src/lib/humanReadableTransactions.js
@@ -4,48 +4,21 @@ import { verifiedContracts, tokens } from '../consts/verifiedContracts'
 import networks from '../consts/networks'
 import ERC20ABI from 'adex-protocol-eth/abi/ERC20'
 
-const ERC20 = new Interface(ERC20ABI)
-const TRANSFER_SIGHASH = ERC20.getSighash(ERC20.getFunction('transfer').format())
+const contractsManager = require('./humanizeTx/HumanContractsManager')
+contractsManager.init()
 
 // @TODO custom parsing for univ2 contracts, exact output, etc.
 export function getTransactionSummary(txn, networkId, accountAddr) {
     const [, value, data] = txn
     const to = getAddress(txn[0])
-    let callSummary, sendSummary
+
     const network = networks.find(x => x.id === networkId || x.chainId === networkId)
     if (!network) return 'Unknown network (unable to parse)'
 
-    const contractKey = network.id + ':' + getAddress(to)
-    const contractInfo = verifiedContracts[contractKey]
-    const tokenInfo = tokens[to]
+    const { summaries, interaction } = contractsManager.getSummary(network, {to, value, data, from: accountAddr})
 
-    const nativeAsset = network ? network.nativeAssetSymbol : 'unknown native token'
-    if (parseInt(value) > 0) sendSummary = `send ${(parseInt(value)/1e18).toFixed(4)} ${nativeAsset} to ${contractInfo ? contractInfo.name : to}`
-    if (data !== '0x') {
-        if (data.startsWith(TRANSFER_SIGHASH)) {
-            const [to, amount] = ERC20.decodeFunctionData('transfer', data)
-            if (tokenInfo) {
-                callSummary = `send ${(amount/Math.pow(10, tokenInfo[1])).toFixed(4)} ${tokenInfo[0]} to ${to}`
-            } else {
-                // @TODO: maybe we can call the contract and get detailed data
-                callSummary = `send ${amount/1e18} unknown token to ${to}`
-            }
-        } else if (contractInfo) {
-            const iface = new Interface(contractInfo.abi)
-            const parsed = iface.parseTransaction({ data, value })
-            // @TODO: some elegant way to try-catch potential issues here
-            if (parsed.name === 'swapExactETHForTokens') {
-                const tokenAddr = parsed.args.path[parsed.args.path.length - 1]
-                const output = tokenInfo ? `${formatUnits(parsed.args.amountOutMin, tokenInfo[1])} ${tokenInfo[0]}` : `${parsed.args.amountOutMin} of token ${tokenAddr}`
-                const contractNote = ` on ${contractInfo.name}`
-                const recipientNote = parsed.args.to.toLowerCase() === accountAddr.toLowerCase() ? `` : ` and send it to ${parsed.args.to}`
-                return `Swap ${formatUnits(value, 18)} ${nativeAsset} for at least ${output}${contractNote}${recipientNote}`
-            } else {
-                callSummary = `Interaction with ${contractInfo.name}: ${parsed.name}`
-            }
-        } else callSummary = `unknown call to ${contractInfo ? contractInfo.name : (tokenInfo ? tokenInfo[0] : to)}`
-    }
-    return [callSummary, sendSummary].filter(x => x).join(', ')
+    // ${summaries.action ? summaries.action + ': ' : ''} ${interaction.name}: 
+    return `${summaries.actions.map(x => x.plain).join(', ')}`
 }
 
 export function getContractName(txn, networkId) {
