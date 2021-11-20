46c201fd932ef429acdb56199a746be7edb844a0
Ivo Georgiev <ivo@strem.io>
2021-11-21 01:27:09 +0200

    humanizing: ERC20 revoking

--- src/lib/humanizers/ERC20.js
+++ src/lib/humanizers/ERC20.js
@@ -7,10 +7,15 @@ const iface = new Interface(abis.ERC20)
 export default {
   [iface.getSighash('approve')]: (txn, network) => {
     const [ approvedAddress, amount ] = iface.parseTransaction(txn).args
+    if (amount.eq(0)) return [`Revoke approval for ${getContractName(approvedAddress, network)} to spend ${getContractName(txn.to, network)}`]
     return [`Approve ${getContractName(approvedAddress, network)} to spend ${token(txn.to, amount)}`]
   },
   [iface.getSighash('transfer')]: (txn, network) => {
     const [ to, amount ] = iface.parseTransaction(txn).args
     return [`Send ${token(txn.to, amount)} to ${getContractName(to, network)}`]
+  },
+  [iface.getSighash('transferFrom')]: (txn, network) => {
+    const [ from, to, amount ] = iface.parseTransaction(txn).args
+    return [`Send ${token(txn.to, amount)} from ${getContractName(from, network)} to ${getContractName(to, network)}`]
   },
 }
