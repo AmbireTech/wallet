7c41e08170d9a7e5957a9041413805515ae8ce35
Ivo Georgiev <ivo@strem.io>
2021-11-21 23:18:00 +0200

    humanizing bugfixes

--- src/lib/humanizers/AaveLendingPoolV2.js
+++ src/lib/humanizers/AaveLendingPoolV2.js
@@ -20,5 +20,9 @@ const AaveMapping = {
     const [ asset, amount, onBehalf ] = iface.parseTransaction(txn).args
     return [`Repay ${token(asset, amount)} to Aave lending pool$${onBehalfText(onBehalf, txn.from)}`]
   },
+  [iface.getSighash('borrow')]: (txn, network) => {
+    const [ asset, amount ] = iface.parseTransaction(txn).args
+    return [`Borrow ${token(asset, amount)} from Aave lending pool$`]
+  },
 }
 export default AaveMapping
