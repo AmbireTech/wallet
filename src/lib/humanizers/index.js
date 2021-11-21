1665bcc3eaa6d7be9814960913ff465ed7f32d20
Ivo Georgiev <ivo@strem.io>
2021-11-21 18:43:29 +0200

    humanizers: add ERC721

--- src/lib/humanizers/index.js
+++ src/lib/humanizers/index.js
@@ -1,11 +1,13 @@
 import UniV2Router from './UniV2Router'
 import ERC20 from './ERC20'
 import AaveLendingPoolV2 from './AaveLendingPoolV2'
+import ERC721 from './ERC721'
 import WETH from './WETH'
 
 export default {
-	...WETH,
 	...UniV2Router,
 	...AaveLendingPoolV2,
 	...ERC20,
+	...ERC721,
+	...WETH,
 }
