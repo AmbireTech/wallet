03b5606b268154d75ba9d6b161e292030a90e933
Ivo Georgiev <ivo@strem.io>
2021-11-21 14:30:14 +0200

    humanizer: on behalf of

--- src/lib/humanizers/AaveLendingPoolV2.js
+++ src/lib/humanizers/AaveLendingPoolV2.js
@@ -3,14 +3,15 @@ import { Interface } from 'ethers/lib/utils'
 import { token } from '../humanReadableTransactions'
 
 const iface = new Interface(abis.AaveLendingPoolV2)
+const addrEq = (a, b) => a.toLowerCase() === b.toLowerCase()
 
 export default {
   [iface.getSighash('deposit')]: (txn, network) => {
-    const [ asset, amount ] = iface.parseTransaction(txn).args
-    return [`Deposit ${token(asset, amount)} to Aave lending pool`]
+    const [ asset, amount, onBehalf ] = iface.parseTransaction(txn).args
+    return [`Deposit ${token(asset, amount)} to Aave lending pool${!addrEq(onBehalf, txn.from) ? ' on behalf of '+onBehalf : ''}`]
   },
   [iface.getSighash('withdraw')]: (txn, network) => {
-    const [ asset, amount ] = iface.parseTransaction(txn).args
-    return [`Withdraw ${token(asset, amount)} from Aave lending pool`]
+    const [ asset, amount, onBehalf ] = iface.parseTransaction(txn).args
+    return [`Withdraw ${token(asset, amount)} from Aave lending pool${!addrEq(onBehalf, txn.from) ? ' on behalf of '+onBehalf : ''}`]
   },
 }
