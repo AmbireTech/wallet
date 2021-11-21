687dc34eb35ef3230a2ced149402450319947610
Tim <tymmesyde@gmail.com>
2021-11-22 00:42:43 +0100

    fix(NumberInput): make it only return int or float

--- src/components/common/NumberInput/NumberInput.js
+++ src/components/common/NumberInput/NumberInput.js
@@ -1,6 +1,13 @@
 import './NumberInput.scss'
 
-const NumberInput = ({ disabled, min, max, label, value, button, onButtonClick, onInput }) => {
+const NumberInput = ({ disabled, min, max, label, value, button, onButtonClick, onInput, onChange }) => {
+    const onInputValue = ({ target }) => {
+        if (!onInput) return
+        if (!target.value.length) return onInput('')
+        const isIntOrFloat = /^[0-9]+\.{0,1}[0-9]*$/g.test(target.value)
+        isIntOrFloat && onInput(target.value)
+    }
+
     return (
         <div className={`number-input ${disabled ? 'disabled' : ''}`}>
             {
@@ -10,7 +17,12 @@ const NumberInput = ({ disabled, min, max, label, value, button, onButtonClick, 
                     null
             }
             <div className="input">
-                <input type="number" disabled={disabled} value={value} min={min} max={max} onInput={({ target }) => onInput(target.value)}/>
+                <input
+                    type="text"
+                    disabled={disabled}
+                    value={value}
+                    onInput={onInputValue}
+                />
                 {
                     button ?
                         <div className="button" onClick={onButtonClick}>
