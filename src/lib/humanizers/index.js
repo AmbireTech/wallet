413025cc85cec65e0dadfaa50fb06deba5d3ce30
Ivo Georgiev <ivo@strem.io>
2021-11-21 20:16:28 +0200

    humanizers: parse Ambire transactions

--- src/lib/humanizers/index.js
+++ src/lib/humanizers/index.js
@@ -3,6 +3,7 @@ import ERC20 from './ERC20'
 import AaveLendingPoolV2 from './AaveLendingPoolV2'
 import ERC721 from './ERC721'
 import WETH from './WETH'
+import AmbireIdentity from './AmbireIdentity'
 
 const all = {
 	...UniRouters,
@@ -10,5 +11,6 @@ const all = {
 	...ERC20,
 	...ERC721,
 	...WETH,
+	...AmbireIdentity,
 }
 export default all
