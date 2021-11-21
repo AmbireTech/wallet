ce9440189d8a2581f7fa7461fb76f12f8bc061ba
Ivo Georgiev <ivo@strem.io>
2021-11-21 14:39:09 +0200

    humanizer: aave repayment, handle max amounts

--- src/lib/humanizers/AaveLendingPoolV2.js
+++ src/lib/humanizers/AaveLendingPoolV2.js
@@ -14,4 +14,8 @@ export default {
     const [ asset, amount, onBehalf ] = iface.parseTransaction(txn).args
     return [`Withdraw ${token(asset, amount)} from Aave lending pool${!addrEq(onBehalf, txn.from) ? ' on behalf of '+onBehalf : ''}`]
   },
+  [iface.getSighash('repay')]: (txn, network) => {
+    const [ asset, amount, onBehalf ] = iface.parseTransaction(txn).args
+    return [`Repay ${token(asset, amount)} to Aave lending pool${!addrEq(onBehalf, txn.from) ? ' on behalf of '+onBehalf : ''}`]
+  },
 }
