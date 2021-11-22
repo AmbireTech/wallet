3b8884e0980ebc81db7ecd6f13cce7f522851a58
Tim <tymmesyde@gmail.com>
2021-11-22 16:53:30 +0100

    refactor: reduce redundancy

--- src/components/common/UnknownAddress/UnknownAddress.js
+++ src/components/common/UnknownAddress/UnknownAddress.js
@@ -2,27 +2,31 @@ import './UnknownAddress.scss'
 
 import { MdOutlineAdd } from 'react-icons/md'
 import { Checkbox } from '..'
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
-const UnknownAddress = ({ onChange, onAddNewAddress }) => {
+const UnknownAddress = ({ address, onChange, onAddNewAddress, isValidAddress, isKnownAddress }) => {
     const [confirmed, setConfirmed] = useState(false)
+    const shouldShow = useMemo(() => isValidAddress(address) && !isKnownAddress(address), [address, isValidAddress, isKnownAddress])
 
     useEffect(() => {
         if (onChange) onChange(confirmed)
     }, [confirmed, onChange])
 
     return (
-        <div id="unknown-address-warning">
-            <Checkbox
-                label="Confirm sending to a previously unknown address"
-                checked={confirmed}
-                onChange={({ target }) => setConfirmed(target.checked)}
-            />
-            <div className="button" onClick={onAddNewAddress}>
-                <MdOutlineAdd/>
-                Add it to the address book
+        shouldShow ?
+            <div id="unknown-address-warning">
+                <Checkbox
+                    label="Confirm sending to a previously unknown address"
+                    checked={confirmed}
+                    onChange={({ target }) => setConfirmed(target.checked)}
+                />
+                <div className="button" onClick={onAddNewAddress}>
+                    <MdOutlineAdd/>
+                    Add it to the address book
+                </div>
             </div>
-        </div>
+            :
+            null
     )
 }
 
