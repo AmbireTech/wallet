18e99b4412642235c52832881bd15fe60933d025
Tim <tymmesyde@gmail.com>
2021-11-22 16:02:10 +0100

    refactor(Modals): add new InputModal component

--- src/components/Modals/index.js
+++ src/components/Modals/index.js
@@ -1,5 +1,7 @@
 import PermissionsModal from './PermissionsModal/PermissionsModal'
+import InputModal from './InputModal/InputModal'
 
 export {
-    PermissionsModal
+    PermissionsModal,
+    InputModal
 }
