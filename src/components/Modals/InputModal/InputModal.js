fbec9a5e3901b1aec09b77d13b2f3065f94d0430
Tim <tymmesyde@gmail.com>
2021-11-22 16:11:50 +0100

    refactor(InputModal): implement inputs validation

--- src/components/Modals/InputModal/InputModal.js
+++ src/components/Modals/InputModal/InputModal.js
@@ -1,15 +1,22 @@
 import './InputModal.scss'
 
+import { createRef, useState } from 'react'
 import { MdCheck, MdClose } from 'react-icons/md'
 import { useModals } from '../../../hooks'
 import { Modal, TextInput, Button } from "../../common"
-import { createRef } from 'react'
 
 const InputModal = ({ title, inputs, onClose }) => {
     const { hideModal } = useModals()
+    const [isDisabled, setDisabled] = useState(true)
 
     const inputsFields = inputs.map(input => ({ ...input, ref: createRef() }))
-    const buttonDisabled = false
+
+    const onInput = () => {
+        const isFormValid = inputsFields
+            .map(({ ref, validate }) => ref.current.value && (validate ? validate(ref.current.value) : true))
+            .every(v => v === true)
+        setDisabled(!isFormValid)
+    }
 
     const onConfirm = () => {
         const values = inputsFields.map(({ ref }) => ref.current.value)
@@ -21,12 +28,12 @@ const InputModal = ({ title, inputs, onClose }) => {
         <Modal id="input-modal" title={title}>
             {
                 inputsFields.map(({ id, label, ref }) => (
-                    <TextInput key={id || label} label={label} onInput={() => {}} ref={ref}/>
+                    <TextInput key={id || label} label={label} onInput={onInput} ref={ref}/>
                 ))
             }
             <div className="buttons">
                 <Button clear small icon={<MdClose/>} onClick={hideModal}>Cancel</Button>
-                <Button small icon={<MdCheck/>} disabled={buttonDisabled} onClick={onConfirm}>Confirm</Button>
+                <Button small icon={<MdCheck/>} disabled={isDisabled} onClick={onConfirm}>Confirm</Button>
             </div>
         </Modal>
     )
