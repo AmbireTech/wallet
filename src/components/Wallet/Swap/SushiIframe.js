d2f5f40050cd4376cfff15f18fbe6434d3d41883
IvoPaunov <ivo.paunov@gmail.com>
2021-11-23 12:50:44 +0200

    use gnosis iframe for swap

--- src/components/Wallet/Swap/SushiIframe.js
+++ src/components/Wallet/Swap/SushiIframe.js
@@ -1,9 +1,5 @@
-import './SushiFrame.scss';
-
-import React, { useEffect, useRef, useState } from 'react'
-
 import { SUSHI_SWAP_FRAME } from '../../../config'
-import { InfiniteProgressBar } from '../../common'
+import GnosisSafeAppIframe from '../../Plugins/GnosisSafeApps/GnosisSafeAppIframe'
 
 const ambireSushi = {
     name: "Ambire swap",
@@ -13,46 +9,14 @@ const ambireSushi = {
 }
 
 export default function SushiSwap({ network, selectedAcc, gnosisConnect, gnosisDisconnect }) {
-    const { chainId } = network || []
-    const [loading, setLoading] = useState(true)
-    const [hash, setHash] = useState('')
-    const iframeRef = useRef(null);
-
-
-    useEffect(() => {
-        const newHash = ambireSushi.url + chainId + selectedAcc
-        setHash(newHash)
-    }, [chainId, selectedAcc])
-
-    useEffect(() => {
-        setLoading(true)
-    }, [hash])
-
-    useEffect(() => {
-        gnosisConnect({
-            selectedAcc,
-            iframeRef,
-            app: ambireSushi
-        });
-
-        return () => {
-            gnosisDisconnect()
-        }
-
-    }, [network, selectedAcc, iframeRef, gnosisConnect, gnosisDisconnect])
-
     return (
-        <div className='iframe-container'>
-            {loading && <InfiniteProgressBar />}
-            <iframe
-                id='swap-frame'
-                key={hash}
-                ref={iframeRef}
-                title='sushi-swap'
-                src={SUSHI_SWAP_FRAME}
-
-                onLoad={() => setLoading(false)}
-            />
-        </div>
+        <GnosisSafeAppIframe
+            network={network}
+            selectedAcc={selectedAcc}
+            gnosisConnect={gnosisConnect}
+            gnosisDisconnect={gnosisDisconnect}
+            selectedApp={ambireSushi}
+            title={'Ambire swap'}
+        />
     )
 }
