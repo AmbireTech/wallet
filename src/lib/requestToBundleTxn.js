815cf5ba05da92ee5af4e26ab0ebc5aabf9f3513
Ivo Georgiev <ivo@strem.io>
2021-11-24 00:28:42 +0200

    requestToBundleTxn: fix redundant log

--- src/lib/requestToBundleTxn.js
+++ src/lib/requestToBundleTxn.js
@@ -6,7 +6,6 @@ const IdentityFactory = new Interface(require('adex-protocol-eth/abi/IdentityFac
 export function toBundleTxn({ to, value, data }, from) {
   if (to === '0x' || !to) {
     const salt = `0x624200000000000000000000${from.slice(2)}`
-    console.log(salt)
     return [accountPresets.identityFactoryAddr, value, IdentityFactory.encodeFunctionData('deploy', [data, salt])]
   }
   return [to, value || '0x0', data || '0x']
