0d041d07f53cc89d41907ecd7148363e3f3fb9e6
Tim <tymmesyde@gmail.com>
2021-11-24 17:20:00 +0100

    refactor(AddressList): show basic placeholder in case there is no addresses to show

--- src/components/common/AddressBook/AddressList/AddressList.js
+++ src/components/common/AddressBook/AddressList/AddressList.js
@@ -3,28 +3,33 @@ import './AddressList.scss'
 import { MdOutlineDelete } from 'react-icons/md'
 
 const AddressList = ({ noAccounts, addresses, onSelectAddress, removeAddress }) => {
+    const items = addresses.filter(({ isAccount }) => !(isAccount && noAccounts))
+
     return (
         <div className="address-list">
             {
-                addresses.filter(({ isAccount }) => !(isAccount && noAccounts)).map(({ isAccount, icon, name, address }) => (
-                    <div className="item" key={address + name}>
-                        <div className="inner" onClick={() => onSelectAddress && onSelectAddress(address)}>
-                            <div className="icon" style={{ backgroundImage: `url(${icon})`}}></div>
-                            <div className="details">
-                                <label>{ name }</label>
-                                <div className="address">{ address }</div>
-                            </div>
-                        </div>
-                        {
-                            !isAccount ? 
-                                <div className="button" onClick={() => removeAddress(name, address)}>
-                                    <MdOutlineDelete/>
+                !items.length ?
+                    <div>Your Address Book is empty</div>
+                    :
+                    items.map(({ isAccount, icon, name, address }) => (
+                        <div className="item" key={address + name}>
+                            <div className="inner" onClick={() => onSelectAddress && onSelectAddress(address)}>
+                                <div className="icon" style={{ backgroundImage: `url(${icon})`}}></div>
+                                <div className="details">
+                                    <label>{ name }</label>
+                                    <div className="address">{ address }</div>
                                 </div>
-                                :
-                                null
-                        }
-                    </div>
-                ))
+                            </div>
+                            {
+                                !isAccount ? 
+                                    <div className="button" onClick={() => removeAddress(name, address)}>
+                                        <MdOutlineDelete/>
+                                    </div>
+                                    :
+                                    null
+                            }
+                        </div>
+                    ))
             }
         </div>
     )
