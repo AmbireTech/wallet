1889268bc1f13ed6e08de3bb518a37abebdd556c
user <default@localhost.dev>
2021-11-24 14:22:35 +0100

    ready to be tested webHID

--- src/lib/ledgerWebHID.js
+++ src/lib/ledgerWebHID.js
@@ -1,149 +1,231 @@
 import TransportWebHID from "@ledgerhq/hw-transport-webhid"
 import AppEth from "@ledgerhq/hw-app-eth"
-import {ethers} from "ethers"
-import EthTx from 'ethereumjs-tx'
-const ethUtil = require("ethereumjs-util");
+import {serialize} from '@ethersproject/transactions'
 
+const EIP_155_CONSTANT = 35
+
+const ethUtil = require("ethereumjs-util")
+const HDNode = require("hdkey")
 
 let connectedDevices = null
 
-const HDPath = "44'/60'/0'/0/0"
+const PARENT_HD_PATH = "44'/60'/0'/0"
 
-export async function ledgerGetAddresses(page, hdPath, t) {
-  console.log(`connectLedger`, page, hdPath)
-  try {
-    let transport
-    connectedDevices = await TransportWebHID.list()
-    console.log(connectedDevices)
-    if (connectedDevices.length && connectedDevices[0].opened) {
-      transport = new TransportWebHID(connectedDevices[0])
-      //transport = await TransportWebHID.open(connectedDevices[0]);
-    } else {
-      transport = await TransportWebHID.request()
+async function getTransport() {
+  connectedDevices = await TransportWebHID.list()
+  console.log(connectedDevices)
+  if (connectedDevices.length && connectedDevices[0].opened) {
+    return new TransportWebHID(connectedDevices[0])
+  } else {
+    try {
+      return await TransportWebHID.request()
+    } catch (e) {
+      throw new Error("Could not request WebHID ledger")
     }
-    console.log(transport)
-    debugger
-    transport.setDebugMode(true)
-    const appEth = new AppEth(transport)
-    const address = await appEth.getAddress().then(o => o.address)
-    /*console.log(addrData);
-    const addresses = [];
-    for(let i = 0; i < 10; i++){
-        addresses.push()
-    }*/
-    if (transport) {
-      connectedDevices = transport.close()
-    }
-    return [address]
-  } catch (error) {
-    throw error
   }
+}
+
+
+export async function ledgerGetAddresses(page, hdPath) {
+  console.log(`connectLedger`, page, hdPath)
+  const returnData = {
+    error: null,
+    addresses: []
+  }
+
+  const transport = await getTransport().catch(err => {
+    returnData.error = err
+  })
+  if (!transport) return returnData
+
+  const accountsData = await getAccounts(transport)
+
+  transport.close()
+
+  if (accountsData.error) {
+    returnData.error = accountsData.error
+  } else {
+    returnData.addresses = accountsData.accounts.map(a => a.address)
+  }
+
+  return returnData
+}
+
+async function getAccounts(transport) {
+  const parentKeyDerivationPath = `m/${PARENT_HD_PATH}`
+  const returnData = {
+    error: null,
+    accounts: []
+  }
+  let ledgerResponse
+  try {
+    ledgerResponse = await getAddressInternal(transport, parentKeyDerivationPath).then(o => o).catch(err => {
+      if (err.statusCode === 25871 || err.statusCode === 27404) {
+        returnData.error = 'Please make sure your ledger is unlocked and running the Ethereum app. ' + err.message
+      } else {
+        returnData.error = 'Could not get address from ledger : ' + err
+      }
+    })
+  } catch (e) {
+    console.log(e)
+  }
+
+  if (!ledgerResponse) {
+    return returnData
+  }
+
+  const hdKey = new HDNode()
+  hdKey.publicKey = Buffer.from(ledgerResponse.publicKey, 'hex')
+  hdKey.chainCode = Buffer.from(ledgerResponse.chainCode, 'hex')
+  const mainAddress = addressOfHDKey(hdKey)
+
+  const initialDerivedKeyInfo = {
+    hdKey,
+    address: mainAddress,
+    derivationPath: parentKeyDerivationPath,
+    baseDerivationPath: PARENT_HD_PATH,
+  }
+
+  returnData.accounts = calculateDerivedHDKeyInfos(initialDerivedKeyInfo, 10)
+  return returnData
+}
+
+
+async function getAddressInternal(transport, parentKeyDerivationPath) {
+  let timeoutHandle
+  const appEth = new AppEth(transport)
+
+  const ledgerTimeout = new Promise((resolve, reject) => {
+    timeoutHandle = setTimeout(() => {
+      return reject(new Error('Device took too long to respond...'))
+    }, 10000)
+  })
+
+  return Promise.race([
+    appEth.getAddress(parentKeyDerivationPath, false, true),
+    ledgerTimeout
+  ]).then((res) => {
+    clearTimeout(timeoutHandle)
+    return res
+  })
 }
 
 export async function ledgerSignTransaction(txn, chainId) {
   console.log(`signTransaction`, txn)
-  try {
-    let transport
-    connectedDevices = await TransportWebHID.list()
-    if (connectedDevices.length && connectedDevices[0].opened) {
-      transport = new TransportWebHID(connectedDevices[0])
-      //transport = await TransportWebHID.open(connectedDevices[0]);
-    } else {
-      transport = await TransportWebHID.request()
-    }
 
-    const unsignedTxObj = {
-      ...txn
-    }
+  const transport = await getTransport().catch(err => {
+    throw err
+  })
 
-    delete unsignedTxObj.from
-    unsignedTxObj.gasLimit = unsignedTxObj.gas
-    delete unsignedTxObj.gas
-    unsignedTxObj.chainId = chainId;
-    unsignedTxObj.r = "0x0";
-    unsignedTxObj.s = "0x0";
-    unsignedTxObj.v = "0x" + chainId.toString(16);
+  const fromAddr = txn.from
 
-    console.log(unsignedTxObj);
-
-    const ethTx = new EthTx(unsignedTxObj)
-    const serialized = ethTx.serialize().toString('hex')
-
-    /*const rsTx = await ethers.utils.resolveProperties(unsignedTxObj)
-    const serialized = ethers.utils.serializeTransaction(rsTx, {
-      r: "0x0",
-      s: "0x0",
-      v: "0x" + chainId
-    }).substr(2)*/
-
-    console.log(serialized)
-    debugger;
-    const appEth = new AppEth(transport)
-    const address = await appEth.getAddress(HDPath).then(o => o.address)
-
-    //const serialized = ethTx.serialize().toString('hex')
-    if (address.toLowerCase() === txn.from.toLowerCase()) {
-      const rsv = await appEth.signTransaction(HDPath, serialized)
-      debugger;
-
-      let v = parseInt(rsv.v, 16);
-      console.log(rsv);
-      console.log(v);
-      const eip55Constant = 35
-
-      const b = ethUtil.toBuffer("0x" + rsv.v);
-      console.log(b.toString('hex'));
-
-      const signedChainId = Math.floor((b[0] - eip55Constant) / 2)
-      if (signedChainId !== chainId) {
-        throw new Error("Too old ledger firmware?")
-      }
-
-
-      console.log(rsv)
-      const signedTxObj = {
-        ...txn,
-        r: "0x" + rsv.r,
-        s: "0x" + rsv.s,
-        v: rsv.v,
-      }
-      console.log(signedTxObj)
-      const signedTx = new EthTx(signedTxObj)
-      return "0x" + signedTx.serialize().toString('hex')
-    }
-    if (transport) {
-      connectedDevices = transport.close()
-    }
-  } catch (error) {
-    throw error
+  const unsignedTxObj = {
+    ...txn,
+    gasLimit: txn.gasLimit || txn.gas,
+    chainId: chainId
   }
+  delete unsignedTxObj.from
+  delete unsignedTxObj.gas
+
+  //console.log("Unsigned TX")
+  //console.log(unsignedTxObj)
+
+  let serializedUnsigned = serialize(unsignedTxObj)
+  const accountsData = await getAccounts(transport)
+  if (accountsData.error) {
+    throw new Error(accountsData.error)
+  }
+
+  //Managing only 1 addr for now
+  const address = accountsData.accounts[0].address
+
+  let serializedSigned
+  if (address.toLowerCase() === fromAddr.toLowerCase()) {
+    let rsvResponse
+    try {
+      rsvResponse = await new AppEth(transport).signTransaction(accountsData.accounts[0].derivationPath, serializedUnsigned.substr(2))
+    } catch (e) {
+      throw new Error("Could not sign transaction " + e)
+    }
+
+    const intV = parseInt(rsvResponse.v, 16)
+    const signedChainId = Math.floor((intV - EIP_155_CONSTANT) / 2)
+
+    if (signedChainId !== chainId) {
+      console.log(rsvResponse)
+      throw new Error("Invalid returned V 0x" + rsvResponse.v)
+    }
+
+    console.log("WTF")
+    console.log(rsvResponse)
+
+    debugger
+    delete unsignedTxObj.v
+    serializedSigned = serialize(unsignedTxObj, {
+      r: "0x" + rsvResponse.r,
+      s: "0x" + rsvResponse.s,
+      v: intV
+    })
+  }
+
+  transport.close()
+
+  return serializedSigned
 }
 
 export async function ledgerSignMessage(hash, signerAddress) {
   console.log(`signMessage`, hash)
-  try {
-    let transport
-    connectedDevices = await TransportWebHID.list()
-    console.log(connectedDevices)
-    if (connectedDevices.length && connectedDevices[0].opened) {
-      transport = new TransportWebHID(connectedDevices[0])
-      //transport = await TransportWebHID.open(connectedDevices[0]);
-    } else {
-      transport = await TransportWebHID.request()
-    }
-    console.log(transport)
-    transport.setDebugMode(true)
-    const appEth = new AppEth(transport)
-    const address = await appEth.getAddress(HDPath).then(o => o.address)
 
-    if (address.toLowerCase() === signerAddress.toLowerCase()) {
-      const rsv = await appEth.signPersonalMessage(HDPath, hash.substr(2))
-      return "0x" + rsv.r + rsv.s + rsv.v.toString(16)
-    }
-    if (transport) {
-      connectedDevices = transport.close()
-    }
-  } catch (error) {
-    throw error
+  const transport = await getTransport().catch(err => {
+    throw err
+  })
+
+  const accountsData = await getAccounts(transport)
+  if (accountsData.error) {
+    throw new Error(accountsData.error)
   }
+
+  //TODO for multiple accs?
+  const account = accountsData.accounts[0]
+
+  let signedMsg
+  if (account.address.toLowerCase() === signerAddress.toLowerCase()) {
+    try {
+      const rsvReply = await new AppEth(transport).signPersonalMessage(account.derivationPath, hash.substr(2))
+      signedMsg = "0x" + rsvReply.r + rsvReply.s + rsvReply.v.toString(16)
+    } catch (e) {
+      throw new Error("Signature denied " + e.message)
+    }
+  }
+  transport.close()
+  return signedMsg
+}
+
+function calculateDerivedHDKeyInfos(initialDerivedKeyInfo, count) {
+  const derivedKeys = []
+  for (let i = 0; i < count; i++) {
+
+    const fullDerivationPath = `m/${initialDerivedKeyInfo.baseDerivationPath}/${i}`
+    const path = `m/${i}`
+    const hdKey = initialDerivedKeyInfo.hdKey.derive(path)
+    const address = addressOfHDKey(hdKey)
+    const derivedKey = {
+      address,
+      hdKey,
+      baseDerivationPath: initialDerivedKeyInfo.baseDerivationPathh,
+      derivationPath: fullDerivationPath,
+    }
+
+    derivedKeys.push(derivedKey)
+  }
+  return derivedKeys
+}
+
+export function addressOfHDKey(hdKey) {
+  const shouldSanitizePublicKey = true
+  const derivedPublicKey = hdKey.publicKey
+  const ethereumAddressUnprefixed = ethUtil
+    .publicToAddress(derivedPublicKey, shouldSanitizePublicKey)
+    .toString('hex')
+  return ethUtil.addHexPrefix(ethereumAddressUnprefixed).toLowerCase()
 }
