3928980724eec09985c42fd68ec7059da78604ca
Tim <tymmesyde@gmail.com>
2021-11-25 00:12:41 +0100

    refactor(TopBar): create a new component for dApps dropdown

--- src/components/Wallet/TopBar/TopBar.js
+++ src/components/Wallet/TopBar/TopBar.js
@@ -1,11 +1,9 @@
 import "./TopBar.scss";
 
-import React, { useState, useCallback } from "react";
-import { FiHelpCircle } from "react-icons/fi";
-import { DropDown, Select, ToolTip } from "../../common";
+import React from "react";
+import { Select } from "../../common";
 import Accounts from "./Accounts/Accounts";
-import { checkClipboardPermission } from "../../../helpers/permissions";
-import { MdOutlineWarning } from "react-icons/md";
+import DApps from "./DApps/DApps";
 
 const TopBar = ({
   connections,
@@ -19,77 +17,17 @@ const TopBar = ({
   setNetwork,
   allNetworks,
 }) => {
-  const [isClipboardGranted, setClipboardGranted] = useState(false);
-
-  const checkPermission = async () => {
-    const status = await checkClipboardPermission()
-    setClipboardGranted(status)
-    return status
-  }
-
-  const readClipboard = useCallback(async () => {
-    if (isClipboardGranted) {
-      const content = await navigator.clipboard.readText();
-      if (content.startsWith('wc:')) connect({ uri: content });
-    } else {
-      const uri = prompt("Enter WalletConnect URI");
-      if (uri) connect({ uri });
-    }
-  }, [connect, isClipboardGranted]);
-
   const networksItems = allNetworks.map(({ id, name, icon }) => ({
     label: name,
     value: id,
     icon
   }))
 
-  const isLegacyWC = ({ bridge }) => /https:\/\/bridge.walletconnect.org/g.test(bridge)
-
   return (
     <div id="topbar">
       <div className="container">
-        <DropDown title="dApps" badge={connections.length} onOpen={() => checkPermission()}>
-          <div id="connect-dapp">
-            <div className="heading">
-              <button disabled={isClipboardGranted} onClick={readClipboard}>
-                Connect dApp
-              </button>
-              <FiHelpCircle size={30} />
-            </div>
-            {isClipboardGranted ? (
-              <label>
-                Automatic connection enabled, just copy a WalletConnect URL and
-                come back to this tab.
-              </label>
-            ) : null}
-          </div>
-          {connections.map(({ session, uri }) => (
-            <div className="item dapps-item" key={session.peerId}>
-              <div className="icon">
-                <img
-                  src={session.peerMeta.icons.filter(x => !x.endsWith('favicon.ico'))[0]}
-                  alt={session.peerMeta.name}
-                ></img>
-              </div>
-              <a href={session.peerMeta.url} target="_blank" rel="noreferrer">
-                <div className="name">{session.peerMeta.name}</div>
-                { 
-                  isLegacyWC(session) ? 
-                    <ToolTip className="session-warning" label="dApp uses legacy WalletConnect bridge which is unreliable and often doesn't work. Please tell the dApp to update to the latest WalletConnect version.">
-                      <MdOutlineWarning/>
-                    </ToolTip>
-                    :
-                    null
-                }
-              </a>
-              <div className="separator"></div>
-              <button onClick={() => disconnect(uri)}>Disconnect</button>
-            </div>
-          ))}
-        </DropDown>
-
+        <DApps connections={connections} connect={connect} disconnect={disconnect}/>
         <Accounts accounts={accounts} selectedAddress={selectedAcc} onSelectAcc={onSelectAcc} onRemoveAccount={onRemoveAccount}/>
-
         <Select defaultValue={network.id} items={networksItems} onChange={value => setNetwork(value)}/>
       </div>
     </div>
