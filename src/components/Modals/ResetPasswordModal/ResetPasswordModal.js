c09a55b1b8c4e6fe2d845c751d2276eb3dc5fa82
Tim <tymmesyde@gmail.com>
2021-11-26 22:58:07 +0100

    refactor(ResetPasswordModal): validate forms

--- src/components/Modals/ResetPasswordModal/ResetPasswordModal.js
+++ src/components/Modals/ResetPasswordModal/ResetPasswordModal.js
@@ -1,13 +1,32 @@
 import './ResetPasswordModal.scss'
 
-import { useState } from 'react'
+import { useState, useMemo, createRef, useEffect, useCallback } from 'react'
 import { Modal, Radios, TextInput, Checkbox, Button } from '../../common'
 import { MdOutlineCheck, MdOutlineClose } from 'react-icons/md'
 
 const ResetPassword = ({ selectedNetwork }) => {
     const [type, setType] = useState(null)
+    const [oldPassword, setOldPassword] = useState('')
+    const [newPassword, setNewPassword] = useState('')
+    const [newPasswordConfirm, setNewPasswordConfirm] = useState('')
+    const [disabled, setDisabled] = useState(true)
 
-    const radios = [
+    const checkboxes = useMemo(() => [
+        {
+            label: `I understand I am only changing the password on the ${selectedNetwork.name} network`,
+            ref: createRef()
+        },
+        {
+            label: `I confirm the fee of <...> to apply this change on ${selectedNetwork.name}`,
+            ref: createRef()
+        },
+        {
+            label: 'I understand I need to wait for 3 days for the change to be confirmed',
+            ref: createRef()
+        }
+    ], [selectedNetwork.name])
+
+    const radios = useMemo(() => [
         {
             label: 'I want to change my password',
             value: 'change'
@@ -16,29 +35,43 @@ const ResetPassword = ({ selectedNetwork }) => {
             label: 'I forgot my old password',
             value: 'forgot'
         }
-    ]
+    ], [])
 
-    const disabled = type === null
+    const onRadioChange = value => {
+        setType(value)
+        setOldPassword('')
+        setNewPassword('')
+        setNewPasswordConfirm('')
+    }
+
+    const validateForm = useCallback(() => {
+        if (radios[0].value === type) setDisabled(!(oldPassword.length >= 8 && newPassword.length >= 8 && newPasswordConfirm.length >= 8) || !(newPassword === newPasswordConfirm))
+        if (radios[1].value === type) setDisabled(!(newPassword.length >= 8 && newPasswordConfirm.length >= 8) || !(newPassword === newPasswordConfirm) || !(checkboxes.every(({ ref }) => ref.current && ref.current.checked)))
+    }, [radios, checkboxes, type, oldPassword, newPassword, newPasswordConfirm])
+
+    useEffect(() => validateForm(), [validateForm, oldPassword, newPassword, newPasswordConfirm])
 
     return (
         <Modal id="reset-password-modal" title="Reset Password">
-            <Radios radios={radios} onChange={value => setType(value)}/>
+            <Radios radios={radios} onChange={onRadioChange}/>
             {
                 radios[0].value === type ?
                     <form>
-                        <TextInput placeholder="Old Password"/>
-                        <TextInput placeholder="New Password"/>
-                        <TextInput placeholder="Confirm New Password"/>
+                        <TextInput placeholder="Old Password" onInput={value => setOldPassword(value)}/>
+                        <TextInput placeholder="New Password" onInput={value => setNewPassword(value)}/>
+                        <TextInput placeholder="Confirm New Password" onInput={value => setNewPasswordConfirm(value)}/>
                     </form> : null
             }
             {
                 radios[1].value === type ?
                     <form>
-                        <TextInput placeholder="New Password"/>
-                        <TextInput placeholder="Confirm New Password"/>
-                        <Checkbox label={`I understand I am only changing the password on the ${selectedNetwork.name} network`}/>
-                        <Checkbox label={`I confirm the fee of <...> to apply this change on ${selectedNetwork.name}`}/>
-                        <Checkbox label="I understand I need to wait for 3 days for the change to be confirmed"/>
+                        <TextInput placeholder="New Password" onInput={value => setNewPassword(value)}/>
+                        <TextInput placeholder="Confirm New Password" onInput={value => setNewPasswordConfirm(value)}/>
+                        {
+                            checkboxes.map(({ label, ref }, i) => (
+                                <Checkbox key={`checkbox-${i}`} ref={ref} label={label} onChange={() => validateForm()}/>
+                            ))
+                        }
                     </form> : null
             }
             <div className="buttons">
