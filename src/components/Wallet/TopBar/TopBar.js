caa750b87488b5e8990011b7c1b17fe441d8f47e
Tim <tymmesyde@gmail.com>
2021-12-01 13:49:42 +0100

    refactor(TopBar): add icons to visualize current network and account

--- src/components/Wallet/TopBar/TopBar.js
+++ src/components/Wallet/TopBar/TopBar.js
@@ -1,10 +1,11 @@
 import "./TopBar.scss";
 
 import React, { useState } from "react";
-import { MdOutlineClose, MdOutlineMenu } from "react-icons/md";
+import { MdOutlineArrowForward, MdOutlineClose, MdOutlineMenu } from "react-icons/md";
 import { Select } from "../../common";
 import Accounts from "./Accounts/Accounts";
 import DApps from "./DApps/DApps";
+import * as blockies from 'blockies-ts';
 
 const TopBar = ({
   connections,
@@ -26,11 +27,19 @@ const TopBar = ({
     icon
   }))
 
+  const account = accounts.find(({ id }) => id === selectedAcc)
+
   return (
     <div id="topbar">
-      <div id="mobile-menu-button" onClick={() => setMenuOpen(!isMenuOpen)}>
-        { isMenuOpen ? <MdOutlineClose/> : <MdOutlineMenu/> }
+      <div id="mobile-menu" onClick={() => setMenuOpen(!isMenuOpen)}>
+        <div className="icon" style={{backgroundImage: `url(${blockies.create({ seed: account.id }).toDataURL()})`}}></div>
+        <MdOutlineArrowForward/>
+        <div className="icon" style={{backgroundImage: `url(${network.icon})`}}></div>
+        <div id="menu-button">
+          { isMenuOpen ? <MdOutlineClose/> : <MdOutlineMenu/> }
+        </div>
       </div>
+
       <div className={`container ${isMenuOpen ? 'open' : ''}`}>
         <DApps connections={connections} connect={connect} disconnect={disconnect}/>
         <Accounts accounts={accounts} selectedAddress={selectedAcc} onSelectAcc={onSelectAcc} onRemoveAccount={onRemoveAccount}/>
