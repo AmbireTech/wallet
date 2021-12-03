0a1e2d5acf78e4283ccb3a77f866897ea0d49f7b
Tim <tymmesyde@gmail.com>
2021-12-04 00:43:14 +0100

    refactor: human readable Movr tx

---
+++ src/lib/humanizers/Movr.js
@@ -0,0 +1,15 @@
+import { abis } from '../../consts/humanizerInfo'
+import { Interface } from 'ethers/lib/utils'
+import { token } from '../humanReadableTransactions'
+import networks from '../../consts/networks'
+
+const iface = new Interface(abis.Movr)
+const getNetwork = chainId => networks.find(n => n.chainId === Number(chainId)).name
+
+const MovrMapping = {
+  [iface.getSighash('outboundTransferTo')]: (txn, network) => {
+    const { middlewareInputToken, amount, tokenToBridge, toChainId } = iface.parseTransaction(txn).args[0]
+    return [`Transfer ${token(middlewareInputToken, amount)} to ${getNetwork(toChainId)} for ${token(tokenToBridge)}`]
+  },
+}
+export default MovrMapping
