674a7c549d7aa04ad29663d5f2de78a887fed562
Tim <tymmesyde@gmail.com>
2021-12-04 23:20:54 +0100

    refactor: human readable tx for Yearn

--- src/consts/YearnVaultABI.js
+++ src/consts/YearnVaultABI.js
@@ -1,5 +1,6 @@
 const YearnVaultABI = [
     "function deposit(uint256 _amount, address recipient)",
+    "function withdraw(uint256 maxShares, address recipient, uint256 maxLoss)",
 ]
 
 export default YearnVaultABI
