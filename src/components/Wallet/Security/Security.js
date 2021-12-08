b467835c05d8c6c9b9593eb9cf3c40a479ea4148
Tim <tymmesyde@gmail.com>
2021-12-08 15:35:46 +0100

    refactor: disable change password option if is in recovery

--- src/components/Wallet/Security/Security.js
+++ src/components/Wallet/Security/Security.js
@@ -121,6 +121,7 @@ const Security = ({
       relayerURL={relayerURL}
       onAddAccount={onAddAccount}
       showSendTxns={showSendTxns}
+      recoveryMode={selectedAccount.primaryKeyBackup}
     />)
   }
   // Address book
