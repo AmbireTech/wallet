2d2768789f30a2550b868b773730bf06dbad6586
Tim <tymmesyde@gmail.com>
2021-12-13 13:27:33 +0100

    refactor(YearnVault): handle Withdraw tx sighash

--- src/lib/humanizers/YearnVault.js
+++ src/lib/humanizers/YearnVault.js
@@ -1,6 +1,5 @@
 import { abis } from '../../consts/humanizerInfo'
 import { Interface } from 'ethers/lib/utils'
-import { token } from '../humanReadableTransactions'
 
 const iface = new Interface(abis.YearnVault)
 
@@ -8,6 +7,10 @@ const YearnMapping = {
   [iface.getSighash('deposit(uint256,address)')]: (txn, network) => {
     const [ amount ] = iface.parseTransaction(txn).args
     return [`Deposit ${amount} to Yearn Vault`]
+  },
+  [iface.getSighash('withdraw(uint256,address)')]: (txn, network) => {
+    const [ amount ] = iface.parseTransaction(txn).args
+    return [`Withdraw ${amount} from Yearn Vault`]
   },
 }
 export default YearnMapping
