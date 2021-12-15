d7e4a436769c70a79870f4ff9d41d5b15a3b2d2f
Ivo Georgiev <ivo@adex.network>
2021-12-15 21:01:20 +0200

    Yearn: humanize vaults properly

--- src/lib/humanizers/YearnVault.js
+++ src/lib/humanizers/YearnVault.js
@@ -1,16 +1,24 @@
-import { abis } from '../../consts/humanizerInfo'
+import { abis, yearnVaults } from '../../consts/humanizerInfo'
 import { Interface } from 'ethers/lib/utils'
+import { token } from '../humanReadableTransactions'
 
 const iface = new Interface(abis.YearnVault)
+
+// add 'y' prefix, eg '10 USDC' will become '10 yUSDC' to signify vault tokens
+const addY = x => x.split(' ').map((x, i) => i == 1 ? 'y'+x : x).join(' ')
 
 const YearnMapping = {
   [iface.getSighash('deposit(uint256,address)')]: (txn, network) => {
     const [ amount ] = iface.parseTransaction(txn).args
-    return [`Deposit ${amount} to Yearn Vault`]
+    const vaultInfo = yearnVaults.find(x => x.addr === txn.to)
+    if (vaultInfo) return [`Deposit ${addtoken(vaultInfo.baseToken, amount)} to Yearn`]
+    return [`Deposit ${amount} units to Yearn`]
   },
   [iface.getSighash('withdraw(uint256,address)')]: (txn, network) => {
     const [ amount ] = iface.parseTransaction(txn).args
-    return [`Withdraw ${amount} from Yearn Vault`]
+    const vaultInfo = yearnVaults.find(x => x.addr === txn.to)
+    if (vaultInfo) return [`Withdraw ${addY(token(vaultInfo.baseToken, amount))} from Yearn`]
+    return [`Withdraw ${amount} units from Yearn`]
   },
 }
 export default YearnMapping
