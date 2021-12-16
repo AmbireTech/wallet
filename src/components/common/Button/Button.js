97e9786d6fae813a12a6a0fe144c691fe312e194
Tim <tymmesyde@gmail.com>
2021-12-16 10:28:29 +0100

    refactor(Button): disable disabled prop causing pointer-events to not trigger

--- src/components/common/Button/Button.js
+++ src/components/common/Button/Button.js
@@ -3,9 +3,9 @@ import './Button.scss'
 const Button = ({ children, className, small, icon, iconAfter, disabled, onClick, red, danger, clear, title, type }) => {
     return (
         <button
-            onClick={onClick}
-            className={`buttonComponent ${className} ${small ? 'small' : ''} ${danger || red ? 'danger' : ''} ${clear ? 'clear' : ''}`}
-            disabled={disabled}
+            onClick={() => !disabled && onClick && onClick()}
+            className={`buttonComponent ${className} ${small ? 'small' : ''} ${danger || red ? 'danger' : ''} ${clear ? 'clear' : ''} ${disabled ? 'disabled' : ''}`}
+            // disabled={disabled} // causing pointer-events to not trigger 
             title={title}
             // used with <form>
             type={type}
