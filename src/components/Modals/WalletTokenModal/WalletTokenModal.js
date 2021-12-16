cf1716573a77f9296b6405c6ff95166362001413
Tim <tymmesyde@gmail.com>
2021-12-16 17:18:47 +0100

    refactor: remove rewards hook

--- src/components/Modals/WalletTokenModal/WalletTokenModal.js
+++ src/components/Modals/WalletTokenModal/WalletTokenModal.js
@@ -6,7 +6,6 @@ import { useModals } from '../../../hooks'
 
 const WalletTokenModal = ({ rewards }) => {
     const { hideModal } = useModals()
-    const { balanceRewards, adxRewards } = rewards
 
     const claimButton = <>
         <ToolTip label="Claiming will be available after the official token launch">
@@ -24,7 +23,7 @@ const WalletTokenModal = ({ rewards }) => {
                 <div className="details">
                     <label>Early users Incentive</label>
                     <div className="balance">
-                        <div className="amount"><span className="primary-accent">{ balanceRewards }</span></div>
+                        <div className="amount"><span className="primary-accent">{ rewards['balance-rewards'] }</span></div>
                         {/* <div className="amount-dollar"><span className="secondary-accent">$</span> 0</div> */}
                     </div>
                 </div>
@@ -48,7 +47,7 @@ const WalletTokenModal = ({ rewards }) => {
                 <div className="details">
                     <label>ADX Staking Bonus</label>
                     <div className="balance">
-                        <div className="amount"><span className="primary-accent">{ adxRewards }</span></div>
+                        <div className="amount"><span className="primary-accent">{ rewards['adx-rewards'] }</span></div>
                         {/* <div className="amount-dollar"><span className="secondary-accent">$</span> 0</div> */}
                     </div>
                 </div>
