8ab80a21c89771c941b883882c04a1e98997ff6c
Tim <tymmesyde@gmail.com>
2021-12-16 10:28:53 +0100

    refactor: implement rewards hook

---
+++ src/hooks/rewards.js
@@ -0,0 +1,40 @@
+import { useEffect, useState, useCallback } from "react"
+import { useToasts } from "./toasts"
+
+const useRewards = ({ relayerURL, account }) => {
+    const { addToast } = useToasts()
+
+    const [balanceRewards, setBalanceRewards] = useState(0)
+
+    const resetRewards = () => {
+        setBalanceRewards(0)
+    }
+
+    const getRewards = useCallback(async () => {
+        try {
+            const response = await fetch(`${relayerURL}/wallet-token/rewards/${account}`)
+            const body = await response.json()
+            if (!body || !body.success) return resetRewards()
+            
+            const { rewards } = body
+            if (!rewards.length) return resetRewards()
+
+            rewards.forEach(({ _id, rewards }) => {
+                const entry = Object.entries(rewards).find(([address]) => address === account)
+                const value = (entry && entry[1]) || 0
+                if (_id === 'balance-rewards') setBalanceRewards(value)
+            })
+        } catch(e) {
+            console.error(e);
+            addToast('Could not fetch rewards.', { error: true })
+        }
+    }, [relayerURL, account, addToast])
+
+    useEffect(() => getRewards(), [getRewards, account])
+
+    return {
+        balanceRewards
+    }
+}
+
+export default useRewards
