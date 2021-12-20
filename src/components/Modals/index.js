a71b3f1ec6eed62f0997c295ad0cd7b33f00663c
Tim <tymmesyde@gmail.com>
2021-12-20 16:42:49 +0100

    refactor(Modals): add basic AddTokenModal

--- src/components/Modals/index.js
+++ src/components/Modals/index.js
@@ -3,11 +3,13 @@ import InputModal from './InputModal/InputModal'
 import SelectSignerAccountModal from './SelectSignerAccountModal/SelectSignerAccountModal'
 import ResetPasswordModal from './ResetPasswordModal/ResetPasswordModal'
 import WalletTokenModal from './WalletTokenModal/WalletTokenModal'
+import AddTokenModal from './AddTokenModal/AddTokenModal'
 
 export {
     PermissionsModal,
     InputModal,
     SelectSignerAccountModal,
     ResetPasswordModal,
-    WalletTokenModal
+    WalletTokenModal,
+    AddTokenModal
 }
