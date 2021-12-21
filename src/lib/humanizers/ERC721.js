8141d2cecf60f1a96279d53c2536c4d3acecf4d0
IvoPaunov <ivo.paunov@gmail.com>
2021-12-21 20:10:00 +0200

    add temp hack to ERC721Mapping

--- src/lib/humanizers/ERC721.js
+++ src/lib/humanizers/ERC721.js
@@ -1,6 +1,6 @@
 import { abis } from '../../consts/humanizerInfo'
 import { Interface } from 'ethers/lib/utils'
-import { getName } from '../humanReadableTransactions'
+import { token, getName } from '../humanReadableTransactions'
 
 const iface = new Interface(abis.ERC721)
 const fromText = (from, txnFrom) => from.toLowerCase() !== txnFrom.toLowerCase() ? ` from ${from}` : ''
@@ -8,7 +8,14 @@ const fromText = (from, txnFrom) => from.toLowerCase() !== txnFrom.toLowerCase()
 const ERC721Mapping = {
   [iface.getSighash('transferFrom')]: (txn, network) => {
     const [ from, to, tokenId ] = iface.parseTransaction(txn).args
-    return [`Send token #${tokenId.toString(10)}${fromText(from, txn.from)} to ${getName(to, network)}`]
+        // HACK: ERC20 overlap
+        // TODO: check if the hack works
+        let tokenData = token(txn.to, tokenId)
+        if(tokenData.includes('units of unknown token')) {
+          tokenData = `token #${tokenId.toString(10)}`
+        }
+
+    return [`Send ${tokenData}${fromText(from, txn.from)} to ${getName(to, network)}`]
   },
   [iface.getSighash('safeTransferFrom(address,address,uint256)')]: (txn, network) => {
     const [ from, to, tokenId ] = iface.parseTransaction(txn).args
