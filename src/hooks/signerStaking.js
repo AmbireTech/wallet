d383bbbc862d113c1c2e5f29853de993dbbe1fff
IvoPaunov <ivo.paunov@gmail.com>
2021-12-21 19:16:27 +0200

    use tokens from const at signer staking hook

--- src/hooks/signerStaking.js
+++ src/hooks/signerStaking.js
@@ -1,11 +1,9 @@
 import { useCallback, useEffect, useState, useMemo } from 'react';
 import { BigNumber, getDefaultProvider, Contract } from 'ethers'
 import networks from '../consts/networks'
+import stakingTokens from '../consts/stakingTokens'
 import ERC20ABI from 'adex-protocol-eth/abi/ERC20.json'
 
-const stakingTokens = [
-    { token: 'ADX-STAKING', address: '0xB6456b57f03352bE48Bf101B46c1752a0813491a' },
-    { token: 'ADX-LOYALTY', address: '0xd9A4cB9dc9296e111c66dFACAb8Be034EE2E1c2C' }]
 
 export default function useSignerStaking({ currentNetwork, accounts, selectedAcc }) {
 
@@ -16,7 +14,6 @@ export default function useSignerStaking({ currentNetwork, accounts, selectedAcc
 
     const [balances, setBalances] = useState([])
     const [hasStaking, setHasStaking] = useState(false)
-    
     const provider = useMemo(() => getDefaultProvider(networkRpc), [networkRpc])
     const contracts = useMemo(() => (stakingTokens.map(({ address }) => new Contract(address, ERC20ABI, provider))), [provider])
 
