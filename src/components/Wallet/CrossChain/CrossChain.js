3c82170ee82f57a0853ff23aaba2c60c1e8fddc8
Tim <tymmesyde@gmail.com>
2021-12-22 13:28:45 +0100

    refactor(CrossChain): move history to a new component

--- src/components/Wallet/CrossChain/CrossChain.js
+++ src/components/Wallet/CrossChain/CrossChain.js
@@ -3,14 +3,14 @@ import './CrossChain.scss'
 import { BsArrowDown } from 'react-icons/bs'
 
 import { useEffect, useState, useCallback, useMemo, useRef } from 'react'
-import { MdOutlineArrowForward, MdOutlineCheck } from 'react-icons/md'
 import { ethers } from 'ethers'
 import { parseUnits } from 'ethers/lib/utils'
 import { NumberInput, Button, Select, Loading, NoFundsPlaceholder } from '../../common'
-import { fetchChains, fetchFromTokens, fetchQuotes, fetchToTokens, checkTxStatus } from '../../../services/movr'
+import { fetchChains, fetchFromTokens, fetchQuotes, fetchToTokens } from '../../../services/movr'
 import networks from '../../../consts/networks'
 import { useToasts } from '../../../hooks/toasts'
 import Quotes from './Quotes/Quotes'
+import History from './History/History'
 
 const CrossChain = ({ addRequest, selectedAccount, portfolio, network, sentTxn }) => {
     const { addToast } = useToasts()
@@ -34,7 +34,6 @@ const CrossChain = ({ addRequest, selectedAccount, portfolio, network, sentTxn }
         const storedQuotesConfirmed = localStorage.quotesConfirmed
         return storedQuotesConfirmed ? JSON.parse(storedQuotesConfirmed) : []
     })
-    const [txStatuses, setTxStatuses] = useState([])
     
     const fromChain = useMemo(() => network.chainId, [network.chainId])
     const formDisabled = !(fromToken && toToken && fromChain && toChain && amount > 0)
@@ -45,9 +44,6 @@ const CrossChain = ({ addRequest, selectedAccount, portfolio, network, sentTxn }
             address: Number(token.address) === 0 ? `0x${'e'.repeat(40)}` : token.address
         }))
         .find(({ address }) => address === tokenAddress), [portfolio.tokens])
-
-    const getNetworkDetails = chainId => networks.find(n => n.chainId === chainId)
-    const formatAmount = (amount, asset) => amount / Math.pow(10, asset.decimals)
 
     const loadChains = useCallback(async () => {
         try {
@@ -168,31 +164,6 @@ const CrossChain = ({ addRequest, selectedAccount, portfolio, network, sentTxn }
         localStorage.quotesConfirmed = JSON.stringify(updatedQuotesConfirmed)
     }
 
-    useEffect(() => {
-        async function getStatuses() {
-            const quotesConfirmedRequestIds = quotesConfirmed.map(({ id }) => id)
-            const quotesConfirmedSent = sentTxn
-                .filter(sent => sent.network === network.id && sent?.requestIds.some(id => quotesConfirmedRequestIds.includes(id)))
-
-            const statuses = await Promise.all(quotesConfirmedSent.map(async ({ hash, requestIds }) => {
-                const { from, to } = quotesConfirmed.find(({ id }) => requestIds.includes(id))
-                const status = await checkTxStatus(hash, from.chainId, to.chainId)
-                return {
-                    ...status,
-                    from,
-                    to,
-                    fromNetwork: getNetworkDetails(from.chainId),
-                    toNetwork: getNetworkDetails(to.chainId),
-                    isPending: !(status.sourceTxStatus === 'COMPLETED' && status.destinationTxStatus === 'COMPLETED')
-                }
-            }))
-
-            console.log(statuses);
-            setTxStatuses(statuses)
-        }
-        getStatuses()
-    }, [sentTxn, quotesConfirmed, network.id])
-
     useEffect(() => setAmount(0), [fromToken])
     useEffect(() => {
         const fromTokenItem = fromTokensItems.find(({ value }) => value === fromToken)
@@ -282,63 +253,11 @@ const CrossChain = ({ addRequest, selectedAccount, portfolio, network, sentTxn }
                                                 </div>
                 }
             </div>
-            <div id="history" className="panel">
-                <div className="title">
-                    Transfer / Swaps History
-                </div>
-                <div>
-                    {
-                        !txStatuses.length ?
-                            <div>No pending transfer/swap on this network.</div>
-                            :
-                            txStatuses.map(({ sourceTx, fromNetwork, toNetwork, from, to, isPending }) => (
-                                <div className="status" key={sourceTx}>
-                                    <div className="summary">
-                                        <div className="route">
-                                            <div className="path">
-                                                <div className="network">
-                                                    <div className="icon" style={{backgroundImage: `url(${fromNetwork.icon})`}}></div>
-                                                    <div className="name">{ fromNetwork.name }</div>
-                                                </div>
-                                                <div className="amount">
-                                                    { formatAmount(from?.amount, from.asset) }
-                                                    <div className="asset">
-                                                        <div className="icon" style={{backgroundImage: `url(${from?.asset?.icon})`}}></div>
-                                                        <div className="name">{ from?.asset?.name }</div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                                <MdOutlineArrowForward/>
-                                            <div className="path">
-                                                <div className="network">
-                                                    <div className="icon" style={{backgroundImage: `url(${toNetwork.icon})`}}></div>
-                                                    <div className="name">{ toNetwork.name }</div>
-                                                </div>
-
-                                                <div className="amount">
-                                                    { formatAmount(to?.amount, to.asset) }
-                                                    <div className="asset">
-                                                        <div className="icon" style={{backgroundImage: `url(${to?.asset?.icon})`}}></div>
-                                                        <div className="name">{ to?.asset?.name }</div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <a href={`${fromNetwork.explorerUrl}/tx/${sourceTx}`} target="_blank" rel="noreferrer">View on Block Explorer</a>
-                                    </div>
-                                    <div className="details">
-                                        {
-                                            isPending ? 
-                                                <Loading/>
-                                                :
-                                                <MdOutlineCheck/>
-                                        }
-                                    </div>
-                                </div>
-                            ))
-                    }
-                </div>
-            </div>
+            <History
+                network={network}
+                sentTxn={sentTxn}
+                quotesConfirmed={quotesConfirmed}
+            />
         </div>
     )
 }
