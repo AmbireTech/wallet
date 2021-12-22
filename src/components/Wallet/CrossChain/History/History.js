c664d40569723ca4b1ed711c5b382e6ccb4ed529
Tim <tymmesyde@gmail.com>
2021-12-22 16:09:00 +0100

    refactor(History): catch error and warn the user the status could not be fetch

--- src/components/Wallet/CrossChain/History/History.js
+++ src/components/Wallet/CrossChain/History/History.js
@@ -1,13 +1,15 @@
 import './History.scss'
 
 import { useState, useEffect } from 'react'
-import { MdOutlineArrowForward, MdOutlineCheck } from 'react-icons/md'
+import { MdOutlineArrowForward, MdOutlineCheck, MdOutlineClose } from 'react-icons/md'
 import { HiOutlineExternalLink } from 'react-icons/hi'
 import { Loading } from '../../../common'
 import { checkTxStatus } from '../../../../services/movr'
 import networks from '../../../../consts/networks'
+import { useToasts } from '../../../../hooks/toasts'
 
 const History = ({ network, sentTxn, quotesConfirmed }) => {
+    const { addToast } = useToasts()
     const [txStatuses, setTxStatuses] = useState([])
 
     const getNetworkDetails = chainId => networks.find(n => n.chainId === chainId)
@@ -21,21 +23,37 @@ const History = ({ network, sentTxn, quotesConfirmed }) => {
 
             const statuses = await Promise.all(quotesConfirmedSent.map(async ({ hash, requestIds }) => {
                 const { from, to } = quotesConfirmed.find(({ id }) => requestIds.includes(id))
-                const status = await checkTxStatus(hash, from.chainId, to.chainId)
-                return {
-                    ...status,
-                    from,
-                    to,
-                    fromNetwork: getNetworkDetails(from.chainId),
-                    toNetwork: getNetworkDetails(to.chainId),
-                    isPending: !(status.sourceTxStatus === 'COMPLETED' && status.destinationTxStatus === 'COMPLETED')
+                const fromNetwork = getNetworkDetails(from.chainId)
+                const toNetwork = getNetworkDetails(to.chainId)
+
+                try {
+                    const status = await checkTxStatus(hash, from.chainId, to.chainId)
+                    return {
+                        ...status,
+                        from,
+                        to,
+                        fromNetwork,
+                        toNetwork,
+                        isPending: !(status.sourceTxStatus === 'COMPLETED' && status.destinationTxStatus === 'COMPLETED')
+                    }
+                } catch(e) {
+                    console.error(e)
+                    addToast('Cross-Chain History: Unable to fetch transfer status.', { error: true })
+                    return {
+                        sourceTx: hash,
+                        from,
+                        to,
+                        fromNetwork,
+                        toNetwork,
+                        statusError: true
+                    }
                 }
             }))
 
             setTxStatuses(statuses)
         }
         getStatuses()
-    }, [sentTxn, quotesConfirmed, network.id])
+    }, [sentTxn, quotesConfirmed, network.id, addToast])
 
     return (
         <div id="history" className="panel">
@@ -47,7 +65,7 @@ const History = ({ network, sentTxn, quotesConfirmed }) => {
                     !txStatuses.length ?
                         <div>No pending transfer/swap on this network.</div>
                         :
-                        txStatuses.map(({ sourceTx, fromNetwork, toNetwork, from, to, isPending }) => (
+                        txStatuses.map(({ sourceTx, fromNetwork, toNetwork, from, to, isPending, statusError }) => (
                             <div className="tx-status" key={sourceTx}>
                                 <div className="summary">
                                     <div className="path">
@@ -82,16 +100,22 @@ const History = ({ network, sentTxn, quotesConfirmed }) => {
                                 <div className="details">
                                     <a href={`${fromNetwork.explorerUrl}/tx/${sourceTx}`} target="_blank" rel="noreferrer">View on Block Explorer <HiOutlineExternalLink/></a>
                                     {
-                                        isPending ? 
-                                            <div className="status pending">
-                                                <Loading/>
-                                                Pending
+                                        statusError ? 
+                                            <div className="status error">
+                                                <MdOutlineClose/>
+                                                Could not fetch status
                                             </div>
                                             :
-                                            <div className="status confirmed">
-                                                <MdOutlineCheck/>
-                                                Confirmed
-                                            </div>
+                                            isPending ? 
+                                                <div className="status pending">
+                                                    <Loading/>
+                                                    Pending
+                                                </div>
+                                                :
+                                                <div className="status confirmed">
+                                                    <MdOutlineCheck/>
+                                                    Confirmed
+                                                </div>
                                     }
                                 </div>
                             </div>
