ab5644ef7c9952aaaf68e0046323d8e65075c724
Tim <tymmesyde@gmail.com>
2021-12-22 14:58:22 +0100

    refactor(History): styling

--- src/components/Wallet/CrossChain/History/History.js
+++ src/components/Wallet/CrossChain/History/History.js
@@ -2,6 +2,7 @@ import './History.scss'
 
 import { useState, useEffect } from 'react'
 import { MdOutlineArrowForward, MdOutlineCheck } from 'react-icons/md'
+import { HiOutlineExternalLink } from 'react-icons/hi'
 import { Loading } from '../../../common'
 import { checkTxStatus } from '../../../../services/movr'
 import networks from '../../../../consts/networks'
@@ -31,7 +32,6 @@ const History = ({ network, sentTxn, quotesConfirmed }) => {
                 }
             }))
 
-            console.log(statuses);
             setTxStatuses(statuses)
         }
         getStatuses()
@@ -48,46 +48,50 @@ const History = ({ network, sentTxn, quotesConfirmed }) => {
                         <div>No pending transfer/swap on this network.</div>
                         :
                         txStatuses.map(({ sourceTx, fromNetwork, toNetwork, from, to, isPending }) => (
-                            <div className="status" key={sourceTx}>
+                            <div className="tx-status" key={sourceTx}>
                                 <div className="summary">
-                                    <div className="route">
-                                        <div className="path">
-                                            <div className="network">
-                                                <div className="icon" style={{backgroundImage: `url(${fromNetwork.icon})`}}></div>
-                                                <div className="name">{ fromNetwork.name }</div>
-                                            </div>
-                                            <div className="amount">
-                                                { formatAmount(from?.amount, from.asset) }
-                                                <div className="asset">
-                                                    <div className="icon" style={{backgroundImage: `url(${from?.asset?.icon})`}}></div>
-                                                    <div className="name">{ from?.asset?.name }</div>
-                                                </div>
-                                            </div>
+                                    <div className="path">
+                                        <div className="network">
+                                            <div className="icon" style={{backgroundImage: `url(${fromNetwork.icon})`}}></div>
+                                            <div className="name">{ fromNetwork.name }</div>
                                         </div>
-                                            <MdOutlineArrowForward/>
-                                        <div className="path">
-                                            <div className="network">
-                                                <div className="icon" style={{backgroundImage: `url(${toNetwork.icon})`}}></div>
-                                                <div className="name">{ toNetwork.name }</div>
-                                            </div>
-
-                                            <div className="amount">
-                                                { formatAmount(to?.amount, to.asset) }
-                                                <div className="asset">
-                                                    <div className="icon" style={{backgroundImage: `url(${to?.asset?.icon})`}}></div>
-                                                    <div className="name">{ to?.asset?.name }</div>
-                                                </div>
+                                        <div className="amount">
+                                            { formatAmount(from?.amount, from.asset) }
+                                            <div className="asset">
+                                                <div className="icon" style={{backgroundImage: `url(${from?.asset?.icon})`}}></div>
+                                                <div className="name">{ from?.asset?.symbol }</div>
                                             </div>
                                         </div>
                                     </div>
-                                    <a href={`${fromNetwork.explorerUrl}/tx/${sourceTx}`} target="_blank" rel="noreferrer">View on Block Explorer</a>
+                                    <MdOutlineArrowForward/>
+                                    <div className="path">
+                                        <div className="network">
+                                            <div className="icon" style={{backgroundImage: `url(${toNetwork.icon})`}}></div>
+                                            <div className="name">{ toNetwork.name }</div>
+                                        </div>
+
+                                        <div className="amount">
+                                            { formatAmount(to?.amount, to.asset) }
+                                            <div className="asset">
+                                                <div className="icon" style={{backgroundImage: `url(${to?.asset?.icon})`}}></div>
+                                                <div className="name">{ to?.asset?.symbol }</div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                                 <div className="details">
+                                    <a href={`${fromNetwork.explorerUrl}/tx/${sourceTx}`} target="_blank" rel="noreferrer">View on Block Explorer <HiOutlineExternalLink/></a>
                                     {
                                         isPending ? 
-                                            <Loading/>
+                                            <div className="status pending">
+                                                <Loading/>
+                                                Pending
+                                            </div>
                                             :
-                                            <MdOutlineCheck/>
+                                            <div className="status confirmed">
+                                                <MdOutlineCheck/>
+                                                Confirmed
+                                            </div>
                                     }
                                 </div>
                             </div>
