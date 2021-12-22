7ccfb98158da2113afd9b7cad61f8e9fdb6bae7d
Tim <tymmesyde@gmail.com>
2021-12-22 17:15:53 +0100

    refactor(TesseractVaultABI): add pricePerShare function

--- src/consts/TesseractVaultABI.js
+++ src/consts/TesseractVaultABI.js
@@ -2,6 +2,7 @@ const TesseractVaultABI = [
     "function token() view returns (address)",
     "function deposit(uint256 _amount, address recipient)",
     "function withdraw(uint256 maxShares, address recipient)",
+    "function pricePerShare() view returns (uint256)",
 ]
 
 export default TesseractVaultABI
