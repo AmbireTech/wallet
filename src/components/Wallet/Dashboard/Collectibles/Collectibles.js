d6f5805b33dee3c433624be4f8c391d7d72d1c72
Ivo Georgiev <ivo@strem.io>
2022-01-05 00:14:10 -0600

    Collectibles: fix crash

--- src/components/Wallet/Dashboard/Collectibles/Collectibles.js
+++ src/components/Wallet/Dashboard/Collectibles/Collectibles.js
@@ -10,7 +10,7 @@ const Collectibles = ({ collectibles }) => {
             :
             <div id="collectibles">
                 {
-                    collectibles.map(({ tokens }) => tokens.map(({ network, address, collectionName, collectionImg, assets }) => assets.map(({ tokenId, assetName, assetImg, balanceUSD }) => (
+                    collectibles.map(({ tokens }) => (tokens || []).map(({ network, address, collectionName, collectionImg, assets }) => assets.map(({ tokenId, assetName, assetImg, balanceUSD }) => (
                         <div className="collectible" key={tokenId}>
                             <NavLink to={`/wallet/nft/${network}/${address}/${tokenId}`}>
                                 <div className="artwork" style={{backgroundImage: `url(${assetImg})`}}/>
