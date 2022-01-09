61014eadad069ad1aaf4527cb7bc29fe8898d02a
Tim <tymmesyde@gmail.com>
2022-01-09 17:46:23 +0100

    fix(ToolTip): make label disapear on mouse leave

--- src/components/common/ToolTip/ToolTip.js
+++ src/components/common/ToolTip/ToolTip.js
@@ -2,15 +2,26 @@ import { useState } from 'react';
 import './ToolTip.scss'
 
 const ToolTip = ({ children, label, className }) => {
+    const [isMouseOver, setIsMouseOver] = useState(false)
     const [mousePosition, setMousePosition] = useState({ x: 0, y: 0})
 
     const margin = 15
     const onMouseMove = ({ clientY, clientX }) => setMousePosition({ x: clientX + margin, y: clientY + margin })
 
     return (
-        <div className={`tooltip ${className ? className : ''}`} onMouseMove={onMouseMove}>
+        <div
+            className={`tooltip ${className ? className : ''}`}
+            onMouseMove={onMouseMove}
+            onMouseLeave={() => setIsMouseOver(false)}
+            onMouseEnter={() => setIsMouseOver(true)}
+        >
             { children }
-            <label style={{top: mousePosition.y, left: mousePosition.x}}>{ label }</label>
+            {
+                isMouseOver ? 
+                    <label style={{top: mousePosition.y, left: mousePosition.x}}>{ label }</label>
+                    :
+                    null
+            }
         </div>
     )
 }
