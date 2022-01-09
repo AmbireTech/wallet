ff93eebe0fa87deb13876b15c746be9fe86e87a0
Tim <tymmesyde@gmail.com>
2022-01-09 18:13:41 +0100

    refactor(ToolTip): revert mouse enter/leave changes and use custom class on label

--- src/components/common/ToolTip/ToolTip.js
+++ src/components/common/ToolTip/ToolTip.js
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import './ToolTip.scss'
 
 const ToolTip = ({ children, label, disabled, className }) => {
-    const [isMouseOver, setIsMouseOver] = useState(false)
     const [mousePosition, setMousePosition] = useState({ x: 0, y: 0})
 
     const margin = 15
@@ -12,13 +11,11 @@ const ToolTip = ({ children, label, disabled, className }) => {
         <div
             className={`tooltip ${className ? className : ''}`}
             onMouseMove={onMouseMove}
-            onMouseLeave={() => setIsMouseOver(false)}
-            onMouseEnter={() => setIsMouseOver(true)}
         >
             { children }
             {
-                isMouseOver && !disabled ? 
-                    <label style={{top: mousePosition.y, left: mousePosition.x}}>{ label }</label>
+                !disabled ? 
+                    <div class="tooltip-label" style={{top: mousePosition.y, left: mousePosition.x}}>{ label }</div>
                     :
                     null
             }
