d76075e5537c2ec8146089ed6bb546e21ef5dee5
Tim <tymmesyde@gmail.com>
2022-01-10 19:15:04 +0100

    refactor: merge Yearn and Tesseract vault abis together

--- src/consts/YearnTesseractVaultABI.js
+++ src/consts/YearnTesseractVaultABI.js
@@ -1,8 +1,8 @@
-const TesseractVaultABI = [
+const YearnVaultABI = [
     "function token() view returns (address)",
     "function deposit(uint256 _amount, address recipient)",
     "function withdraw(uint256 maxShares, address recipient)",
     "function pricePerShare() view returns (uint256)",
 ]
 
-export default TesseractVaultABI
+export default YearnVaultABI
--- src/consts/YearnVaultABI.js
+++
@@ -1,7 +0,0 @@
-const YearnVaultABI = [
-    "function pricePerShare() view returns (uint256)",
-    "function deposit(uint256 _amount, address recipient)",
-    "function withdraw(uint256 maxShares, address recipient)",
-]
-
-export default YearnVaultABI
