d412e9b39a5948c56b177be9c47a40f883338a5d
Gergana Mihaylova <gergana@devlabs.bg>
2022-01-11 14:05:05 +0200

    Refactor: ternary operators

--- src/components/Wallet/Dashboard/Collectibles/Collectibles.js
+++ src/components/Wallet/Dashboard/Collectibles/Collectibles.js
@@ -5,34 +5,40 @@ import CollectiblesPlaceholder from './CollectiblesPlaceholder/CollectiblesPlace
 import { Loading } from '../../../common'
 
 const Collectibles = ({ portfolio, isPrivateMode }) => {
+
+    if (portfolio.areProtocolsLoading) return <Loading />;
+
+    if (!portfolio.collectibles.length || isPrivateMode) {
+        return (
+            <CollectiblesPlaceholder
+                isPrivateMode={isPrivateMode}
+                collectiblesLength={portfolio.collectibles.length}
+            />
+        );
+    }
+
     return (
-        portfolio.areProtocolsLoading ?
-            <Loading/>
-            :
-            !portfolio.collectibles.length || isPrivateMode ?
-                <CollectiblesPlaceholder isPrivateMode={isPrivateMode} collectiblesLength={portfolio.collectibles.length} />
-                :
-                <div id="collectibles">
-                {
-                    portfolio.collectibles.map(({ network, address, collectionName, collectionImg, assets }) => (assets || []).map(({ tokenId, assetName, assetImg, balanceUSD }) => (
-                        <div className="collectible" key={tokenId}>
-                            <NavLink to={`/wallet/nft/${network}/${address}/${tokenId}`}>
-                                <div className="artwork" style={{backgroundImage: `url(${assetImg})`}}/>
-                                <div className="info">
-                                    <div className="collection">
-                                        <div className="collection-icon" style={{backgroundImage: `url(${collectionImg})`}}></div>
-                                        <span className="collection-name">{ collectionName }</span>
-                                    </div>
-                                    <div className="details">
-                                        <div className="name">{ assetName }</div>
-                                        <div className="value"><span className="purple-highlight">$</span> {balanceUSD.toFixed(2) }</div>
-                                    </div>
+        <div id="collectibles">
+            {
+                portfolio.collectibles.map(({ network, address, collectionName, collectionImg, assets }) => (assets || []).map(({ tokenId, assetName, assetImg, balanceUSD }) => (
+                    <div className="collectible" key={tokenId}>
+                        <NavLink to={`/wallet/nft/${network}/${address}/${tokenId}`}>
+                            <div className="artwork" style={{backgroundImage: `url(${assetImg})`}}/>
+                            <div className="info">
+                                <div className="collection">
+                                    <div className="collection-icon" style={{backgroundImage: `url(${collectionImg})`}}></div>
+                                    <span className="collection-name">{ collectionName }</span>
                                 </div>
-                            </NavLink>
-                        </div>
-                    )))
-                }
-            </div>            
+                                <div className="details">
+                                    <div className="name">{ assetName }</div>
+                                    <div className="value"><span className="purple-highlight">$</span> {balanceUSD.toFixed(2) }</div>
+                                </div>
+                            </div>
+                        </NavLink>
+                    </div>
+                )))
+            }
+        </div>            
     )
 }
 
