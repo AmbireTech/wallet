4ed6cfa9d2a3b3b38e21277a00ddbe06cea32b3e
Emil <stojnovsky@gmail.com>
2022-01-13 11:20:27 +0200

    custom tokens visible with zero balance

--- src/hooks/portfolio.js
+++ src/hooks/portfolio.js
@@ -56,7 +56,10 @@ async function supplementTokensDataFromNetwork({ walletAddr, network, tokensData
 
     const tokenBalances = (await Promise.all(calls.map(callTokens => {
         return getTokenListBalance({ walletAddr, tokens: callTokens, network, updateBalance })
-    }))).flat()
+    }))).flat().filter(t => {
+        return extraTokens.some(et => t.address === et.address) ? true : (parseFloat(t.balance) > 0)
+    })
+console.log('tokenBalances', tokenBalances)
     return tokenBalances
   }
   
