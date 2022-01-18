eb95c5426da77a20963b810afda90ed29ade9f42
Tim <tymmesyde@gmail.com>
2022-01-18 14:26:00 +0100

    fix(SendTransaction): layout button issues

--- src/components/SendTransaction/Actions.js
+++ src/components/SendTransaction/Actions.js
@@ -25,28 +25,28 @@ export default function Actions({ estimation, feeSpeed, approveTxn, rejectTxn, s
   }
 
   const signButtonLabel = signingStatus && signingStatus.inProgress ?
-    (<span><Loading/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Signing...</span>)
-    : (<span>Sign and send</span>)
+    <><Loading/>Signing...</>
+    : <><MdOutlineCheck/>Sign and send</>
 
   const isRecoveryMode = signingStatus && signingStatus.finalBundle && signingStatus.finalBundle.recoveryMode
   if (signingStatus && signingStatus.quickAcc) {
     return (<>
-      <div>
-        {signingStatus.confCodeRequired === 'otp' ? (<b>Please enter your OTP code and your password.</b>) : (<></>)}
-        {signingStatus.confCodeRequired === 'email' ? (<b>A confirmation code was sent to your email, please enter it along with your password.</b>) : (<></>)}
+      <div className='confirmation-code-info'>
+        {signingStatus.confCodeRequired === 'otp' ? <>Please enter your OTP code and your password.</> : null}
+        {signingStatus.confCodeRequired === 'email' ? <>A confirmation code was sent to your email, please enter it along with your password.</> : null}
       </div>
-      {<TextInput
-        small
-        password
-        required
-        minLength={3}
-        placeholder='Password'
-        value={quickAccCredentials.passphrase}
-        style={isRecoveryMode ? { visibility: 'hidden' } : {} }
-        disabled={isRecoveryMode}
-        onChange={value => setQuickAccCredentials({ ...quickAccCredentials, passphrase: value })}
-      ></TextInput>}
       <form ref={form} className='quickAccSigningForm' onSubmit={e => { e.preventDefault() }}>
+        <TextInput
+          small
+          password
+          required
+          minLength={3}
+          placeholder='Password'
+          value={quickAccCredentials.passphrase}
+          style={isRecoveryMode ? { visibility: 'hidden' } : {} }
+          disabled={isRecoveryMode}
+          onChange={value => setQuickAccCredentials({ ...quickAccCredentials, passphrase: value })}
+        ></TextInput>
         {/* Changing the autoComplete prop to a random string seems to disable it in more cases */}
         <TextInput
           small
@@ -58,22 +58,24 @@ export default function Actions({ estimation, feeSpeed, approveTxn, rejectTxn, s
           value={quickAccCredentials.code}
           onChange={value => setQuickAccCredentials({ ...quickAccCredentials, code: value })}
         ></TextInput>
-        {rejectButton}
-        <Button small className='approveTxn'
-          onClick={() => {
-            if (!form.current.checkValidity()) return
-            approveTxn({ quickAccCredentials })
-          }}
-        >
-          {signButtonLabel}
-        </Button>
+        <div className='buttons'>
+          {rejectButton}
+          <Button className='approveTxn'
+            onClick={() => {
+              if (!form.current.checkValidity()) return
+              approveTxn({ quickAccCredentials })
+            }}
+          >
+            {signButtonLabel}
+          </Button>
+        </div>
       </form>
     </>)
   }
 
   return (<div className='buttons'>
       {rejectButton}
-      <Button className='approveTxn' icon={<MdOutlineCheck/>} disabled={!estimation || signingStatus} onClick={approveTxn}>
+      <Button className='approveTxn' disabled={!estimation || signingStatus} onClick={approveTxn}>
         {signButtonLabel}
       </Button>
   </div>)
