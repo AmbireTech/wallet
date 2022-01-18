ed29c8f29c2b7d68b58bd88a475774a6bf7b4865
Tim <tymmesyde@gmail.com>
2022-01-18 11:35:33 +0100

    refactor(humanizers): extended for WETH

--- src/lib/humanizers/WETH.js
+++ src/lib/humanizers/WETH.js
@@ -2,15 +2,25 @@ import { abis } from 'consts/humanizerInfo'
 import { Interface } from 'ethers/lib/utils'
 import { nativeToken } from 'lib/humanReadableTransactions'
 
+const toExtended = (action, network, amount) => {
+  return [
+      action,
+      {
+        type: 'token',
+        ...nativeToken(network, amount, true)
+      }
+    ]
+}
+
 const iface = new Interface(abis.WETH)
 const WETHMapping = {
-  [iface.getSighash('deposit')]: (txn, network) => {
+  [iface.getSighash('deposit')]: (txn, network, { extended = false }) => {
     const { value } = iface.parseTransaction(txn)
-    return [`Wrap ${nativeToken(network, value)}`]
+    return !extended ? [`Wrap ${nativeToken(network, value)}`] : toExtended('Wrap', network, value)
   },
-  [iface.getSighash('withdraw')]: (txn, network) => {
+  [iface.getSighash('withdraw')]: (txn, network, { extended = false }) => {
     const [ amount ] = iface.parseTransaction(txn).args
-    return [`Unwrap ${nativeToken(network, amount)}`]
+    return !extended ? [`Unwrap ${nativeToken(network, amount)}`] : toExtended('Unwrap', network, amount)
   },
 }
 export default WETHMapping
