095e6c1b7b2a0da3432b3ed1b8a948bc77a4d382
Tim <tymmesyde@gmail.com>
2022-01-18 08:01:39 +0100

    refactor(humanizers): extended mapping for withdraw and deposit on Aave

--- src/lib/humanizers/AaveLendingPoolV2.js
+++ src/lib/humanizers/AaveLendingPoolV2.js
@@ -7,13 +7,32 @@ const onBehalfText = (onBehalf, txnFrom) => onBehalf.toLowerCase() !== txnFrom.t
   ? ' on behalf of '+onBehalf
   : ''
 
+const toExtended = (action, word, token, onBehalf, txn) => {
+  return [
+    action,
+    {
+      type: 'token',
+      ...token
+    },
+    word,
+    {
+      type: 'address',
+      address: txn.to,
+      name: 'Aave Lending Pool'
+    },
+    onBehalfText(onBehalf, txn.from)
+  ]
+}
+
 const AaveMapping = {
-  [iface.getSighash('deposit')]: (txn, network) => {
+  [iface.getSighash('deposit')]: (txn, network, { extended }) => {
     const [ asset, amount, onBehalf ] = iface.parseTransaction(txn).args
+    if (extended) return toExtended('Deposit', 'to', token(asset, amount, true), onBehalf, txn)
     return [`Deposit ${token(asset, amount)} to Aave lending pool${onBehalfText(onBehalf, txn.from)}`]
   },
-  [iface.getSighash('withdraw')]: (txn, network) => {
+  [iface.getSighash('withdraw')]: (txn, network, { extended }) => {
     const [ asset, amount, onBehalf ] = iface.parseTransaction(txn).args
+    if (extended) return toExtended('Withdraw', 'from', token(asset, amount, true), onBehalf, txn)
     return [`Withdraw ${token(asset, amount)} from Aave lending pool${onBehalfText(onBehalf, txn.from)}`]
   },
   [iface.getSighash('repay')]: (txn, network) => {
