39adabcf57c14db2a3a7e4d023f33789ae19a16e
Tim <tymmesyde@gmail.com>
2022-01-19 08:54:17 +0100

    refactor: extended tx summary for ERC721

--- src/lib/humanizers/ERC721.js
+++ src/lib/humanizers/ERC721.js
@@ -5,14 +5,33 @@ import { getName } from 'lib/humanReadableTransactions'
 const iface = new Interface(abis.ERC721)
 const fromText = (from, txnFrom) => from.toLowerCase() !== txnFrom.toLowerCase() ? ` from ${from}` : ''
 
-const ERC721Mapping = {
-  [iface.getSighash('transferFrom')]: (txn, network) => {
-    const [ from, to, tokenId ] = iface.parseTransaction(txn).args
-    return [`Send token #${tokenId.toString(10)}${fromText(from, txn.from)} to ${getName(to, network)}`]
+const toExtended = (tokenId, from, to, txn, network) => [[
+  'Send',
+  {
+    type: 'erc721',
+    address: txn.to,
+    network: network.id,
+    id: tokenId,
+    name: `Token #${tokenId.toString(10)}${fromText(from, txn.from)}`,
   },
-  [iface.getSighash('safeTransferFrom(address,address,uint256)')]: (txn, network) => {
+  'to',
+  {
+    type: 'address',
+    address: to,
+    name: getName(to, network)
+  }
+]]
+
+const ERC721Mapping = {
+  [iface.getSighash('transferFrom')]: (txn, network, { extended = false }) => {
     const [ from, to, tokenId ] = iface.parseTransaction(txn).args
-    return [`Send token #${tokenId.toString(10)}${fromText(from, txn.from)} to ${getName(to, network)}`]
+    return !extended ? [`Send token #${tokenId.toString(10)}${fromText(from, txn.from)} to ${getName(to, network)}`]
+    : toExtended(tokenId, from, to, txn, network)
+  },
+  [iface.getSighash('safeTransferFrom(address,address,uint256)')]: (txn, network, { extended = false }) => {
+    const [ from, to, tokenId ] = iface.parseTransaction(txn).args
+    return !extended ? [`Send token #${tokenId.toString(10)}${fromText(from, txn.from)} to ${getName(to, network)}`]
+    : toExtended(tokenId, from, to, txn, network)
   }
 }
 export default ERC721Mapping
