7f7911e1f9005b8905e15410be56036349b703d1
Tim <tymmesyde@gmail.com>
2022-01-19 08:59:04 +0100

    refactor(TxnPreview): cleaner code

--- src/components/common/TxnPreview/TxnPreview.js
+++ src/components/common/TxnPreview/TxnPreview.js
@@ -20,6 +20,65 @@ function getNetworkSymbol(networkId) {
   const network = networks.find(x => x.id === networkId)
   return network ? network.nativeAssetSymbol : 'UNKNW'
 }
+
+function parseExtendedSummaryItem(item, i, networkDetails) {
+  if (item.length === 1) return item
+
+  if (i === 0) return (<div className={`action ${item.toLowerCase()}`} key={`item-${i}`}>{ item }</div>)
+
+  if (!item.type) return (<div className='word' key={`item-${i}`}>{ item }</div>)
+
+  if (item.type === 'token') return (
+    <div className='token' key={`item-${i}`}>
+      { item.amount > 0 ? <span>{ item.amount }</span> : null }
+      { item.address ? <div className='icon' style={{ backgroundImage: `url(${getTokenIcon(networkDetails.id, item.address)})` }}></div> : null }
+      { item.symbol }
+    </div>
+  )
+
+  if (item.type === 'address') return (
+    <a
+      className='address'
+      key={`item-${i}`}
+      href={item.address ? `${networkDetails.explorerUrl}/address/${item.address}` : null}
+      target="_blank"
+      rel="noreferrer"
+      onClick={e => e.stopPropagation()}
+    >
+      <ToolTip disabled={!item.address} label={item.address}>
+        { item.name ? item.name : item.address }
+        { item.address ? <HiOutlineExternalLink/> : null }
+      </ToolTip>
+    </a>
+  )
+
+  if (item.type === 'network') return (
+    <div className='network' key={`item-${i}`}>
+      { item.icon ? <div className='icon' style={{ backgroundImage: `url(${item.icon})` }}></div> : null }
+      { item.name }
+    </div>
+  )
+
+  if (item.type === 'erc721') {
+    const canShowLink = item.network && item.address && item.id
+    return (
+      <a
+        className='erc721'
+        key={`item-${i}`}
+        href={canShowLink ? `#/wallet/nft/${item.network}/${item.address}/${item.id}` : null}
+        target="_blank"
+        rel="noreferrer"
+        onClick={e => e.stopPropagation()}
+      >
+        { item.name }
+        { canShowLink ? <HiOutlineExternalLink/> : null }
+      </a>
+    )
+  }
+
+  return <></>
+}
+
 export default function TxnPreview ({ txn, onDismiss, network, account, isFirstFailing, mined, disableExpand }) {
   const [isExpanded, setExpanded] = useState(false)
   const contractName = getName(txn[0], network)
@@ -27,68 +86,7 @@ export default function TxnPreview ({ txn, onDismiss, network, account, isFirstF
   const networkDetails = networks.find(({ id }) => id === network)
   const extendedSummary = getTransactionSummary(txn, network, account, { mined, extended: true })
 
-  const summary = (extendedSummary.map(entry => {
-    return Array.isArray(entry) ? // If entry is extended summary
-      entry.map((item, i) => {
-        if (item.length === 1) return item
-
-        if (i === 0) return (<div className={`action ${item.toLowerCase()}`} key={`item-${i}`}>{ item }</div>)
-
-        if (!item.type) return (<div className='word' key={`item-${i}`}>{ item }</div>)
-
-        if (item.type === 'token') return (
-          <div className='token' key={`item-${i}`}>
-            { item.amount > 0 ? <span>{ item.amount }</span> : null }
-            { item.address ? <div className='icon' style={{ backgroundImage: `url(${getTokenIcon(network, item.address)})` }}></div> : null }
-            { item.symbol }
-          </div>
-        )
-
-        if (item.type === 'address') return (
-          <a
-            className='address'
-            key={`item-${i}`}
-            href={item.address ? `${networkDetails.explorerUrl}/address/${item.address}` : null}
-            target="_blank"
-            rel="noreferrer"
-            onClick={e => e.stopPropagation()}
-          >
-            <ToolTip disabled={!item.address} label={item.address}>
-              { item.name ? item.name : item.address }
-              { item.address ? <HiOutlineExternalLink/> : null }
-            </ToolTip>
-          </a>
-        )
-
-        if (item.type === 'network') return (
-          <div className='network' key={`item-${i}`}>
-            { item.icon ? <div className='icon' style={{ backgroundImage: `url(${item.icon})` }}></div> : null }
-            { item.name }
-          </div>
-        )
-
-        if (item.type === 'erc721') {
-          const canShowLink = item.network && item.address && item.id
-          return (
-            <a
-              className='erc721'
-              key={`item-${i}`}
-              href={canShowLink ? `#/wallet/nft/${item.network}/${item.address}/${item.id}` : null}
-              target="_blank"
-              rel="noreferrer"
-              onClick={e => e.stopPropagation()}
-            >
-              { item.name }
-              { canShowLink ? <HiOutlineExternalLink/> : null }
-            </a>
-          )
-        }
-
-        return <></>
-      })
-      :
-      (entry)
-  }))
+  const summary = (extendedSummary.map(entry => Array.isArray(entry) ? entry.map((item, i) => parseExtendedSummaryItem(item, i, networkDetails)) : (entry))) // If entry is extended summary parse it
 
   return (
     <div className={isFirstFailing ? 'txnPreview firstFailing' : 'txnPreview'}>
