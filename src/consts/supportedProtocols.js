ef577e6bbf30864c69139cc35d96c1d231cb1f44
Ivo Georgiev <ivo@strem.io>
2022-01-21 11:56:23 +0200

    supportedProtocols updated

--- src/consts/supportedProtocols.js
+++ src/consts/supportedProtocols.js
@@ -22,12 +22,12 @@ const suportedProtocols = [
         network: 'binance-smart-chain',
         protocols: ['pancakeswap'],
         balancesProvider: 'velcro'
-    },
+    }/*,
     {
         network: 'arbitrum',
         protocols: [],
         balancesProvider: 'velcro'
-    }
+    }*/
 ]
 
 export default suportedProtocols
