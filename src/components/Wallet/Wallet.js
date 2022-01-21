3d4ff16bd0e585788b083402f2c5e8394f149616
0xdex18 <iam.digital@mailnova.com>
2022-01-21 11:12:49 +0100

    tx replacement option with relayer nonces
    https://github.com/AmbireTech/ambire-wallet/issues/36

--- src/components/Wallet/Wallet.js
+++ src/components/Wallet/Wallet.js
@@ -79,7 +79,7 @@ export default function Wallet(props) {
     },
     {
       path: '/transactions',
-      component: <Transactions relayerURL={props.relayerURL} selectedAcc={props.selectedAcc} selectedNetwork={props.network} addRequest={props.addRequest} eligibleRequests={props.eligibleRequests} showSendTxns={props.showSendTxns} />
+      component: <Transactions relayerURL={props.relayerURL} selectedAcc={props.selectedAcc} selectedNetwork={props.network} addRequest={props.addRequest} eligibleRequests={props.eligibleRequests} showSendTxns={props.showSendTxns} showSendTxnsForReplacement={props.showSendTxnsForReplacement} />
     },
     {
       path: '/swap',
