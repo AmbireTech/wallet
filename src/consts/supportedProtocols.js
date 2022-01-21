d0a2be0c23c5edf5e553066f5ccf8247ff7a9ab5
Ivo Georgiev <ivo@strem.io>
2022-01-21 19:19:37 +0200

    use Velcro for NFTs

--- src/consts/supportedProtocols.js
+++ src/consts/supportedProtocols.js
@@ -5,7 +5,7 @@ const suportedProtocols = [
         network: 'ethereum',
         protocols: ['1inch', 'aave-v2', 'alchemix', 'badger', 'balancer-v2', 'compound', 'cream', 'curve', 'sushiswap', 'uniswap-v2', 'uniswap-v3', 'nft'],
         balancesProvider: 'velcro',
-        nftsProvider: 'zapper'
+        nftsProvider: 'velcro'
     },
     {
         network: 'polygon',
