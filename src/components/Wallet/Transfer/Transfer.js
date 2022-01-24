9e6d79540d5b08dea0a0f088ff1840ae5ed176b6
Tim <tymmesyde@gmail.com>
2022-01-24 13:36:46 +0100

    refactor(Transfer): show binance address warning for native asset transfer on Polygon and BSC

--- src/components/Wallet/Transfer/Transfer.js
+++ src/components/Wallet/Transfer/Transfer.js
@@ -7,10 +7,11 @@ import { useEffect, useMemo, useState } from 'react'
 import { ethers } from 'ethers'
 import { Interface } from 'ethers/lib/utils'
 import { useToasts } from 'hooks/toasts'
-import { TextInput, NumberInput, Button, Select, Loading, AddressBook, AddressWarning, NoFundsPlaceholder } from 'components/common'
+import { TextInput, NumberInput, Button, Select, Loading, AddressBook, AddressWarning, NoFundsPlaceholder, Checkbox } from 'components/common'
 import { validateSendTransferAddress, validateSendTransferAmount } from 'lib/validations/formValidations'
 import { isValidAddress } from 'lib/address'
 import Addresses from './Addresses/Addresses'
+import { MdInfo } from 'react-icons/md'
 
 const ERC20 = new Interface(require('adex-protocol-eth/abi/ERC20'))
 
@@ -28,6 +29,7 @@ const Transfer = ({ history, portfolio, selectedAcc, selectedNetwork, addRequest
     const [address, setAddress] = useState('')
     const [disabled, setDisabled] = useState(true)
     const [addressConfirmed, setAddressConfirmed] = useState(false)
+    const [binanceAddressConfirmed, setBinanceAddressConfirmed] = useState(false)
     const [newAddress, setNewAddress] = useState('')
     const [validationFormMgs, setValidationFormMgs] = useState({ 
         success: { 
@@ -53,6 +55,8 @@ const Transfer = ({ history, portfolio, selectedAcc, selectedNetwork, addRequest
         const { balanceRaw, decimals } = selectedAsset
         return ethers.utils.formatUnits(balanceRaw, decimals)
     }, [selectedAsset])
+
+    const showBinanceAddressWarning = useMemo(() => Number(tokenAddress) === 0 && ['polygon', 'binance-smart-chain'].includes(selectedNetwork.id), [tokenAddress, selectedNetwork])
 
     const setMaxAmount = () => onAmountChange(maxAmount)
 
@@ -98,6 +102,7 @@ const Transfer = ({ history, portfolio, selectedAcc, selectedNetwork, addRequest
     useEffect(() => {
         setAmount(0)
         setBigNumberHexAmount('')
+        setBinanceAddressConfirmed(false)
     }, [asset])
 
     useEffect(() => {
@@ -120,8 +125,8 @@ const Transfer = ({ history, portfolio, selectedAcc, selectedNetwork, addRequest
             }
         })
 
-        setDisabled(!(isValidRecipientAddress.success && isValidSendTransferAmount.success))
-    }, [address, amount, selectedAcc, selectedAsset, addressConfirmed, isKnownAddress, addToast])
+        setDisabled(!isValidRecipientAddress.success || !isValidSendTransferAmount.success || (showBinanceAddressWarning && !binanceAddressConfirmed))
+    }, [address, amount, selectedAcc, selectedAsset, addressConfirmed, showBinanceAddressWarning, binanceAddressConfirmed, isKnownAddress, addToast])
 
     const amountLabel = <div className="amount-label">Available Amount: <span>{ maxAmount } { selectedAsset?.symbol }</span></div>
 
@@ -173,6 +178,20 @@ const Transfer = ({ history, portfolio, selectedAcc, selectedNetwork, addRequest
                                     onChange={(value) => setAddressConfirmed(value)}
                                     isKnownAddress={isKnownAddress}
                                 />
+                                {
+                                    showBinanceAddressWarning ? 
+                                        <Checkbox 
+                                            id="binance-address-warning"
+                                            label={<span id="binance-address-warning-label">
+                                                I confirm this address is not a Binance address: <br/>Binance does not support ${selectedAsset?.symbol} deposits from smart wallets
+                                                <a href='https://help.ambire.com/hc/en-us/articles/4415473743506-Statement-on-MATIC-BNB-deposits-to-Binance' target='_blank' rel='noreferrer'><MdInfo size={20}/></a>
+                                            </span>}
+                                            checked={binanceAddressConfirmed}
+                                            onChange={({ target }) => setBinanceAddressConfirmed(target.checked)}
+                                        />
+                                        :
+                                        null
+                                }
                                 <Button icon={<AiOutlineSend/>} disabled={disabled} onClick={sendTx}>Send</Button>
                             </div>
                             :
