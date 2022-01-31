7b2c7a4c6fe69431120d9f3ec58386eac43dd177
Ivo Georgiev <ivo@strem.io>
2022-01-31 14:17:50 +0200

    WALLET: SupplyController: formatting

--- contracts/WALLETSupplyController.sol
+++ contracts/WALLETSupplyController.sol
@@ -68,67 +68,67 @@ contract WALLETSupplyController {
 
 	// Rewards distribution
 	bytes32 public lastRoot;
-  mapping (address => uint) public claimed;
-  uint public penaltyBps = 0;
+	mapping (address => uint) public claimed;
+	uint public penaltyBps = 0;
 
-  function setPenaltyBps(uint _penaltyBps) external {
+	function setPenaltyBps(uint _penaltyBps) external {
 		require(hasGovernance[msg.sender], "NOT_GOVERNANCE");
-    require(penaltyBps <= 10000, "BPS_IN_RANGE");
-    penaltyBps = _penaltyBps;
-    // @TODO logs
-  }
+		require(penaltyBps <= 10000, "BPS_IN_RANGE");
+		penaltyBps = _penaltyBps;
+		// @TODO logs
+	}
 
-  function setRoot(bytes32 newRoot) external {
+	function setRoot(bytes32 newRoot) external {
 		require(hasGovernance[msg.sender], "NOT_GOVERNANCE");
 		lastRoot = newRoot;
 		// @TODO: logs?
 	}
 
-  function claimWithRootUpdate(
-    // claim() args
-    address recipient, uint totalRewardInTree, bytes32[] calldata proof, uint toBurnBps,
-    // args for updating the root
-    bytes32 newRoot, bytes calldata signature
-  ) external {
-    address signer = SignatureValidator.recoverAddrImpl(newRoot, signature, false);
-    require(hasGovernance[signer], "NOT_GOVERNANCE");
-    lastRoot = newRoot;
-    claim(recipient, totalRewardInTree, proof, toBurnBps);
-  }
+	function claimWithRootUpdate(
+		// claim() args
+		address recipient, uint totalRewardInTree, bytes32[] calldata proof, uint toBurnBps,
+		// args for updating the root
+		bytes32 newRoot, bytes calldata signature
+	) external {
+		address signer = SignatureValidator.recoverAddrImpl(newRoot, signature, false);
+		require(hasGovernance[signer], "NOT_GOVERNANCE");
+		lastRoot = newRoot;
+		claim(recipient, totalRewardInTree, proof, toBurnBps);
+	}
 
-  // claim() has two modes, either receive the full amount as xWALLET (staked WALLET) or burn some (penaltyBps) and receive the rest immediately in $WALLET
-  // toBurnBps is a safety parameter that serves two purposes:
-  // 1) prevents griefing attacks/frontrunning where governance sets penalties higher before someone's claim() gets mined
-  // 2) ensures that the sender really does have the intention to burn some of their tokens but receive the rest immediatey
-  // set toBurnBps to 0 to receive the tokens as xWALLET, set it to the current penaltyBps to receive immediately
-  // There is an edge case: when penaltyBps is set to 0, you pass 0 to receive everything immediately; this is intended
-  function claim(address recipient, uint totalRewardInTree, bytes32[] memory proof, uint toBurnBps) public {
-    // Check the merkle proof
-    bytes32 leaf = keccak256(abi.encode(recipient, totalRewardInTree));
-    require(MerkleProof.isContained(leaf, proof, lastRoot), "LEAF_NOT_FOUND");
+	// claim() has two modes, either receive the full amount as xWALLET (staked WALLET) or burn some (penaltyBps) and receive the rest immediately in $WALLET
+	// toBurnBps is a safety parameter that serves two purposes:
+	// 1) prevents griefing attacks/frontrunning where governance sets penalties higher before someone's claim() gets mined
+	// 2) ensures that the sender really does have the intention to burn some of their tokens but receive the rest immediatey
+	// set toBurnBps to 0 to receive the tokens as xWALLET, set it to the current penaltyBps to receive immediately
+	// There is an edge case: when penaltyBps is set to 0, you pass 0 to receive everything immediately; this is intended
+	function claim(address recipient, uint totalRewardInTree, bytes32[] memory proof, uint toBurnBps) public {
+		// Check the merkle proof
+		bytes32 leaf = keccak256(abi.encode(recipient, totalRewardInTree));
+		require(MerkleProof.isContained(leaf, proof, lastRoot), "LEAF_NOT_FOUND");
 
-    uint toClaim = totalRewardInTree - claimed[recipient];
-    claimed[recipient] = totalRewardInTree;
+		uint toClaim = totalRewardInTree - claimed[recipient];
+		claimed[recipient] = totalRewardInTree;
 
-    if (toBurnBps == penaltyBps) {
-      // Claiming in $WALLET directly: some tokens get burned immediately, but the rest go to you
-      // @TODO penalty
-      WALLET.mint(recipient, toClaim);
-    } else if (toBurnBps == 0) {
-      // @TODO stake
-      WALLET.mint(recipient, toClaim);
-    } else {
-      revert("INVALID_TOBURNBPS");
-    }
+		if (toBurnBps == penaltyBps) {
+			// Claiming in $WALLET directly: some tokens get burned immediately, but the rest go to you
+			// @TODO penalty
+			WALLET.mint(recipient, toClaim);
+		} else if (toBurnBps == 0) {
+			// @TODO stake
+			WALLET.mint(recipient, toClaim);
+		} else {
+			revert("INVALID_TOBURNBPS");
+		}
 
-    // @TODO: special logs here?
-  }
+		// @TODO: special logs here?
+	}
 
-  // In case funds get stuck
-  function withdraw(IERC20 token, address to, uint256 tokenAmount) external {
-    require(hasGovernance[msg.sender], "NOT_GOVERNANCE");
-    // AUDIT: SafeERC20 or similar not needed; this is a trusted (governance only) method that doesn't modify internal accounting
-    // so sucess/fail does not matter
-    token.transfer(to, tokenAmount);
-  }
+	// In case funds get stuck
+	function withdraw(IERC20 token, address to, uint256 tokenAmount) external {
+		require(hasGovernance[msg.sender], "NOT_GOVERNANCE");
+		// AUDIT: SafeERC20 or similar not needed; this is a trusted (governance only) method that doesn't modify internal accounting
+		// so sucess/fail does not matter
+		token.transfer(to, tokenAmount);
+	}
 }
