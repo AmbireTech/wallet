9ee2b3773f49318974bf45b47b507e88591e9511
Tim <tymmesyde@gmail.com>
2022-02-03 13:48:08 +0100

    refactor: display claimable wallet tokens on Rewards button

--- src/components/Modals/WalletTokenModal/WalletTokenModal.js
+++ src/components/Modals/WalletTokenModal/WalletTokenModal.js
@@ -1,16 +1,9 @@
 import './WalletTokenModal.scss'
 
-import { useEffect, useMemo, useState, useCallback } from 'react'
+import { useMemo } from 'react'
 import { Button, Modal, ToolTip } from 'components/common'
 import { MdOutlineClose } from 'react-icons/md'
-import { useModals, useRelayerData } from 'hooks'
-import { Contract } from 'ethers'
-import { Interface } from 'ethers/lib/utils'
-import { getProvider } from 'lib/provider'
-
-import WALLETVestings from 'consts/WALLETVestings'
-import WALLETInitialClaimableRewards from 'consts/WALLETInitialClaimableRewards'
-import WALLETSupplyControllerABI from 'consts/WALLETSupplyControllerABI'
+import { useModals } from 'hooks'
 
 const multiplierBadges = [
     {
@@ -56,87 +49,19 @@ const MultiplierBadges = ({ rewards }) => {
     </div>)
 }
 
-const useWalletTokenInfo = (relayerURL) => {
-    const [cacheBreak, setCacheBreak] = useState(() => Date.now())
-    useEffect(() => {
-        if ((Date.now() - cacheBreak) > 5000) setCacheBreak(Date.now())
-        const intvl = setTimeout(() => setCacheBreak(Date.now()), 30000)
-        return () => clearTimeout(intvl)
-    }, [cacheBreak])
-
-    const tokenInfoUrl = (relayerURL) ? `${relayerURL}/wallet-token/info?cacheBreak=${cacheBreak}` : null
-    return useRelayerData(tokenInfoUrl)
-}
-
-const supplyControllerAddress = '0x94b668337ce8299272ca3cb0c70f3d786a5b6ce5'
-const supplyControllerInterface = new Interface(WALLETSupplyControllerABI)
-const WalletTokenModal = ({ relayerURL, rewards, account, network, addRequest }) => {
+const WalletTokenModal = ({ claimableWalletToken, rewards }) => {
     const { hideModal } = useModals()
 
-    const walletTokenInfo = useWalletTokenInfo(relayerURL)
-
-    const provider = useMemo(() => getProvider('ethereum'), [])
-    const supplyController = useMemo(() => new Contract(supplyControllerAddress, WALLETSupplyControllerABI, provider), [provider])
-    const initialClaimableEntry = WALLETInitialClaimableRewards.find(x => x.addr === account.id)
-    const initialClaimable = initialClaimableEntry ? initialClaimableEntry.totalClaimable : 0
-    const vestingEntry = WALLETVestings.find(x => x.addr === account.id)
-
-    const [currentClaimStatus, setCurrentClaimStatus] = useState({ loading: true, claimed: 0, mintableVesting: 0, error: null })
-    useEffect(() => {
-        (async () => {
-            const toNum = x => parseInt(x.toString()) / 1e18
-            const [mintableVesting, claimed] = await Promise.all([
-                vestingEntry ? await supplyController.mintableVesting(vestingEntry.addr, vestingEntry.end, vestingEntry.rate).then(toNum) : null,
-                initialClaimableEntry ? await supplyController.claimed(initialClaimableEntry.addr).then(toNum) : null
-            ])
-            return { mintableVesting, claimed }
-        })()
-            .then(status => setCurrentClaimStatus(status))
-            .catch(e => {
-                console.error('getting claim status', e)
-                setCurrentClaimStatus({ error: e.message || e })
-            })
-    }, [supplyController, vestingEntry, initialClaimableEntry])
-
-    const claimableNow = initialClaimable - currentClaimStatus.claimed
-    const disabledReason = network.id !== 'ethereum' ? 'Switch to Ethereum to claim' : (
-        currentClaimStatus.error ? `Claim status error: ${currentClaimStatus.error}` : null
-    )
-    const claimDisabledReason = claimableNow === 0 ? 'No rewards are claimable' : null
-    const claimEarlyRewards = useCallback(() => {
-        addRequest({
-            id: 'claim_'+Date.now(),
-            chainId: network.chainId,
-            type: 'eth_sendTransaction',
-            account: account.id,
-            txn: {
-                to: supplyControllerAddress,
-                value: '0x0',
-                data: supplyControllerInterface.encodeFunctionData('claim', [
-                    initialClaimableEntry.addr,
-                    initialClaimableEntry.totalClaimableBN,
-                    initialClaimableEntry.proof,
-                    0, // penalty bps, at the moment we run with 0; it's a safety feature to hardcode it
-                    '0x0000000000000000000000000000000000000000' // staking addr, no need to pass this
-                ])
-            }
-        })    
-    }, [initialClaimableEntry, network.chainId, account.id, addRequest])
-    const claimVesting = useCallback(() => {
-        addRequest({
-            id: 'claimVesting_'+Date.now(),
-            chainId: network.chainId,
-            account: account.id,
-            type: 'eth_sendTransaction',
-            txn: {
-                to: supplyControllerAddress,
-                value: '0x0',
-                data: supplyControllerInterface.encodeFunctionData('mintVesting', [vestingEntry.addr, vestingEntry.end, vestingEntry.rate])
-            }
-        })    
-    }, [vestingEntry, network.chainId, account.id, addRequest])
-
-    const claimableWalletTokenUsd = !walletTokenInfo.isLoading && !currentClaimStatus.loading && claimableNow ? (walletTokenInfo.data?.usdPrice * claimableNow).toFixed(2) : '...'
+    const {
+        vestingEntry,
+        currentClaimStatus,
+        claimableNow,
+        claimableNowUsd,
+        disabledReason,
+        claimDisabledReason,
+        claimEarlyRewards,
+        claimVesting
+    } = claimableWalletToken
 
     const modalButtons = <>
         <Button clear icon={<MdOutlineClose/>} onClick={() => hideModal()}>Close</Button>
@@ -189,7 +114,7 @@ const WalletTokenModal = ({ relayerURL, rewards, account, network, addRequest })
                         }</span></div>
                         <div className="amount usd">
                             <span className="secondary-accent">$</span>
-                            { claimableWalletTokenUsd }
+                            { claimableNowUsd }
                         </div>
                     </div>
                 </div>
