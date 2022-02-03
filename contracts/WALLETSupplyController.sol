c87c043414bc1d684f2aa72f90ca7636e7964620
Ivo Georgiev <ivo@strem.io>
2022-02-03 14:44:59 +0200

    WALLETSupplyController: disallow custom recipient

--- contracts/WALLETSupplyController.sol
+++ contracts/WALLETSupplyController.sol
@@ -112,14 +112,14 @@ contract WALLETSupplyController {
 
 	function claimWithRootUpdate(
 		// claim() args
-		address recipient, uint totalRewardInTree, bytes32[] calldata proof, uint toBurnBps, IStakingPool stakingPool,
+		uint totalRewardInTree, bytes32[] calldata proof, uint toBurnBps, IStakingPool stakingPool,
 		// args for updating the root
 		bytes32 newRoot, bytes calldata signature
 	) external {
 		address signer = SignatureValidator.recoverAddrImpl(newRoot, signature, false);
 		require(hasGovernance[signer], "NOT_GOVERNANCE");
 		lastRoot = newRoot;
-		claim(recipient, totalRewardInTree, proof, toBurnBps, stakingPool);
+		claim(totalRewardInTree, proof, toBurnBps, stakingPool);
 	}
 
 	// claim() has two modes, either receive the full amount as xWALLET (staked WALLET) or burn some (penaltyBps) and receive the rest immediately in $WALLET
@@ -128,7 +128,9 @@ contract WALLETSupplyController {
 	// 2) ensures that the sender really does have the intention to burn some of their tokens but receive the rest immediatey
 	// set toBurnBps to 0 to receive the tokens as xWALLET, set it to the current penaltyBps to receive immediately
 	// There is an edge case: when penaltyBps is set to 0, you pass 0 to receive everything immediately; this is intended
-	function claim(address recipient, uint totalRewardInTree, bytes32[] memory proof, uint toBurnBps, IStakingPool stakingPool) public {
+	function claim(uint totalRewardInTree, bytes32[] memory proof, uint toBurnBps, IStakingPool stakingPool) public {
+		address recipient = msg.sender;
+
 		require(totalRewardInTree <= MAX_CLAIM_NODE, "MAX_CLAIM_NODE");
 		require(lastRoot != bytes32(0), "EMPTY_ROOT");
 
