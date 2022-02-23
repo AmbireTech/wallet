4cbebe8c0f09e1c0071f3d430db4cf6deb2c7991
IvoPaunov <ivo.paunov@gmail.com>
2022-02-23 18:21:43 +0200

    fix formatFloatTokenAmount

--- src/lib/formatters.js
+++ src/lib/formatters.js
@@ -1,10 +1,17 @@
 export function formatFloatTokenAmount(amount, useGrouping = true, maximumFractionDigits = 18) {
     if (isNaN(amount) || isNaN(parseFloat(amount)) || !(typeof amount === 'number' || typeof amount === 'string')) return amount
-    return ((typeof amount === 'number') ? amount : parseFloat(amount))
-        .toLocaleString(undefined,
-            {
-                useGrouping,
-                maximumFractionDigits: (!maximumFractionDigits || (maximumFractionDigits > 18)) ? 18 : maximumFractionDigits,
-                minimumFractionDigits: 2
-            })
+
+    try {
+        const minimumFractionDigits = 2
+        return ((typeof amount === 'number') ? amount : parseFloat(amount))
+            .toLocaleString(undefined,
+                {
+                    useGrouping,
+                    maximumFractionDigits: Math.max(minimumFractionDigits, maximumFractionDigits),
+                    minimumFractionDigits
+                })
+    } catch (err) {
+        console.error(err)
+        return amount
+    }
 }
