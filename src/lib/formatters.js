bb9d6d9a47fb87e204533cd7fc83a444a3f119ea
IvoPaunov <ivo.paunov@gmail.com>
2022-02-23 11:31:00 +0200

    fix formatFloatTokenAmount

--- src/lib/formatters.js
+++ src/lib/formatters.js
@@ -1,3 +1,4 @@
 export function formatFloatTokenAmount(amount, useGrouping = true, maximumSignificantDigits = 4) {
+    if (isNaN(amount)) return amount
     return amount.toLocaleString('fullwide', { useGrouping, maximumSignificantDigits })
 }   
