bcbb0acaa6ea1dab197e663b3bf28dd2356066c7
IvoPaunov <ivo.paunov@gmail.com>
2022-02-23 12:53:23 +0200

    update formatFloatTokenAmount

--- src/lib/formatters.js
+++ src/lib/formatters.js
@@ -1,4 +1,4 @@
 export function formatFloatTokenAmount(amount, useGrouping = true, maximumSignificantDigits = 4) {
-    if (isNaN(amount) || isNaN(parseFloat(amount))) return amount
-    return parseFloat(amount).toLocaleString('fullwide', { useGrouping, maximumSignificantDigits })
+    if (isNaN(amount) || isNaN(parseFloat(amount)) || !(typeof amount === 'number' || typeof amount === 'string')) return amount
+    return ((typeof amount === 'number') ? amount : parseFloat(amount)).toLocaleString('fullwide', { useGrouping, maximumSignificantDigits })
 }   
