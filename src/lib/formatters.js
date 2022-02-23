fb55db1d04807844fd1486d56f706eef0843cfc8
IvoPaunov <ivo.paunov@gmail.com>
2022-02-23 12:23:21 +0200

    format txn preview token amount

--- src/lib/formatters.js
+++ src/lib/formatters.js
@@ -1,4 +1,4 @@
 export function formatFloatTokenAmount(amount, useGrouping = true, maximumSignificantDigits = 4) {
-    if (isNaN(amount)) return amount
-    return amount.toLocaleString('fullwide', { useGrouping, maximumSignificantDigits })
+    if (isNaN(amount) || isNaN(parseFloat(amount))) return amount
+    return parseFloat(amount).toLocaleString('fullwide', { useGrouping, maximumSignificantDigits })
 }   
