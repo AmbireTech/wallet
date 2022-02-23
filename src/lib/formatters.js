8f392f6dbdceda54c65bb814d73a9fe3bde43cfd
IvoPaunov <ivo.paunov@gmail.com>
2022-02-23 17:52:54 +0200

    fix polygon rpc and amounts formatting

--- src/lib/formatters.js
+++ src/lib/formatters.js
@@ -1,4 +1,10 @@
 export function formatFloatTokenAmount(amount, useGrouping = true, maximumFractionDigits = 18) {
     if (isNaN(amount) || isNaN(parseFloat(amount)) || !(typeof amount === 'number' || typeof amount === 'string')) return amount
-    return ((typeof amount === 'number') ? amount : parseFloat(amount)).toLocaleString(undefined, { useGrouping, maximumFractionDigits, minimumFractionDigits: 2 })
+    return ((typeof amount === 'number') ? amount : parseFloat(amount))
+        .toLocaleString(undefined,
+            {
+                useGrouping,
+                maximumFractionDigits: (!maximumFractionDigits || (maximumFractionDigits > 18)) ? 18 : maximumFractionDigits,
+                minimumFractionDigits: 2
+            })
 }
