fb97c4f07f4b98e7f7d652439f8fc8cace6d4291
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2022-03-04 15:45:20 +0200

    Fixed pending to withdraw amount, revert remaining time in wallet card

--- src/components/Modals/WalletEarnDetailsModal/WalletEarnDetailsModal.js
+++ src/components/Modals/WalletEarnDetailsModal/WalletEarnDetailsModal.js
@@ -4,19 +4,21 @@ import { MdClose } from 'react-icons/md'
 import { useModals } from 'hooks'
 import { Modal, Button } from 'components/common'
 import { ToolTip } from 'components/common' 
-import { useWalletEarnDetails } from 'hooks'
 
-const WalletEarnDetailsModal = ({ apy, accountId = null, title = 'Details' }) => {
+const WalletEarnDetailsModal = ({ 
+    apy, 
+    title = 'Details',
+    balance, 
+    poolShare, 
+    allTimeRewards, 
+    totalDeposit, 
+    totalWithdraws, 
+    pendingToUnlock, 
+    readyToWithdraw, 
+    remainingTime }) => {
     const { hideModal } = useModals()
 
-    const msToDaysHours = ms => {
-        const day = 24 * 60 * 60 * 1000
-        const days = Math.floor(ms / day)
-        const hours = Math.floor((ms % day) / (60 * 60 * 1000))
-        return days < 1 ? `${hours} hours` : `${days} days`
-    }
     
-    const { balance, poolShare, allTimeRewards, totalDeposit, totalWithdraws, pendingToUnlock, readyToWithdraw, remainingTime } = useWalletEarnDetails({accountId})
     const buttons = (<Button clear small icon={<MdClose />} onClick={hideModal}>Close</Button>)
    
     return (
@@ -27,25 +29,25 @@ const WalletEarnDetailsModal = ({ apy, accountId = null, title = 'Details' }) =>
             <div className="wrapper odd-rows-bg">
                 <div>Balance</div>
                 <div>
-                    <ToolTip label="* Warning: Because you have sent/received staking share token (Loyalty Pool: WALLET-LOYALTY), the reward shown may be less than your actual reward.">
-                        {balance} WALLET* (Pool share: {(poolShare * 100).toFixed(2)}%)
+                    <ToolTip label="* Warning: Because you have sent/received staking share token, the reward shown may be less than your actual reward.">
+                        {parseFloat(balance).toFixed(4)} WALLET* (Pool share: {(poolShare * 100).toFixed(2)}%)
                     </ToolTip>
                 </div>
             </div>
             <div className="wrapper">
-                <div>All time rewards</div><div>{allTimeRewards} WALLET</div>
+                <div>All time rewards</div><div>{parseFloat(allTimeRewards).toFixed(4)} WALLET</div>
             </div>
             <div className="wrapper odd-rows-bg">
-                <div>Total deposit</div><div>{totalDeposit} WALLET</div>
+                <div>Total deposit</div><div>{parseFloat(totalDeposit).toFixed(4)} WALLET</div>
             </div>
             <div className="wrapper">
-                <div>Total withdraws</div><div>{totalWithdraws} WALLET</div>
+                <div>Total withdraws</div><div>{parseFloat(totalWithdraws).toFixed(4)} WALLET</div>
             </div>
             <div className="wrapper odd-rows-bg">
-                <div>Pending to unlock</div><div>{remainingTime ? `${msToDaysHours(remainingTime)} until ${pendingToUnlock} WALLET becomes available for withdraw.` : 'N/A'}</div>
+                <div>Pending to unlock</div><div>{remainingTime ? `${remainingTime} until ${parseFloat(pendingToUnlock).toFixed(4)} WALLET becomes available for withdraw.` : '0.0 WALLET'}</div>
             </div>
             <div className="wrapper">
-                <div>Ready to withdraw</div><div>{readyToWithdraw} WALLET</div>
+                <div>Ready to withdraw</div><div>{parseFloat(readyToWithdraw).toFixed(4)} WALLET</div>
             </div>
         </Modal>
     )
