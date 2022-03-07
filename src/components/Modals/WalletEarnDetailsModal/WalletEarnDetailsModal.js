276c1c9af528ecd7b869b2c69b329ad274dfd0ec
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2022-03-07 17:11:16 +0200

    Removed walletEardDetails hook from wallet card and added it in walletEarnDetailsModal, added loader in the modal, fixed disabling of the withdraw btn in Cards

--- src/components/Modals/WalletEarnDetailsModal/WalletEarnDetailsModal.js
+++ src/components/Modals/WalletEarnDetailsModal/WalletEarnDetailsModal.js
@@ -4,51 +4,63 @@ import { MdClose } from 'react-icons/md'
 import { useModals } from 'hooks'
 import { Modal, Button } from 'components/common'
 import { ToolTip } from 'components/common' 
+import { useWalletEarnDetails } from 'hooks'
+import { Loading } from 'components/common'
 
-const WalletEarnDetailsModal = ({ 
-    apy, 
-    title = 'Details',
-    balance, 
-    poolShare, 
-    allTimeRewards, 
-    totalDeposit, 
-    totalWithdraws, 
-    pendingToUnlock, 
-    readyToWithdraw, 
-    remainingTime }) => {
+const WalletEarnDetailsModal = ({ title = 'Details', apy, accountId, msToDaysHours }) => {
     const { hideModal } = useModals()
-
     
+    const { 
+        details,
+        isLoading
+        } = useWalletEarnDetails({accountId})
+
+    const { balance, 
+        poolShare, 
+        allTimeRewards, 
+        totalDeposit, 
+        totalWithdraws, 
+        pendingToUnlock, 
+        readyToWithdraw, 
+        remainingTime } = details
     const buttons = (<Button clear small icon={<MdClose />} onClick={hideModal}>Close</Button>)
    
     return (
         <Modal id="wallet-more-details-modal" title={title} buttons={buttons}>
-            <div className="wrapper">
-                <div>Annual Percentage Yield (APY)</div><div>{apy}%</div>
-            </div>
-            <div className="wrapper odd-rows-bg">
-                <div>Balance</div>
-                <div>
-                    <ToolTip label="* Warning: Because you have sent/received staking share token (xWallet pool), the reward shown may be less than your actual reward.">
-                        {parseFloat(balance).toFixed(4)} WALLET* (Pool share: {(poolShare * 100).toFixed(2)}%)
-                    </ToolTip>
+           {isLoading ? (
+           <>
+                <div className="wrapper">
+                    <div>Annual Percentage Yield (APY)</div><div>{apy}%</div>
                 </div>
-            </div>
-            <div className="wrapper">
-                <div>All time rewards</div><div>{parseFloat(allTimeRewards).toFixed(4)} WALLET</div>
-            </div>
-            <div className="wrapper odd-rows-bg">
-                <div>Total deposit</div><div>{parseFloat(totalDeposit).toFixed(4)} WALLET</div>
-            </div>
-            <div className="wrapper">
-                <div>Total withdraws</div><div>{parseFloat(totalWithdraws).toFixed(4)} WALLET</div>
-            </div>
-            <div className="wrapper odd-rows-bg">
-                <div>Pending to unlock</div><div>{remainingTime ? `${remainingTime} until ${parseFloat(pendingToUnlock).toFixed(4)} WALLET becomes available for withdraw.` : '0.0 WALLET'}</div>
-            </div>
-            <div className="wrapper">
-                <div>Ready to withdraw</div><div>{parseFloat(readyToWithdraw).toFixed(4)} WALLET</div>
-            </div>
+                <div className="wrapper odd-rows-bg">
+                    <div>Balance</div>
+                    <div>
+                        <ToolTip label="* Warning: Because you have sent/received staking share token (xWallet pool), the reward shown may be less than your actual reward.">
+                            {parseFloat(balance).toFixed(4)} WALLET* (Pool share: {(poolShare * 100).toFixed(2)}%)
+                        </ToolTip>
+                    </div>
+                </div>
+                <div className="wrapper">
+                    <div>All time rewards</div><div>{parseFloat(allTimeRewards).toFixed(4)} WALLET</div>
+                </div>
+                <div className="wrapper odd-rows-bg">
+                    <div>Total deposit</div><div>{parseFloat(totalDeposit).toFixed(4)} WALLET</div>
+                </div>
+                <div className="wrapper">
+                    <div>Total withdraws</div><div>{parseFloat(totalWithdraws).toFixed(4)} WALLET</div>
+                </div>
+                <div className="wrapper odd-rows-bg">
+                    <div>Pending to unlock</div>
+                    <div>
+                        {remainingTime > 0 
+                            ? `${msToDaysHours(remainingTime)} until ${parseFloat(pendingToUnlock).toFixed(4)} WALLET becomes available for withdraw.` 
+                            : 'No pending unlocks'}
+                    </div>
+                </div>
+                <div className="wrapper">
+                    <div>Ready to withdraw</div><div>{parseFloat(readyToWithdraw).toFixed(4)} WALLET</div>
+                </div>
+            </>): <div className='loading-wrapper'><Loading/></div>}
         </Modal>
     )
 }
