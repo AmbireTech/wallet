397d82a9be60d69c9d6449b02f318627fed944c6
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2022-03-08 11:30:10 +0200

    fixed filter checks

--- src/hooks/portfolio.js
+++ src/hooks/portfolio.js
@@ -78,7 +78,7 @@ export default function usePortfolio({ currentNetwork, account, useStorage }) {
 
     const [tokensByNetworks, setTokensByNetworks] = useState([])
     // Added unsupported networks (fantom and moonbeam) as default values with empty arrays to prevent crashes
-    const [otherProtocolsByNetworks, setOtherProtocolsByNetworks] = useState(supportedProtocols.filter(item => !item.protocols.length))
+    const [otherProtocolsByNetworks, setOtherProtocolsByNetworks] = useState(supportedProtocols.filter(item => !item.protocols || !item.protocols.length))
 
     const [balance, setBalance] = useState({
         total: {
