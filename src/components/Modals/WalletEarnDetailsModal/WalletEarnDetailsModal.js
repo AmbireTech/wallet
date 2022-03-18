b3ec69c4cbbeffd073017acbfbaeae00cf6e028d
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2022-03-18 12:32:28 +0200

    fixed walletEarnDetailsMoidal token label

--- src/components/Modals/WalletEarnDetailsModal/WalletEarnDetailsModal.js
+++ src/components/Modals/WalletEarnDetailsModal/WalletEarnDetailsModal.js
@@ -7,7 +7,7 @@ import { Modal, Button } from 'components/common'
 import { useWalletEarnDetails } from 'hooks'
 import { Loading } from 'components/common'
 
-const WalletEarnDetailsModal = ({ title = 'Details', apy, accountId, msToDaysHours, addresses }) => {
+const WalletEarnDetailsModal = ({ title = 'Details', apy, accountId, msToDaysHours, addresses, tokenLabel }) => {
     const { hideModal } = useModals()
     
     const { 
@@ -37,24 +37,24 @@ const WalletEarnDetailsModal = ({ title = 'Details', apy, accountId, msToDaysHou
                     <div>
                         {/* The tool tip will be activated after improvements of handling transactions */}
                         {/* <ToolTip label="* Warning: Because you have sent/received staking share token (xWallet pool), the reward shown may be less than your actual reward."> */}
-                            {parseFloat(balance).toFixed(4)} WALLET (Pool share: {(poolShare * 100).toFixed(2)}%)
+                            {parseFloat(balance).toFixed(4)} {tokenLabel} (Pool share: {(poolShare * 100).toFixed(2)}%)
                         {/* </ToolTip> */}
                     </div>
                 </div>
                 <div className="wrapper">
-                    <div>All time rewards</div><div>{parseFloat(allTimeRewards).toFixed(4)} WALLET</div>
+                    <div>All time rewards</div><div>{parseFloat(allTimeRewards).toFixed(4)} {tokenLabel}</div>
                 </div>
                 <div className="wrapper odd-rows-bg">
-                    <div>Total deposit</div><div>{parseFloat(totalDeposit).toFixed(4)} WALLET</div>
+                    <div>Total deposit</div><div>{parseFloat(totalDeposit).toFixed(4)} {tokenLabel}</div>
                 </div>
                 <div className="wrapper">
-                    <div>Total withdraws</div><div>{parseFloat(totalWithdraws).toFixed(4)} WALLET</div>
+                    <div>Total withdraws</div><div>{parseFloat(totalWithdraws).toFixed(4)} {tokenLabel}</div>
                 </div>
                 <div className="wrapper odd-rows-bg">
                     <div>Total pending to unlock</div>
                     <div>
                         {remainingTime > 0 
-                            ? `${msToDaysHours(remainingTime)} until ${parseFloat(pendingToUnlock).toFixed(4)} WALLET becomes available for withdraw.` 
+                            ? `${msToDaysHours(remainingTime)} until ${parseFloat(pendingToUnlock).toFixed(4)} ${tokenLabel} becomes available for withdraw.` 
                             : 'No pending unlocks'}
                     </div>
                 </div>
@@ -62,7 +62,7 @@ const WalletEarnDetailsModal = ({ title = 'Details', apy, accountId, msToDaysHou
                     <div>Ready to withdraw</div>
                     <div>
                         {parseFloat(readyToWithdraw) > 0 
-                        ? `${parseFloat(readyToWithdraw).toFixed(4)} WALLET`
+                        ? `${parseFloat(readyToWithdraw).toFixed(4)} ${tokenLabel}`
                         : 'No amount for withdrawing'}
                     </div>
                 </div>
