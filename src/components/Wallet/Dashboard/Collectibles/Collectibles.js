0e03fe4f4f8f695177e8b02faca4f4fd237f0206
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2022-03-22 11:41:02 +0200

    fixed handle-uri func in collectibles

--- src/components/Wallet/Dashboard/Collectibles/Collectibles.js
+++ src/components/Wallet/Dashboard/Collectibles/Collectibles.js
@@ -8,6 +8,7 @@ const Collectibles = ({ portfolio, isPrivateMode }) => {
     const handleUri = uri => {
         uri = uri.startsWith('data:application/json') ? uri.replace('data:application/json;utf8,', '') : uri
 
+        if (uri.split('/')[0] === 'data:image') return uri
         if (uri.startsWith('ipfs://')) return uri.replace(/ipfs:\/\/ipfs\/|ipfs:\/\//g, 'https://ipfs.io/ipfs/')
         if (uri.split('/')[2].endsWith('mypinata.cloud')) return 'https://ipfs.io/ipfs/' + uri.split('/').slice(4).join('/')
         
