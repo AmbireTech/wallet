b11d8126c440375694c4bcc3a6b315ad3efcfae0
Ivo Georgiev <ivo@strem.io>
2022-03-29 18:09:53 +0300

    UX improvement: show the total pending to be claimed rewards, nto total claimable; otherwise users do not know thye are receiving rewards

--- src/components/Wallet/TopBar/WalletTokenButton/WalletTokenButton.js
+++ src/components/Wallet/TopBar/WalletTokenButton/WalletTokenButton.js
@@ -6,10 +6,10 @@ import useClaimableWalletToken from "./useClaimableWalletToken";
 
 const WalletTokenButton = ({ rewardsData, account = {}, network, hidePrivateValue, addRequest }) => {
     const claimableWalletToken = useClaimableWalletToken({ account, network, addRequest })
-    const { claimableNow, currentClaimStatus } = claimableWalletToken
-    const claimableTokensTotal = currentClaimStatus && !currentClaimStatus.loading ?
-        (claimableNow + (currentClaimStatus.mintableVesting || 0)).toFixed(3) : '...'
-    
+    const { currentClaimStatus } = claimableWalletToken
+    const totalLifetimeRewards = rewardsData.data?.rewards?.map(x => x.rewards[account.id] || 0).reduce((a, b) => a + b, 0)
+    const pendingTokensTotal = currentClaimStatus && !currentClaimStatus.loading ?
+        ((totalLifetimeRewards || 0) - (currentClaimStatus.claimed || 0) + (currentClaimStatus.mintableVesting || 0)).toFixed(3) : '...'
     const [rewards, setRewards] = useState({})
     const { isLoading, data, errMsg } = rewardsData
     
@@ -36,7 +36,7 @@ const WalletTokenButton = ({ rewardsData, account = {}, network, hidePrivateValu
                 <Button small border disabled onClick={showWalletTokenModal}>Unavailable</Button>
             </ToolTip>
             :
-            <Button small border disabled={isLoading} onClick={showWalletTokenModal}>{ isLoading ? '...' : hidePrivateValue(claimableTokensTotal) } WALLET</Button>
+            <Button small border disabled={isLoading} onClick={showWalletTokenModal}>{ isLoading ? '...' : hidePrivateValue(pendingTokensTotal) } WALLET</Button>
     )
 }
 
