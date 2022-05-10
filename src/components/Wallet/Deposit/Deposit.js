c283f8689182e4f945d8d6b7a6b79e0c055d0c49
Jordan Enev <jordantoen@gmail.com>
2022-05-10 12:04:41 +0300

    Fix: Assets Migrations feature uses the whole `network` object.

--- src/components/Wallet/Deposit/Deposit.js
+++ src/components/Wallet/Deposit/Deposit.js
@@ -11,7 +11,7 @@ import networks from 'consts/networks'
 import AssetsMigrationBanner from 'components/Wallet/AssetsMigration/AssetsMigrationBanner'
 
 export default function Deposit({ selectedAcc, selectedNetwork, accounts, addRequest, relayerURL, portfolio }) {
-    const networkDetails = networks.find(({ id }) => id === selectedNetwork)
+    const networkDetails = networks.find(({ id }) => id === selectedNetwork.id)
     const [qrCodeUrl, setQrCodeUrl] = useState('')
 
     const generateQRCode = useCallback(() => {
