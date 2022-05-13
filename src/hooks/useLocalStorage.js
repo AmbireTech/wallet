60ccbb132ee012c4591831d8ddc8f0537ed36476
Kaloyan Kosev <superkalo@devlabs.bg>
2022-05-13 16:11:34 +0300

    Change: migrate the useStorage hook from the local to the shared one

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,4 +1,4 @@
-import useStorage from "./useStorage";
+import useStorage from 'common/src/hooks/useStorage'
 
 export default function useLocalStorage({ key, defaultValue, isStringStorage, setInit }) {
   return useStorage({ storage: localStorage, key, defaultValue, isStringStorage, setInit })
