e72fd022ae534eea03d368c678394a763015799e
Gergana Mihaylova <gergana@devlabs.bg>
2022-05-13 12:45:44 +0300

    WIP: recovery lock send tx

--- src/components/Wallet/Security/PendingRecoveryNotice/PendingRecoveryNotice.js
+++ src/components/Wallet/Security/PendingRecoveryNotice/PendingRecoveryNotice.js
@@ -1,7 +1,11 @@
 import { MdOutlineWarningAmber } from "react-icons/md"
 import buildRecoveryBundle from 'lib/recoveryBundle'
+import { Bundle } from 'adex-protocol-eth'
 
-const PendingRecoveryNotice = ({ recoveryLock, showSendTxns, selectedAccount, selectedNetwork }) => {
+// 10% in geth and most EVM chain RPCs; relayer wants 12%
+const RBF_THRESHOLD = 1.14
+
+const PendingRecoveryNotice = ({ recoveryLock, showSendTxns, selectedAccount, selectedNetwork, relayerURL }) => {
     const isAlreadyInitiated = recoveryLock && recoveryLock.status !== 'requestedButNotInitiated'
     const createRecoveryRequest = async () => {
         if (isAlreadyInitiated) return
@@ -13,7 +17,44 @@ const PendingRecoveryNotice = ({ recoveryLock, showSendTxns, selectedAccount, se
         )
         showSendTxns(bundle)
     }
+    const bundle = recoveryLock && new Bundle({ ...recoveryLock, ...selectedAccount, network: selectedNetwork.id })
+    console.log('bundle', bundle, selectedAccount)
+
+    const mapToBundle = (relayerBundle, extra = {}) => (new Bundle({
+        ...relayerBundle,
+        nonce: relayerBundle.nonce.num,
+        gasLimit: null,
+        replacesTxId: recoveryLock.txHash,
+        // Instruct the relayer to abide by this minimum fee in USD per gas, to ensure we are truly replacing the txn
+        minFeeInUSDPerGas: relayerBundle.feeInUSDPerGas * RBF_THRESHOLD,
+        ...extra
+    }))
+    const cancelByReplacing = relayerBundle => showSendTxns(mapToBundle(relayerBundle, {
+     txns: [[selectedAccount.id, '0x0', '0x']],
+    }))
+    const cancel = () => {
+        // debugger
+        // @TODO relayerless
+        bundle.cancel({ relayerURL, fetch })
+          .then(({ success, message }) => {
+            if (!success) {
+              if (message.includes('not possible to cancel')) {
+                // addToast('Transaction already picked up by the network, you will need to pay a fee to replace it with a cancellation transaction.')
+              } else {
+                // addToast(`Not possible to cancel: ${message}, you will need to pay a fee to replace it with a cancellation transaction.`)
+              }
+              cancelByReplacing(bundle)
+            } else {
+            //   addToast('Transaction cancelled successfully')
+            }
+          })
+          .catch(e => {
+            console.error(e)
+            // cancelByReplacing(bundle)
+          })
+      }
     const recoveryLockStatus = recoveryLock ? recoveryLock.status : 'requestedButNotInitiated'
+    console.log({recoveryLock, recoveryLockStatus})
     const remainingTime = seconds => {
         if (seconds > 86400) return `${Math.ceil(seconds / 86400)} days`
         else return `${Math.ceil(seconds/1440)} hours`
@@ -30,7 +71,7 @@ const PendingRecoveryNotice = ({ recoveryLock, showSendTxns, selectedAccount, se
                 recoveryLockStatus === 'initiationTxnPending' ?
                     <>Initiation transaction is currently pending. Once mined, you will need to wait {remainingTime(recoveryLock.timelock)} for the reset to be done on {selectedNetwork.name}.</> :
                 recoveryLockStatus === 'waitingTimelock' ?
-                    <>Password reset on {selectedNetwork.name} is currently pending. {remainingTime(recoveryLock.remaining)} remaining.</> :
+                    <>Password reset on {selectedNetwork.name} is currently pending. {remainingTime(recoveryLock.remaining)} remaining. <div onClick={cancel}>Cancel</div></> :
                 recoveryLockStatus === 'ready' ?
                     <>Password reset on {selectedNetwork.name} is now complete! You can start signing transactions with your new password!</> :
                 recoveryLockStatus === 'failed' ?
