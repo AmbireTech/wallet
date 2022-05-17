e176be432d23c5ad949123d8dc4694da257b68da
0xdex18 <iam.digital@mailnova.com>
2022-05-17 13:56:11 +0200

    draft: typedSignatureData v1

--- src/App.js
+++ src/App.js
@@ -150,7 +150,7 @@ function AppInner() {
 
   // Network shouldn't matter here
   const everythingToSign = useMemo(() => requests
-    .filter(({ type, account }) => (type === 'personal_sign' || type === 'eth_sign' || type === 'eth_signTypedData_v4')
+    .filter(({ type, account }) => (type === 'personal_sign' || type === 'eth_sign' || type === 'eth_signTypedData_v4' || type === 'eth_signTypedData')
       && account === selectedAcc
     ), [requests, selectedAcc])
 
