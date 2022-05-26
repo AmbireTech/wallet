16a6041b69e7b73db9bfad833ce874fdcfbb9dae
Gergana Mihaylova <gergana@devlabs.bg>
2022-05-26 12:25:33 +0300

    Fix: simplify logic and encapsulate

--- src/hooks/utmTracking.js
+++ src/hooks/utmTracking.js
@@ -1,36 +1,48 @@
-import { useCallback, useEffect } from 'react'
+import { useCallback, useEffect, useMemo } from 'react'
 import { useLocation } from 'react-router-dom'
 
 export default function useUtmTracking ({ useStorage }) {
     const { search } = useLocation()
-    const searchParams = new URLSearchParams(search)
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    const current = {}
-    for (const p of searchParams) {
-        if (p && p[0].includes('utm')) {
-            const key = p[0].substring(p[0].indexOf('utm_')+4)            
-            current[key] = p[1];
-        }
-    }
     
-    const [utm, setUtm] = useStorage({
+    const currentUtm = useMemo(() => {
+        const searchParams = new URLSearchParams(search)
+        const current = {}        
+        for (const p of searchParams) {
+            if (p && p[0].startsWith('utm_')) {
+                const key = p[0].substring(p[0].indexOf('utm_')+4)            
+                current[key] = p[1];
+            }
+        }
+        return current
+    }, [search])
+    
+    const [utm, _setUtm] = useStorage({
         key: 'utm',
         defaultValue: []
     })
-
-    const setNewCampaign = useCallback((campaign) => {
-        setUtm((prev) => [
+    
+    const setUtm = useCallback(_utm => {
+        _setUtm((prev) => [
             ...prev,
-            campaign
+            {
+                ..._utm,
+                identityCompleted: false,
+                date: new Date().valueOf()
+            }
         ])
-    }, [setUtm])
+    }, [_setUtm])
+
+    const resetUtm = () => {
+        _setUtm([])
+    }
+
+    const getLatestUtmData = useCallback(() => utm.length && utm.map((el, i) => i === utm.length-1 ? { ...el, identityCompleted: true } : el), [utm])
     
     useEffect(() => {
-        if (Object.keys(current).length) {
-            setNewCampaign({...current, identityCompleted: false, date: new Date().valueOf() })
-        } 
-    }, [current, utm.length, setNewCampaign])
+        if (Object.keys(currentUtm).length) {
+            setUtm(currentUtm)
+        }
+    }, [currentUtm, search, setUtm])
 
-    return { utm, setUtm }
+    return { utm, resetUtm, getLatestUtmData }
 }
