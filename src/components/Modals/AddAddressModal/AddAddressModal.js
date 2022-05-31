d97f681996f8ef23d3f0e4e307a9737770a046f6
Tomek <t.rokita@stremio.com>
2022-05-31 22:20:18 +0100

    Basic ens domains support

--- src/components/Modals/AddAddressModal/AddAddressModal.js
+++ src/components/Modals/AddAddressModal/AddAddressModal.js
@@ -5,15 +5,17 @@ import { MdCheck, MdClose } from 'react-icons/md'
 import { useModals } from 'hooks'
 import { Modal, TextInput, Button, ToolTip } from "components/common"
 import { resolveUDomain } from 'lib/unstoppableDomains'
+import { isEnsDomain, resolveENSDomain } from 'lib/ensDomains'
 
 const AddAddressModal = ({ title, inputs, selectedNetwork, onClose }) => {
     const { hideModal } = useModals()
     const [isDisabled, setDisabled] = useState(true)
     const [uDAddress, setUDAddress] = useState('')
+    const [ensAddress, setEnsAddress] = useState('')
     const timer = useRef(null)
 
     const inputsFields = useMemo(() => inputs.map(input => ({ ...input, ref: createRef() })), [inputs])
-    const getUDomain = async(value) => {
+    const getUDomain = async (value) => {
         return await resolveUDomain(value, null, selectedNetwork.unstoppableDomainsChain)
     }
 
@@ -22,21 +24,29 @@ const AddAddressModal = ({ title, inputs, selectedNetwork, onClose }) => {
             clearTimeout(timer.current)
         }
 
-        const validateForm = async() => {
+        const validateForm = async () => {
             const isFound = inputsFields.find(item => item.label === 'Address / Unstoppable domainsⓇ')
+            const domain = isFound && isFound.ref && isFound.ref.current.value
+            if (!domain) return;
+            const isValidEnsDomain = isEnsDomain(domain)
             let uDAddr = null
+            let ensAddr = null
             if (isFound) {
-                if (!isFound.ref) return
-                uDAddr = await getUDomain(isFound.ref.current.value)
+                uDAddr = await getUDomain(domain)
                 timer.current = null
                 setUDAddress(uDAddr)
+                if (isValidEnsDomain) {
+                    ensAddr = await resolveENSDomain(domain)
+                    timer.current = null
+                    setEnsAddress(ensAddr)
+                }
             }
 
             const isFormValid = inputsFields
                 .map(({ ref, validate, label }) => {
                     const isUDField = label === 'Address / Unstoppable domainsⓇ'
-                    const value = isUDField && uDAddr ? uDAddr : ref.current.value
-
+                    const value = isUDField && uDAddr ? uDAddr : isUDField && ensAddr ? ensAddr : ref.current.value
+                    console.log(value, "wartosc")
                     if (!validate) return !!value
 
                     return validate(value)
@@ -46,21 +56,23 @@ const AddAddressModal = ({ title, inputs, selectedNetwork, onClose }) => {
             setDisabled(!isFormValid)
         }
 
-        timer.current = setTimeout(async() => {
+        timer.current = setTimeout(async () => {
             return validateForm().catch(console.error)
         }, 500)
     }
 
     const onConfirm = () => {
         let values = inputsFields.map(({ ref }) => ref.current.value)
-        if (uDAddress) values.push(uDAddress ? true : false)
+        if (uDAddress) values.push({ type: 'ud' })
+        if (ensAddress) values.push({ type: 'ens' })
+        values.push({ type: 'pub' })
         onClose && onClose(values)
         hideModal()
     }
 
     const buttons = <>
-        <Button clear small icon={<MdClose/>} onClick={hideModal}>Cancel</Button>
-        <Button small icon={<MdCheck/>} disabled={isDisabled} onClick={onConfirm}>Confirm</Button>
+        <Button clear small icon={<MdClose />} onClick={hideModal}>Cancel</Button>
+        <Button small icon={<MdCheck />} disabled={isDisabled} onClick={onConfirm}>Confirm</Button>
     </>
 
     return (
@@ -68,7 +80,7 @@ const AddAddressModal = ({ title, inputs, selectedNetwork, onClose }) => {
             {
                 inputsFields.map(({ id, label, placeholder, ref }) => (
                     <div key={id + label}>
-                        <TextInput label={label} placeholder={placeholder} onInput={onInput} ref={ref}/>
+                        <TextInput label={label} placeholder={placeholder} onInput={onInput} ref={ref} />
                         {(label === 'Address / Unstoppable domainsⓇ') &&
                             <ToolTip label={!uDAddress ? 'You can use Unstoppable domainsⓇ' : 'Valid Unstoppable domainsⓇ domain'}>
                                 <span id="udomains-logo" className={uDAddress ? 'ud-logo-active ' : ''} />
