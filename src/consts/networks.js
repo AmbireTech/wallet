9e6c86acfd638ef670b081fdc1285ddf3035d96a
superKalo <superkalo@devlabs.bg>
2022-06-06 11:57:39 +0300

    Add: map the networks config and inject the web specific icon path in there

---
+++ src/consts/networks.js
@@ -0,0 +1,26 @@
+import networks from 'ambire-common/src/constants/networks'
+
+/**
+ * Maps each network (by its `id`) to a web-specific icon.
+ * Note: In case of adding a new network, don't forget to map it's icon here 🤞
+ * @enum {string}
+ */
+ export const networkIconsById = {
+  ethereum: '/resources/networks/ethereum.png',
+  polygon: '/resources/networks/polygon.png',
+  avalanche: '/resources/networks/avalanche.png',
+  'binance-smart-chain': '/resources/networks/bsc.png',
+  fantom: '/resources/networks/fantom.png',
+  moonbeam: '/resources/networks/moonbeam.png',
+  moonriver: '/resources/networks/moonriver.png',
+  arbitrum: '/resources/networks/arbitrum.svg',
+  gnosis: '/resources/networks/gnosis.png',
+  kucoin: '/resources/networks/kucoin.svg',
+  optimism: '/resources/networks/optimism.jpg',
+  andromeda: '/resources/networks/andromeda.svg',
+  rinkeby: '/resources/networks/rinkeby.png',
+  cronos: '/resources/networks/cronos.png',
+  aurora: '/resources/networks/aurora.png',
+}
+
+export default networks.map(network => ({ ...network, icon: networkIconsById[network.id]}))
