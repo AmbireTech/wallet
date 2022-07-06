6f56ee50e31b93fc518c209ce2e38d1aa0a72dfc
Kaloyan Kosev <superkalo@devlabs.bg>
2022-07-06 13:59:25 +0300

    Change: migrate AAVELendingPoolAbi to ambire-common

--- src/consts/AAVELendingPoolAbi.js
+++
@@ -1,9 +0,0 @@
-const AAVELendingPoolAbi = [
-    "function getLendingPool() view returns (address address)",
-    "function getReservesList() view returns (address[] memory)",
-    "function getReserveData(address asset) view returns (tuple(uint256 availableLiquidity, uint256 totalStableDebt, uint256 totalVariableDebt, uint256 liquidityRate, uint256 variableBorrowRate, uint256 stableBorrowRate, uint256 averageStableBorrowRate, uint256 liquidityIndex, uint256 variableBorrowIndex, uint40 lastUpdateTimestamp) memory)",
-    "function deposit(address asset, uint256 amount, address onBehalfOf, uint16 referralCode)",
-    "function withdraw(address asset, uint256 amount, address to)",
-]
-
-export default AAVELendingPoolAbi
