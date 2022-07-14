eef6186c6c2b67108899af19a09ab16d3a17fb47
IvoPaunov <ivo.paunov@gmail.com>
2022-07-14 16:21:24 +0300

    dapp catalog load integrated dapps

--- src/components/Wallet/DappsCatalog/DappsCatalog.js
+++ src/components/Wallet/DappsCatalog/DappsCatalog.js
@@ -1,15 +1,16 @@
 import NETWORKS from 'consts/networks'
 import { TextInput, ToolTip } from 'components/common'
-
+import GnosisSafeAppIframe from 'components/Plugins/GnosisSafeApps/GnosisSafeAppIframe'
 import './DappsCatalog.scss'
-import { useCallback } from 'react'
+import { useCallback, Fragment } from 'react'
 import { MdInfo, MdSearch } from 'react-icons/md'
-import { AiOutlineStar, AiFillStar } from 'react-icons/ai'
+import { AiOutlineStar, AiFillStar, AiOutlineClose } from 'react-icons/ai'
+import { Button } from 'components/common'
 
 
-const DappsCatalog = ({ network, dappsCatalog }) => {
+const DappsCatalog = ({ network, dappsCatalog, selectedAcc, gnosisConnect, gnosisDisconnect }) => {
 
-  const { toggleFavorite, favorites, filteredCatalog, onCategorySelect, categoryFilter, search, onSearchChange, categories } = dappsCatalog
+  const { isDappMode, currentDappData, toggleFavorite, favorites, filteredCatalog, onCategorySelect, categoryFilter, search, onSearchChange, categories, loadCurrentDappData } = dappsCatalog
 
   const sortFiltered = useCallback((filteredItems) => {
     return filteredItems.map(item => {
@@ -50,14 +51,14 @@ const DappsCatalog = ({ network, dappsCatalog }) => {
   }, [])
 
   const onFavoriteClick = useCallback((e, item) => {
-    console.log({item})
+    console.log({ item })
     toggleFavorite(item)
     e.stopPropagation()
   }, [toggleFavorite])
 
   const openDapp = (item) => {
     if (item.type === 'integrated') {
-      window.open(item.url, '_self')
+      loadCurrentDappData(item)
     } else {
       window.open(item.url, '_blank')
     }
@@ -65,92 +66,111 @@ const DappsCatalog = ({ network, dappsCatalog }) => {
 
   return (
     <section id='dappCatalog'>
-      <div className='filter-section'>
-        <div className='input-icon'>
-          <TextInput value={search} onChange={onSearchChange} placeholder='Search filter' icon={<MdSearch />} />
-        </div>
-        <div className='categories'>
-          {categories.map(c => {
-            return <span
-              className={`category category-${c.name}${categoryFilter?.name === c.name ? ' selected' : ''}`}
-              onClick={() => onCategorySelect(c)}>{c.name}</span>
-          })}
-        </div>
-      </div>
+      {isDappMode && currentDappData ?
+        < GnosisSafeAppIframe
+          network={network}
+          selectedApp={currentDappData}
+          selectedAcc={selectedAcc}
+          // removeApp={removeCustomPlugin}
+          gnosisConnect={gnosisConnect}
+          gnosisDisconnect={gnosisDisconnect}
 
-      <div className='catalogItems'>
-        {
-          sortFiltered(filteredCatalog).map(item => {
-            return <div className={`catalogItem${item.supported ? '' : ' not-supported'}`}
-              onClick={() => openDapp(item)}>
-              <span className={`favorite${favorites[item.url] ? ' selected' : ''}`} onClick={(e) => onFavoriteClick(e, item)}> {
-                favorites[item.url]
-                  ? <AiFillStar />
-                  : <AiOutlineStar />
-              }</span>
-              <div className='logoSplit'>
-                <div className='logo'>
-                  <img src={item.logo} alt={item.name} />
-                </div>
-                <div className='content'>
-                  <span className='title'>{item.title}</span>
-                  <span className='description'>{item.description}</span>
-                </div>
-              </div>
-
-              <div className='aligned-tag-rows'>
-                {
-                  !!item.networks?.length &&
-                  <div className='tag-row network-tag-row'>
-                    <ToolTip htmlContent={getNetworkTooltipContent(item.networks)} >
-                      {
-                        !item.supported &&
-                        <span className='tag unsupported'>{network.id} unsupported</span>
-                      }
-                      {
-                        item.networks?.slice(0, 3).map((n) => {
-                          const network = NETWORKS.find(an => an.id === n)
-                          if (network) {
-                            return <span className='tag network-tag'
-                              style={{ backgroundImage: `url(${network.icon})` }}></span>
-                          }
-                          return null
-                        })
-                      }
-                      {
-                        item.networks?.length > 3 &&
-                        <span className='tag network-tag network-tag-more'>...</span>
-                      }
-                    </ToolTip>
-                  </div>
-                }
-
-                <div className='tag-row tag-types'>
-                  <span className={`tag type-tag type-tag-${item.type}`}>{item.type}</span>
-                </div>
-              </div>
-
+        />
+        :
+        <Fragment>
+          <div className='filter-section'>
+            <div className='input-icon'>
+              <TextInput value={search} onChange={onSearchChange} placeholder='Search filter' icon={<MdSearch />} />
             </div>
-          })
-        }
+            <div className='categories'>
+              {categories.map(c => {
+                return <span
+                  className={`category category-${c.name}${categoryFilter?.name === c.name ? ' selected' : ''}`}
+                  onClick={() => onCategorySelect(c)}>{c.name}</span>
+              })}
+            </div>
+          </div>
 
-        {
-          // Please someone fixes this in CSS (pixel perfect aligned cols when items not 3 in a row)
-          filteredCatalog.length % 3 !== 0 &&
-          [...Array(3 - filteredCatalog.length % 3)].map(a => {
-            return <div className='catalogItem-filler'></div>
-          })
-        }
-      </div>
+          <div className='catalogItems'>
+            {
+              sortFiltered(filteredCatalog).map(item => {
+                return <div className={`catalogItem${item.supported ? '' : ' not-supported'}`}
+                  onClick={() => openDapp(item)}>
+                  <span className={`favorite${favorites[item.url] ? ' selected' : ''}`} onClick={(e) => onFavoriteClick(e, item)}> {
+                    favorites[item.url]
+                      ? <AiFillStar />
+                      : <AiOutlineStar />
+                  }</span>
+                  <div className='logoSplit'>
+                    <div className='logo'>
+                      <img src={item.logo} alt={item.name} />
+                    </div>
+                    <div className='content'>
+                      <span className='title'>{item.title}</span>
+                      <span className='description'>{item.description}</span>
+                    </div>
+                  </div>
 
-      {
-        !filteredCatalog.length &&
-        <div className='no-dapp-found'>No dApp found in our list matching your criteria</div>
+                  <div className='aligned-tag-rows'>
+                    {
+                      !!item.networks?.length &&
+                      <div className='tag-row network-tag-row'>
+                        <ToolTip htmlContent={getNetworkTooltipContent(item.networks)} >
+                          {
+                            !item.supported &&
+                            <span className='tag unsupported'>{network.id} unsupported</span>
+                          }
+                          {
+                            item.networks?.slice(0, 3).map((n) => {
+                              const network = NETWORKS.find(an => an.id === n)
+                              if (network) {
+                                return <span className='tag network-tag'
+                                  style={{ backgroundImage: `url(${network.icon})` }}></span>
+                              }
+                              return null
+                            })
+                          }
+                          {
+                            item.networks?.length > 3 &&
+                            <span className='tag network-tag network-tag-more'>...</span>
+                          }
+                        </ToolTip>
+                      </div>
+                    }
+
+                    <div className='tag-row tag-types'>
+                      <span className={`tag type-tag type-tag-${item.type}`}>{item.type}</span>
+                    </div>
+                  </div>
+
+                </div>
+              })
+            }
+
+            {
+              // Please someone fixes this in CSS (pixel perfect aligned cols when items not 3 in a row)
+              filteredCatalog.length % 3 !== 0 &&
+              [...Array(3 - filteredCatalog.length % 3)].map(a => {
+                return <div className='catalogItem-filler'></div>
+              })
+            }
+          </div>
+
+          {
+            !filteredCatalog.length &&
+            <div className='no-dapp-found'>No dApp found in our list matching your criteria</div>
+          }
+
+          <div className='info-wc'>
+            <MdInfo /> Note: any dApp that supports walletconnect can be connected as well
+          </div>
+        </Fragment>
       }
-
-      <div className='info-wc'>
-        <MdInfo /> Note: any dApp that supports walletconnect can be connected as well
-      </div>
+      {isDappMode && currentDappData &&
+        <Button className='exit-btn' red mini icon={<AiOutlineClose />}
+          onClick={() => loadCurrentDappData(null)}
+        ></Button>
+      }
     </section>
   )
 }
