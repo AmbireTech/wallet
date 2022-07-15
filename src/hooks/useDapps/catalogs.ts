79258449b73f66ae109e6303f50d760d7f95c169
IvoPaunov <ivo.paunov@gmail.com>
2022-07-15 18:09:54 +0300

    update dapps catalog

--- src/hooks/useDapps/catalogs.ts
+++ src/hooks/useDapps/catalogs.ts
@@ -13,11 +13,11 @@ const DEFAULT_CATALOG: Array<DappManifestData> = [
   {
     name: 'transaction_builder',
     title: 'Transaction Builder',
-    url: '#/wallet/gnosis/plugins/txbuilder',
+    url: 'https://safe-apps.dev.gnosisdev.com/tx-builder/',
     logo: 'https://safe-apps.dev.gnosisdev.com/tx-builder/tx-builder.png',
     description: 'Build your transaction from scratch (Power users)',
     type: 'integrated',
-    networks: []
+    networks: ['ethereum', 'polygon', 'avalanche', 'fantom', 'arbitrum', 'harmony', 'optimism']
   },
   {
     name: 'uniswap',
@@ -25,7 +25,7 @@ const DEFAULT_CATALOG: Array<DappManifestData> = [
     url: 'https://app.uniswap.org',
     logo: 'https://app.uniswap.org/favicon.png',
     description: 'Uniswap decentralised exchange',
-    type: 'walletconnect',
+    type: 'integrated',
     networks: ['ethereum', 'polygon']
   },
   {
@@ -34,15 +34,6 @@ const DEFAULT_CATALOG: Array<DappManifestData> = [
     url: 'https://app.sushi.com',
     logo: 'https://res.cloudinary.com/sushi-cdn/image/fetch/f_auto,c_limit,w_48,q_auto/https://app.sushi.com/images/logo.svg',
     description: 'Sushiswap decentralised exchange',
-    type: 'walletconnect',
-    networks: ['ethereum', 'polygon', 'binance-smart-chain', 'fantom', 'gnosis']
-  },
-  {
-    name: 'opensea',
-    title: 'Opensea',
-    url: '#/wallet/gnosis/plugins/opensea',
-    logo: 'https://opensea.io/static/images/logos/opensea.svg',
-    description: 'Opensea NFT marketplace',
     type: 'integrated',
     networks: ['ethereum', 'polygon', 'binance-smart-chain', 'fantom', 'gnosis']
   },
@@ -79,23 +70,14 @@ const DEFAULT_CATALOG: Array<DappManifestData> = [
     url: 'https://app.aave.com/',
     logo: 'https://www.gitbook.com/cdn-cgi/image/width=40,height=40,fit=contain,dpr=1,format=auto/https%3A%2F%2F2799188404-files.gitbook.io%2F~%2Ffiles%2Fv0%2Fb%2Fgitbook-legacy-files%2Fo%2Fspaces%252F-M6U5cfvZsVW8zOEpVl1%252Favatar-1595317514145.png%3Fgeneration%3D1595317514421109%26alt%3Dmedia',
     description: 'Decentralised exchange and lending platform',
-    type: 'walletconnect',
-    networks: ['ethereum', 'polygon', 'avalanche', 'fantom', 'arbitrum', 'harmony', 'optimism']
-  },
-  {
-    name: 'txbuilder',
-    title: 'TxBuilder',
-    url: 'https://safe-apps.dev.gnosisdev.com/tx-builder/',
-    logo: 'https://safe-apps.dev.gnosisdev.com/tx-builder/logo.svg',
-    description: 'Local dapp test with some lorem ipsum stuff',
     type: 'integrated',
     networks: ['ethereum', 'polygon', 'avalanche', 'fantom', 'arbitrum', 'harmony', 'optimism']
   },
   {
     name: 'paraswap',
     title: 'ParaSwap',
-    url: 'https://paraswap.io',
-    logo: 'https://paraswap.io/paraswap.svg',
+    url: 'https://app.paraswap.io/',
+    logo: 'https://assets.website-files.com/617aa5e4225be2555942852c/6214d5c4db4ce4d976b5f1f9_logo_paraswap-handbook%20copy%201.svg',
     description: 'ParaSwap allows dApps and traders to get the best DEX liquidity by aggregating multiple markets and offering the best rates',
     type: 'integrated',
     networks: ['ethereum', 'polygon', 'avalanche', 'fantom', 'arbitrum', 'harmony', 'optimism']
@@ -108,6 +90,15 @@ const DEFAULT_CATALOG: Array<DappManifestData> = [
     description: 'Cross-chain DeFi & DEX aggregator, farming, asset management, fiat on-ramp',
     type: 'integrated',
     networks: ['ethereum', 'polygon', 'avalanche', 'fantom', 'arbitrum', 'harmony', 'optimism']
+  },
+  {
+    name: 'rarible',
+    title: 'Rarible',
+    url: 'https://rarible.com/',
+    logo: 'https://www.freelogovectors.net/svg11/rarible-logo-freelogovectors.net.svg',
+    description: 'CommunityCommunity-centric NFT marketplace',
+    type: 'walletconnect',
+    networks: ['ethereum']
   },
 ]
 
