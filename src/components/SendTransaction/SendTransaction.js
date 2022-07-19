6ae65f05f7f41d5fb7bfef3eb0e51501a9200dc2
Kaloyan Kosev <superkalo@devlabs.bg>
2022-07-19 15:00:44 +0300

    Change: migrate requestToBundleTxn to ambire common

--- src/components/SendTransaction/SendTransaction.js
+++ src/components/SendTransaction/SendTransaction.js
@@ -23,7 +23,7 @@ import {
   toHexAmount,
  } from './helpers'
 import { fetchPost } from 'lib/fetch'
-import { toBundleTxn } from 'lib/requestToBundleTxn'
+import { toBundleTxn } from 'ambire-common/src/services/requestToBundleTxn'
 import { getProvider } from 'lib/provider'
 import { MdInfo } from 'react-icons/md'
 import { useCallback } from 'react'
