6be6e165ea9d6fdaaaf5f5432b349bc937b92146
Emil <stojnovsky@gmail.com>
2022-07-20 17:55:42 +0300

    change text for cache balance from velcro

--- src/components/Wallet/Dashboard/OutdatedBalancesMsg/OutdatedBalancesMsg.js
+++ src/components/Wallet/Dashboard/OutdatedBalancesMsg/OutdatedBalancesMsg.js
@@ -1,7 +1,7 @@
 const OutdatedBalancesMsg = ({ selectedAccount, selectedNetwork }) => {
     return (
         <div className="notification-hollow warning">
-            <h4>Dashboard balances could be incomplete or outdated.</h4>
+            <h4>Dashboard balances could appear incomplete or outdated.</h4>
             <p>
             We are currently experiencing technical difficulties with our third party services, so what you see on the dashboard may be incomplete or outdated. You can check <a href={selectedNetwork.explorerUrl+'/address/'+ selectedAccount} target='_blank' rel='noreferrer'>{selectedNetwork.explorerUrl.split('/')[2]}</a> to see your current balances. However, you can use Ambire normally with any connected dApp.
             </p>
