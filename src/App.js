04f3b9f85e69d4db40d64a71d20c290de8bfc432
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2022-08-09 16:28:15 +0300

    renamed thankYouUTM into campaignUTM

--- src/App.js
+++ src/App.js
@@ -255,13 +255,13 @@ function AppInner() {
   const rewardsData = useRelayerData(rewardsUrl)
 
   // Checks if Thank you page needs to be shown
-  const thankYouUTM = useOneTimeQueryParam('utm_campaign')
+  const campaignUTM = useOneTimeQueryParam('utm_campaign')
   const [showThankYouPage, setShowThankYouPage] = useLocalStorage({
       key: 'showThankYouPage',
       defaultValue: false
   })
   const handleSetShowThankYouPage = useCallback(() => setShowThankYouPage(true), [setShowThankYouPage])
-  useEffect(() => thankYouUTM && handleSetShowThankYouPage(), [handleSetShowThankYouPage, thankYouUTM])
+  useEffect(() => campaignUTM && handleSetShowThankYouPage(), [handleSetShowThankYouPage, campaignUTM])
 
   return (<>
     <Prompt
