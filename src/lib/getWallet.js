b9ed63c710777f7b9b4d28615bccff55344c19bc
Simon Males <sime@sime.net.au>
2022-08-11 18:00:04 +0200

    chore(trezor): Upgrade to Trezor Connect v9

--- src/lib/getWallet.js
+++ src/lib/getWallet.js
@@ -1,5 +1,5 @@
 import { TrezorSubprovider } from '@0x/subproviders/lib/src/subproviders/trezor' // https://github.com/0xProject/0x-monorepo/issues/1400
-import TrezorConnect from 'trezor-connect'
+import TrezorConnect from '@trezor/connect'
 import { ethers } from 'ethers'
 import HDNode from 'hdkey'
 import { LedgerSubprovider } from '@0x/subproviders/lib/src/subproviders/ledger' // https://github.com/0xProject/0x-monorepo/issues/1400
