77191fa00890a868894393bfb11b229611ab279c
Petromir Petrov <petromir@devlabs.bg>
2022-09-08 14:50:23 +0300

    add: working unbond modal

--- src/components/Modals/WalletTokenModal/UnbondModal/UnbondModal.js
+++ src/components/Modals/WalletTokenModal/UnbondModal/UnbondModal.js
@@ -2,25 +2,31 @@ import './UnbondModal.scss'
 
 import { createPortal } from 'react-dom'
 import { Button } from 'components/common'
-import { MdClose, MdOutlineClose } from 'react-icons/md'
+import { MdOutlineClose } from 'react-icons/md'
+import Lottie from 'lottie-react'
+import AnimationData from './assets/animation.json'
+import Circle from './assets/circle.svg'
 
 const UnbondModal = ({ isVisible, hideModal }) => {
   const root = document.getElementById('root')
   return isVisible ? createPortal(
     <div id="unbond-modal">
       <div className='modal'>
-        <div className='heading'>
-          <div className="title">This is a test modal</div>
-          <div className="close" onClick={hideModal}>
-            <MdClose/>
-          </div>
-        </div>
         <div className='content'>
-          <p>This is content</p>
+          <div className='danger-animation-wrapper'>
+            <Lottie className='danger-animation' animationData={AnimationData} background="transparent" speed="1" loop autoplay />
+            <img src={Circle} alt='circle' className='danger-animation-circle'/>
+          </div>
+          <span className='warning-title'>Warning</span>
+          <p className='warning-text'>
+            This procedure will claim only 50% of your outstanding 
+            rewards as $WALLET, and permanently burn the rest. 
+            Are you sure?
+          </p>
         </div>
         <div className='buttons'>
-          <Button danger icon={<MdOutlineClose/>} onClick={() => console.log('Unbonded')}>Unbond</Button>
-          <Button clear icon={<MdOutlineClose/>} onClick={hideModal}>Close</Button>
+          <Button className='button' danger onClick={() => console.log('Unbonded')}>Yes, Claim anyway</Button>
+          <Button className='button' clear icon={<MdOutlineClose/>} onClick={hideModal}>Close</Button>
         </div>
       </div>
     </div>
