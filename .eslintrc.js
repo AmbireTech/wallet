0ea801c8458c65cc7e43ffba6ee9d83c07db8f81
Kaloyan Kosev <superkalo@devlabs.bg>
2022-09-30 18:38:15 +0300

    Add: config eslint to resolve relative import paths

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,12 @@ module.exports = {
   extends: ['./node_modules/ambire-common/.eslintrc.js'],
   parserOptions: {
     project: './tsconfig.json'
+  },
+  settings: {
+    // So that the eslint is able to resolve relative import paths
+    // {@link https://stackoverflow.com/a/63451047/1333836}
+    'import/resolver': {
+      typescript: {}
+    },
   }
 }
