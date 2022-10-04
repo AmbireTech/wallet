474d5ac96099ec630c55a9a8e6131f07e4fbb78f
Petromir <petromir@devlabs.bg>
2022-10-04 17:16:28 +0300

    change: format and removed Close button in OutdatedBalancesMsg

--- src/components/Wallet/Dashboard/OutdatedBalancesMsg/OutdatedBalancesMsg.js
+++ src/components/Wallet/Dashboard/OutdatedBalancesMsg/OutdatedBalancesMsg.js
@@ -1,6 +1,6 @@
 import styles from './OutdatedBalancesMsg.module.scss'
 import { ReactComponent as AlertIcon } from 'resources/icons/alert.svg'
-import { ReactComponent as CloseIcon } from 'resources/icons/close.svg'
+// import { ReactComponent as CloseIcon } from 'resources/icons/close.svg'
 
 const OutdatedBalancesMsg = ({ selectedAccount, selectedNetwork }) => {
     return (
@@ -9,12 +9,24 @@ const OutdatedBalancesMsg = ({ selectedAccount, selectedNetwork }) => {
             <div className={styles.body}>
                 <h4 className={styles.title}>Dashboard balances could appear incomplete or outdated.</h4>
                 <p className={styles.text}>
-                    We are currently experiencing technical difficulties with our third party services, so what you see on the dashboard may be incomplete or outdated. You can check <a href={selectedNetwork.explorerUrl+'/address/'+ selectedAccount} target='_blank' rel='noreferrer'>{selectedNetwork.explorerUrl.split('/')[2]}</a> to see your current balances. However, you can use Ambire normally with any connected dApp.
+                    We are currently experiencing technical difficulties with our third party services, 
+                    so what you see on the dashboard may be incomplete or outdated. You can check
+                    {' '}
+                    <a
+                        href={selectedNetwork.explorerUrl+'/address/'+ selectedAccount} 
+                        target='_blank' 
+                        rel='noreferrer'
+                        className={styles.link}
+                    >
+                        {selectedNetwork.explorerUrl.split('/')[2]}
+                    </a>
+                    {' '}
+                    to see your current balances. However, you can use Ambire normally with any connected dApp.
                 </p>
             </div>
-            <button className={styles.close}>
+            {/* <button className={styles.close}>
                 <CloseIcon />
-            </button>
+            </button> */}
         </div>
     )
 }
