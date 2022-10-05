51ace965a295847a0fa6cfc62f56ee4b6298979c
Petromir Petrov <petromir@devlabs.bg>
2022-10-05 16:55:11 +0300

    change: BalanceItem layout

--- src/components/Wallet/Dashboard/Balances/BalanceItem/BalanceItem.js
+++ src/components/Wallet/Dashboard/Balances/BalanceItem/BalanceItem.js
@@ -3,19 +3,18 @@ import styles from './BalanceItem.module.scss'
 const BalanceItem = ({ onClick, icon, name, value, decimalValue, children }) => (
   <div className={styles.balanceContainer}>
       <div className={styles.otherBalance} onClick={onClick}>
+          <label className={styles.network}>
+              { icon }
+              <div className={styles.name}>
+                { name }
+              </div>
+          </label>
           <label>
               <span className={styles.purpleHighlight}>
                 $
               </span>
               { value }
               .{ decimalValue }
-          </label>
-          <label className={styles.network}>
-              on
-              { icon }
-              <div className={styles.name}>
-                { name }
-              </div>
           </label>
       </div>
     { children }
