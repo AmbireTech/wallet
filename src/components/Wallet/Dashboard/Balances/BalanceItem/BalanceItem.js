f96810352b985ee82b8b50f2d2a2452c4bdaa207
Petromir Petrov <petromir@devlabs.bg>
2022-10-06 18:25:30 +0300

    remove: uneccessary classes and markup from BalanceItem

--- src/components/Wallet/Dashboard/Balances/BalanceItem/BalanceItem.js
+++ src/components/Wallet/Dashboard/Balances/BalanceItem/BalanceItem.js
@@ -1,23 +1,20 @@
 import styles from './BalanceItem.module.scss'
 
-const BalanceItem = ({ onClick, icon, name, value, decimalValue, children }) => (
-  <div className={styles.balanceContainer}>
-      <div className={styles.otherBalance} onClick={onClick}>
-          <label className={styles.network}>
-              { icon }
-              <div className={styles.name}>
-                { name }
-              </div>
-          </label>
-          <label>
-              <span className={styles.purpleHighlight}>
-                $
-              </span>
-              { value }
-              .{ decimalValue }
-          </label>
-      </div>
-    { children }
+const BalanceItem = ({ onClick, icon, name, value, decimalValue}) => (
+  <div className={styles.balanceContainer} onClick={onClick}>
+        <label className={styles.network}>
+            { icon }
+            <div className={styles.name}>
+              { name }
+            </div>
+        </label>
+        <label>
+            <span className={styles.purpleHighlight}>
+              $
+            </span>
+            { value }
+            .{ decimalValue }
+        </label>
   </div>
 )
 
