122cded59e85605a869909b98f37e78b0bd74a7b
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2022-10-11 14:24:01 +0300

    changed msg in DAppIncompatibilityWarningMsg.js

--- src/components/common/DAppIncompatibilityWarningMsg/DAppIncompatibilityWarningMsg.js
+++ src/components/common/DAppIncompatibilityWarningMsg/DAppIncompatibilityWarningMsg.js
@@ -2,11 +2,13 @@ import './DAppIncompatibilityWarningMsg.scss'
 
 import { IoIosWarning } from 'react-icons/io'
 
-export default function DAppIncompatibilityWarningMsg({title = 'Warning'}) {
+export default function DAppIncompatibilityWarningMsg(
+  {
+    title = 'Warning',
+    msg = 'The signatures are not working. This is a highly disruptive practice, as it breaks support for all smart wallets (Ambire, Gnosis Safe and others). We recommend you report this to the dApp ASAP and ask them to fix it.'
+  }) {
     return (<div id='dangerMsg'>
-    <div className='err-title'><IoIosWarning/>{title}</div>
-    <div className='error-message'>
-      We detected that this dApp intentionally blocks smart contract calls. This is a highly disruptive practice, as it breaks support for all smart wallets (Ambire, Gnosis Safe and others). We recommend you report this to the dApp ASAP and ask them to fix it.
-    </div>
+    <div className='err-title'><IoIosWarning/>{ title }</div>
+    <div className='error-message'>{ msg }</div>
   </div>) 
 }
