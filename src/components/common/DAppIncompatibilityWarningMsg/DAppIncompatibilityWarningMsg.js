9c959658530249c77d21218678acfe1a626ee5b6
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2022-10-11 15:45:59 +0300

    Change the DAppIncompatibilityWarningMsg default msg

--- src/components/common/DAppIncompatibilityWarningMsg/DAppIncompatibilityWarningMsg.js
+++ src/components/common/DAppIncompatibilityWarningMsg/DAppIncompatibilityWarningMsg.js
@@ -5,7 +5,7 @@ import { IoIosWarning } from 'react-icons/io'
 export default function DAppIncompatibilityWarningMsg(
   {
     title = 'Warning',
-    msg = 'The signatures are not working. This is a highly disruptive practice, as it breaks support for all smart wallets (Ambire, Gnosis Safe and others). We recommend you report this to the dApp ASAP and ask them to fix it.'
+    msg = 'It is highly likely that this dapp does not support smart wallet signatures. This is a highly disruptive practice, as it breaks support for all smart wallets (Ambire, Gnosis Safe and others). We recommend you report this to the dApp ASAP and ask them to fix it.'
   }) {
     return (<div id='dangerMsg'>
     <div className='err-title'><IoIosWarning/>{ title }</div>
