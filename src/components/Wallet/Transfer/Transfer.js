50a4b1e9363ffa38992f55fa1fc1d591b2771a2b
Petromir Petrov <petromir@devlabs.bg>
2022-10-13 16:06:14 +0300

    add: Sell section to Transfer

--- src/components/Wallet/Transfer/Transfer.js
+++ src/components/Wallet/Transfer/Transfer.js
@@ -1,19 +1,25 @@
 import { useLocation } from 'react-router-dom'
 import { useState } from 'react'
 import { withRouter } from 'react-router'
-import accountPresets from 'ambire-common/src/constants/accountPresets'
 import { useParams } from 'react-router'
+import cn from 'classnames'
+import accountPresets from 'ambire-common/src/constants/accountPresets'
 import { isValidAddress } from 'ambire-common/src/services/address'
 
-import Addresses from './Addresses/Addresses'
 import { Panel } from 'components/common'
-
 import Send from './Send/Send'
+import Sell from './Sell/Sell'
+import Addresses from './Addresses/Addresses'
+
+import networks from 'consts/networks'
+
 import styles from './Transfer.module.scss'
 
 const Transfer = (props) => {
-    const { portfolio, selectedNetwork, addressBook } = props
+    const { portfolio, selectedNetwork, addressBook, selectedAcc, relayerURL } = props
     const { addresses, addAddress, removeAddress } = addressBook
+
+    const networkDetails = networks.find(({ id }) => id === selectedNetwork.id)
     
     const { state } = useLocation()
     const { tokenAddressOrSymbol } = useParams()
@@ -22,23 +28,47 @@ const Transfer = (props) => {
 
     const [asset, setAsset] = useState(tokenAddress)
     const [gasTankDetails] = useState(state ? state : null)
+    const [currentTab, setCurrentTab] = useState('send')
     const [address, setAddress] = useState(gasTankDetails ? accountPresets.feeCollector : '')
     
     const selectedAsset = portfolio?.tokens.find(({ address }) => address === asset)
 
+    const handleOpenSend = () => setCurrentTab('send')
+
+    const handleOpenSell = () => setCurrentTab('sell')
+
     return (
         <div className={styles.wrapper} style={{ justifyContent: gasTankDetails ? 'center' : '' }}>
            <Panel title="" className={styles.panel}>
-               <Send 
-                {...props}
-                address={address} 
-                setAddress={setAddress} 
-                gasTankDetails={gasTankDetails} 
-                asset={asset} 
-                setAsset={setAsset} 
-                tokenAddress={tokenAddress} 
-                selectedAsset={selectedAsset} 
-               />
+                <div className={styles.tabs}>
+                    <button 
+                        onClick={handleOpenSend}
+                        className={cn(styles.tabsButton, {[styles.active]: currentTab === 'send'})}
+                    >
+                        Send
+                    </button>
+                    <button 
+                        onClick={handleOpenSell}
+                        className={cn(styles.tabsButton, {[styles.active]: currentTab === 'sell'})}
+                    >
+                        Sell Crypto
+                    </button>
+                </div>
+                {currentTab === 'send' ? <Send 
+                    {...props}
+                    address={address} 
+                    setAddress={setAddress} 
+                    gasTankDetails={gasTankDetails} 
+                    asset={asset} 
+                    setAsset={setAsset} 
+                    tokenAddress={tokenAddress} 
+                    selectedAsset={selectedAsset} 
+                /> : <Sell 
+                    walletAddress={selectedAcc}
+                    networkDetails={networkDetails}
+                    relayerURL={relayerURL}
+                    portfolio={portfolio}
+                />}
            </Panel>
            {!gasTankDetails && <Addresses
                 selectedAsset={selectedAsset}
