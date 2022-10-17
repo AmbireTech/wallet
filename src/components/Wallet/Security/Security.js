f0cb235e86f737c0b8fb91fabad672fd641a61d7
Petromir <petromir@devlabs.bg>
2022-10-17 18:37:36 +0300

    change: redesign Security

--- src/components/Wallet/Security/Security.js
+++ src/components/Wallet/Security/Security.js
@@ -1,26 +1,18 @@
-import './Security.scss'
-
-// import { MdOutlineRemove } from 'react-icons/md'
 import { RiDragDropLine } from 'react-icons/ri'
 import { useState, useEffect, useCallback } from 'react'
-import { Loading } from 'components/common'
+import { Loading, Panel } from 'components/common'
 import { AbiCoder, keccak256 } from 'ethers/lib/utils'
-// import accountPresets from 'ambire-common/src/constants/accountPresets'
-import { useRelayerData, useModals } from 'hooks'
-// import ResetPasswordModal from 'components/Modals/ResetPasswordModal/ResetPasswordModal'
-// import AddAuthSigner from './AddAuthSigner/AddAuthSigner'
+import cn from 'classnames'
+import { useRelayerData } from 'hooks'
 import { useToasts } from 'hooks/toasts'
 import { useDropzone } from 'react-dropzone'
 import { validateImportedAccountProps, fileSizeValidator } from 'lib/validations/importedAccountValidations'
-// import OtpTwoFAModal from 'components/Modals/OtpTwoFAModal/OtpTwoFAModal'
-// import OtpTwoFADisableModal from 'components/Modals/OtpTwoFADisableModal/OtpTwoFADisableModal'
 import Backup from './Backup/Backup'
 import PendingRecoveryNotice from './PendingRecoveryNotice/PendingRecoveryNotice'
-// import useConstants from 'hooks/useConstants'
 import Alert from './Alert/Alert'
-import Signers from './Signers'
+import Signers from './Signers/Signers'
 
-
+import styles from './Security.module.scss'
 
 const REFRESH_INTVL = 40000
 
@@ -98,135 +90,58 @@ const Security = ({
           // but that's not finalized yet
           && accHash(selectedAccount.signer) !== privileges[selectedAccount.signer.quickAccManager]
       )
-    )
-
-  // const privList = Object.entries(privileges)
-  //   .map(([addr, privValue]) => {
-  //     if (!privValue) return null
-  
-  //     const addressName = getName(humanizerInfo, addr) || null
-  //     const isQuickAcc = addr === accountPresets.quickAccManager
-  //     const privText = isQuickAcc
-  //       ? `Email/password signer (${selectedAccount.email || 'unknown email'})`
-  //       : `${addr} ${addressName && addressName !== addr ? `(${addressName})` : ''}`
-  //     const signerAddress = isQuickAcc
-  //       ? selectedAccount.signer.quickAccManager
-  //       : selectedAccount.signer.address
-  //     const isSelected = signerAddress === addr
-  //     const canChangePassword = isQuickAcc && !hasPendingReset
-
-  //     return (
-  //       <li key={addr}>
-  //         <TextInput className="depositAddress" value={privText} disabled />
-  //         <div className="btns-wrapper">
-  //           {isQuickAcc && (otpEnabled !== null) && (otpEnabled ? 
-  //             (<Button red onClick={handleDisableOtp} small className='buttonComponent'>Disable 2FA</Button>) : 
-  //             (<Button onClick={handleEnableOtp} small className='buttonComponent'>Enable 2FA</Button>)
-  //           )}
-  //           {isQuickAcc && (<Button
-  //             disabled={!canChangePassword}
-  //             title={hasPendingReset ? 'Account recovery already in progress' : ''}
-  //             onClick={showResetPasswordModal} small className='buttonComponent'>Change password</Button>
-  //           )}
-  //           <Button
-  //             disabled={isSelected}
-  //             title={isSelected ? 'Signer is already default' : ''}
-  //             onClick={() =>
-  //               onMakeDefaultBtnClicked(selectedAccount, addr, isQuickAcc)
-  //             }
-  //             small
-  //             className='buttonComponent'
-  //           >
-  //             Make default
-  //           </Button>
-  //           <Button
-  //             onClick={() => onRemoveBtnClicked(addr)}
-  //             small
-  //             red
-  //             icon={<MdOutlineRemove/>}
-  //             title={
-  //               isSelected ? 'Cannot remove the currently used signer' : ''
-  //             }
-  //             disabled={isSelected}
-  //             className='buttonComponent'
-  //           >
-  //             Remove
-  //           </Button>
-  //         </div>
-  //       </li>
-  //     )
-  //   })
-  //   .filter(x => x)
-  // const privList = []
+  )
 
   const showLoading = isLoading && !data
   const signersFragment = relayerURL ? (
   <div>
     <Alert network={selectedNetwork.name} />
-    <div className="panel" id="signers">
+    <Panel className={styles.panel} title="Authorized signers" titleClassName={styles.title}>
       {hasPendingReset && !showLoading && (<PendingRecoveryNotice
         recoveryLock={recoveryLock}
         showSendTxns={showSendTxns}
         selectedAccount={selectedAccount}
         selectedNetwork={selectedNetwork}
       />)}
-      
-      <div className="panel-title">Authorized signers</div>
       {errMsg && (
-        <h3 className="error">Error getting authorized signers: {errMsg}</h3>
+        <h3 className={styles.error}>Error getting authorized signers: {errMsg}</h3>
       )}
-      {showLoading && <Loading />}
-      <ul className="content">
-        {!showLoading && <Signers 
-          relayerURL={relayerURL}
-          relayerData={data} 
-          signers={Object.entries(privileges)} 
-          selectedAccount={selectedAccount}
-          onAddAccount={onAddAccount}
-          cacheBreak={cacheBreak}
-          setCacheBreak={setCacheBreak}
-          selectedAcc={selectedAcc}
-          selectedNetwork={selectedNetwork}
-          addRequest={addRequest}
-          showSendTxns={showSendTxns}
-          hasPendingReset={hasPendingReset}
-        />}
-        {/* {!showLoading && Object.entries(privileges).map(([addr, privValue]) => (
-        <Signer 
-          addr={addr}
-          privValue={privValue}
-          selectedAccount={selectedAccount}
-          hasPendingReset={hasPendingReset}
-          relayerData={data}
-          handleDisableOtp={handleDisableOtp}
-          handleEnableOtp={handleEnableOtp}
-          onMakeDefaultBtnClicked={onMakeDefaultBtnClicked}
-          showResetPasswordModal={showResetPasswordModal}
-        />
-      ))} */}
-      </ul>
-    </div>
-    {/* <div className="panel">
-      <div className="panel-title">Add new signer</div>
-      <AddAuthSigner
-        onAddBtnClicked={onAddBtnClickedHandler}
+      {(!showLoading && Object.entries(privileges)) ? <Signers 
+        relayerURL={relayerURL}
+        relayerData={data} 
+        signers={Object.entries(privileges)} 
+        onAddAccount={onAddAccount}
+        cacheBreak={cacheBreak}
+        setCacheBreak={setCacheBreak}
+        selectedAcc={selectedAcc}
+        selectedAccount={selectedAccount}
         selectedNetwork={selectedNetwork}
-        selectedAcc={selectedAccount}
-      />
-    </div> */}
+        addRequest={addRequest}
+        showSendTxns={showSendTxns}
+        hasPendingReset={hasPendingReset}
+      /> : <Loading />}
+    </Panel>
   </div>) : (
-    <div className="panel">
-      <div className="panel-title">Authorized signers</div>
-      <h3 className="error">
+    <Panel className={styles.panel} title="Authorized signers">
+      <h3 className={styles.error}>
         Unsupported: not connected to a relayer.
       </h3>
-    </div>
+    </Panel>
   )
   return (
-    <section id="security" className={(isDragActive ? 'activeStyle ' : '') + (isDragAccept ? 'acceptStyle ' : '') + (isDragReject ? 'rejectStyle ' : '')} {...getRootProps()}>
+    <section className={cn(
+      styles.wrapper,
+      {
+        [styles.activeStyle]: isDragActive,
+        [styles.acceptStyle]: isDragAccept,
+        [styles.rejectStyle]: isDragReject
+      })
+    }
+      {...getRootProps()}
+    >
       {
         (isDragAccept || isDragReject)
-        && (<div className={isDragAccept ? 'acceptStyleIcon' : 'rejectStyleIcon'}><RiDragDropLine size={100}/></div>)
+        && (<div className={isDragAccept ? styles.acceptStyleIcon : styles.rejectStyleIcon}><RiDragDropLine size={100}/></div>)
       }
       
       <input {...getInputProps()} />
