50fc82bc7e3bf61a217fbf1c6e0d137c7bef18e8
Petromir Petrov <petromir@devlabs.bg>
2022-10-19 08:57:47 +0300

    change: integrate the new Tab component in Transfer

--- src/components/Wallet/Transfer/Transfer.js
+++ src/components/Wallet/Transfer/Transfer.js
@@ -2,11 +2,9 @@ import { useLocation } from 'react-router-dom'
 import { useState } from 'react'
 import { withRouter } from 'react-router'
 import { useParams } from 'react-router'
-import cn from 'classnames'
 import accountPresets from 'ambire-common/src/constants/accountPresets'
 import { isValidAddress } from 'ambire-common/src/services/address'
 
-import { Panel } from 'components/common'
 import Send from './Send/Send'
 import Sell from './Sell/Sell'
 import Addresses from './Addresses/Addresses'
@@ -14,6 +12,7 @@ import Addresses from './Addresses/Addresses'
 import networks from 'consts/networks'
 
 import styles from './Transfer.module.scss'
+import Tabs from 'components/common/Tabs/Tabs'
 
 const Transfer = (props) => {
     const { portfolio, selectedNetwork, addressBook, selectedAcc, relayerURL } = props
@@ -28,48 +27,37 @@ const Transfer = (props) => {
 
     const [asset, setAsset] = useState(tokenAddress)
     const [gasTankDetails] = useState(state ? state : null)
-    const [currentTab, setCurrentTab] = useState('send')
     const [address, setAddress] = useState(gasTankDetails ? accountPresets.feeCollector : '')
     
     const selectedAsset = portfolio?.tokens.find(({ address }) => address === asset)
 
-    const handleOpenSend = () => setCurrentTab('send')
-
-    const handleOpenSell = () => setCurrentTab('sell')
-
     return (
         <div className={styles.wrapper} style={{ justifyContent: gasTankDetails ? 'center' : '' }}>
-           <Panel className={styles.panel}>
-                <div className={styles.tabs}>
-                    <button 
-                        onClick={handleOpenSend}
-                        className={cn(styles.tabsButton, {[styles.active]: currentTab === 'send'})}
-                    >
-                        Send
-                    </button>
-                    <button 
-                        onClick={handleOpenSell}
-                        className={cn(styles.tabsButton, {[styles.active]: currentTab === 'sell'})}
-                    >
-                        Sell Crypto
-                    </button>
-                </div>
-                {currentTab === 'send' ? <Send 
-                    {...props}
-                    address={address} 
-                    setAddress={setAddress} 
-                    gasTankDetails={gasTankDetails} 
-                    asset={asset} 
-                    setAsset={setAsset} 
-                    tokenAddress={tokenAddress} 
-                    selectedAsset={selectedAsset} 
-                /> : <Sell 
-                    walletAddress={selectedAcc}
-                    networkDetails={networkDetails}
-                    relayerURL={relayerURL}
-                    portfolio={portfolio}
-                />}
-           </Panel>
+            <Tabs 
+                firstTabLabel='Send'
+                secondTabLabel='Sell Crypto'
+                firstTab={
+                    <Send 
+                        {...props}
+                        address={address} 
+                        setAddress={setAddress} 
+                        gasTankDetails={gasTankDetails} 
+                        asset={asset} 
+                        setAsset={setAsset} 
+                        tokenAddress={tokenAddress} 
+                        selectedAsset={selectedAsset} 
+                    />
+                }
+                secondTab={
+                    <Sell 
+                        walletAddress={selectedAcc}
+                        networkDetails={networkDetails}
+                        relayerURL={relayerURL}
+                        portfolio={portfolio}
+                    />
+                }
+                panelClassName={styles.panel}
+            />
            {!gasTankDetails && <Addresses
                 selectedAsset={selectedAsset}
                 selectedNetwork={selectedNetwork}
