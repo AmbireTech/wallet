774b28c7f9117f0c4ddbcd400c6d303af783ea8b
Petromir Petrov <petromir@devlabs.bg>
2022-10-19 08:43:32 +0300

    change: nofundsplaceholder to scss modules, fix: title align

--- src/components/common/NoFundsPlaceholder/NoFundsPlaceholder.js
+++ src/components/common/NoFundsPlaceholder/NoFundsPlaceholder.js
@@ -1,12 +1,12 @@
-import './NoFundsPlaceholder.scss'
-
 import { GiReceiveMoney } from 'react-icons/gi'
 import { NavLink } from 'react-router-dom'
 import { Button } from 'components/common'
 
+import styles from './NoFundsPlaceholder.module.scss'
+
 const NoFundsPlaceholder = () => {
     return (
-        <div id="no-funds-placeholder">
+        <div className={styles.wrapper}>
             <label>You don't have any funds on this account.</label>
             <NavLink to="/wallet/deposit">
                 <Button small icon={<GiReceiveMoney/>}>Deposit</Button>
