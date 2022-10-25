2eb3dc8fe7a02da8539b7674c68f6557dc4a0ec1
Petromir Petrov <petromir@devlabs.bg>
2022-10-25 18:02:38 +0300

    add: tabClassName prop to Tabs

--- src/components/common/Tabs/Tabs.js
+++ src/components/common/Tabs/Tabs.js
@@ -9,6 +9,7 @@ const Tabs = ({
   firstTab,
   secondTab,
   panelClassName,
+  tabClassName,
   footer
 }) => {
   const [currentTab, setCurrentTab] = useState(1)
@@ -21,13 +22,13 @@ const Tabs = ({
       <div className={styles.tabs}>
         <button 
           onClick={handleOpenFirst}
-          className={cn(styles.tabsButton, {[styles.active]: currentTab === 1})}
+          className={cn(styles.tabsButton, tabClassName, {[styles.active]: currentTab === 1})}
         >
           {firstTabLabel}
         </button>
         <button 
           onClick={handleOpenSecond}
-          className={cn(styles.tabsButton, {[styles.active]: currentTab === 2})}
+          className={cn(styles.tabsButton, tabClassName, {[styles.active]: currentTab === 2})}
         >
           {secondTabLabel}
         </button>
