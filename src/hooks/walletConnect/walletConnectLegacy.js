ec9d2c82412f0b21ae52a1c5913972945cca6e6e
0xdex18 <iam.digital@mailnova.com>
2022-10-27 16:22:17 +0200

    remove old logic - (before pancakeswap fix)

--- src/hooks/walletConnect/walletConnectLegacy.js
+++ src/hooks/walletConnect/walletConnectLegacy.js
@@ -27,8 +27,6 @@ const checkIsOffline = connectionId => {
   const errors = connectionErrors.filter(x => x.connectionId === connectionId)
   // if no errors, return false, else check time diff
   return errors.length > 0 && errors.find(({ time } = {}) => time > (Date.now() - timePastForConnectionErr))
-  //return errors.length > 1 && errors.slice(-2)
-  //    .every(({ time } = {}) => time > (Date.now() - timePastForConnectionErr))
 }
 
 export default function useWalletConnectLegacy({ account, chainId, clearWcClipboard, allNetworks, setNetwork, useStorage }) {
