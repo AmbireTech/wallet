653c6d4b84fa8857db96c6f8e8f6bdfc451b6b20
Petromir Petrov <petromir@devlabs.bg>
2022-10-27 17:56:25 +0300

    add: AddOrHideTokenButton in Protocols

---
+++ src/components/Wallet/Dashboard/Protocols/AddOrHideTokenButton/AddOrHideTokenButton.js
@@ -0,0 +1,11 @@
+import styles from './AddOrHideTokenButton.module.scss'
+
+const AddOrHideTokenButton = () => {
+  return (
+    <button className={styles.wrapper}>
+      Add or Hide Token
+    </button>
+  )
+}
+
+export default AddOrHideTokenButton
