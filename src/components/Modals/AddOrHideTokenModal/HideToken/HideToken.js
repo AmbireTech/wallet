2d7ce77f79087b34ad5e4277b9a0f14724f3b85a
Petromir <petromir@devlabs.bg>
2022-10-31 12:59:33 +0200

    add: hide token logic and style

--- src/components/Modals/AddOrHideTokenModal/HideToken/HideToken.js
+++ src/components/Modals/AddOrHideTokenModal/HideToken/HideToken.js
@@ -1,9 +1,72 @@
-import styles from './HideToken.module.scss'
+import { useMemo } from 'react'
+import {
+  MdVisibilityOff as VisibleIcon,
+  MdRemoveRedEye as HiddenIcon
+} from 'react-icons/md'
 
-const HideToken = () => (
-  <div className={styles.wrapper}>
-    Hide Token
-  </div>
-)
+import Token from 'components/Modals/AddOrHideTokenModal/Token/Token'
+
+import styles from './HideToken.module.scss'
+import { TextInput } from 'components/common'
+
+const HideToken = ({
+  network,
+  account,
+  portfolio,
+  userSorting, 
+  sortType
+}) => {
+  const { hiddenTokens, onAddHiddenToken, onRemoveHiddenToken, tokens } = portfolio
+
+  const hideToken = (token) => onAddHiddenToken(token)
+
+  const unhideToken = (token) => onRemoveHiddenToken(token.address)
+
+  const sortedTokens = useMemo(() => {
+    const tempTokens = tokens.concat(hiddenTokens).sort((a, b) => {
+      if (sortType === 'custom' && userSorting.tokens?.items?.[`${account}-${network.chainId}`]?.length) {
+        const sorted = userSorting.tokens.items[`${account}-${network.chainId}`].indexOf(a.address) - userSorting.tokens.items[`${account}-${network.chainId}`].indexOf(b.address)
+        return sorted
+      } else {
+        const decreasing = b.balanceUSD - a.balanceUSD
+        if (decreasing === 0) return a.symbol.localeCompare(b.symbol)
+        return decreasing
+      }
+    })
+
+    return [...new Map(tempTokens.map(token => [token.address, token])).values()]
+  }, [tokens, hiddenTokens, userSorting, sortType, account, network.chainId])
+
+  return (
+    <div className={styles.wrapper}>
+      <TextInput
+        small
+        label="Token Address or Symbol"
+        placeholder="Input token address or symbol"
+        // onInput={value => onInput(value)}
+        className={styles.addressInput}
+      />
+      <div className={styles.tokens}>
+        {sortedTokens.map((token) => {
+          const {tokenImageUrl, network, symbol, address, isHidden} = token
+          return (
+            <Token
+              key={address}
+              icon={tokenImageUrl}
+              name={symbol}
+              symbol={`(${network.toUpperCase()})`}
+              className={styles.token}
+            >
+              {!isHidden ? 
+                <HiddenIcon className={styles.icon} color="#27e8a7" onClick={() => hideToken(token)} /> :
+                <VisibleIcon className={styles.icon} color="#F21A61" onClick={() => unhideToken(token)} />
+              }
+            </Token>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
 
 export default HideToken
