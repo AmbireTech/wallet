967541ddfc17deaedea6d5d87fd3318b766fbdfc
Petromir Petrov <petromir@devlabs.bg>
2022-11-01 13:41:28 +0200

    add: wrapperEndChildren prop to CollectiblesWrapper

--- src/components/Wallet/Dashboard/Collectibles/CollectiblesWrapper/CollectiblesWrapper.js
+++ src/components/Wallet/Dashboard/Collectibles/CollectiblesWrapper/CollectiblesWrapper.js
@@ -1,12 +1,13 @@
 import cn from 'classnames'
 import styles from './CollectiblesWrapper.module.scss'
 
-const CollectiblesWrapper = ({wrapperChildren, children, className}) => (
+const CollectiblesWrapper = ({wrapperChildren, children, wrapperEndChildren, className}) => (
   <div className={styles.wrapper}>
     {wrapperChildren}
     <div className={cn(styles.collectiblesWrapper, className)}>
       {children}
-    </div>    
+    </div>
+    {wrapperEndChildren} 
   </div>        
 )
 
