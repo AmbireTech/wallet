6fd69d7b0c763cdc6549a2730ed390b69363f4a6
Petromir Petrov <petromir@devlabs.bg>
2022-11-02 16:51:43 +0200

    change: move tokens to tokens.js from TokensPlaceholder

--- src/components/Wallet/Dashboard/Tokens/TokensPlaceholder/TokensPlaceholder.js
+++ src/components/Wallet/Dashboard/Tokens/TokensPlaceholder/TokensPlaceholder.js
@@ -2,6 +2,7 @@ import { NavLink } from 'react-router-dom'
 import { Button } from 'components/common'
 import { useLocalStorage } from 'hooks'
 
+import tokens from './tokens'
 import TokensWrapper from 'components/Wallet/Dashboard/Tokens/TokensWrapper/TokensWrapper'
 
 import { ReactComponent as DepositIcon } from 'components/Wallet/SideBar/images/deposit.svg'
@@ -14,26 +15,6 @@ import tokenStyles from 'components/Wallet/Dashboard/Tokens/Token/Token.module.s
 import styles from './TokensPlaceholder.module.scss'
 
 const TokensPlaceholder = ({ onClickAddToken, onClickShowToken, footer }) => {
-    const tokens = [
-        {
-            icon: 'https://storage.googleapis.com/zapper-fi-assets/tokens/ethereum/0xade00c28244d5ce17d72e40330b1c318cd12b7c3.png',
-            symbol: 'ADX',
-            balance: 170027.26,
-            balanceUSD: 138880.64
-        },
-        {
-            icon: 'https://storage.googleapis.com/zapper-fi-assets/tokens/ethereum/0x0000000000000000000000000000000000000000.png',
-            symbol: 'ETH',
-            balance: 61.77,
-            balanceUSD: 283170.08
-        },
-        {
-            icon: 'https://storage.googleapis.com/zapper-fi-assets/tokens/ethereum/0x2260fac5e5542a773aa44fbcfedf7c193bc2c599.png',
-            symbol: 'WBTC',
-            balance: 0.03,
-            balanceUSD: 2227.24
-        },
-    ]
 
     const listHiddenTokens = useLocalStorage({ key: 'hiddenTokens'})
     let hiddenTokensCount = 0
