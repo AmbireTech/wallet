651fd4b048b64d04656cca4d605944d4274e07af
Petromir Petrov <petromir@devlabs.bg>
2022-11-02 16:36:08 +0200

    add: hidden collectibles label in CollectiblesPlaceholder

--- src/components/Wallet/Dashboard/Collectibles/CollectiblesPlaceholder/CollectiblesPlaceholder.js
+++ src/components/Wallet/Dashboard/Collectibles/CollectiblesPlaceholder/CollectiblesPlaceholder.js
@@ -1,36 +1,52 @@
+import collectibles from './collectibles'
+
+import { useLocalStorage } from 'hooks'
 import Collectible from 'components/Wallet/Dashboard/Collectibles/Collectible/Collectible'
 import CollectiblesWrapper from 'components/Wallet/Dashboard/Collectibles/CollectiblesWrapper/CollectiblesWrapper'
 
-import collectibles from './collectibles'
-
 import styles from './CollectiblesPlaceholder.module.scss'
 
-const CollectiblesPlaceholder = ({ isPrivateMode, collectiblesLength, footer }) => (
-    <CollectiblesWrapper 
-        className={styles.blur}
-        wrapperChildren={
-            <div className={styles.placeholderOverlay}>
-                <div className={styles.placeholderText}>
-                    {(isPrivateMode && collectiblesLength) ? 'You can\'t see collectibles in private mode' : 'You don\'t have any collectibles (NFTs) yet' }
+const CollectiblesPlaceholder = ({ isPrivateMode, collectiblesLength, footer, onClickShowCollectible }) => {
+    const listHiddenCollectibles = useLocalStorage({ key: 'hiddenCollectibles'})
+    let hiddenCollectiblesCount = 0
+    
+    if (listHiddenCollectibles && listHiddenCollectibles[0] !== null) {
+        hiddenCollectiblesCount = listHiddenCollectibles[0].length
+    }
+
+    return (
+        <CollectiblesWrapper 
+            className={styles.blur}
+            wrapperChildren={
+                <div className={styles.placeholderOverlay}>
+                    <div className={styles.placeholderText}>
+                        {(isPrivateMode && collectiblesLength) ? <h2>
+                            You can't see collectibles in private mode
+                        </h2> : <h2>You don't have any collectibles (NFTs) yet</h2>}
+                        {hiddenCollectiblesCount > 0 && (<p clear onClick={onClickShowCollectible}>
+                            There are also {hiddenCollectiblesCount} hidden collectibles. Click to configure
+                        </p>)}
+                    </div>
                 </div>
-            </div>
-        }
-        wrapperEndChildren={footer}
-    >
-        {
-            collectibles.map(({ collectionName, collectionImg, name, image, price}) => (
-                <Collectible
-                    key={name}
-                    collectionIcon={collectionImg}
-                    collectionName={collectionName}
-                    image={image}
-                    name={name}
-                    price={price}
-                    href="/wallet"
-                />
-            ))
-        }
-    </CollectiblesWrapper>
-)
+            }
+            wrapperEndChildren={footer}
+        >
+            {
+                collectibles.map(({ collectionName, collectionImg, name, image, price}) => (
+                    <Collectible
+                        key={name}
+                        collectionIcon={collectionImg}
+                        collectionName={collectionName}
+                        image={image}
+                        name={name}
+                        price={price}
+                        href="/wallet"
+                    />
+                ))
+            }
+        </CollectiblesWrapper>
+    )
+
+}
 
 export default CollectiblesPlaceholder
