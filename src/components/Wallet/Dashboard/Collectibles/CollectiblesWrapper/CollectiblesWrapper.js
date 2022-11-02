cc113d493f4ba9f2a8c2acc7041a215938c3cb9b
Petromir Petrov <petromir@devlabs.bg>
2022-11-02 15:47:46 +0200

    add: .relative div that wraps wrapperChildren and .collectiblesWrapper

--- src/components/Wallet/Dashboard/Collectibles/CollectiblesWrapper/CollectiblesWrapper.js
+++ src/components/Wallet/Dashboard/Collectibles/CollectiblesWrapper/CollectiblesWrapper.js
@@ -3,9 +3,11 @@ import styles from './CollectiblesWrapper.module.scss'
 
 const CollectiblesWrapper = ({wrapperChildren, children, wrapperEndChildren, className}) => (
   <div className={styles.wrapper}>
-    {wrapperChildren}
-    <div className={cn(styles.collectiblesWrapper, className)}>
-      {children}
+    <div className={styles.relative}>
+      {wrapperChildren}
+      <div className={cn(styles.collectiblesWrapper, className)}>
+        {children}
+      </div>
     </div>
     {wrapperEndChildren} 
   </div>        
