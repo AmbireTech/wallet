a01e73c78afeac1a83aedf5e3b65b3e85aa70eb9
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2022-11-02 15:19:10 +0200

    Fixed params for 'nativeToken' and 'token' funcs im swappin

--- src/lib/humanizers/Swappin.js
+++ src/lib/humanizers/Swappin.js
@@ -8,8 +8,8 @@ const SwappinMapping = (humanizerInfo) => {
     return {
         [swappin.getSighash('swap')]: (txn, network, { extended = false }) => {
             const { desc } = swappin.parseTransaction(txn).args
-            const paymentSrcToken = Number(desc.srcToken) === 0 ? nativeToken(network, desc.amount, true) : token(humanizerInfo, desc.srcToken, parseFloat(desc.amount), true)
-            const paymentToken = Number(desc.dstToken) === 0 ? nativeToken(network, desc.minReturnAmount, true) : token(humanizerInfo, desc.dstToken, parseFloat(desc.minReturnAmount), true)
+            const paymentSrcToken = Number(desc.srcToken) === 0 ? nativeToken(network, desc.amount, extended) : token(humanizerInfo, desc.srcToken, parseFloat(desc.amount), extended)
+            const paymentToken = Number(desc.dstToken) === 0 ? nativeToken(network, desc.minReturnAmount, extended) : token(humanizerInfo, desc.dstToken, parseFloat(desc.minReturnAmount), extended)
             
             return !extended
                 ? [`Swap ${paymentSrcToken} for at least ${paymentToken} on Swappin`]
