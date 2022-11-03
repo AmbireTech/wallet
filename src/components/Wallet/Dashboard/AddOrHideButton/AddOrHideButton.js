86eb41a3ddd01ef524f10d8dbe43ff12ac440521
Petromir Petrov <petromir@devlabs.bg>
2022-11-03 17:51:16 +0200

    change: es6 return instead of return in AddOrHideButton

--- src/components/Wallet/Dashboard/AddOrHideButton/AddOrHideButton.js
+++ src/components/Wallet/Dashboard/AddOrHideButton/AddOrHideButton.js
@@ -1,14 +1,11 @@
 import styles from './AddOrHideButton.module.scss'
 
-const AddOrHideButton = ({ onClick, children }) => {
-
-  return (
-    <button className={styles.wrapper} onClick={onClick}>
-      Add or Hide
-      {' '}
-      { children }
-    </button>
-  )
-}
+const AddOrHideButton = ({ onClick, children }) => (
+  <button className={styles.wrapper} onClick={onClick}>
+    Add or Hide
+    {' '}
+    { children }
+  </button>
+)
 
 export default AddOrHideButton
