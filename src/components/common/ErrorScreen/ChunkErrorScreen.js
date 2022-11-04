cc289c14959648e1984cc0c361ae0444253a1cac
Kaloyan Kosev <superkalo@devlabs.bg>
2022-11-04 12:15:19 +0200

    Add: chunk failed to load screen

---
+++ src/components/common/ErrorScreen/ChunkErrorScreen.js
@@ -0,0 +1,29 @@
+import './ErrorScreen.scss'
+import { Button } from 'components/common'
+
+const ChunkErrorScreen = () => {
+  const refresh = (e) => {
+    e.preventDefault()
+    window.location.reload(true)
+  }
+
+  return (
+    <div className="error-screen">
+      <div className="error-screen-content panel">
+        <a href={window.location.origin}>
+          <div className="logo" />
+        </a>
+        <h1>The Ambire app has been updated ✅</h1>
+        <p>Please <a href="/" className="error-screen-content-refresh-btn" onClick={refresh}>refresh</a> to see the latest content.</p>
+        <p>If your attempt to refresh takes you to back here,<br />please contact us via our Help Center.</p>
+        <div className="error-screen-buttons">
+          <a href="https://help.ambire.com/hc/en-us/categories/4404980091538-Ambire-Wallet" target="_blank" rel="noreferrer">
+            <Button title="Visit the Help center" small>Help center</Button>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ChunkErrorScreen
