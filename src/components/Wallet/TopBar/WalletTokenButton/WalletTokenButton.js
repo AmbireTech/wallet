7f62453a9a8af507f05aa8610e492db478570672
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2022-11-09 16:21:42 +0200

    modified renderRewardsButtonText in walletTokenButton to be the same as in the mobile-app

--- src/components/Wallet/TopBar/WalletTokenButton/WalletTokenButton.js
+++ src/components/Wallet/TopBar/WalletTokenButton/WalletTokenButton.js
@@ -2,10 +2,12 @@ import useDynamicModal from "hooks/useDynamicModals"
 import { Button, ToolTip, Loading } from "components/common"
 import WalletTokenModal from "components/Modals/WalletTokenModal/WalletTokenModal"
 import useClaimableWalletToken from 'ambire-common/src/hooks/useClaimableWalletToken'
+import { useCallback } from 'react'
 
 import styles from './WalletTokenButton.module.scss'
 
 const WalletTokenButton = ({ rewardsData, account = {}, network, hidePrivateValue, addRequest, relayerURL, useRelayerData }) => {
+    const { isLoading: rewardsIsLoading, errMsg: rewardsErrMsg, lastUpdated: rewardsLastUpdated } = rewardsData
     const claimableWalletToken = useClaimableWalletToken({
         relayerURL,
         useRelayerData,
@@ -14,41 +16,48 @@ const WalletTokenButton = ({ rewardsData, account = {}, network, hidePrivateValu
         addRequest,
         totalLifetimeRewards: rewardsData.rewards.totalLifetimeRewards,
         walletUsdPrice: rewardsData.rewards.walletUsdPrice,
-        rewardsLastUpdated: rewardsData.lastUpdated
+        rewardsLastUpdated
       })
 
     const { currentClaimStatus, pendingTokensTotal } = claimableWalletToken
-    const { isLoading: isRewardsDataLoading, errMsg } = rewardsData
-    const isLoading = isRewardsDataLoading || currentClaimStatus.loading
     const showWalletTokenModal = useDynamicModal(WalletTokenModal, { claimableWalletToken, accountId: account.id }, { rewards: rewardsData.rewards })
-    const renderRewardsButtonText = () => {
-        if (currentClaimStatus.loading && !currentClaimStatus.lastUpdated) {
+    const renderRewardsButtonText = useCallback(() => {
+        // The rewards value depends on both - the currentClaimStatus and the
+        // rewards data. Therefore - require both data sets to be loaded.
+        const hasErrorAndNoPrevValues =
+          (currentClaimStatus.error || rewardsErrMsg) &&
+          (!currentClaimStatus.lastUpdated || !rewardsLastUpdated)
+        if (hasErrorAndNoPrevValues) {
+          return 'Rewards'
+        }
+    
+        // Display loading state only if prev data is missing for any of both data sets.
+        // For all other cases - display the prev data instead of loading indicator,
+        // so that the UI doesn't jump by switching loading indicator on and off.
+        const isCurrentClaimStatusLoadingAndNoPrevData =
+          currentClaimStatus.loading && !currentClaimStatus.lastUpdated
+        const isRewardsDataLoadingAndNoPrevData = rewardsIsLoading && !rewardsLastUpdated
+        if (isCurrentClaimStatusLoadingAndNoPrevData || isRewardsDataLoadingAndNoPrevData) {
           return (<span><Loading/></span>)
         }
     
-        if (isRewardsDataLoading && !rewardsData.lastUpdated) {
-          return (<span><Loading/></span>)
-        }
-    
-        return `${hidePrivateValue(pendingTokensTotal)} $ WALLETS`
-    }
+        return `${hidePrivateValue(pendingTokensTotal)} $WALLETs`
+    }, [currentClaimStatus.error, currentClaimStatus.lastUpdated, currentClaimStatus.loading, hidePrivateValue, pendingTokensTotal, rewardsErrMsg, rewardsIsLoading, rewardsLastUpdated])
 
     return (
-        !isLoading && (errMsg || currentClaimStatus.error) ?
+        !relayerURL ?
             <ToolTip label="WALLET rewards are not available without a connection to the relayer">
-                <Button small border disabled onClick={showWalletTokenModal}>Unavailable</Button>
+                <Button small border disabled>Rewards</Button>
             </ToolTip>
             :
             <Button
                 small
                 border
-                disabled={isLoading}
                 onClick={showWalletTokenModal}
                 className={styles.button}
                 style={{ textTransform: 'none'}}
             >
-                { renderRewardsButtonText() }
-                
+                { renderRewardsButtonText() }  
             </Button>
     )
 }
