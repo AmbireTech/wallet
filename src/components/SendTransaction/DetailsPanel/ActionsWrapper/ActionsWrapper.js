e2eb9976fc685d5e09b26b015a9178586e408f0c
Petromir <petromir@devlabs.bg>
2022-11-11 15:23:04 +0200

    change: simplify ActionsWrapper styles

--- src/components/SendTransaction/DetailsPanel/ActionsWrapper/ActionsWrapper.js
+++ src/components/SendTransaction/DetailsPanel/ActionsWrapper/ActionsWrapper.js
@@ -1,11 +1,6 @@
-import cn from 'classnames'
-
+import { Alert } from 'components/common';
 import FailingTxn from "components/SendTransaction/DetailsPanel/FailingTxn/FailingTxn";
 import Actions from "./Actions/Actions";
-
-import { ReactComponent as InfoIcon } from 'resources/icons/information.svg'
-
-import styles from './ActionsWrapper.module.scss';
 
 const ActionsWrapper = ({
   signingStatus,
@@ -28,56 +23,53 @@ const ActionsWrapper = ({
 }) => {
 
   return canProceed && (
-    <>
+    <div>
       {estimation &&
       estimation.success &&
       estimation.isDeployed === false &&
       bundle.gasLimit ? (
-        <div className={styles.firstTxNote}>
-          <div className={styles.firstTxNoteTitle}>
-            <InfoIcon />
-            Note
-          </div>
-          <div className={styles.firstTxNoteMessage}>
-            Because this is your first Ambire transaction, this fee is{" "}
-            {((60000 / bundle.gasLimit) * 100).toFixed()}% higher than usual
-            because we have to deploy your smart wallet. Subsequent
-            transactions will be cheaper
-          </div>
-        </div>
+      <Alert
+        type="information"
+        title="Note"
+        text={`
+          Because this is your first Ambire transaction, this fee is 
+          ${((60000 / bundle.gasLimit) * 100).toFixed()}% higher than usual
+          because we have to deploy your smart wallet. Subsequent
+          transactions will be cheaper
+        `}
+        iconNextToTitle={true}
+      />
       ) : null}
 
-      <div className={styles.actionsWrapper}>
-        {bundle.signer.quickAccManager && !relayerURL ? (
-          <FailingTxn message="Signing transactions with an email/password account without being connected to the relayer is unsupported." />
-        ) : (
-          <div className={cn(styles.section, styles.actions)}>
-            <Actions
-              estimation={estimation}
-              rejectTxn={rejectTxn}
-              cancelSigning={() => setSigningStatus(null)}
-              signingStatus={signingStatus}
-              setSigningStatus={setSigningStatus}
-              feeSpeed={feeSpeed}
-              isGasTankEnabled={
-                currentAccGasTankState.isEnabled && !!relayerURL
-              }
-              network={network}
-              account={account}
-              bundle={bundle}
-              isMounted={isMounted}
-              onDismiss={onDismiss}
-              relayerURL={relayerURL}
-              isInt={isInt}
-              replaceTx={replaceTx}
-              currentAccGasTankState={currentAccGasTankState}
-              onBroadcastedTxn={onBroadcastedTxn}
-              resolveMany={resolveMany}
-            />
-          </div>
-        )}
-      </div>
-    </>
+      {bundle.signer.quickAccManager && !relayerURL ? (
+        <FailingTxn message="Signing transactions with an email/password account without being connected to the relayer is unsupported." />
+      ) : (
+        <div>
+          <Actions
+            estimation={estimation}
+            rejectTxn={rejectTxn}
+            cancelSigning={() => setSigningStatus(null)}
+            signingStatus={signingStatus}
+            setSigningStatus={setSigningStatus}
+            feeSpeed={feeSpeed}
+            isGasTankEnabled={
+              currentAccGasTankState.isEnabled && !!relayerURL
+            }
+            network={network}
+            account={account}
+            bundle={bundle}
+            isMounted={isMounted}
+            onDismiss={onDismiss}
+            relayerURL={relayerURL}
+            isInt={isInt}
+            replaceTx={replaceTx}
+            currentAccGasTankState={currentAccGasTankState}
+            onBroadcastedTxn={onBroadcastedTxn}
+            resolveMany={resolveMany}
+          />
+        </div>
+      )}
+    </div>
   )
 }
 
