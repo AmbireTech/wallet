5448c1b7e96a98d229f7493bc005dbe5c207b6f0
Simeon Krasimirov Petkov <simeonpetkov11999977@gmail.com>
2022-11-16 13:09:47 +0200

    Add: init function for configurable providers. Remove local getProvider and replaced with getProvider from common

--- src/hooks/notifications.js
+++ src/hooks/notifications.js
@@ -5,7 +5,7 @@ import { formatUnits } from 'ethers/lib/utils'
 import { useToasts } from './toasts'
 import networks from 'consts/networks'
 import AMBIRE_ICON from 'resources/icon.png'
-import { getProvider } from 'lib/provider'
+import { getProvider } from 'ambire-common/src/services/provider'
 import useConstants from './useConstants'
 
 const REQUEST_TITLE_PREFIX = 'Ambire Wallet: '
