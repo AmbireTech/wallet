e6a8ff29824485f942a06e419c2df1e7187e0216
Petromir Petrov <petromir@devlabs.bg>
2022-11-19 21:32:17 +0200

    add: className prop to Image

--- src/components/common/Image/Image.js
+++ src/components/common/Image/Image.js
@@ -5,12 +5,12 @@ import { ReactComponent as FallbackIcon } from 'resources/icons/fallback.svg'
 
 import styles from  './Image.module.scss'
 
-export default function Image({ src, fallback, size = 64, alt = 'image', imageClassName, failedClassName }) {
+export default function Image({ src, fallback, size = 64, alt = 'image', className, imageClassName, failedClassName }) {
 
   const [failed, setFailed] = useState(false)
 
   return (
-    <div className={styles.image} >
+    <div className={cn(styles.image, className)} >
       {
         failed
           ? (fallback || <FallbackIcon className={cn(imageClassName, {[failedClassName]: failed && failedClassName})} />)
