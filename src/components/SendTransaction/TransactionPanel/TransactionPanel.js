cca3138ca78bd89ccd623d1ee52641388bfc591e
Petromir Petrov <petromir@devlabs.bg>
2022-11-25 17:42:04 +0200

    change: Title inside panels in SendTransaction

--- src/components/SendTransaction/TransactionPanel/TransactionPanel.js
+++ src/components/SendTransaction/TransactionPanel/TransactionPanel.js
@@ -11,51 +11,54 @@ const TransactionPanel = ({
   REJECT_MSG,
   resolveMany,
   signingStatus,
-  panelClassName
+  panelClassName,
+  panelTitleClassName,
 }) => {
   return (
     <Panel className={cn(panelClassName, styles.wrapper)}>
-      <div
-        className={cn(styles.listOfTransactions, {
-          [styles.frozen]: !bundle.requestIds,
-        })}
-      >
-        {bundle.txns.map((txn, i) => {
-          const isFirstFailing =
-            estimation &&
-            !estimation.success &&
-            estimation.firstFailing === i;
-          // we need to re-render twice per minute cause of DEX deadlines
-          const min = Math.floor(Date.now() / 30000);
-          return (
-            <TxnPreview
-              key={[...txn, i].join(":")}
-              // pasing an unused property to make it update
-              minute={min}
-              onDismiss={
-                bundle.requestIds &&
-                (() =>
-                  resolveMany([bundle.requestIds[i]], {
-                    message: REJECT_MSG,
-                  }))
-              }
-              txn={txn}
-              network={bundle.network}
-              account={bundle.identity}
-              isFirstFailing={isFirstFailing}
-              disableDismiss={!!signingStatus}
-              disableDismissLabel={
-                "Cannot modify transaction bundle while a signing procedure is pending"
-              }
-              addressLabel={!!bundle.meta && bundle.meta.addressLabel}
-            />
-          );
-        })}
+      <div>
+        <h2 className={cn(panelTitleClassName, styles.title)}>{ bundle.txns.length } Transaction{ bundle.txns.length > 1 ? 's' : '' } Waiting</h2>
+        <div
+          className={cn(styles.listOfTransactions, {
+            [styles.frozen]: !bundle.requestIds,
+          })}
+        >
+          {bundle.txns.map((txn, i) => {
+            const isFirstFailing =
+              estimation &&
+              !estimation.success &&
+              estimation.firstFailing === i;
+            // we need to re-render twice per minute cause of DEX deadlines
+            const min = Math.floor(Date.now() / 30000);
+            return (
+              <TxnPreview
+                key={[...txn, i].join(":")}
+                // pasing an unused property to make it update
+                minute={min}
+                onDismiss={
+                  bundle.requestIds &&
+                  (() =>
+                    resolveMany([bundle.requestIds[i]], {
+                      message: REJECT_MSG,
+                    }))
+                }
+                txn={txn}
+                network={bundle.network}
+                account={bundle.identity}
+                isFirstFailing={isFirstFailing}
+                disableDismiss={!!signingStatus}
+                disableDismissLabel={
+                  "Cannot modify transaction bundle while a signing procedure is pending"
+                }
+                addressLabel={!!bundle.meta && bundle.meta.addressLabel}
+              />
+            );
+          })}
+        </div>
       </div>
-
       {bundle.requestIds && <Alert
         title="Degen tip"
-        type="info"
+        type="degenTip"
         text="
           You can sign multiple transactions at once. Add more
           transactions to this batch by interacting with a connected
