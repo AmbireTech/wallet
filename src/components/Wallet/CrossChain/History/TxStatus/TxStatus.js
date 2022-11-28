e6970158d29753fe419ac724c6f45aad4c1c4108
Petromir Petrov <petromir@devlabs.bg>
2022-11-28 15:33:47 +0200

    change: use TxStatusComponent in TxStatus

--- src/components/Wallet/CrossChain/History/TxStatus/TxStatus.js
+++ src/components/Wallet/CrossChain/History/TxStatus/TxStatus.js
@@ -4,9 +4,9 @@ import { Loading } from 'components/common'
 
 import { MdOutlineCheck, MdOutlineClose } from 'react-icons/md'
 import { ReactComponent as ExternalLinkIcon } from 'resources/icons/external-link.svg'
-import { ReactComponent as SwapIcon } from 'resources/icons/cross-chain.svg'
 
 import styles from './TxStatus.module.scss'
+import TxStatusComponent from 'components/Wallet/CrossChain/TxStatusComponent/TxStatusComponent'
 
 const formatAmount = (amount, asset) => amount / Math.pow(10, asset.decimals)
 
@@ -14,56 +14,18 @@ const TxStatus = ({
   data: { sourceTx, fromNetwork, toNetwork, toAsset, toAmount, from, to, serviceTimeMinutes, isPending, statusError },
 }) => {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.summary}>
-        <div className={styles.networks}>
-          {/* From Network */}
-          <div className={styles.network}>
-            <div className={styles.iconWrapper}>
-              <img src={fromNetwork.icon} alt="" className={styles.icon} />
-            </div>
-            <div className={styles.networkName}>
-              <p>From</p>
-              <h4 className={styles.name}>{fromNetwork.name}</h4>
-            </div>
-          </div>
-          {/* To Network */}
-          <div className={cn(styles.network, styles.toNetwork)}>
-            <div className={styles.iconWrapper}>
-              <img src={toNetwork.icon} alt="" className={styles.icon} />
-            </div>
-            <div className={styles.networkName}>
-              <p>To</p>
-              <h4 className={styles.name}>{toNetwork.name}</h4>
-            </div>
-          </div>
-        </div>
-        <div className={styles.tokens}>
-          {/* From Token */}
-          <div className={styles.token}>
-            <p className={styles.amount}>
-              {from.amount ? formatAmount(from.amount, from.asset) : ''}
-            </p>
-            <div className={styles.iconWrapper}>
-              <img className={styles.icon} alt="" src={from?.asset?.icon} />
-            </div>
-            <p className={styles.amount}>{from?.asset?.symbol}</p>
-          </div>
-          {/* Swap Icon  */}
-          <SwapIcon className={styles.swapIcon} />
-          {/* To Token */}
-          <div className={cn(styles.token, styles.toToken)}>
-            <p className={styles.amount}>
-            {to.amount ? formatAmount(to.amount, to.asset) : ''}
-            {toAsset && toAmount ? formatAmount(parseFloat(toAmount), toAsset) : ''}
-            </p>
-            <div className={styles.iconWrapper}>
-              <img className={styles.icon} alt="" src={to?.asset?.icon} />
-            </div>
-            <p className={styles.amount}>{to?.asset?.symbol}</p>
-          </div>
-        </div>
-      </div>
+    <TxStatusComponent
+      fromNetworkIcon={fromNetwork.icon}
+      fromNetworkName={fromNetwork.name}
+      toNetworkIcon={toNetwork.icon}
+      toNetworkName={toNetwork.name}
+      fromTokenName={from?.asset?.symbol}
+      fromTokenAmount={from.amount ? formatAmount(from.amount, from.asset) : ''}
+      fromTokenIcon={from?.asset?.icon}
+      toTokenName={to?.asset?.symbol}
+      toTokenAmount={to.amount ? formatAmount(to.amount, to.asset) : ((toAsset && toAmount) ? formatAmount(parseFloat(toAmount), toAsset) : '')}
+      toTokenIcon={to?.asset?.icon}
+    >
       <div className={styles.details}>
         <a
           className={styles.explorerLink}
@@ -91,7 +53,7 @@ const TxStatus = ({
           </p>
         )}
       </div>
-    </div>
+    </TxStatusComponent>
   )
 }
 
