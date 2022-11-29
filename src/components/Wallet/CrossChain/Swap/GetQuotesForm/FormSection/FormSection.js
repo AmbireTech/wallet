f9d6329015784a52437ae5440913c61b7cd556b6
Petromir Petrov <petromir@devlabs.bg>
2022-11-29 17:28:33 +0200

    add: Cross-Chain FormSection

---
+++ src/components/Wallet/CrossChain/Swap/GetQuotesForm/FormSection/FormSection.js
@@ -0,0 +1,13 @@
+import cn from 'classnames'
+
+import { Loading } from 'components/common'
+
+import styles from './FormSection.module.scss'
+
+const LoadingFormElement = ({ isLoading, children, smaller }) => isLoading ? (
+  <div className={cn(styles.wrapper, {[styles.smaller]: smaller})}>
+    <Loading />
+  </div>
+) : children
+
+export default LoadingFormElement
