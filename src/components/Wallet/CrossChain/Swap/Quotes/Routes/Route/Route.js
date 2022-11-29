708d68c1569f1c084e0478e0be2c26d3c09255a5
Petromir Petrov <petromir@devlabs.bg>
2022-11-29 17:28:47 +0200

    redesign: Cross-Chain Route

--- src/components/Wallet/CrossChain/Swap/Quotes/Routes/Route/Route.js
+++ src/components/Wallet/CrossChain/Swap/Quotes/Routes/Route/Route.js
@@ -1,57 +1,57 @@
 import cn from 'classnames'
+import Footer from './Footer/Footer'
+
+import { ReactComponent as SwapIcon } from 'resources/icons/cross-chain.svg'
 
 import styles from './Route.module.scss'
+import Separator from './Separator/Separator'
 
-const formatAmount = (amount, asset) => amount / Math.pow(10, asset.decimals)
+const formatAmount = (amount, asset) => (amount / Math.pow(10, asset.decimals))
 
-const Route = ({ data: { bridgeStep, bridgeFee, maxServiceTime, serviceTime, middlewareRoute, middlewareFee } }) => (
+const Route = ({
+  data: { bridgeStep, middlewareRoute, middlewareFee, bridgeFee, maxServiceTime, serviceTime, fromAsset, toAsset },
+}) => (
   <div className={styles.wrapper}>
-    <div className={styles.info}>
+    <div className={styles.body}>
+      <div className={styles.iconWrapper}>
+        <img className={styles.icon} src={fromAsset.icon} alt="" />
+        <p className={styles.fromAmount}>
+          {middlewareRoute
+            ? formatAmount(middlewareRoute.fromAmount, middlewareRoute.fromAsset)
+            : formatAmount(bridgeStep.fromAmount, bridgeStep.fromAsset)}
+        </p>
+      </div>
+      <Separator />
       {middlewareRoute ? (
-        <div className={styles.middleware}>
-          <div className={styles.icon} style={{ backgroundImage: `url(${middlewareRoute.protocol.icon})` }}></div>
-          <div className={styles.name}>{middlewareRoute.protocol.displayName}</div>
+        <div className={styles.middlewareWrapper}>
+          <div className={cn(styles.topItem, styles.middleware)}>
+            <img className={styles.topItemIcon} src={middlewareRoute.protocol.icon} alt="" />
+            <h4 className={styles.topItemName}>{middlewareRoute.protocol.displayName}</h4>
+          </div>
+          <img className={styles.middlewareAssetIcon} src={middlewareRoute.fromAsset.icon} alt="" />
+          <img className={styles.middlewareAssetIcon} src={middlewareRoute.toAsset.icon} alt="" />
         </div>
       ) : null}
-      <div className={styles.bridge}>
-        <div className={styles.icon} style={{ backgroundImage: `url(${bridgeStep.protocol.icon})` }}></div>
-        <div className={styles.name}>{bridgeStep.protocol.displayName}</div>
-      </div>
-    </div>
-    <div className={styles.summary}>
-      <div className={styles.amounts}>
-        {middlewareRoute ? (
-          <div className={cn(styles.amount, styles.middleware)}>
-            {formatAmount(middlewareRoute.fromAmount, middlewareRoute.fromAsset)} {middlewareRoute.fromAsset.symbol}
-          </div>
-        ) : null}
-        <div className={cn(styles.amount, styles.bridge)}>
-          {formatAmount(bridgeStep.toAmount, bridgeStep.toAsset)} {bridgeStep.toAsset.symbol}
+      {middlewareRoute ? <Separator /> : null}
+      <div className={cn(styles.iconWrapper, styles.swapIconWrapper)}>
+        <SwapIcon className={styles.swapIcon} />
+        <div className={styles.topItem}>
+          <img className={styles.topItemIcon} src={bridgeStep.protocol.icon} alt="" />
+          <h4 className={styles.topItemName}>{bridgeStep.protocol.displayName}</h4>
         </div>
       </div>
-      <div className={styles.fees}>
-        {middlewareFee ? (
-          <div className={cn(styles.fee, styles.middleware)}>
-            {middlewareFee ? (
-              <>
-                Fee: {middlewareFee} {middlewareRoute?.protocolFees?.asset?.symbol}
-              </>
-            ) : null}
-          </div>
-        ) : null}
-        {bridgeFee ? (
-          <div className={cn(styles.fee, styles.bridge)}>
-            {bridgeFee ? (
-              <>
-                Fee: {bridgeFee} {bridgeStep?.protocolFees?.asset?.symbol}
-              </>
-            ) : null}
-          </div>
-        ) : null}
+      <Separator />
+      <div className={styles.iconWrapper}>
+        <img src={toAsset.icon} alt="" className={styles.icon} />
+        <p className={styles.toAmount}>{formatAmount(bridgeStep.toAmount, bridgeStep.toAsset)}</p>
       </div>
-      <div className={styles.time}>{`ETA ${serviceTime / 60} minutes`}</div>
-      <div className={styles.time}>{`Max ETA ${maxServiceTime / 60} minutes`}</div>
     </div>
+    <Footer
+      fee={bridgeFee + middlewareFee}
+      feeSymbol={toAsset.symbol}
+      serviceTime={serviceTime}
+      maxServiceTime={maxServiceTime}
+    />
   </div>
 )
 
