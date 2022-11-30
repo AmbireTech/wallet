25f3ad77ccf9c1651b2fadaeafcf3688ce4ee6bf
Petromir Petrov <petromir@devlabs.bg>
2022-11-30 14:15:39 +0200

    move: formatAmount to CrossChain.js

--- src/components/Wallet/CrossChain/CrossChain.js
+++ src/components/Wallet/CrossChain/CrossChain.js
@@ -5,6 +5,18 @@ import Swap from './Swap/Swap'
 
 import styles from './CrossChain.module.scss'
 
+export const formatAmount = (amount, asset) => {
+    let decimals = 4
+    const formatedAmount =  amount / Math.pow(10, asset.decimals)
+  
+    if (formatedAmount < 100) {
+      decimals = 5
+    }
+  
+    // Remove trailing zeros
+    return formatedAmount.toFixed(decimals).replace(/0+$/, '').replace(/[.]$/, '')
+}
+
 const CrossChain = ({ addRequest, selectedAccount, portfolio, network, relayerURL }) => {
     const [quotesConfirmed, setQuotesConfirmed] = useLocalStorage({ key: 'quotesConfirmed', defaultValue: [] })
 
