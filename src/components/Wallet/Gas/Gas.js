48a7f8999b3c09d3bef6914ea6b2857b9951d54c
Petromir Petrov <petromir@devlabs.bg>
2022-12-02 15:17:03 +0200

    fix: Gas.js network not supported error style

--- src/components/Wallet/Gas/Gas.js
+++ src/components/Wallet/Gas/Gas.js
@@ -2,38 +2,39 @@ import LeftPanel from './LeftPanel/LeftPanel'
 import RightPanel from './RightPanel/RightPanel'
 
 import styles from './Gas.module.scss'
+import { Alert } from 'components/common'
 
-const Gas = ({ 
-    selectedNetwork, 
-    relayerURL, 
-    portfolio, 
-    selectedAccount, 
-    gasTankState, 
-    setGasTankState
- }) => {
-    
-    return selectedNetwork.isGasTankAvailable ? (
-        <section className={styles.wrapper}>
-            <LeftPanel
-                network={selectedNetwork}
-                relayerURL={relayerURL}
-                portfolio={portfolio}
-                account={selectedAccount}
-                gasTankState={gasTankState}
-                setGasTankState={setGasTankState}
-                panelClassName={styles.panel}
-            />
-            <RightPanel
-                network={selectedNetwork}
-                relayerURL={relayerURL}
-                portfolio={portfolio}
-                account={selectedAccount}
-                gasTankState={gasTankState}
-                setGasTankState={setGasTankState}
-                panelClassName={styles.panel}
-            /> 
-        </section>
-    ) : <h3 className="error">Gas Tank is not available on {selectedNetwork.id.toUpperCase()}</h3>
-}
+const Gas = ({ selectedNetwork, relayerURL, portfolio, selectedAccount, gasTankState, setGasTankState }) =>
+  selectedNetwork.isGasTankAvailable ? (
+    <section className={styles.wrapper}>
+      <LeftPanel
+        network={selectedNetwork}
+        relayerURL={relayerURL}
+        portfolio={portfolio}
+        account={selectedAccount}
+        gasTankState={gasTankState}
+        setGasTankState={setGasTankState}
+        panelClassName={styles.panel}
+      />
+      <RightPanel
+        network={selectedNetwork}
+        relayerURL={relayerURL}
+        portfolio={portfolio}
+        account={selectedAccount}
+        gasTankState={gasTankState}
+        setGasTankState={setGasTankState}
+        panelClassName={styles.panel}
+      />
+    </section>
+  ) : (
+    <div className={styles.unavailable}>
+      <Alert
+        type="danger"
+        title={`Gas Tank is not available on ${
+          selectedNetwork.id.charAt(0).toUpperCase() + selectedNetwork.id.slice(1)
+        }`} // Capitalize the network id
+      />
+    </div>
+  )
 
 export default Gas
