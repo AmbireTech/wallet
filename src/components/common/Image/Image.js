91466b34a7c40118272c109bc4b06f6a7bb078bb
Petromir Petrov <petromir@devlabs.bg>
2022-12-13 14:25:52 +0200

    add: custom className prop to Image

--- src/components/common/Image/Image.js
+++ src/components/common/Image/Image.js
@@ -3,15 +3,15 @@ import styles from  './Image.module.scss'
 import cn from 'classnames'
 import { FaImage } from 'react-icons/fa'
 
-export default function Image({ src, fallback, size = 64, alt = 'image' }) {
+export default function Image({ src, fallback, className, size = 64, alt = 'image' }) {
 
   const [failed, setFailed] = useState(false)
 
   return (
-    <div className={cn(styles.image)} >
+    <div className={cn(styles.image, className)} >
       {
         failed
-          ? (fallback || <FaImage/>)
+          ? (fallback || <FaImage />)
           : <img src={src}
                  draggable='false'
                  alt={alt}
