5fe3d9d3cd76c1a8475fb598c4e0e0d7f2d4578d
Petromir Petrov <petromir@devlabs.bg>
2022-12-19 18:18:44 +0200

    change: setFailed to false when src changes, Image

--- src/components/common/Image/Image.js
+++ src/components/common/Image/Image.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styles from  './Image.module.scss'
 import cn from 'classnames'
 import { FaImage } from 'react-icons/fa'
@@ -6,6 +6,10 @@ import { FaImage } from 'react-icons/fa'
 export default function Image({ src, fallback, className, size = 64, alt = 'image' }) {
 
   const [failed, setFailed] = useState(false)
+
+  useEffect(() => {
+    setFailed(false)
+  }, [src])
 
   return (
     <div className={cn(styles.image, className)} >
