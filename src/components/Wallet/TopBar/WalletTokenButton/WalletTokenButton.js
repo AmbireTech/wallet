c1356286b7c1f1140e459d3c85654cf2cc2bb33e
Emil <stojnovsky@gmail.com>
2022-12-22 08:52:19 +0200

    Revert "Fix the wrong values of the wallet rewards"

--- src/components/Wallet/TopBar/WalletTokenButton/WalletTokenButton.js
+++ src/components/Wallet/TopBar/WalletTokenButton/WalletTokenButton.js
@@ -37,10 +37,7 @@ const WalletTokenButton = ({ rewardsData, accountId, network, hidePrivateValue, 
         const isCurrentClaimStatusLoadingAndNoPrevData =
           currentClaimStatus.loading && !currentClaimStatus.lastUpdated
         const isRewardsDataLoadingAndNoPrevData = rewardsIsLoading && !rewardsLastUpdated
-        const isMatchingRewardsDataAccWithCurrAcc = rewardsData.rewards.accountAddr.toLowerCase() === accountId.toLowerCase() 
-        if (isCurrentClaimStatusLoadingAndNoPrevData
-            || isRewardsDataLoadingAndNoPrevData
-            || !isMatchingRewardsDataAccWithCurrAcc) {
+        if (isCurrentClaimStatusLoadingAndNoPrevData || isRewardsDataLoadingAndNoPrevData) {
           return (<span><Loading/></span>)
         }
         
@@ -55,7 +52,7 @@ const WalletTokenButton = ({ rewardsData, accountId, network, hidePrivateValue, 
         }
     
         return `${hidePrivateValue(pendingTokensTotal)} $WALLET`
-    }, [currentClaimStatus, hidePrivateValue, pendingTokensTotal, rewardsErrMsg, rewardsIsLoading, rewardsLastUpdated, vestingEntry, accountId, rewardsData.rewards.accountAddr])
+    }, [currentClaimStatus, hidePrivateValue, pendingTokensTotal, rewardsErrMsg, rewardsIsLoading, rewardsLastUpdated, vestingEntry])
 
     return (
         !relayerURL ?
