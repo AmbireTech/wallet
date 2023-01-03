8cbe702ada76d39100cd9ade3ad4101067b77743
Petromir Petrov <petromir@devlabs.bg>
2023-01-03 14:58:00 +0200

    improve: SDK markup and style

--- src/components/SDK/SDK.js
+++ src/components/SDK/SDK.js
@@ -4,11 +4,13 @@ import styles from './SDK.module.scss'
 const SDK = ({children}) => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.header}>
-        <span className={styles.tempLogo}>Logo</span>
-      </div>
-      <div className={styles.body}>
-        {children}
+      <div className={styles.headerAndBody}>
+        <div className={styles.header}>
+          <span className={styles.tempLogo}>Logo</span>
+        </div>
+        <div className={styles.body}>
+          {children}
+        </div>
       </div>
       <div className={styles.footer}>
         <p className={styles.footerText}>Powered by Ambire Wallet</p>
