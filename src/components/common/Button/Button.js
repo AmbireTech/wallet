5f3c7938a45b7debee0bd23366044a2958442d6f
Petromir Petrov <petromir@devlabs.bg>
2023-01-03 15:31:55 +0200

    redesign, simplify: Button(breaking the other style)

--- src/components/common/Button/Button.js
+++ src/components/common/Button/Button.js
@@ -1,21 +1,47 @@
+import cn from 'classnames'
+import { AiOutlineLoading } from 'react-icons/ai'
 import styles from './Button.module.scss'
 
-const Button = ({ children, className, small, mini, icon, iconAfter, form, disabled, onClick, red, primaryGradient, purpleGradient, greenGradient, danger, clear, border, title, type, textOnly, secondary, full }) => {
-    return (
-        <button
-            onClick={(...args) => !disabled && onClick && onClick.apply(this, args)}
-            className={`${styles.wrapper} ${className} ${small ? styles.small : ''} ${mini ? styles.mini : ''} ${primaryGradient ? styles.primaryGradient : ''} ${purpleGradient ? styles.purpleGradient : ''} ${greenGradient ? styles.greenGradient : ''} ${danger || red ? styles.danger : ''} ${clear ? styles.clear : ''} ${border ? styles.border : ''} ${disabled ? styles.disabled : ''} ${textOnly ? styles.textOnly : ''} ${secondary ? styles.secondary : ''} ${full ? styles.full : ''}`}
-            // disabled={disabled} // causing pointer-events to not trigger
-            title={title}
-            // used with <form>
-            type={type}
-            form={form}
-        >
-            { icon ? <div className={styles.iconButton}>{ icon }</div> : null }
-            { children }
-            { iconAfter ? <div className={styles.iconButton}>{ iconAfter }</div> : null }
-        </button>
-    )
+const Button = ({
+  variant,
+  color,
+  size,
+  loading,
+  startIcon,
+  endIcon,
+  disabled,
+  onClick,
+  className,
+  children,
+  title,
+  type,
+  form,
+}) => {
+  return (
+    <button
+      onClick={(...args) => !disabled && onClick && onClick.apply(this, args)}
+      className={cn(styles.wrapper, className, styles[color], styles[variant], styles[size], {
+        [styles.disabled]: disabled || loading,
+        [styles.loading]: loading,
+      })}
+      // disabled={disabled} // causing pointer-events to not trigger
+      title={title}
+      // used with <form>
+      type={type}
+      form={form}
+    >
+      {startIcon && startIcon}
+      {!loading ? children : <div className={styles.loadingInner}>
+        <AiOutlineLoading />
+        <p className={styles.loadingText}>Loading...</p>
+      </div>}
+      {endIcon && endIcon}
+    </button>
+  )
+}
+
+Button.defaultProps = {
+  variant: 'outlined',
 }
 
 export default Button
