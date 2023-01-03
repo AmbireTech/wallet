bf5a011fbf5059e0d8d662e2dbd4e0bc660c2950
Petromir Petrov <petromir@devlabs.bg>
2023-01-03 14:56:43 +0200

    fix: AddAccount background theming

--- src/components/AddAccount/AddAccount.js
+++ src/components/AddAccount/AddAccount.js
@@ -1,4 +1,4 @@
-import styles from './AddAccount.module.scss'
+import cn from 'classnames'
 import { useState, useEffect, useCallback } from 'react'
 import { Link } from 'react-router-dom'
 import LoginOrSignup from 'components/LoginOrSignupForm/LoginOrSignupForm'
@@ -24,11 +24,14 @@ import { validateImportedAccountProps, fileSizeValidator } from 'lib/validations
 import LatticeModal from 'components/Modals/LatticeModal/LatticeModal'
 import Lottie from 'lottie-react'
 import AnimationData from './assets/confirm-email.json'
+
+import { useThemeContext } from 'components/ThemeProvider/ThemeProvider'
+
+import styles from './AddAccount.module.scss'
+// Icons
+import { ReactComponent as AmbireLogo } from 'resources/logo.svg'
 import { AiOutlineReload } from 'react-icons/ai'
 import { ReactComponent as ChevronLeftIcon } from 'resources/icons/chevron-left.svg'
-
-import { ReactComponent as AmbireLogo } from 'resources/logo.svg'
-// Icons
 import { ReactComponent as TrezorIcon } from 'resources/providers/trezor.svg'
 import { ReactComponent as LedgerIcon } from 'resources/providers/ledger.svg'
 import { ReactComponent as GridPlusIcon } from 'resources/providers/grid-plus.svg'
@@ -44,6 +47,7 @@ const EMAIL_AND_TIMER_REFRESH_TIME = 5000
 const RESEND_EMAIL_TIMER_INITIAL = 60000
 
 export default function AddAccount({ relayerURL, onAddAccount, utmTracking, pluginData }) {
+  const { theme } = useThemeContext()
   const [signersToChoose, setChooseSigners] = useState(null)
   const [err, setErr] = useState('')
   const [addAccErr, setAddAccErr] = useState('')
@@ -483,7 +487,7 @@ export default function AddAccount({ relayerURL, onAddAccount, utmTracking, plug
   </>)
 
   if (!relayerURL) {
-    return (<div className={styles.loginSignupWrapper}>
+    return (<div className={cn(styles.loginSignupWrapper, styles[theme])}>
       <AmbireLogo className={styles.logo} />
       <section className={styles.addAccount}>
         <div className={styles.loginOthers}>
@@ -496,7 +500,7 @@ export default function AddAccount({ relayerURL, onAddAccount, utmTracking, plug
     </div>)
   }
   //TODO: Would be great to create Ambire spinners(like 1inch but simpler) (I can have a look at them if you need)
-  return (<div className={styles.loginSignupWrapper}>
+  return (<div className={cn(styles.loginSignupWrapper, styles[theme])}>
       { requiresEmailConfFor ?
         (<div className={`${styles.emailConf}`}>
           <Lottie className={styles.emailAnimation} animationData={AnimationData} background="transparent" speed="1" loop autoplay />
