8afbfac062e12b36d7334e66adff6a7b3016552d
Gergana Mihaylova <gergana@devlabs.bg>
2023-01-10 17:50:34 +0200

    Change: pending formatting of balance

--- src/components/Wallet/Dashboard/Tokens/Token/Pending/Pending.js
+++ src/components/Wallet/Dashboard/Tokens/Token/Pending/Pending.js
@@ -1,8 +1,8 @@
 import cn from 'classnames'
 import styles from './Pending.module.scss'
+import { formatFloatTokenAmount } from 'lib/formatters'
 
-const Pending = ({ hidePrivateValue, balance, pending, unconfirmed, extraMargin }) => {
-
+const Pending = ({ hidePrivateValue, balance, decimals, pending, unconfirmed, extraMargin }) => {
   return (unconfirmed || pending) ? (
     <div className={cn(styles.wrapper, { [styles.extraMargin]: extraMargin })}>
       <div className={styles.ovals}>
@@ -19,7 +19,7 @@ const Pending = ({ hidePrivateValue, balance, pending, unconfirmed, extraMargin 
       </div>
       <h3 className={styles.estimated}>
         <span>
-          { hidePrivateValue(balance) }
+          { hidePrivateValue(formatFloatTokenAmount(Number(balance).toFixed((balance < 1) ? 8 : 4), true, decimals))  }
         </span>
         {' '}
         Estimated balance
