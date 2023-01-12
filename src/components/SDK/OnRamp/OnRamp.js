767f2f1afedd2eece749d92bfd3a1e1cdbf590f3
Petromir <petromir@devlabs.bg>
2023-01-12 12:55:13 +0200

    update: OnRamp to match the latest design

--- src/components/SDK/OnRamp/OnRamp.js
+++ src/components/SDK/OnRamp/OnRamp.js
@@ -1,67 +1,75 @@
-import React from 'react'
+import { useState } from 'react'
 import networks from 'ambire-common/src/constants/networks'
 
 import binanceNetworks from './helpers/binanceNetworks'
+import { networkIconsById } from 'consts/networks'
 import { fetchPost } from 'lib/fetch'
 
 import { useLocalStorage, useAccounts } from 'hooks'
-import { Button } from 'components/common'
+import { Button, Select } from 'components/common'
 
 import { ReactComponent as Illustration } from './images/illustration.svg'
 
 import styles from './OnRamp.module.scss'
 
 const OnRamp = ({ relayerURL }) => {
-  const { selectedAcc } = useAccounts(useLocalStorage)
-  const chosenNetwork = React.createRef()
-  const networkCodes = networks
-    .filter((network) => network.id in binanceNetworks)
-    .map((network) => ({
-      code: binanceNetworks[network.id],
-      name: network.name,
-    }))
+	const { selectedAcc } = useAccounts(useLocalStorage)
+	// const chosenNetwork = React.createRef()
+	const [selectedNetwork, setSelectedNetwork] = useState(null)
+	const networkItems = networks
+		.filter((network) => network.id in binanceNetworks)
+		.map((network) => ({
+			value: binanceNetworks[network.id],
+			label: network.name,
+			icon: networkIconsById[network.id]
+		}))
 
-  const handleBuyCrypto = async () => {
-    const networkCode = chosenNetwork.current.value
-    const fetchData = await fetchPost(`${relayerURL}/binance-connect/sign`, { address: selectedAcc, networkCode })
-    const signature = encodeURIComponent(fetchData.signature)
-    const { merchantCode, timestamp } = fetchData
+	const handleBuyCrypto = async () => {
+		if (!selectedNetwork) return
 
-    const iframeUrl = `https://www.binancecnt.com/en/pre-connect?merchantCode=${merchantCode}&timestamp=${timestamp}&cryptoAddress=${selectedAcc}&cryptoNetwork=${networkCode}&signature=${signature}`
-    window.open(iframeUrl, 'binance-connect', 'menubar=1,resizable=1,width=400,height=640')
-  }
+		const fetchData = await fetchPost(`${relayerURL}/binance-connect/sign`, {
+			address: selectedAcc,
+			networkCode: selectedNetwork
+		})
+		const signature = encodeURIComponent(fetchData.signature)
+		const { merchantCode, timestamp } = fetchData
 
-  const handleFinalize = () => {
-    window.parent.postMessage(
-      {
-        address: selectedAcc,
-        type: 'finishRamp',
-      },
-      '*'
-    )
-  }
+		const iframeUrl = `https://www.binancecnt.com/en/pre-connect?merchantCode=${merchantCode}&timestamp=${timestamp}&cryptoAddress=${selectedAcc}&cryptoNetwork=${selectedNetwork}&signature=${signature}`
+		window.open(iframeUrl, 'binance-connect', 'menubar=1,resizable=1,width=400,height=640')
+	}
 
-  return (
-    <div className={styles.wrapper}>
-      <h1 className={styles.title}>Do you want to buy crypto?</h1>
-      <Illustration className={styles.illustration} />
-      <select ref={chosenNetwork}>
-        {networkCodes.map((network, id) => (
-          <option key={id} value={network.code}>
-            {network.name}
-          </option>
-        ))}
-      </select>
-      <div className={styles.buttons}>
-        <Button border small className={styles.button} onClick={handleBuyCrypto}>
-          Buy Crypto with Fiat
-        </Button>
-        <Button primaryGradient small className={styles.button} onClick={handleFinalize}>
-          Finalize Registration
-        </Button>
-      </div>
-    </div>
-  )
+	const handleFinalize = () => {
+		window.parent.postMessage(
+			{
+				address: selectedAcc,
+				type: 'finishRamp'
+			},
+			'*'
+		)
+	}
+
+	return (
+		<div className={styles.wrapper}>
+			<h1 className={styles.title}>Do you want to buy crypto?</h1>
+			<Illustration className={styles.illustration} />
+			<Select
+				items={networkItems}
+				onChange={({ value }) => setSelectedNetwork(value)}
+				defaultValue={selectedNetwork}
+				className={styles.select}
+				selectInputClassName={styles.selectInput}
+				selectMenuClassName={styles.selectMenu}
+			/>
+			<div className={styles.buttons}>
+				<Button primaryGradient small className={styles.button} onClick={handleBuyCrypto}>
+					Buy Crypto with Fiat
+				</Button>
+				<Button border small className={styles.button} onClick={handleFinalize}>
+					Continue to application
+				</Button>
+			</div>
+		</div>
+	)
 }
 
 export default OnRamp
