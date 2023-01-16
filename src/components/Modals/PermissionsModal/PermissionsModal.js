f7fc3edcf94d2676ed8ddd692eba42c23b71f289
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2023-01-16 13:43:42 +0200

    restored email confirmation in permission modal

--- src/components/Modals/PermissionsModal/PermissionsModal.js
+++ src/components/Modals/PermissionsModal/PermissionsModal.js
@@ -1,12 +1,14 @@
 import './PermissionsModal.scss'
 
-import { useState } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { MdCheck, MdClose, MdOutlineCheck } from 'react-icons/md'
 import { useModals, usePermissions } from 'hooks'
 import { useToasts } from 'hooks/toasts'
 import { askForPermission } from 'lib/permissions'
-import { Modal, Toggle, Button, Checkbox } from 'components/common'
+import { Modal, Toggle, Button, Checkbox, ToolTip } from 'components/common'
 import { isFirefox } from 'lib/isFirefox'
+import { fetchGet } from 'lib/fetch'
+import { AiOutlineReload } from 'react-icons/ai'
 import { BiExport } from 'react-icons/bi'
 import accountPresets from 'ambire-common/src/constants/accountPresets'
 
@@ -16,13 +18,50 @@ const PermissionsModal = ({ relayerIdentityURL, account, onAddAccount, isCloseBt
     const { hideModal } = useModals()
     const { isNoticationsGranted, isClipboardGranted, modalHidden, setModalHidden } = usePermissions()
     const { addToast } = useToasts()
+    const [isEmailConfirmed, setEmailConfirmed] = useState(false)
+    const [isEmailResent, setEmailResent] = useState(false)
     const [isJsonBackupDownloaded, setIsJsonBackupDownloaded] = useState(isBackupOptout)
+    const [resendTimeLeft, setResendTimeLeft] = useState(60000)
 
     const days = Math.ceil(accountPresets.quickAccTimelock / 86400)
     const areBlockedPermissions = (!isFirefox() && !isClipboardGranted) || !isNoticationsGranted
-    const isAccountNotConfirmed = account.emailConfRequired
+    const isAccountNotConfirmed = account.emailConfRequired && !isEmailConfirmed
     const buttonDisabled = isAccountNotConfirmed || (!modalHidden && areBlockedPermissions)
+    const sendConfirmationEmail = async () => {
+        try {
+            const response = await fetchGet(relayerIdentityURL + '/resend-verification-email')
+            if (!response.success) throw new Error('Relayer did not return success.')
+
+            addToast('Verification email sent!')
+            setEmailResent(true)
+        } catch(e) {
+            console.error(e)
+            addToast('Could not resend verification email.' + e.message || e, { error: true })
+            setEmailResent(false)
+        }
+    }
     
+    const checkEmailConfirmation = useCallback(async () => {
+        try {
+            const identity = await fetchGet(relayerIdentityURL)
+            if (identity) {
+                const { emailConfirmed } = identity.meta
+                const isConfirmed = !!emailConfirmed
+                setEmailConfirmed(isConfirmed)
+
+                if (isConfirmed && account.emailConfRequired) {
+                    onAddAccount({
+                        ...account,
+                        emailConfRequired: false
+                    })
+                }
+            }
+        } catch(e) {
+            console.error(e);
+            addToast('Could not check email confirmation.', { error: true })
+        }
+    }, [relayerIdentityURL, account, onAddAccount, addToast])
+
     const requestNotificationsPermission = async () => {
         const status = await askForPermission('notifications')
         if (!status) addToast(toastErrorMessage('Notifications'), { error: true })
@@ -33,6 +72,12 @@ const PermissionsModal = ({ relayerIdentityURL, account, onAddAccount, isCloseBt
         if (!status) addToast(toastErrorMessage('Clipboard'), { error: true })
     }
     
+    useEffect(() => {
+        !isEmailConfirmed && checkEmailConfirmation()
+        const emailConfirmationInterval = setInterval(() => !isEmailConfirmed && checkEmailConfirmation(), 3000)
+        return () => clearInterval(emailConfirmationInterval)
+    }, [isEmailConfirmed, checkEmailConfirmation])
+
     const handleDoneOrIgnoreBtnsClicked = () => {
         hideModal()
         if (showThankYouPage) openThankYouPage()
@@ -86,8 +131,43 @@ const PermissionsModal = ({ relayerIdentityURL, account, onAddAccount, isCloseBt
         })
     }
 
+    useEffect(() => {
+        const resendInterval = setInterval(() => setResendTimeLeft(resendTimeLeft => resendTimeLeft > 0 ? resendTimeLeft - 1000 : 0), 1000)
+        return () => clearTimeout(resendInterval)
+    }, [])
+
     return (
         <Modal id="permissions-modal" title="We need a few things 🙏" buttons={buttons} isCloseBtnShown={isCloseBtnShown} onClose={handleOnClose}>
+            {
+                account.email ? 
+                    <div className="permission">
+                    <div className="details">
+                        <div className="name">Email Verification</div>
+                        <div className="description">
+                            <b>Confirming your email is mandatory so that we can make sure your account can be recovered in case access is lost.</b>
+                            &nbsp;We already sent an email, please check your inbox.
+                        </div>
+                    </div>
+                    <div className="status">
+                        { 
+                            !isEmailConfirmed ?
+                                <label>Waiting for<br/>your confirmation</label>
+                                : 
+                                <span className="check-icon"><MdOutlineCheck/></span>
+                        }
+                        { 
+                            !isEmailConfirmed && !isEmailResent ? 
+                                <ToolTip label={`Will be available in ${resendTimeLeft / 1000} seconds`} disabled={resendTimeLeft === 0}>
+                                    <Button mini clear icon={<AiOutlineReload/>} disabled={resendTimeLeft !== 0} onClick={sendConfirmationEmail}>Resend</Button>
+                                </ToolTip>
+                                :
+                                null
+                        }
+                    </div>
+                </div>
+                :
+                null
+            }
             <div className="permission">
                 <div className="details">
                     <div className="name">Notifications Permission</div>
