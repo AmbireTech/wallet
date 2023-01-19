8b2725c9ba6b96f81eb17a3cbb002adefc802e66
Petromir <petromir@devlabs.bg>
2023-01-19 12:03:53 +0200

    format: Button component

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,45 +1,57 @@
 import cn from 'classnames'
 import { FC } from 'react'
+
 import { AiOutlineLoading } from 'react-icons/ai'
-import styles from './Button.module.scss'
+
 import { ButtonProps } from './types'
 
-const Button:FC<ButtonProps> = ({
-  variant = 'primary',
-  size = 'normal',
-  loading,
-  startIcon,
-  endIcon,
-  disabled,
-  onClick,
-  className,
-  children,
-  title,
-  type,
-  form,
-  loadingText
+import styles from './Button.module.scss'
+
+const Button: FC<ButtonProps> = ({
+	variant = 'primary',
+	size,
+	loading,
+	startIcon,
+	endIcon,
+	disabled,
+	onClick,
+	className,
+	children,
+	title,
+	type,
+	form,
+	loadingText
 }) => {
-  return (
-    <button
-      onClick={(...args) => !disabled && onClick && onClick.apply(this, args)}
-      className={cn(styles.wrapper, styles[variant], styles[size], {
-        [styles.disabled]: disabled || loading,
-        [styles.loading]: loading,
-      }, className)}
-      // disabled={disabled} // causing pointer-events to not trigger
-      title={title}
-      // used with <form>
-      type={type}
-      form={form}
-    >
-      {startIcon && startIcon}
-      {!loading ? children : <div className={styles.loadingInner}>
-        <AiOutlineLoading />
-        <p className={styles.loadingText}>{loadingText || "Loading..."}</p>
-      </div>}
-      {endIcon && endIcon}
-    </button>
-  )
+	return (
+		<button
+			onClick={(...args) => !disabled && onClick && onClick.apply(this, args)}
+			className={cn(
+				styles.wrapper,
+				styles[variant],
+				{
+					[styles.disabled]: disabled || loading,
+					[styles.loading]: loading,
+          [styles[size || '']]: size 
+				},
+				className
+			)}
+			// disabled={disabled} // causing pointer-events to not trigger
+			title={title}
+			// used with <form>
+			type={type}
+			form={form}>
+			{startIcon && startIcon}
+			{!loading ? (
+				children
+			) : (
+				<div className={styles.loadingInner}>
+					<AiOutlineLoading />
+					<p className={styles.loadingText}>{loadingText || 'Loading...'}</p>
+				</div>
+			)}
+			{endIcon && endIcon}
+		</button>
+	)
 }
 
 export default Button
