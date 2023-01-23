066639507694f275432534b5977a67d7cbf742cf
Petromir <petromir@devlabs.bg>
2023-01-23 17:05:03 +0200

    add: Addresses to AddressBook(search logic)

---
+++ src/components/common/AddressBook/Addresses/Addresses.js
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+import { AddressList, TextInput } from 'components/common'
+
+import styles from './Addresses.module.scss'
+
+const handleSearch = ({ addresses, query }) => {
+  if (!addresses) return []
+	if (!query) return addresses
+
+	return addresses.filter(({ name, address }) => {
+		const nameMatch = name.toLowerCase().includes(query.toLowerCase())
+		const addressMatch = address.toLowerCase().includes(query.toLowerCase())
+
+		return nameMatch || addressMatch
+	})
+}
+
+export default function Addresses({ addresses, selectAddress, removeAddress }) {
+	const [query, setQuery] = useState('')
+
+	return (
+		<div className={styles.wrapper}>
+			<TextInput
+				inputContainerClass={styles.input}
+				small
+				onInput={(value) => setQuery(value)}
+				value={query}
+				placeholder="Search..."
+			/>
+			<AddressList
+				addresses={handleSearch({ addresses, query })}
+				onSelectAddress={selectAddress}
+				removeAddress={removeAddress}
+				addressClassName={styles.address}
+			/>
+		</div>
+	)
+}
