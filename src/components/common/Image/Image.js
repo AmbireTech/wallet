658568559af63754477f351a500033658afbb77e
Jordan Enev <jordantoen@gmail.com>
2023-01-30 17:24:00 +0200

    Add: Documentation.

--- src/components/common/Image/Image.js
+++ src/components/common/Image/Image.js
@@ -8,6 +8,7 @@ import styles from  './Image.module.scss'
 export default function Image({ src, fallback, size = 64, alt = 'image', className, imageClassName, failedClassName }) {
   // In case we have a falsy `src` value (undefined, empty string, etc.) <img> `onError` callback is not being triggered,
   // because the `src` prop is not being attached at all to the <img> tag (I guess JSX omits falsy props for the `src` prop).
+  // That's the reason why we set `failed` value initially, based on the `src` value.
   const [failed, setFailed] = useState(!src)
 
   return (
