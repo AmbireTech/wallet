060357b4723387bcaca697817dc143d59e5cb650
Jordan Enev <jordantoen@gmail.com>
2023-01-31 15:38:03 +0200

    Add: useEffect for monitoring `src` value and updating `failed` value.

--- src/components/common/Image/Image.js
+++ src/components/common/Image/Image.js
@@ -1,15 +1,21 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import cn from 'classnames'
 
 import { ReactComponent as FallbackIcon } from 'resources/icons/fallback.svg'
 
 import styles from  './Image.module.scss'
 
-export default function Image({ src, fallback, size = 64, alt = 'image', className, imageClassName, failedClassName }) {
+const isFailed = src => !src
+
+function Image({ src, fallback, size = 64, alt = 'image', className, imageClassName, failedClassName }) {
   // In case we have a falsy `src` value (undefined, empty string, etc.) <img> `onError` callback is not being triggered,
   // because the `src` prop is not being attached at all to the <img> tag (I guess JSX omits falsy props for the `src` prop).
   // That's the reason why we set `failed` value initially, based on the `src` value.
-  const [failed, setFailed] = useState(!src)
+  const [failed, setFailed] = useState(isFailed(src))
+
+  useEffect(() => {
+    setFailed(isFailed(src))
+  },[src])
 
   return (
     <div className={cn(styles.image, className)} >
@@ -21,10 +27,11 @@ export default function Image({ src, fallback, size = 64, alt = 'image', classNa
                  alt={alt}
                  style={{ maxWidth: size, maxHeight: size }}
                  onError={() => setFailed(true)}
-                 onLoad={() => setFailed(false)}
                  className={cn(imageClassName, {[failedClassName]: failed && failedClassName})}
           />
       }
     </div>
   )
 }
+
+export default React.memo(Image)
