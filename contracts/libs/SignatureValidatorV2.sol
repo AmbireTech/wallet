2bd8f10d61cc03fed5e120c6de86f558dba4807b
Ivo Georgiev <ivo@ambire.com>
2023-02-25 11:58:48 +0200

    Revert "SignatureValidatorV2: try the chainlink schnorr verification"
    This reverts commit 8cb664da2f5b52227387f69cde805ddfa97204bf.

--- contracts/libs/SignatureValidatorV2.sol
+++ contracts/libs/SignatureValidatorV2.sol
@@ -24,7 +24,6 @@ library SignatureValidator {
 	bytes4 constant internal ERC1271_MAGICVALUE_BYTES32 = 0x1626ba7e;
 	// secp256k1 group order
 	uint256 constant internal Q = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141;
-	uint256 constant internal HALF_Q = (Q >> 1) + 1;
 
 	function recoverAddr(bytes32 hash, bytes memory sig) internal view returns (address) {
 		return recoverAddrImpl(hash, sig, false);
@@ -53,42 +52,18 @@ library SignatureValidator {
 		// {sig}{verifier}{mode}
 		} else if (mode == SignatureMode.Schnorr) {
 			// last uint8 is for mode
-			(uint signingPubKeyX, uint signature, address nonceTimesGeneratorAddress, uint8 pubKeyYParity,) = abi.decode(sig, (uint, uint, address, uint8, uint8));
-			require(signingPubKeyX < HALF_Q, "Public-key x >= HALF_Q");
-			// Avoid signature malleability from multiple representations for ℤ/Qℤ elts
-			require(signature < Q, "signature must be reduced modulo Q");
+			(bytes32 e, bytes32 px, bytes32 s, uint8 parity,) = abi.decode(sig, (bytes32, bytes32, bytes32, uint8, uint8));
+			// ecrecover = (m, v, r, s);
+			bytes32 sp = bytes32(Q - mulmod(uint256(s), uint256(px), Q));
+			bytes32 ep = bytes32(Q - mulmod(uint256(e), uint256(px), Q));
 
-			// Forbid trivial inputs, to avoid ecrecover edge cases. The main thing to
-			// avoid is something which causes ecrecover to return 0x0: then trivial
-			// signatures could be constructed with the nonceTimesGeneratorAddress input
-			// set to 0x0.
-			// solium-disable-next-line indentation
-			require(nonceTimesGeneratorAddress != address(0) && signingPubKeyX > 0 && signature > 0 && hash > 0, "no zero inputs allowed");
-
-			// solium-disable-next-line indentation
-			uint256 msgChallenge = // "e"
-				// solium-disable-next-line indentation
-				uint256(keccak256(abi.encodePacked(signingPubKeyX, pubKeyYParity,
-				hash, nonceTimesGeneratorAddress))
-			);
-
-			// Verify msgChallenge * signingPubKey + signature * generator ==
-			//        nonce * generator
-			//
-			// https://ethresear.ch/t/you-can-kinda-abuse-ecrecover-to-do-ecmul-in-secp256k1-today/2384/9
-			// The point corresponding to the address returned by
-			// ecrecover(-s*r,v,r,e*r) is (r⁻¹ mod Q)*(e*r*R-(-s)*r*g)=e*R+s*g, where R
-			// is the (v,r) point. See https://crypto.stackexchange.com/a/18106
-			//
-			return ecrecover(
-				bytes32(Q - mulmod(signingPubKeyX, signature, Q)),
-				// https://ethereum.github.io/yellowpaper/paper.pdf p. 24, "The
-				// value 27 represents an even y value and 28 represents an odd
-				// y value."
-				(pubKeyYParity == 0) ? 27 : 28,
-				bytes32(signingPubKeyX),
-				bytes32(mulmod(msgChallenge, signingPubKeyX, Q))
-			);
+			require(sp != 0);
+			// the ecrecover precompile implementation checks that the `r` and `s`
+			// inputs are non-zero (in this case, `px` and `ep`), thus we don't need to
+			// check if they're zero.
+			address R = ecrecover(sp, parity, px, ep);
+			require(R != address(0), "ecrecover failed");
+			return address(bytes20(keccak256(abi.encodePacked(R, uint8(parity), px, hash)))); // or e == keccak256(...)
 		} else if (mode == SignatureMode.SmartWallet) {
 			// 32 bytes for the addr, 1 byte for the type = 33
 			require(sig.length > 33, "SV_LEN_WALLET");
