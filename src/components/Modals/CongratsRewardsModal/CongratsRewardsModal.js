fe0ada4612e4e70b8c1e53d788df5930c6a38452
Petromir <petromir@devlabs.bg>
2023-03-08 14:41:21 +0200

    change: improve CongratsRewardsModal style and markup

--- src/components/Modals/CongratsRewardsModal/CongratsRewardsModal.js
+++ src/components/Modals/CongratsRewardsModal/CongratsRewardsModal.js
@@ -1,34 +1,41 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 import Lottie from 'lottie-react'
+import { useModals } from 'hooks'
+
 import { Button, Modal } from 'components/common'
 import AnimationData from './assets/confetti-animation.json'
+import WalletTokenCongratsImage from './assets/wallet-token-congrats.png'
 
 import styles from './CongratsRewardsModal.module.scss'
 
 const CongratsRewardsModal = ({ pendingTokensTotal }) => {
-    const [count, setCount] = useState(0)
-    const initialRewardsCount = 3
-
-    useEffect(() => {
-        if (count < initialRewardsCount ) {
-            setCount(prevState => prevState += 1)
-        }
-    }, [count])
+    const { hideModal } = useModals()
 
     return (
-        <>
-            <Modal className={styles.wrapper} title="Woo-hoo!" buttons={<Button>More confetti</Button>}>
-                <Lottie className={styles.confettiAnimation} animationData={AnimationData} background="transparent" speed="1" loop autoplay />
-                <div className={styles.innerContent}>
-                    <div id="rewardId" className={styles.reward} />
-                    <div className={styles.logo}></div>
-                    <h2>You just received { pendingTokensTotal } $WALLET!</h2>
-                    <p>You have a balance of $1,000 or more in your Ambire wallet - this means you are eligible to earn WALLET rewards!</p>
-                    <p>The bigger your account balance, the more rewards you earn, and you can claim them every Monday.</p>
-                    <a href='https://blog.ambire.com/wallet-rewards-mechanism-explained-start-accumulating-value-before-the-token-is-launched/' target='_blank' rel='noreferrer'>Learn more about WALLET rewards</a>   
-                </div>
-            </Modal>
-        </>   
+        <Modal 
+            size="sm" 
+            className={styles.wrapper} 
+            contentClassName={styles.content} 
+            title="Woo-hoo!" 
+            isCloseBtnShown={false} 
+            buttons={<Button onClick={hideModal}>Close</Button>}
+        >
+            <Lottie className={styles.confettiAnimation} animationData={AnimationData} background="transparent" speed="1" loop={false} autoplay />
+            <img className={styles.image} src={WalletTokenCongratsImage} alt="wallet-congrats" />
+            <h2 className={styles.title}>You just received { pendingTokensTotal } $WALLET!</h2>
+            <p className={styles.text}>
+                You have a balance of $1,000 or more in your Ambire wallet - this means you are eligible to earn WALLET rewards!<br/>
+                The bigger your account balance, the more rewards you earn, and you can claim them every Monday.
+            </p>
+            <a 
+                href='https://blog.ambire.com/wallet-rewards-mechanism-explained-start-accumulating-value-before-the-token-is-launched/' 
+                target='_blank' 
+                rel='noreferrer'
+                className={styles.link}
+            >
+                Learn more about WALLET rewards
+            </a>   
+        </Modal>
     )
 }
 
