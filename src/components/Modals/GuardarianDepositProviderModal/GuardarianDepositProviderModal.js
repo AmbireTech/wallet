b97fd3602142cbddd217521e9ffa19258fc97838
Petromir <petromir@devlabs.bg>
2023-03-24 13:44:20 +0200

    add: loading state for GuardarianDepositProviderModal button

--- src/components/Modals/GuardarianDepositProviderModal/GuardarianDepositProviderModal.js
+++ src/components/Modals/GuardarianDepositProviderModal/GuardarianDepositProviderModal.js
@@ -189,8 +189,8 @@ const GuardarianDepositProviderModal = ({ relayerURL, walletAddress, selectedNet
                 || guardarian?.estimateInfo?.isLoading 
                 || guardarian?.txn?.isLoading
                 || guardarian?.amount === ''
-                || sendTransactionLoading
-            } 
+            }
+            loading={sendTransactionLoading}
             onClick={sendTxn}
             className={styles.button}
             variant="primaryGradient"
