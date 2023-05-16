235b8b17925d0233e6ce42e22036434f14546ec6
Jordan Enev <jordantoen@gmail.com>
2023-05-16 16:46:53 +0300

    Fix: In certain circumstances, Cypress is skipping part of the string when type(). Because of this, we add a small delay. Fix: Add a small delay, before clicking on Recipient confirmation checkboxes, because it takes a while to be visible to the end user (because of React re-renders).

--- cypress/e2e/transfer.cy.ts
+++ cypress/e2e/transfer.cy.ts
@@ -16,11 +16,13 @@ describe('Transfering funds', () => {
     // Wait for the initial wallet load
     cy.wait(3000)
 
-    cy.get('[data-testid="amount"]').clear().type('0.0000001')
+    cy.get('[data-testid="amount"]').clear().type('0.0000001', { delay: 100 })
     // Here we are sending funds to a random Recipient's address intentionally,
     // in order to overcome the Relayer logic regarding sending funds to a known address (3 or more txns sent to the same address).
     // If so (3+ txns) - the confirmation code is skipped and our test will fail.
-    cy.get('[data-testid="recipient"]').type(Wallet.createRandom().address)
+    cy.get('[data-testid="recipient"]').type(Wallet.createRandom().address, { delay: 100 })
+    // Wait a bit, because of React re-renders, both checkboxes are not visible immediately
+    cy.wait(1000)
     cy.get('[data-testid="unknownAddressWarning"]').click({force: true})
     cy.get('[data-testid="binance-address-warning-label"]').click()
     cy.get('[data-testid="send"]').click()
@@ -28,14 +30,14 @@ describe('Transfering funds', () => {
     cy.wait(2000)
 
     cy.get('[data-testid="approveTxn"]').click()
-    cy.get('[data-testid="password"]').type(Cypress.env('ACCOUNT_PASSWORD'))
+    cy.get('[data-testid="password"]').type(Cypress.env('ACCOUNT_PASSWORD'), { delay: 100 })
 
     // Wait for the email code.
     // Later we can make a polling mechanism for checking the code, instead of waiting 30 seconds.
     cy.wait(30000)
 
     cy.task('get-confirm-code').then(code => {
-      cy.get('[data-testid="confirmationCode"]').type(code)
+      cy.get('[data-testid="confirmationCode"]').type(code, { delay: 100 })
       cy.get('[data-testid="confirmSigning"]').click()
 
       cy.wait(10000)
