b7ac250fe94341c206bdcdf0269ccb4af433d1d4
Petromir <petromir@devlabs.bg>
2023-06-09 12:19:27 +0300

    replace: useOfflineStatus with isOffline, relayerData

--- src/hooks/relayerData.js
+++ src/hooks/relayerData.js
@@ -1,6 +1,10 @@
 import useRelayerDataCommon from 'ambire-common/src/hooks/useRelayerData'
 import { useOfflineStatus } from 'context/OfflineContext/OfflineContext'
 
-const useRelayerData = (props) => useRelayerDataCommon({ fetch, useOfflineStatus, ...props })
+const useRelayerData = (props) => {
+  const isOffline = useOfflineStatus()
+  
+  return useRelayerDataCommon({ fetch, isOffline, ...props })
+}
 
 export default useRelayerData
