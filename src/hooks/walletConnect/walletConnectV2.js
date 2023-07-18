713d6eeacdc1edd2b6f92dfa7e46422b2ed84d43
IvoPaunov <ivo.paunov@gmail.com>
2023-07-18 17:24:44 +0300

    fix: wc2 possible error

--- src/hooks/walletConnect/walletConnectV2.js
+++ src/hooks/walletConnect/walletConnectV2.js
@@ -247,7 +247,10 @@ export default function useWalletConnectV2({
       })
 
       // NOTE: looks like optionalNamespaces can empty object {} and requiredNamespaces to have the eip155
-      const incomingNamespaces = optionalNamespaces.eip155 || requiredNamespaces.eip155
+      const incomingNamespaces = {
+        ...(optionalNamespaces?.eip155 || {}),
+        ...(requiredNamespaces?.eip155 || {})
+      }
 
       const namespaces = {
         eip155: {
