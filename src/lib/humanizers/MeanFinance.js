aff11ca50db7e84a3978f4d8a0885b94b24fa8a4
Yosif Hamed <josif.hameed@gmail.com>
2023-12-07 16:36:30 +0200

    Add humanizations of terminate and withdraw from mean finance

--- src/lib/humanizers/MeanFinance.js
+++ src/lib/humanizers/MeanFinance.js
@@ -1,5 +1,6 @@
 import { Interface } from 'ethers/lib/utils'
 import { token, getName } from 'lib/humanReadableTransactions'
+import { MdDesktopAccessDisabled } from 'react-icons/md'
 
 function getInterval(seconds) {
   if (seconds <= 60) return `${seconds} seconds`
@@ -8,6 +9,14 @@ function getInterval(seconds) {
   if (seconds <= 60 * 60 * 24 * 7) return `${seconds / 60 / 60 / 24} days`
   if (seconds <= 60 * 60 * 24 * 30) return `${seconds / 60 / 60 / 24 / 7} weeks`
 }
+function getAddress(humanizerInfo, address) {
+  return {
+    type: 'address',
+    address,
+    name: getName(humanizerInfo, address)
+  }
+}
+
 const MeanFinance = (humanizerInfo) => {
   const iface = new Interface(humanizerInfo.abis.DCAHub)
 
@@ -15,14 +24,8 @@ const MeanFinance = (humanizerInfo) => {
     [iface.getSighash(
       'deposit(address,address,uint256,uint32,uint32,address,(address,uint8[])[])'
     )]: (txn, network, { extended }) => {
-      // @TODO add address check
       const { _from, _to, _amountOfSwaps, _swapInterval, _owner, _permissions, _amount } =
         iface.parseTransaction(txn).args
-      console.log('watafak')
-      console.log({
-        type: 'token',
-        ...token(humanizerInfo, _to, -1, true)
-      })
       if (extended)
         return [
           [
@@ -47,7 +50,49 @@ const MeanFinance = (humanizerInfo) => {
             }
           ]
         ]
-      return ['wawtafak']
+      return [
+        `Swap ${token(humanizerInfo, _from, _amount)} for ${getName(
+          humanizerInfo,
+          _to
+        )} Split into ${_amountOfSwaps} swaps over ${getInterval(
+          _swapInterval * _amountOfSwaps
+        )} via ${txn.to}`
+      ]
+    },
+    [iface.getSighash('terminate')]: (txn, network, { extended }) => {
+      const { _positionId, _recipientUnswapped, _recipientSwapped } =
+        iface.parseTransaction(txn).args
+      if (extended)
+        return [
+          ['Terminate', `position ${_positionId}`],
+          txn.from !== _recipientUnswapped
+            ? ['Send', 'unswapped tokens to', getAddress(humanizerInfo, _recipientUnswapped)]
+            : ['Withdraw', 'unswapped tokens'],
+          txn.from !== _recipientSwapped
+            ? ['Send', 'swapped tokens to', getAddress(humanizerInfo, _recipientSwapped)]
+            : ['Withdraw', 'swapped tokens']
+        ]
+      return [
+        `Terminate position ${_positionId}, send unswapped tokens to ${_recipientUnswapped} and send swapped tokens to ${_recipientSwapped}`
+      ]
+    },
+    [iface.getSighash('withdrawSwapped')]: (txn, network, { extended }) => {
+      const { _positionId, _recipient } = iface.parseTransaction(txn).args
+      if (extended)
+        return [
+          _recipient === txn.from
+            ? ['Withdraw', `swapped tokens from position ${_positionId}`]
+            : [
+                'Send',
+                `swapped tokens from position ${_positionId} to`,
+                getAddress(humanizerInfo, _recipient)
+              ]
+        ]
+      return [
+        _recipient === txn.from
+          ? [`Withdraw swapped tokens from position ${_positionId}`]
+          : [`Send swapped tokens from position ${_positionId} to ${_recipient}`]
+      ]
     }
   }
 }
