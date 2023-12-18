8e32e8b754f9a2d8d54d9d7afac0ed0fe084dc6d
Yosif Hamed <josif.hameed@gmail.com>
2023-12-18 16:12:55 +0200

    Add call in Joe humanizer

--- src/lib/humanizers/Joe.js
+++ src/lib/humanizers/Joe.js
@@ -1031,6 +1031,36 @@ const Joe = (humanizerInfo) => {
           amountOutMin
         )} ${txn.from !== to ? ` and send swapped to ${getName(humanizerInfo, to)}` : ''}`
       ]
+    },
+    [iface.getSighash('swapAVAXForExactTokens(uint256,address[],address,uint256)')]: (
+      txn,
+      network,
+      { extended }
+    ) => {
+      const [amountOutMin, path, to, deadline] = iface.parseTransaction(txn).args
+
+      if (extended)
+        return [
+          [
+            'Swap',
+            { type: 'token', ...nativeToken(network, txn.value, true) },
+            'for at least',
+            { type: 'token', ...token(humanizerInfo, path[path.length - 1], amountOutMin, true) },
+            ...(txn.from !== to
+              ? [
+                  'and send swapped to',
+                  { type: 'address', address: to, name: getName(humanizerInfo, to) }
+                ]
+              : [])
+          ]
+        ]
+      return [
+        `Swap ${nativeToken(network, txn.value)} for at least ${token(
+          humanizerInfo,
+          path[path.length - 1],
+          amountOutMin
+        )} ${txn.from !== to ? ` and send swapped to ${getName(humanizerInfo, to)}` : ''}`
+      ]
     }
   }
 }
