eb768b2e11955de0b984a2ad7a220e7850e33510
Yosif Hamed <josif.hameed@gmail.com>
2023-12-29 14:04:19 +0200

    Add humanization for Lido Matic staking

--- src/lib/humanizers/Lido.js
+++ src/lib/humanizers/Lido.js
@@ -2,23 +2,10 @@ import { Interface } from 'ethers/lib/utils'
 import { token, getName, nativeToken } from 'lib/humanReadableTransactions'
 import { MdDesktopAccessDisabled } from 'react-icons/md'
 
+const MATIC_ON_ETH_ADDRESS = '0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0'
 const Lido = (humanizerInfo) => {
-  const iface = new Interface([
-    {
-      constant: false,
-      inputs: [
-        {
-          name: '_referral',
-          type: 'address'
-        }
-      ],
-      name: 'submit',
-      outputs: [],
-      payable: true,
-      stateMutability: 'payable',
-      type: 'function'
-    }
-  ])
+  const ifaceETH = new Interface(humanizerInfo.abis.LidoStETH)
+  const ifaceMATIC = new Interface(humanizerInfo.abis.LidoStMATIC)
   function getAddress(humanizerInfo, address) {
     return {
       type: 'address',
@@ -26,18 +13,10 @@ const Lido = (humanizerInfo) => {
       name: getName(humanizerInfo, address)
     }
   }
+  // 0x7c9f4c87d911613fe9ca58b579f737911aad2d43
   return {
-    [iface.getSighash('submit')]: (txn, network, { extended }) => {
-      const { _positionId, _recipient } = iface.parseTransaction(txn).args
-      console.log([
-        'Stake',
-        {
-          type: 'token',
-          ...nativeToken(network, txn.value, true)
-        },
-        'to',
-        getAddress(humanizerInfo, txn.to)
-      ])
+    [ifaceETH.getSighash('submit')]: (txn, network, { extended }) => {
+      const { _positionId, _recipient } = ifaceETH.parseTransaction(txn).args
       if (extended)
         return [
           _recipient === txn.from
@@ -57,6 +36,14 @@ const Lido = (humanizerInfo) => {
           ? [`Withdraw swapped tokens from position ${_positionId}`]
           : [`Send swapped tokens from position ${_positionId} to ${_recipient}`]
       ]
+    },
+    [ifaceMATIC.getSighash('submit')]: (txn, network, { extended }) => {
+      const { _amount } = ifaceMATIC.parseTransaction(txn).args
+      if (extended)
+        return [
+          ['Stake', { type: 'token', ...token(humanizerInfo, MATIC_ON_ETH_ADDRESS, _amount, true) }]
+        ]
+      return [[`Stake ${token(humanizerInfo, MATIC_ON_ETH_ADDRESS, _amount)}`]]
     }
   }
 }
