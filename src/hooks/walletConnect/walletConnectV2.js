3e4febc5f82f6a18195463453e1755a35ef24922
Petromir Petrov <petromir@devlabs.bg>
2024-01-04 12:35:41 +0200

    add: try catch to wc keychain fix

--- src/hooks/walletConnect/walletConnectV2.js
+++ src/hooks/walletConnect/walletConnectV2.js
@@ -180,14 +180,23 @@ export default function useWalletConnectV2({
           // Should happen in extremely rare cases in which the user's
           // storage is corrupted and the keychain is not found.
           if (e?.message?.includes('No matching key. keychain')) {
-            web3wallet.engine.signClient.session.delete(topic, 'USER_DISCONNECTED')
+            try {
+              await web3wallet.engine.signClient.session.delete(topic, 'USER_DISCONNECTED')
+            } catch {
+              addToast(
+                'Could not disconnect from the dApp. Please contact support if the issue persists.',
+                {
+                  error: true
+                }
+              )
+            }
           }
           console.log('WC2 disconnect error', e)
         }
       }
       setIsConnecting(false)
     },
-    [web3wallet]
+    [web3wallet, addToast]
   )
 
   const resolveMany = (ids, resolution) => {
