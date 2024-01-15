1005e2c296c15d38ec8a1170f7eb725ddaab74ad
Petromir Petrov <petromir@devlabs.bg>
2024-01-15 14:48:19 +0200

    fix: wc eth_signTypedData_v4 type Permit

--- src/hooks/walletConnect/walletConnectV2.js
+++ src/hooks/walletConnect/walletConnectV2.js
@@ -248,18 +248,29 @@ export default function useWalletConnectV2({
         }
       })
 
-      // NOTE: looks like optionalNamespaces can empty object {} and requiredNamespaces to have the eip155
-      const incomingNamespaces = {
-        ...(optionalNamespaces?.eip155 || {}),
-        ...(requiredNamespaces?.eip155 || {})
-      }
+      const incomingEvents = [
+        ...new Set([
+          ...(optionalNamespaces?.eip155?.events || []),
+          ...(requiredNamespaces?.eip155?.events || [])
+        ])
+      ]
+
+      const incomingMethods = [
+        ...new Set([
+          // Filter out unsupported optional methods. Note: Can't be done for required methods.
+          ...(optionalNamespaces?.eip155?.methods || []).filter((method) =>
+            WC2_SUPPORTED_METHODS.includes(method)
+          ),
+          ...(requiredNamespaces?.eip155?.methods || [])
+        ])
+      ]
 
       const namespaces = {
         eip155: {
           chains: supportedChains,
           accounts: supportedChains.map((a) => `${a}:${account}`),
-          methods: incomingNamespaces.methods,
-          events: incomingNamespaces.events
+          methods: incomingMethods,
+          events: incomingEvents
         }
       }
 
@@ -329,7 +340,8 @@ export default function useWalletConnectV2({
         const connection = getConnectionFromSessionTopic(topic)
 
         if (connection) {
-          const dappName = connection.session?.peer.metadata?.name || ''
+          const dappName = connection.peer?.metadata.name || ''
+
           if (method === 'personal_sign' || wcRequest.method === 'eth_sign') {
             txn = wcRequest.params[wcRequest.method === 'personal_sign' ? 0 : 1]
             requestAccount = wcRequest.params[wcRequest.method === 'personal_sign' ? 1 : 0]
@@ -379,10 +391,6 @@ export default function useWalletConnectV2({
 
                 return
               }
-              addToast('dApp tried to sign a token permit which does not support Smart Wallets.', {
-                error: true
-              })
-              return
             }
           } else if (method === 'wallet_addEthereumChain') {
             const incomingChainId = wcRequest.params[0]
