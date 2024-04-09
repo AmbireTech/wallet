31047dab02ce9d2a96e17bb9c96f7451b32a6904
Gergana Mihaylova <gergana@devlabs.bg>
2024-04-09 11:35:46 +0300

    Fix: handleUri second element not found

--- src/components/Wallet/Collectible/Collectible.js
+++ src/components/Wallet/Collectible/Collectible.js
@@ -39,7 +39,7 @@ const handleUri = (uri) => {
   if (uri.split('/')[0] === 'data:image') return uri
   if (uri.startsWith('ipfs://'))
     return uri.replace(/ipfs:\/\/ipfs\/|ipfs:\/\//g, 'https://ipfs.io/ipfs/')
-  if (uri.split('/')[2].endsWith('mypinata.cloud'))
+  if (uri.split('/')[2] && uri.split('/')[2].endsWith('mypinata.cloud'))
     return `https://ipfs.io/ipfs/${uri.split('/').slice(4).join('/')}`
 
   return uri
