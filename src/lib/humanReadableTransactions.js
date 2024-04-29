24c99184d3f07db178bd2715674575b00cef3bff
Yosif Hamed <josif.hameed@gmail.com>
2024-04-29 15:37:15 +0300

    Add Remove liquidity parser

--- src/lib/humanReadableTransactions.js
+++ src/lib/humanReadableTransactions.js
@@ -69,6 +69,29 @@ function parseActions(actions){
           i++
           continue
         }
+
+
+
+        if(
+          // are valid [obj]
+          actions[i].length==2 &&
+          actions[i+1].length==2 &&
+          Array.isArray(actions[i]) && 
+          Array.isArray(actions[i+1]) &&
+          // are actual Unwrap and Sweep 
+          typeof actions[i][0] === 'string' &&
+          actions[i][0].startsWith('Unwrap') &&
+          actions[i][1].type==='token' &&
+          typeof actions[i+1][0] === 'string' && 
+          actions[i+1][0].startsWith('Sweep') && 
+          actions[i+1][1].type==='token'
+          ){
+            result.push(["Remove liquidity and withdraw", actions[i][1], actions[i+1][1]])
+            // skip next ccall, since two were merged
+            i++
+            continue
+          }
+
       result.push(actions[i])
       continue
   }
