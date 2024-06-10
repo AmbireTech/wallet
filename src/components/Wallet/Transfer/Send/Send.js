01ec5c71869fdc01dbd7bc7c7425f1855bfcabf0
Yosif Hamed <josif.hameed@gmail.com>
2024-06-10 17:28:31 +0300

    Change text for pickign/selecting token

--- src/components/Wallet/Transfer/Send/Send.js
+++ src/components/Wallet/Transfer/Send/Send.js
@@ -365,7 +365,7 @@ const Send = ({
           defaultValue={asset}
           items={sortedAssetsItems}
           onChange={({ value }) => value && setAsset(value)}
-          placeholder={{label:'Pick a token', icon:fallbackCoin}}
+          placeholder={{label:'Select a token', icon:fallbackCoin}}
         />
         {feeBaseTokenWarning ? (
           <p className={styles.gasTankConvertMsg}>
